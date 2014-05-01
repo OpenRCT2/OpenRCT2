ae637f62913ef9d8491a52730fc25a92c7579d8f
IntelOrca <ted@brambles.org>
2014-05-01 14:13:31 +0100

    fix case and add author to source code header

--- src/peep.h
+++ src/peep.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2014 Ted John
+ * Copyright (c) 2014 Ted John, Peter Hill
  * OpenRCT2, an open source clone of Roller Coaster Tycoon 2.
  *
  * This file is part of OpenRCT2.
@@ -77,7 +77,7 @@ typedef struct {
 	uint8 current_ride;				// 0x68
 	uint8 pad_6a;					// 0x6A Part of current_ride?
 	uint8 current_train;   	        // 0x6B
-	uint8 current_car;				// 0x6c
+	uint8 current_car;				// 0x6C
 	uint8 current_seat;				// 0x6d
 	uint8 pad_6C[0x2e];
 	uint32 id;						// 0x9C
