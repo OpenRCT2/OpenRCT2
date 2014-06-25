2e849668c8a874d42bd7c21599cc58e41f081517
Duncan Frost <duncans_pumpkin@hotmail.co.uk>
2014-06-25 18:01:10 +0100

    Added more to window_peep

--- src/window_peep.c
+++ src/window_peep.c
@@ -28,94 +28,56 @@
 #include "window.h"
 #include "window_dropdown.h"
 
+enum WINDOW_PEEP_PAGE {
+	WINDOW_PEEP_OVERVIEW,
+	WINDOW_PEEP_STATS,
+	WINDOW_PEEP_RIDES,
+	WINDOW_PEEP_FINANCE,
+	WINDOW_PEEP_THOUGHTS,
+	WINDOW_PEEP_INVENTORY
+};
+
+enum WINDOW_PEEP_WIDGET_IDX {
+	WIDX_BACKGROUND,
+	WIDX_TITLE,
+	WIDX_CLOSE,
+	WIDX_PAGE_BACKGROUND,
+	WIDX_TAB_1,
+	WIDX_TAB_2,
+	WIDX_TAB_3,
+	WIDX_TAB_4,
+	WIDX_TAB_5,
+	WIDX_TAB_6,
+};
+
 void window_peep_emptysub(){};
 
-static rct_widget window_peep_widgets[] = { //Only first 4 fields done so far
-	{ 1,	0,		0,		191,	0,		156,	0x0FFFFFFFF, STR_NONE },						// panel / background
-	{ 20,	0,		1,		190,	1,		14,		865, 829},			// title bar
-	{ 21,	0,		179,	189,	2,		13,		824, 828},			// close x button
-	{ 2,	1,		1,		191,	43,		156,	0x0FFFFFFFF, STR_NONE },						// tab content panel
-	{ 8, 	1,		3,		33,		17,		70,		STR_PAGE_1, STR_NONE },						// page dropdown
-	{ 8, 	1, 		73,		64,		17,		69,		876, STR_NONE },						// page dropdown button
-	{ 8, 	1,		65,		95,		17,		70,		0x0FFFFFFFF, STR_INFORMATION_TYPE_TIP },		// information type dropdown
-	{ 8, 	1, 		96,		126,	17,		69,		876, STR_INFORMATION_TYPE_TIP },		// information type dropdown button
-	{ 8,	1,		127,	157,	17,		69,		5192, STR_SHOW_GUESTS_ON_MAP_TIP },	// map
-	{ 8,	1,		158,	188,	17,		43,		0x02000144E, STR_INDIVIDUAL_GUESTS_TIP },	// tab 1
-	{ 12,	1,		3,		166,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 17,	1,		3,		166,	72,		326,	3,				STR_NONE },						// guest list
-	{ 12,	1,		3,		166,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 9,	1,		167,	190,	72,		326,	3,				STR_NONE },	
-	{ 9,	1,		167,	190,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 9,	1,		167,	190,	72,		326,	3,				STR_NONE },						// guest list
-	{ 9,	1,		167,	190,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 26,	0,		0,		1,		72,		326,	3,				STR_NONE },
-	{ -1,	-1,		-1,		20,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 0,	0,		829,	21,		72,		326,	3,				STR_NONE },						// guest list
-	{ 0,	0,		828,	258,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ -1,	-1,		-1,		264,	72,		326,	3,				STR_NONE },	
-	{ 0,	32,		1938,	264,	72,		326,	3,				STR_NONE },						// guest list
-	{ 0,	32,		1940,	264,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 0,	32,		1941,	264,	72,		326,	3,				STR_NONE },	
-	{ 0,	32,		1943,	264,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 0,	32,		1944,	26,		72,		326,	3,				STR_NONE },						// guest list
-	{ 0,	32,		-1,		-1,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 156,	0,		865,	0,		72,		326,	3,				STR_NONE },
-	{ 13,	0,		824,	0,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 14,	0,		-1,		-1,		72,		326,	3,				STR_NONE },						// guest list
-	{ 43,	0,		5198,	8192,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 43,	0,		5198,	8192,	72,		326,	3,				STR_NONE },
-	{ 43,	0,		5198,	8192,	72,		326,	3,				STR_NONE },
-	{ 43,	0,		5198,	8192,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 43,	0,		5198,	8192,	72,		326,	3,				STR_NONE },						// guest list
-	{ 43,	0,		5198,	8192,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 143,	0,		2,		0,		72,		326,	3,				STR_NONE },
-	{ 191,	0,		0,		156,	72,		326,	3,				STR_NONE },						// guest list
-	{ 190,	0,		1,		14,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 189,	0,		2,		13,		72,		326,	3,				STR_NONE },	
-	{ 191,	0,		43,		156,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 33,	0,		17,		43,		72,		326,	3,				STR_NONE },						// guest list
-	{ 64,	0,		17,		43,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 95,	0,		17,		43,		72,		326,	3,				STR_NONE },
-	{ 126,	0,		17,		43,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 157,	0,		17,		43,		72,		326,	3,				STR_NONE },						// guest list
-	{ 188,	0,		17,		43,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 1,	0,		0,		191,	72,		326,	3,				STR_NONE },	
-	{ 20,	0,		1,		190,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 21,	0,		179,	189,	72,		326,	3,				STR_NONE },
-	{ 2,	1,		0,		191,	72,		326,	3,				STR_NONE },
-	{ 8,	1,		3,		33,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 8,	1,		34,		64,		72,		326,	3,				STR_NONE },						// guest list
-	{ 8,	1,		65,		95,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 8,	1,		96,		126,	72,		326,	3,				STR_NONE },
-	{ 8,	1,		127,	157,	72,		326,	3,				STR_NONE },						// guest list
-	{ 8,	1,		158,	188,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 26,	0,		0,		1,		72,		326,	3,				STR_NONE },	
-	{ -1,	-1,		-1,		20,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 0,	0,		829,	21,		72,		326,	3,				STR_NONE },						// guest list
-	{ 0,	0,		828,	258,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ -1,	-1,		-1,		264,	72,		326,	3,				STR_NONE },
-	{ 0,	32,		1938,	264,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 0,	32,		1940,	264,	72,		326,	3,				STR_NONE },						// guest list
-	{ 0,	32,		1941,	264,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 0,	32,		1942,	264,	72,		326,	3,				STR_NONE },
-	{ 0,	32,		1943,	264,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 0,	32,		1944,	26,		72,		326,	3,				STR_NONE },
-	{ 73,	1,		-1,		-1,		72,		326,	3,				STR_NONE },
-	{ 14,	0,		1463,	0,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 15,	0,		824,	0,		72,		326,	3,				STR_NONE },						// guest list
-	{ 73,	1,		-1,		-1,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 70,	0,		3440,	0,		72,		326,	3,				STR_NONE },	
-	{ 69,	0,		876,	0,		72,		326,	3,				STR_NONE },						// guest list
-	{ 70,	0,		-1,		-1,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 69,	0,		876,	0,		72,		326,	3,				STR_NONE },	
-	{ 69,	0,		5192,	0,		17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 43,	0,		5198,	8192,	72,		326,	3,				STR_NONE },						// guest list
-	{ 43,	0,		5198,	8192,	17,		43,		0x02000144E,	STR_SUMMARISED_GUESTS_TIP },	// tab 2
-	{ 70,	1,		3,		0,		72,		326,	3,				STR_NONE },
+rct_widget window_peep_overview_widgets[] = { 
+	{ WWT_FRAME,	0, 0,	191,	0,		156,	0x0FFFFFFFF,	STR_NONE },
+	{ WWT_CAPTION,	0, 1,	190,	1,		14,		865,			STR_WINDOW_TITLE_TIP },
+	{ WWT_CLOSEBOX, 0, 179, 189,	2,		13,		824,			STR_CLOSE_WINDOW_TIP },
+	{ WWT_RESIZE,	1, 1,	191,	43,		156,	0x0FFFFFFFF,	STR_NONE },
+	{ WWT_TAB,		1, 3,	33,		17,		43,		0x2000144E,		1938 },
+	{ WWT_TAB,		1, 73,	64,		17,		43,		0x2000144E,		1940},
+	{ WWT_TAB,		1, 65,	95,		17,		43,		0x2000144E,		1941},
+	{ WWT_TAB,		1, 96,	126,	17,		43,		0x2000144E,		1942},
+	{ WWT_TAB,		1, 127, 157,	17,		43,		0x2000144E,		1943},
+	{ WWT_TAB,		1, 158, 188,	17,		43,		0x2000144E,		1944},
+	{ WWT_12,		1, 3,	166,	45,		56,		0x0FFFFFFFF,	STR_NONE},
+	{ WWT_VIEWPORT, 1, 3,	166,	57,		143,	0x0FFFFFFFF,	STR_NONE },
+	{ WWT_12,		1, 3,	166,	144,	154,	0x0FFFFFFFF,	STR_NONE},
+	{ WWT_FLATBTN,	1, 167, 190,	45,		68,		0x1436,			1706},
+	{ WWT_FLATBTN,	1, 167, 190,	69,		92,		0x1430,			1055},
+	{ WWT_FLATBTN,	1, 167, 190,	93,		116,	0x142F,			STR_LOCATE_SUBJECT_TIP},
+	{ WWT_FLATBTN,	1, 167, 190,	117,	140,	0x1444,			1930},
 	{ WIDGETS_END },
 };
 
-static void* window_guest_list_events[] = {
+rct_widget *window_peep_page_widgets[] = {
+	window_peep_overview_widgets
+};
+
+static void* window_peep_overview_events[] = {
 	0x696A75,
 	0x696A06,
 	0x696FBE,
@@ -146,6 +108,20 @@ static void* window_guest_list_events[] = {
 	0x69707C
 };
 
+void* window_peep_page_events[] = {
+	window_peep_overview_events
+};
+
+uint32 window_peep_page_enabled_widgets[] = {
+	(1 << WIDX_CLOSE) |
+	(1 << WIDX_TAB_1) |
+	(1 << WIDX_TAB_2) |
+	(1 << WIDX_TAB_3) |
+	(1 << WIDX_TAB_4) |
+	(1 << WIDX_TAB_5) |
+	(1 << WIDX_TAB_6)
+};
+
 /**
  * rct2: 0x006989E9
  *
@@ -160,9 +136,28 @@ void window_peep_open(rct_peep* peep){
 
 	window = window_bring_to_front_by_id(WC_PEEP, peep->sprite_index);
 	if (window == NULL){
-		window = window_create_auto_pos(192, 157, (uint32*)window_guest_list_events, WC_PEEP, 0);
-		window->widgets = window_peep_widgets;
+		window = window_create_auto_pos(192, 157, (uint32*)window_peep_overview_events, WC_PEEP, 0);
+		window->widgets = window_peep_overview_widgets;
+		window->enabled_widgets = window_peep_page_enabled_widgets[0];
+		window->number = peep->sprite_index;
+		window->page = 0;
+		window->var_482 = 0;
+		window->var_48E = 0;
+		window->var_490 = 0;
+		window->var_492 = 0;
+		window->var_494 = 0;
+		//Call 6987a6
+		window->min_width = 192;
+		window->min_height = 157;
+		window->max_width = 500;
+		window->max_height = 450;
+		window->flags = 8;
+		window->var_476 = 0;
+		window->var_47A = -1;
+		window->colours[0] = 1;
+		window->colours[1] = 15;
+		window->colours[2] = 15;
 	}
-	RCT2_CALLPROC_X(0x006989E9, 0, 0, 0, (int)peep, 0, 0, 0);
+	//RCT2_CALLPROC_X(0x006989E9, 0, 0, 0, (int)peep, 0, 0, 0);
 	
 }
