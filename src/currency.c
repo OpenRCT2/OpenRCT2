c9444737c950fafda143a6d04d94d2e7cd81471b
ZedThree <zed.three@gmail.com>
2014-07-27 12:22:01 +0200

    Fix Franc and Guilder symbols, and whitespace

--- src/currency.c
+++ src/currency.c
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- 
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- 
+
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -21,14 +21,14 @@
 #include "currency.h"
 
 const rct_currency_spec g_currency_specs[CURRENCY_END] = {
-  {10	, "\xA3"	, 1},	//British Pound
-  {10	, "\x24"	, 1},	//US Dollar	   
-  {10	, "FRF"		, 1},	//French Franc 
-  {10	, "DM"		, 1},	//Deutsche Mark
-  {1000	, "\xA5"	, 1},	//Japanese Yen 
-  {10	, "Pts"		, 0},	//Spanish Peseta
-  {1000	, "L"		, 1},	//Italian Lira 
-  {10	, "Dfl."	, 1},	//Dutch Guilder 
-  {10	, "kr."		, 0},	//Swedish Krona
-  {10	, "\xb5"	, 1},	//Euro
+	 {10	, "\xA3"	, 1},	//British Pound
+	 {10	, "\x24"	, 1},	//US Dollar
+	 {10	, "F"		, 0},	//French Franc
+	 {10	, "DM"		, 1},	//Deutsche Mark
+	 {1000	, "\xA5"	, 1},	//Japanese Yen
+	 {10	, "Pts"		, 0},	//Spanish Peseta
+	 {1000	, "L"		, 1},	//Italian Lira
+	 {10	, "fl."		, 1},	//Dutch Guilder
+	 {10	, "kr."		, 0},	//Swedish Krona
+	 {10	, "\xb5"	, 1},	//Euro
 };
