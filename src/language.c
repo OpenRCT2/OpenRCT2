306ff79bc772d879ae64da0e0d8f03979bae346f
IntelOrca <ted@brambles.org>
2014-08-21 22:54:27 +0100

    fix merge error

--- src/language.c
+++ src/language.c
@@ -42,12 +42,8 @@ const char *language_filenames[LANGUAGE_COUNT] = {
 	"dutch",			// LANGUAGE_DUTCH
 	"french",			// LANGUAGE_FRENCH
 	"hungarian",		// LANGUAGE_HUNGARIAN
-<<<<<<< HEAD
 	"polish",			// LANGUAGE_POLISH
 	"spanish_sp"		// LANGUAGE_SPANISH
-=======
-	"polish"			// LANGUAGE_POLISH
->>>>>>> new-ride-window
 };
 
 int gCurrentLanguage = LANGUAGE_UNDEFINED;
