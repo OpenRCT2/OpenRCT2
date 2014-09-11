ac7c204284682f342f4b142a8ad99dc264cff1b9
hexdec <hexdec@users.noreply.github.com>
2014-09-11 18:55:19 +0200

    Changed place on WC_MUSIC_CREDITS and WC_PUBLISHER_CREDITS.

--- src/window_credits.c
+++ src/window_credits.c
@@ -86,7 +86,7 @@ void window_music_credits_open()
 	rct_window* window;
 
 	// Check if window is already open
-	window = window_bring_to_front_by_id(WC_PUBLISHER_CREDITS, 0);
+	window = window_bring_to_front_by_id(WC_MUSIC_CREDITS, 0);
 	if (window != NULL)
 		return;
 
@@ -96,7 +96,7 @@ void window_music_credits_open()
 		510,
 		314,
 		(uint32*)window_music_credits_events,
-		WC_PUBLISHER_CREDITS,
+		WC_MUSIC_CREDITS,
 		0
 		);
 
