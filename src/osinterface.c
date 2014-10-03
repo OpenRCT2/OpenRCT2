bfbcbbc996e8ef4cefa678f4f345418e55db4e34
zsilencer <zsilencergame@gmail.com>
2014-10-03 16:28:39 -0600

    small fixes
    palette effects
    
    small fixes
    
    palette effects
    
    small fixes
    
    Merge branch 'test'
    
    Conflicts:
    	src/window_music_credits.c
    
    palette effects
    
    palette effects
    
    small fixes
    
    Merge branch 'test'
    
    Conflicts:
    	src/window_music_credits.c
    
    palette effects
    
    small fixes
    
    Merge branch 'test'
    
    Conflicts:
    	src/window_music_credits.c
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2
    
    added window_music_credits.c
    
    added window_music_credits.c
    
    Merge branch 'master' of https://github.com/zsilencer/OpenRCT2

--- src/osinterface.c
+++ src/osinterface.c
@@ -276,14 +276,15 @@ void osinterface_update_palette(char* colours, int start_index, int num_colours)
 		exit(1);
 	}
 
-	for (i = start_index; i < start_index + num_colours; i++) {
-		base[i].r = colours[(i * 4) + 2];
-		base[i].g = colours[(i * 4) + 1];
-		base[i].b = colours[(i * 4) + 0];
+	for (i = 0; i < 256; i++) {
+		base[i].r = colours[2];
+		base[i].g = colours[1];
+		base[i].b = colours[0];
 		base[i].a = 0;
+		colours += 4;
 	}
 
-	if (SDL_SetPaletteColors(_palette, &base[start_index], start_index, num_colours)) {
+	if (SDL_SetPaletteColors(_palette, base, 0, 256)) {
 		RCT2_ERROR("SDL_SetPaletteColors failed %s", SDL_GetError());
 		exit(1);
 	}
