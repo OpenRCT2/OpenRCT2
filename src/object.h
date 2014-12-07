1ed6e443caf9639dd605669c538e7b77712dda85
IntelOrca <ted@brambles.org>
2014-12-07 21:45:42 +0000

    refactor object loading, part 1

--- src/object.h
+++ src/object.h
@@ -47,6 +47,7 @@ typedef struct {
 extern int object_entry_group_counts[];
 extern int object_entry_group_encoding[];
 
+int object_load_entry(const char *path, rct_object_entry *outEntry);
 void object_list_load();
 void set_load_objects_fail_reason();
 int object_read_and_load_entries(FILE *file);
@@ -58,9 +59,12 @@ void object_unload(int groupIndex, rct_object_entry_extended *entry);
 int object_get_scenario_text(rct_object_entry *entry);
 void object_free_scenario_text();
 int object_get_length(rct_object_entry *entry);
-rct_object_entry *object_get_next(rct_object_entry *entry);
-int object_calculate_checksum(rct_object_entry *entry, char *data, int dataLength);
+int object_entry_compare(const rct_object_entry *a, const rct_object_entry *b);
+int object_calculate_checksum(const rct_object_entry *entry, const char *data, int dataLength);
 int object_paint(int type, int eax, int ebx, int ecx, int edx, int esi, int edi, int ebp);
+rct_object_entry *object_get_next(rct_object_entry *entry);
 int sub_6A9F42(FILE *file, rct_object_entry* entry);
+
+rct_object_entry *object_list_find(rct_object_entry *entry);
 
 #endif
