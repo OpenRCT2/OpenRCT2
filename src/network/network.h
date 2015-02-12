a8ca6dace55f39242d3d6c2260286ca3e01e032b
IntelOrca <ted@brambles.org>
2015-02-12 16:23:49 +0000

    add chat feature

--- src/network/network.h
+++ src/network/network.h
@@ -112,6 +112,8 @@ void network_send_map();
 void network_send_chat(const char* text);
 void network_send_gamecmd(uint32 eax, uint32 ebx, uint32 ecx, uint32 edx, uint32 esi, uint32 edi, uint32 ebp);
 
+void network_open_chat_box();
+
 void network_print_error();
 
 #ifdef __cplusplus
