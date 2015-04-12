2bd851e26aa22a1598264b4cd59deaa0d49a58da
Duncan Frost <duncans_pumpkin@hotmail.co.uk>
2015-04-12 17:47:52 +0100

    Fix small flag mistake

--- src/ride/vehicle.c
+++ src/ride/vehicle.c
@@ -1017,7 +1017,7 @@ static void vehicle_update(rct_vehicle *vehicle)
 					vehicle->velocity <= 0x20000
 				)
 			) {
-				vehicle->update_flags |= VEHICLE_UPDATE_FLAG_15;
+				vehicle->update_flags |= VEHICLE_UPDATE_FLAG_7;
 			}
 		}
 	}
