5afa920ecb936d930a4f9ad3bc92487b2f0c67cc
IntelOrca <ted@brambles.org>
2015-05-21 00:57:18 +0100

    add simple twitch follower peep rename

--- src/network/twitch.c
+++ src/network/twitch.c
@@ -1,3 +1,8 @@
+#include "../addresses.h"
+#include "../config.h"
+#include "../localisation/localisation.h"
+#include "../peep/peep.h"
+#include "../world/sprite.h"
 #include "http.h"
 #include "twitch.h"
 
@@ -50,4 +55,71 @@ bool twitch_get_followers(const twitch_info *twitchInfo, int *outNumFollowers, c
 json_error:
 	http_request_json_dispose(jsonResponse);
 	return false;
+}
+
+void twitch_update_peeps()
+{
+	char buffer[256];
+
+	twitch_info twitchInfo;
+	twitchInfo.channel = gConfigTwitch.channel;
+
+	if (twitchInfo.channel == NULL)
+		return;
+
+	int numFollowers;
+	char **followers;
+
+	if (!twitch_get_followers(&twitchInfo, &numFollowers, &followers)) {
+		return;
+	}
+
+	bool *followerExists = calloc(numFollowers, sizeof(bool));
+	int peepsToName = numFollowers;
+
+	uint16 spriteIndex;
+	rct_peep *peep;
+
+	// Check what followers are already in the park
+	FOR_ALL_PEEPS(spriteIndex, peep) {
+		if (peep->name_string_idx >= 0x8000 && peep->name_string_idx < 0x9000) {
+			format_string(buffer, peep->name_string_idx, NULL);
+
+			for (int i = 0; i < numFollowers; i++) {
+				if (_strcmpi(buffer, followers[i]) == 0) {
+					followerExists[i] = true;
+					peepsToName--;
+					break;
+				}
+			}
+		}
+	}
+
+	// Rename non-named peeps to followers that aren't currently in the park.
+	if (peepsToName > 0) {
+		int followerIndex = -1;
+		FOR_ALL_PEEPS(spriteIndex, peep) {
+			for (int i = followerIndex + 1; i < numFollowers; i++) {
+				if (!followerExists[i]) {
+					followerIndex = i;
+					break;
+				}
+			}
+
+			if (peep->name_string_idx < 0x8000 || peep->name_string_idx >= 0x9000) {
+				rct_string_id newStringId = user_string_allocate(4, followers[followerIndex]);
+				if (newStringId != STR_NONE) {
+					peep->name_string_idx = newStringId;
+					peep->flags |= PEEP_FLAGS_TRACKING;
+
+					peepsToName--;
+					if (peepsToName == 0)
+						break;
+				}
+			}
+		}
+	}
+
+	free(followers);
+	free(followerExists);
 }
