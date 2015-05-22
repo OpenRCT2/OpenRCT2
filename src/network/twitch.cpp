6e13d118968605974f48a31e7eeed0b5c0831e90
IntelOrca <ted@brambles.org>
2015-05-22 06:00:29 +0100

    add twitch message integration

--- src/network/twitch.cpp
+++ src/network/twitch.cpp
@@ -32,11 +32,12 @@ enum {
 	TWITCH_STATE_JOINED,
 	TWITCH_STATE_WAITING,
 	TWITCH_STATE_GET_FOLLOWERS,
+	TWITCH_STATE_GET_MESSAGES,
 	TWITCH_STATE_LEAVING,
 	TWITCH_STATE_LEFT
 };
 
-#define PULSE_TIME (2 * 60 * 1000)
+#define PULSE_TIME (30 * 1000)
 
 const char *TwitchBaseUrl = "https://api.twitch.tv/kraken/";
 const char *TwitchExtendedBaseUrl = "http://openrct.ursalabs.co/api/1/";
@@ -44,13 +45,17 @@ const char *TwitchExtendedBaseUrl = "http://openrct.ursalabs.co/api/1/";
 static int _twitchState = TWITCH_STATE_LEFT;
 static bool _twitchIdle = true;
 static uint32 _twitchLastPulseTick = 0;
+static int _twitchLastPulseOperation = 1;
 static http_json_response *_twitchJsonResponse;
 
 static void twitch_join();
 static void twitch_leave();
 static void twitch_get_followers();
+static void twitch_get_messages();
 
 static void twitch_parse_followers();
+static void twitch_parse_messages();
+static void twitch_parse_chat_message(const char *message);
 
 void twitch_update()
 {
@@ -74,12 +79,24 @@ void twitch_update()
 			uint32 timeSinceLastPulse = currentTime - _twitchLastPulseTick;
 			if (_twitchLastPulseTick == 0 || timeSinceLastPulse > PULSE_TIME) {
 				_twitchLastPulseTick = currentTime;
-				twitch_get_followers();
+				_twitchLastPulseOperation = (_twitchLastPulseOperation + 1) % 2;
+				switch (_twitchLastPulseOperation + TWITCH_STATE_GET_FOLLOWERS) {
+				case TWITCH_STATE_GET_FOLLOWERS:
+					twitch_get_followers();
+					break;
+				case TWITCH_STATE_GET_MESSAGES:
+					if (gConfigTwitch.enable_news)
+						twitch_get_messages();
+					break;
+				}
 			}
 			break;
 		}
 		case TWITCH_STATE_GET_FOLLOWERS:
 			twitch_parse_followers();
+			break;
+		case TWITCH_STATE_GET_MESSAGES:
+			twitch_parse_messages();
 			break;
 		}
 	} else {
@@ -152,6 +169,23 @@ static void twitch_get_followers()
 	http_request_json_async(url, [](http_json_response *jsonResponse) -> void {
 		_twitchJsonResponse = jsonResponse;
 		_twitchState = TWITCH_STATE_GET_FOLLOWERS;
+		_twitchIdle = true;
+	});
+}
+
+/**
+ * GET /channel/:channel/messages
+ */
+static void twitch_get_messages()
+{
+	char url[256];
+	sprintf(url, "%schannel/%s/messages", TwitchExtendedBaseUrl, gConfigTwitch.channel);
+
+	_twitchState = TWITCH_STATE_WAITING;
+	_twitchIdle = false;
+	http_request_json_async(url, [](http_json_response *jsonResponse) -> void {
+		_twitchJsonResponse = jsonResponse;
+		_twitchState = TWITCH_STATE_GET_MESSAGES;
 		_twitchIdle = true;
 	});
 }
@@ -280,10 +314,32 @@ static void twitch_parse_followers()
 	gfx_invalidate_screen();
 }
 
+static void twitch_parse_messages()
+{
+	http_json_response *jsonResponse = _twitchJsonResponse;
+	if (json_is_array(jsonResponse->root)) {
+		int messageCount = json_array_size(jsonResponse->root);
+		for (int i = 0; i < messageCount; i++) {
+			json_t *jsonMessage = json_array_get(jsonResponse->root, i);
+			if (!json_is_object(jsonMessage))
+				continue;
+
+			json_t *jsonText = json_object_get(jsonMessage, "message");
+			const char *text = json_string_value(jsonText);
+
+			twitch_parse_chat_message(text);
+		}
+	}
+
+	http_request_json_dispose(_twitchJsonResponse);
+	_twitchJsonResponse = NULL;
+	_twitchState = TWITCH_STATE_JOINED;
+}
+
 /**
  * Like strchr but allows searching for one of many characters.
  */
-char *strchrm(const char *str, const char *find)
+static char *strchrm(const char *str, const char *find)
 {
 	const char *result = NULL;
 	do {
@@ -295,9 +351,10 @@ char *strchrm(const char *str, const char *find)
 			fch++;
 		}
 	} while (*str++ != 0);
+	return NULL;
 }
 
-char *strskipwhitespace(const char *str)
+static char *strskipwhitespace(const char *str)
 {
 	while (*str == ' ' || *str == '\t')
 		str++;
@@ -305,7 +362,7 @@ char *strskipwhitespace(const char *str)
 	return (char*)str;
 }
 
-void twitch_parse_chat_message(const char *message)
+static void twitch_parse_chat_message(const char *message)
 {
 	char buffer[256], *ch;
 
@@ -318,10 +375,14 @@ void twitch_parse_chat_message(const char *message)
 	strncpy(buffer, message, ch - message);
 
 	if (_strcmpi(buffer, "news") == 0) {
-		ch = strskipwhitespace(ch);
-		strncpy(buffer, ch, sizeof(buffer) - 1);
+		if (gConfigTwitch.enable_news) {
+			ch = strskipwhitespace(ch);
 
-		news_item_add_to_queue_raw(NEWS_ITEM_BLANK, buffer, 0);
+			buffer[0] = (char)FORMAT_TOPAZ;
+			strncpy(buffer + 1, ch, sizeof(buffer) - 2);
+
+			news_item_add_to_queue_raw(NEWS_ITEM_BLANK, buffer, 0);
+		}
 	}
 }
 
