ca4f2d0fa37ac644bd0d1846472add8282a45826
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2015-07-03 06:53:27 +0100

    remove libs from orct repo, add dependencies pull to prebuilds, update unix scripts

--- pre-build.ps1
+++ pre-build.ps1
@@ -1,15 +1,11 @@
 $path = Split-Path $Script:MyInvocation.MyCommand.Path
-$zip = $path+'\lib\orcalibs.zip'
+$zip = $path+'\orctlibs.zip'
 $libs = $path+'\lib'
-$libcurl = Test-Path $path\lib\libcurl\
-$jansson = Test-Path $path\lib\jansson\
-$sdl = Test-Path $path\lib\sdl\
-if (!$libcurl -or !$jansson -or !$sdl) {
-	Invoke-WebRequest http://misozmiric.com/ted/openrct2/orcalibs-vs.zip -OutFile $path\lib\orcalibs.zip
-	rm $path\lib\libcurl -r -Force -ErrorAction SilentlyContinue
-	rm $path\lib\jansson -r -Force -ErrorAction SilentlyContinue
-	rm $path\lib\sdl -r -Force -ErrorAction SilentlyContinue
+$libsTest = Test_path $libs
+if (!$libsTest) {
+	mkdir $libs
+	Invoke-WebRequest https://download.openrct2.website/dev/lib/vs -OutFile $path\orctlibs.zip
 	[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null
 	[System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $libs)
-	rm $path\lib\orcalibs.zip -Force -ErrorAction SilentlyContinue
+	rm $path\orctlibs.zip -Force -ErrorAction SilentlyContinue
 }
