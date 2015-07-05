946811068fcd3e82bd98ce86dc44c0eca1c87c6d
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2015-07-05 21:08:16 +0100

    add local libversion checks for VS (via powershell)

--- pre-build.ps1
+++ pre-build.ps1
@@ -1,11 +1,33 @@
+#init
+$libversion = 1
 $path = Split-Path $Script:MyInvocation.MyCommand.Path
 $zip = $path+'\orctlibs.zip'
 $libs = $path+'\lib'
+$libsVFile = $path+'\libversion'
 $libsTest = Test-Path $libs
-if (!$libsTest) {
-	mkdir $libs
+
+#libs version test
+$libsVersionTest = Test-Path $libsVFile
+$currentVersion = 0
+$needsdownload = $true
+if ($libsVersionTest) {
+    $currentVersion = [IO.File]::ReadAllText($libsVFile)
+}
+if ($currentVersion -ge $libversion) {
+    $needsdownload = $false
+}
+
+#download
+if (!$libsTest -or $needsdownload) {
+	if ($libsTest) {
+        rm $libs -Recurse -Force
+    }
+    mkdir $libs
 	Invoke-WebRequest https://download.openrct2.website/dev/lib/vs -OutFile $path\orctlibs.zip
 	[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null
 	[System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $libs)
 	rm $path\orctlibs.zip -Force -ErrorAction SilentlyContinue
+    $libversion | Set-Content $libsVFile
+} else {
+    echo 'All libs up to date'
 }
