b08df938b0a4ac6d741a78e9a8613a19c541cfff
Gymnasiast <m.o.steenbeek@gmail.com>
2015-09-03 13:36:32 +0200

    Only force installation on Travis

--- install.sh
+++ install.sh
@@ -74,7 +74,14 @@ if [[ `uname` == "Darwin" ]]; then
         popd
     fi
 elif [[ `uname` == "Linux" ]]; then
-    sudo apt-get install -y binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 cmake wine
+	if [[ -z "$TRAVIS" ]]; then
+	    sudo apt-get install -y binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 cmake
+		if [[ -z "$DISABLE_G2_BUILD" ]]; then
+			sudo apt-get install -y wine
+		fi
+	else
+		sudo apt-get install -y --force-yes binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 cmake
+	fi
 fi
 
 if [[ ! -f $cachedir/SDL2-devel-${SDL2_PV}-mingw.tar.gz ]]; then
