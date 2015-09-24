ac956433d653badeb8deafdc56d82dccffd2b787
Michał Janiszewski <janisozaur@gmail.com>
2015-09-24 22:47:14 +0200

    Refactor project build scripts
    This is mostly refactoring of cmake scripts, but also touches build.sh
    and install.sh. The cmake should be much more palatable now for major
    Linux distributions.
    
    At the same time, new configuration is introduced to Linux version, one
    with Twitch enabled.
    
    Please note, however, this change requires update of orctlibs.zip for
    mingw builds.

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ mkdir -p $cachedir
 
 echo `uname`
 
+# Sets default target to "linux", if none specified
+TARGET=${TARGET-linux}
+
+echo TARGET = $TARGET
+
 if [[ `uname` == "Darwin" ]]; then
     echo "Installation of OpenRCT2 assumes you have homebrew and use it to install packages."
 
@@ -84,81 +89,96 @@ elif [[ `uname` == "Linux" ]]; then
 			sudo apt-get install -y wine
 		fi
 	else
-		apt-cache search libsdl2
-		apt-cache policy libsdl2-dev:i386
-		apt-cache policy libsdl2-dev
-		sudo apt-get install -y --force-yes binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 cmake libsdl2-dev:i386 libsdl2-ttf-dev:i386 gcc-4.8 pkg-config:i386 g++-4.8-multilib gcc-4.8-multilib libjansson-dev:i386 libspeex-dev:i386 libspeexdsp-dev:i386 libcurl4-openssl-dev:i386 libcrypto++-dev:i386
-		export CC=gcc-4.8
-		export CXX=g++-4.8
+		case "$TARGET" in
+			"linux")
+				sudo apt-get install -y --force-yes cmake libsdl2-dev:i386 libsdl2-ttf-dev:i386 gcc-4.8 pkg-config:i386 g++-4.8-multilib gcc-4.8-multilib libjansson-dev:i386 libspeex-dev:i386 libspeexdsp-dev:i386 libcurl4-openssl-dev:i386 libcrypto++-dev:i386
+				wget https://launchpad.net/ubuntu/+archive/primary/+files/libjansson4_2.7-1ubuntu1_i386.deb
+				wget https://launchpad.net/ubuntu/+archive/primary/+files/libjansson-dev_2.7-1ubuntu1_i386.deb
+				sudo dpkg -i libjansson4_2.7-1ubuntu1_i386.deb
+				sudo dpkg -i libjansson-dev_2.7-1ubuntu1_i386.deb
+				sudo apt-get install -f
+				export CC=gcc-4.8
+				export CXX=g++-4.8
+				;;
+			"windows")
+				sudo apt-get install -y --force-yes binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 cmake
+				;;
+			*)
+				echo "unkown target $TARGET"
+				exit 1
+		esac
 	fi
 fi
 
-if [[ ! -f $cachedir/SDL2-devel-${SDL2_PV}-mingw.tar.gz ]]; then
-    wget http://libsdl.org/release/SDL2-devel-${SDL2_PV}-mingw.tar.gz --output-document $cachedir/SDL2-devel-${SDL2_PV}-mingw.tar.gz;
-fi
-if [[ ! -f $cachedir/SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz ]]; then
-    wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz --output-document $cachedir/SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz;
-fi
-if [[ ! -d $cachedir/SDL2-${SDL2_PV} ]]; then
-    pushd $cachedir
-        tar -xzf SDL2-devel-${SDL2_PV}-mingw.tar.gz
-    popd
-fi
-if [[ ! -d $cachedir/SDL2_ttf-${SDL2_TTF_PV} ]]; then
-    pushd $cachedir
-        tar -xzf SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz
-    popd
-fi
-if [[ ! -f $cachedir/orctlibs.zip ]]; then
-    curl https://download.openrct2.website/dev/lib/mingw -o $cachedir/orctlibs.zip;
-fi
-if [[ ! -d $cachedir/orctlibs ]]; then
-    mkdir -p $cachedir/orctlibs
-    pushd $cachedir/orctlibs
-        unzip -uaq ../orctlibs.zip
-    popd
-fi
+if [[ $TARGET == "windows" ]]; then
+	if [[ ! -f $cachedir/SDL2-devel-${SDL2_PV}-mingw.tar.gz ]]; then
+		wget http://libsdl.org/release/SDL2-devel-${SDL2_PV}-mingw.tar.gz --output-document $cachedir/SDL2-devel-${SDL2_PV}-mingw.tar.gz;
+	fi
+	if [[ ! -f $cachedir/SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz ]]; then
+		wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz --output-document $cachedir/SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz;
+	fi
+	if [[ ! -d $cachedir/SDL2-${SDL2_PV} ]]; then
+		pushd $cachedir
+			tar -xzf SDL2-devel-${SDL2_PV}-mingw.tar.gz
+		popd
+	fi
+	if [[ ! -d $cachedir/SDL2_ttf-${SDL2_TTF_PV} ]]; then
+		pushd $cachedir
+			tar -xzf SDL2_ttf-devel-${SDL2_TTF_PV}-mingw.tar.gz
+		popd
+	fi
+	if [[ ! -f $cachedir/orctlibs.zip ]]; then
+		curl https://dl.dropboxusercontent.com/u/1323345/orctlibs.zip -o $cachedir/orctlibs.zip;
+	fi
+	if [[ ! -d $cachedir/orctlibs ]]; then
+		mkdir -p $cachedir/orctlibs
+		pushd $cachedir/orctlibs
+			unzip -uaq ../orctlibs.zip
+			ls -lR .
+		popd
+	fi
 
-# Apply platform patch
-mingw_patch=libsdl2-mingw-2.0.3-fix-platform-detection-for-mingw.patch
-if [[ ! -f $cachedir/$mingw_patch ]]; then
-    wget "https://github.com/anyc/anyc-overlay/raw/master/media-libs/libsdl2-mingw/files/$mingw_patch" --output-document $cachedir/$mingw_patch;
+	# Apply platform patch
+	mingw_patch=libsdl2-mingw-2.0.3-fix-platform-detection-for-mingw.patch
+	if [[ ! -f $cachedir/$mingw_patch ]]; then
+		wget "https://github.com/anyc/anyc-overlay/raw/master/media-libs/libsdl2-mingw/files/$mingw_patch" --output-document $cachedir/$mingw_patch;
 
-    # XXX not sure how to make this idempotent.
-    pushd $cachedir/SDL2-${SDL2_PV}/i686-w64-mingw32/include/SDL2/
-        echo "Applying patch."
-        patch -p2 < ../../../../$mingw_patch
-    popd
+		# XXX not sure how to make this idempotent.
+		pushd $cachedir/SDL2-${SDL2_PV}/i686-w64-mingw32/include/SDL2/
+			echo "Applying patch."
+			patch -p2 < ../../../../$mingw_patch
+		popd
+	fi
+
+	if [[ ! -d /usr/local/cross-tools ]]; then
+		sudo mkdir -p /usr/local/cross-tools
+	fi
+	if [[ ! -d /usr/local/cross-tools/i686-w64-mingw32 ]]; then
+		sudo cp -r $cachedir/SDL2-${SDL2_PV}/i686-w64-mingw32 /usr/local/cross-tools/
+		sudo cp -r $cachedir/SDL2_ttf-${SDL2_TTF_PV}/i686-w64-mingw32 /usr/local/cross-tools/
+	fi
+	if [[ ! -d /usr/local/cross-tools/orctlibs ]]; then
+		sudo mkdir -p /usr/local/cross-tools/orctlibs
+		mkdir -p lib
+		sudo cp -rf $cachedir/orctlibs/glob/* /usr/local/cross-tools/orctlibs/.
+		cp -rf $cachedir/orctlibs/local/* ./lib/.
+	fi
+
+	if [[ ! -f $cachedir/i686-w64-mingw32-pkg-config ]]; then
+		# If this fails to work because of newlines, be sure you are running this
+		# script with Bash, and not sh. We should really move this to a separate
+		# file.
+		echo -e "#!/bin/sh\nexport PKG_CONFIG_PATH=/usr/local/cross-tools/i686-w64-mingw32/lib/pkgconfig:/usr/local/cross-tools/orctlibs/lib/pkgconfig\npkg-config \$@" > $cachedir/i686-w64-mingw32-pkg-config;
+		for i in /usr/local/cross-tools/orctlibs/lib/pkgconfig/*.pc
+		do
+			cat $i
+		done
+	fi
+
+	chmod +x $cachedir/i686-w64-mingw32-pkg-config
+	sudo cp $cachedir/i686-w64-mingw32-pkg-config /usr/local/bin/
+
+	ls -al /usr/local/bin | grep pkg-config
+	cat /usr/local/bin/i686-w64-mingw32-pkg-config
+# $TARGET == "windows"
 fi
-
-if [[ ! -d /usr/local/cross-tools ]]; then
-    sudo mkdir -p /usr/local/cross-tools
-fi
-if [[ ! -d /usr/local/cross-tools/i686-w64-mingw32 ]]; then
-    sudo cp -r $cachedir/SDL2-${SDL2_PV}/i686-w64-mingw32 /usr/local/cross-tools/
-    sudo cp -r $cachedir/SDL2_ttf-${SDL2_TTF_PV}/i686-w64-mingw32 /usr/local/cross-tools/
-fi
-if [[ ! -d /usr/local/cross-tools/orctlibs ]]; then
-    sudo mkdir -p /usr/local/cross-tools/orctlibs
-    mkdir -p lib
-    sudo cp -rf $cachedir/orctlibs/glob/* /usr/local/cross-tools/orctlibs/.
-    cp -rf $cachedir/orctlibs/local/* ./lib/.
-fi
-
-if [[ ! -f $cachedir/i686-w64-mingw32-pkg-config ]]; then
-    # If this fails to work because of newlines, be sure you are running this
-    # script with Bash, and not sh. We should really move this to a separate
-    # file.
-    echo -e "#!/bin/sh\nexport PKG_CONFIG_LIBDIR=/usr/local/cross-tools/i686-w64-mingw32/lib/pkgconfig\npkg-config \$@" > $cachedir/i686-w64-mingw32-pkg-config;
-fi
-
-set -x
-dpkg -L libsdl2-dev
-dpkg -L libsdl2-ttf-dev
-set +x
-
-chmod +x $cachedir/i686-w64-mingw32-pkg-config
-sudo cp $cachedir/i686-w64-mingw32-pkg-config /usr/local/bin/
-
-ls -al /usr/local/bin | grep pkg-config
-cat /usr/local/bin/i686-w64-mingw32-pkg-config
