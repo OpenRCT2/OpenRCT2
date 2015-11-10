6e63519bceee3a50f27621253cff50b42224b22b
Michał Janiszewski <janisozaur@gmail.com>
2015-11-10 22:30:38 +0100

    Make `scale` a float
    There's no problem in `scale` being a floating point value, however,
    since it's only NN scaling so far, it looks best for integer values

--- src/config.h
+++ src/config.h
@@ -167,7 +167,7 @@ typedef struct {
 	uint8 upper_case_banners;
 	uint8 allow_loading_with_incorrect_checksum;
 	uint8 steam_overlay_pause;
-	sint8 scale;
+	float scale;
 } general_configuration;
 
 typedef struct {
