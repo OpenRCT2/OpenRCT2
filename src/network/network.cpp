54c05da55a0b74cf8c33df9ef0adb97b7e203df4
Jacob Kruzicki <jacon500@gmail.com>
2015-11-12 11:53:08 -0600

    # This is a combination of 9 commits. # The first commit's message is: # This is a combination of 5 commits. # The first commit's message is: rebase d2aa736
    # This is the 2nd commit message:
    
    remove git merge seperator
    
    # This is the 3rd commit message:
    
    Fix #2231
    # This is the 4th commit message:
    
    Fix #2235 anti clockwise no longer crashes
    
    Issue cause by %4 not handling negative numbers.
    
    # This is the 5th commit message:
    
    Only play message sound in normal playing mode, fixes #2125
    
    # This is the 2nd commit message:
    
    Adjust Arial placement and sizes
    
    # This is the 3rd commit message:
    
    Use language codes for Korean and Russian
    
    # This is the 4th commit message:
    
    improve command line version information and add headless check
    
    # This is the 5th commit message:
    
    Correct two instances of American spelling
    # This is the 6th commit message:
    
    Update changelog
    # This is the 7th commit message:
    
    stop clients from pausing multiplayer games
    
    # This is the 8th commit message:
    
    stop desynchronisation when placing scenery, fixes #2111
    
    # This is the 9th commit message:
    
    disable peep watching thought for multiplayer as its client specific

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -615,7 +615,7 @@ void Network::UpdateServer()
 
 	switch (advertise_status) {
 	case ADVERTISE_STATUS_UNREGISTERED:
-		if (SDL_TICKS_PASSED(SDL_GetTicks(), last_advertise_time + MASTER_SERVER_REGISTER_TIME)) {
+		if (last_advertise_time == 0 || SDL_TICKS_PASSED(SDL_GetTicks(), last_advertise_time + MASTER_SERVER_REGISTER_TIME)) {
 			AdvertiseRegister();
 		}
 		break;
@@ -994,8 +994,7 @@ void Network::AdvertiseHeartbeat()
 	// Send the heartbeat request
 	std::string url = GetMasterServerUrl()
 		+ std::string("?command=heartbeat&token=") + advertise_token
-		+ std::string("key=") + advertise_key
-		+ std::string("players=") + std::to_string(network_get_num_players());
+		+ std::string("&players=") + std::to_string(network_get_num_players());
 
 	// TODO send status data (e.g. players) via JSON body
 
