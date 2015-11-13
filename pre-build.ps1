44e3e74a65d8a3f05768f7ba4c64fb5cd4e93d34
Michał Janiszewski <janisozaur@gmail.com>
2015-11-13 23:33:24 +0100

    Change pre-built link to http://openrct2.website/

--- pre-build.ps1
+++ pre-build.ps1
@@ -4,7 +4,7 @@ $path = Split-Path $Script:MyInvocation.MyCommand.Path
 $zip = $path+'\orctlibs.zip'
 $libs = $path+'\lib'
 $libsVFile = $path+'\libversion'
-$liburl = 'https://openrct.net/launcher/libs/orctlibs_vs.zip'
+$liburl = 'https://openrct2.website/files/orctlibs-vs.zip'
 $libsTest = Test-Path $libs
 
 #libs version test
