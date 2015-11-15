485705f1cbc9c55f14f4f8337056476aa637b72c
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2015-11-15 10:05:22 +0000

    Fix incorrect parameter type

--- src/world/map.h
+++ src/world/map.h
@@ -387,7 +387,7 @@ void map_clear_all_elements();
 rct_map_element *map_get_large_scenery_segment(int x, int y, int z, int direction, int sequence);
 bool map_large_scenery_get_origin(
 	int x, int y, int z, int direction, int sequence,
-	int *outX, int *outY, int *outZ, rct_map_element* outElement
+	int *outX, int *outY, int *outZ, rct_map_element** outElement
 );
 
 rct_map_element *map_get_track_element_at(int x, int y, int z);
