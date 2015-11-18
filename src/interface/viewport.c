ec24338a9ef905f1002ad3e0a68b249906387f8d
U-ASUS-EEEPC\Cameron Hall <cameronghall@cox.net>
2015-11-18 14:36:21 -0600

    Refactor and fix another bug

--- src/interface/viewport.c
+++ src/interface/viewport.c
@@ -1664,6 +1664,26 @@ static int callcode_push1(int address, int stackvar, registers *regs)
 }
 
 /**
+ * Helper function for viewport_surface_paint_setup
+ * mapElement = esi;
+ * unk = cl
+ */
+static inline int helper(rct_map_element *mapElement, registers *regs)
+{	
+	regs->bl = mapElement->properties.surface.slope;
+	regs->di = regs->bx;
+	regs->di &= 0xF;
+	regs->di <<= regs->cl;
+	regs->ebx &= 0x10;
+	regs->si = regs->di;
+	regs->si = (uint16)regs->si >> 4;
+	regs->di |= regs->si;
+	regs->di &= 0xF;
+	regs->bx |= regs->di;
+	return regs->ebx;
+}
+
+/**
  * rct2: 0x66062C
  * edx = height
  * esi = mapElement
@@ -1703,9 +1723,8 @@ void viewport_surface_paint_setup(int eax, int ebx, int height, rct_map_element 
 	
 	//callcode_push1(0x660671, saved_esi, &regs); return;
 	
-	uint16 unk = (mapElement->properties.surface.slope & 0xF) << regs.cl; //unk = di
-	
-	RCT2_GLOBAL(0x9E3278, uint32) = (mapElement->properties.surface.slope & 0x10) | ((unk | (unk >> 4)) & 0xF);
+	regs.ebx = helper(mapElement, &regs);
+	RCT2_GLOBAL(0x9E3278, uint32) = regs.ebx;
 	
 	//callcode_push1(0x660698, saved_esi, &regs); return;
 	
@@ -1718,71 +1737,56 @@ void viewport_surface_paint_setup(int eax, int ebx, int height, rct_map_element 
 	regs.bp = y;
 	//callcode_push1(0x6606C5, saved_esi, &regs); return;
 	
-	if(x >= 0x2000 || y >= 0x2000)
-		goto loc_660781;
-	
-	//callcode_push1(0x6606DA, saved_esi, &regs); return;
-	
-	rct_map_element *mapElement2 = map_get_surface_element_at(x/32, y/32);
-	
-	regs.esi = (int)mapElement2;
-	
-	//callcode_push1(0x6606F9, saved_esi, &regs); return;
-	
-	RCT2_GLOBAL(0x9E3248, rct_map_element *) = mapElement2;
-	regs.al = mapElement2->type & 3;
-	regs.ah = mapElement2->properties.surface.terrain >> 5;
-	regs.al |= regs.ah;
-	//callcode_push1(0x66070E, saved_esi, &regs); return;
-	
-	//Now, there's another landscape smoothing bug in the following lines of code.
-	regs.eax &= 0xFF;
-	RCT2_GLOBAL(0x9E325C, uint32) = regs.eax;
-	
-	/*
-	regs.bl = mapElement2->properties.surface.slope;
-	regs.di = regs.bx;
-	regs.di &= 0xF;
-	regs.di <<= regs.cl;
-	regs.ebx &= 0x10;
-	regs.si = regs.di;
-	regs.si = (uint16)regs.si >> 4;
-	regs.di |= regs.si;
-	regs.di &= 0xF;
-	regs.bx |= regs.di;
-	RCT2_GLOBAL(0x9E3270, uint32) = regs.ebx;
-	*/
-	
-	unk = (mapElement2->properties.surface.slope & 0xF) << regs.cl; //unk = di
-	
-	RCT2_GLOBAL(0x9E3270, uint32) = (mapElement2->properties.surface.slope & 0x10) | ((unk | (unk >> 4)) & 0xF);
-	//callcode_push1(0x66073F, saved_esi, &regs); return;
-	regs.ax = regs.dx;
-	regs.edi = RCT2_GLOBAL(0x9E3278, uint32);
-	//callcode_push1(0x660748, saved_esi, &regs); return;
-	regs.ax = (uint16)regs.ax >> 4;
-	assert(regs.ax == height/16);
-	mapElement2 = RCT2_GLOBAL(0x9E3248, rct_map_element *);
-	
-	regs.esi = (int)mapElement2;
-	//callcode_push1(0x660752, saved_esi, &regs); return;
-	
-	//SAVE
-	saved_ecx = regs.ecx;
-	
-	regs.ah = mapElement2->base_height >> 1;
-	regs.cx = regs.ax;
-	regs.al += RCT2_ADDRESS(0x97B4E4, uint8)[regs.edi];
-	regs.cl += RCT2_ADDRESS(0x97B504, uint8)[regs.edi];
-	regs.ah += RCT2_ADDRESS(0x97B4C4, uint8)[regs.ebx];
-	regs.ch += RCT2_ADDRESS(0x97B4A4, uint8)[regs.ebx];
-	RCT2_GLOBAL(0x9E3280, uint16) = regs.ax;
-	RCT2_GLOBAL(0x9E3288, uint16) = regs.cx;
-	
-	//RESTORE
-	regs.ecx = saved_ecx;
-	
-loc_660781:
+	if(x < 0x2000 && y < 0x2000)
+	{	
+		//callcode_push1(0x6606DA, saved_esi, &regs); return;
+		
+		rct_map_element *mapElement2 = map_get_surface_element_at(x/32, y/32);
+		
+		regs.esi = (int)mapElement2;
+		
+		//callcode_push1(0x6606F9, saved_esi, &regs); return;
+		
+		RCT2_GLOBAL(0x9E3248, rct_map_element *) = mapElement2;
+		regs.al = mapElement2->type & 3;
+		regs.ah = mapElement2->properties.surface.terrain >> 5;
+		regs.al |= regs.ah;
+		//callcode_push1(0x66070E, saved_esi, &regs); return;
+		
+		//Now, there's another landscape smoothing bug in the following lines of code.
+		regs.eax &= 0xFF;
+		RCT2_GLOBAL(0x9E325C, uint32) = regs.eax;
+
+		regs.ebx = helper(mapElement2, &regs);	
+		RCT2_GLOBAL(0x9E3270, uint32) = regs.ebx;
+		
+		//callcode_push1(0x66073F, saved_esi, &regs); return;
+		regs.ax = regs.dx;
+		regs.edi = RCT2_GLOBAL(0x9E3278, uint32);
+		//callcode_push1(0x660748, saved_esi, &regs); return;
+		regs.ax = (uint16)regs.ax >> 4;
+		assert(regs.ax == height/16);
+		mapElement2 = RCT2_GLOBAL(0x9E3248, rct_map_element *);
+		
+		regs.esi = (int)mapElement2;
+		//callcode_push1(0x660752, saved_esi, &regs); return;
+		
+		//SAVE
+		saved_ecx = regs.ecx;
+		
+		regs.ah = mapElement2->base_height >> 1;
+		regs.cx = regs.ax;
+		regs.al += RCT2_ADDRESS(0x97B4E4, uint8)[regs.edi];
+		regs.cl += RCT2_ADDRESS(0x97B504, uint8)[regs.edi];
+		regs.ah += RCT2_ADDRESS(0x97B4C4, uint8)[regs.ebx];
+		regs.ch += RCT2_ADDRESS(0x97B4A4, uint8)[regs.ebx];
+		RCT2_GLOBAL(0x9E3280, uint16) = regs.ax;
+		RCT2_GLOBAL(0x9E3288, uint16) = regs.cx;
+		
+		//RESTORE
+		regs.ecx = saved_ecx;
+	}
+//loc_660781:
 	callcode_push1(0x660781, saved_esi, &regs); return;
 }
 
