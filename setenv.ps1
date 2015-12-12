60d06ae9b760f1841c8fb3248fb4aed375c003d0
IntelOrca <ted@brambles.org>
2015-12-12 14:23:17 +0000

    move projects and add environment setup script

---
+++ setenv.ps1
@@ -0,0 +1,36 @@
+# Setups a PowerShell environment for OpenRCT2 development
+
+function AppExists($app)
+{
+    $result = (Get-Command $app -CommandType Application -ErrorAction SilentlyContinue)
+    return ($result -ne $null -and $result.Count -gt 0)
+}
+
+function AddPath($path)
+{
+    $env:path = "$path;$env:path"
+}
+
+$rootPath = Split-Path $Script:MyInvocation.MyCommand.Path
+$scriptsPath = "$rootPath\scripts"
+Write-Host "Setting up OpenRCT2 development environment for Windows" -ForegroundColor Cyan
+
+$appExists = @{}
+$appExists["msbuild"] = AppExists("msbuild");
+$appExists["7za"]     = AppExists("7za");
+$appExists["7z"]      = AppExists("7z");
+
+if (-not $appExists["msbuild"])
+{
+    $lookPath = Join-Path ${env:ProgramFiles(x86)} "MSBuild\14.0\Bin\amd64\MSBuild.exe"
+    if (Test-Path $lookPath)
+    {
+        AddPath($lookPath)
+    }
+    else
+    {
+        Write-Host "Warning: msbuild not found, please include it in your PATH" -ForegroundColor Yellow
+    }
+}
+
+AddPath($scriptsPath)
--- test.bat
+++
@@ -1,2 +0,0 @@
-msbuild .\projects\openrct2.sln "/p:Configuration=Release with Tests" /p:Platform=Win32
-.\build\Release\openrct2.exe test
