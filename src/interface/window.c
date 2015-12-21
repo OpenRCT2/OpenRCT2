d610fb59705a6eacdafbd01f603cdfee6e2411c0
Michał Janiszewski <janisozaur@gmail.com>
2015-12-21 22:17:13 +0100

    Remove call to 0x0040701D

--- src/interface/window.c
+++ src/interface/window.c
@@ -2424,9 +2424,6 @@ void textinput_cancel()
 	// Close the new text input window
 	window_close_by_class(WC_TEXTINPUT);
 
-	// The following code is only necessary for the old Windows text input dialog. In theory this isn't used anymore, but can
-	// still be triggered via original code paths.
-	RCT2_CALLPROC_EBPSAFE(0x0040701D);
 	if (RCT2_GLOBAL(RCT2_ADDRESS_TEXTINPUT_WINDOWCLASS, uint8) != 255) {
 		RCT2_CALLPROC_EBPSAFE(0x006EE4E2);
 		w = window_find_by_number(
