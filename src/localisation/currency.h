92faa602f69d21ba686a96c5b6756578e9c67945
Harry Lam <harrylf2@live.hk>
2016-02-03 13:31:38 +0800

    Adding Chinese Currencies, part 2

--- src/localisation/currency.h
+++ src/localisation/currency.h
@@ -38,6 +38,9 @@ typedef enum {
 	CURRENCY_WON,				// South Korean Won
 	CURRENCY_ROUBLE,			// Russian Rouble
 	CURRENCY_CZECH_KORUNA,		// Czech koruna
+	CURRENCY_HKD,			// Russian Rouble
+	CURRENCY_TWD,			// Russian Rouble
+	CURRENCY_YUAN,			// Russian Rouble
 
 	CURRENCY_END				// Last item
 } CURRENCY_TYPE;
