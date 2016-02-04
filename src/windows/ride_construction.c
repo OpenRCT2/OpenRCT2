217047c1a6cb423dd0820c417518a81b03f5a400
danidoedel <da-ba@gmx.de>
2016-02-04 21:07:45 +0100

    Fix #2881 Disable banking for steep to flat track

--- src/windows/ride_construction.c
+++ src/windows/ride_construction.c
@@ -720,6 +720,12 @@ static void window_ride_construction_resize(rct_window *w)
 			(1ULL << WIDX_BANK_STRAIGHT) |
 			(1ULL << WIDX_BANK_RIGHT);
 	}
+	// Disable banking if the start track is steep and the end of the track becomes flat.
+	if ((_previousTrackSlopeEnd == TRACK_SLOPE_DOWN_60 || _previousTrackSlopeEnd == TRACK_SLOPE_UP_60) && _currentTrackSlopeEnd == TRACK_SLOPE_NONE) {
+		disabledWidgets |=
+			(1ULL << WIDX_BANK_LEFT) |
+			(1ULL << WIDX_BANK_RIGHT);
+	}
 	if (!is_track_enabled(TRACK_SLOPE) && !is_track_enabled(TRACK_SLOPE_STEEP)) {
 		if (rideType != RIDE_TYPE_REVERSE_FREEFALL_COASTER && rideType != RIDE_TYPE_AIR_POWERED_VERTICAL_COASTER) {
 			// Disable all slopes
