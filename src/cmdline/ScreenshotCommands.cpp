78f87cd86c820930ec4bbcd6aaadc0984013cec6
IntelOrca <ted@brambles.org>
2016-03-20 18:13:17 +0000

    fix screenshot command line

--- src/cmdline/ScreenshotCommands.cpp
+++ src/cmdline/ScreenshotCommands.cpp
@@ -17,8 +17,8 @@ const CommandLineCommand CommandLine::ScreenshotCommands[]
 
 static exitcode_t HandleScreenshot(CommandLineArgEnumerator *argEnumerator)
 {
-    const char * * argv = (const char * *)argEnumerator->GetArguments() + argEnumerator->GetIndex() - 1;
-    int argc = argEnumerator->GetCount() - argEnumerator->GetIndex() + 1;
+    const char * * argv = (const char * *)argEnumerator->GetArguments() + argEnumerator->GetIndex();
+    int argc = argEnumerator->GetCount() - argEnumerator->GetIndex();
     int result = cmdline_for_screenshot(argv, argc);
     if (result < 0) {
         return EXITCODE_FAIL;
