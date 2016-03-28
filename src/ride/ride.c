5abb21065c43f3261b14ac5270aa3b16ad0893b3
X7123M3-256 <X123M3-256@users.noreply.github.com>
2016-03-28 21:57:00 +0100

    Implement disable train length limits cheat

--- src/ride/ride.c
+++ src/ride/ride.c
@@ -4543,7 +4543,7 @@ train_ref vehicle_create_train(int rideIndex, int x, int y, int z, int vehicleIn
 {
 	rct_ride *ride = get_ride(rideIndex);
 
-	uint8 trainLayout[42];
+	uint8 trainLayout[RIDE_MAX_CARS_PER_TRAIN];//TODO -work out what's going on here
 	ride_entry_get_train_layout(ride->subtype, ride->num_cars_per_train, trainLayout);
 
 	train_ref train = { NULL, NULL };
@@ -7409,7 +7409,7 @@ void ride_update_max_vehicles(int rideIndex)
 	rct_ride *ride;
 	rct_ride_entry *rideEntry;
 	rct_ride_entry_vehicle *vehicleEntry;
-	uint8 trainLayout[16], numCarsPerTrain, numVehicles;
+	uint8 trainLayout[RIDE_MAX_CARS_PER_TRAIN], numCarsPerTrain, numVehicles;
 	int trainLength, maxNumTrains;
 
 	ride = get_ride(rideIndex);
@@ -7417,6 +7417,7 @@ void ride_update_max_vehicles(int rideIndex)
 		return;
 
 	rideEntry = get_ride_entry(ride->subtype);
+
 	if (rideEntry->cars_per_flat_ride == 0xFF) {
 		ride->num_cars_per_train = max(rideEntry->min_cars_in_train, ride->num_cars_per_train);
 		ride->min_max_cars_per_train = rideEntry->max_cars_in_train | (rideEntry->min_cars_in_train << 4);
@@ -7446,7 +7447,9 @@ void ride_update_max_vehicles(int rideIndex)
 		}
 		int newCarsPerTrain = max(ride->proposed_num_cars_per_train, rideEntry->min_cars_in_train);
 		maxCarsPerTrain = max(maxCarsPerTrain, rideEntry->min_cars_in_train);
-		newCarsPerTrain = min(maxCarsPerTrain, newCarsPerTrain);
+		if(!gCheatsDisableTrainLengthLimit) {
+			newCarsPerTrain = min(maxCarsPerTrain, newCarsPerTrain);
+		}
 		ride->min_max_cars_per_train = maxCarsPerTrain | (rideEntry->min_cars_in_train << 4);
 
 		switch (ride->mode) {
@@ -7664,7 +7667,9 @@ money32 ride_set_vehicles(uint8 rideIndex, uint8 setting, uint8 value, uint32 fl
 
 		invalidate_test_results(rideIndex);
 		rideEntry = get_ride_entry(ride->subtype);
-		value = clamp(rideEntry->min_cars_in_train, value, rideEntry->max_cars_in_train);
+		if(!gCheatsDisableTrainLengthLimit) {
+			value = clamp(rideEntry->min_cars_in_train, value, rideEntry->max_cars_in_train);
+		}
 		ride->proposed_num_cars_per_train = value;
 		break;
 	case RIDE_SET_VEHICLES_COMMAND_TYPE_RIDE_ENTRY:
@@ -7690,7 +7695,9 @@ money32 ride_set_vehicles(uint8 rideIndex, uint8 setting, uint8 value, uint32 fl
 			preset = ride_get_unused_preset_vehicle_colour(ride->type, ride->subtype);
 		}
 		ride_set_vehicle_colours_to_random_preset(ride, preset);
-		ride->proposed_num_cars_per_train = clamp(rideEntry->min_cars_in_train, ride->proposed_num_cars_per_train, rideEntry->max_cars_in_train);
+		if(!gCheatsDisableTrainLengthLimit) {
+			ride->proposed_num_cars_per_train = clamp(rideEntry->min_cars_in_train, ride->proposed_num_cars_per_train, rideEntry->max_cars_in_train);
+		}
 		break;
 	default:
 		log_error("Unknown vehicle command.");
