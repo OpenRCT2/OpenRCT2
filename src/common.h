OpenRCT2/OpenRCT2

commit 288d11a1961270615d2657039bfbcfac677d04ed

Add platform define for x86

src/common.h
@@ -34,6 +34,11 @@
 #endif
 #define abstract = 0
 
+#if defined(__i386__) || defined(_M_I86)
+#define PLATFORM_X86
+#endif
+
+#ifdef PLATFORM_X86
 #ifdef __GNUC__
 #define FASTCALL __attribute__((fastcall))
 #elif _MSC_VER
@@ -42,5 +47,8 @@
 #pragma message "Not using fastcall calling convention, please check your compiler support"
 #define FASTCALL
 #endif
+#else // PLATFORM_X86
+#define FASTCALL
+#endif // PLATFORM_X86
 
 #endif

src/addresses.c
@@ -12,7 +12,8 @@
 
 int DISABLE_OPT RCT2_CALLPROC_X(int address, int _eax, int _ebx, int _ecx, int _edx, int _esi, int _edi, int _ebp)
 {
-	int result;
+	int result = 0;
+#ifdef PLATFORM_X86
 	#ifdef _MSC_VER
 	__asm {
 		push ebp
@@ -56,13 +57,15 @@ int DISABLE_OPT RCT2_CALLPROC_X(int address, int _eax, int _ebx, int _ecx, int _
 		: "eax","ecx","edx","esi","edi","memory"
 	);
 	#endif
+#endif // PLATFORM_X86
 	// lahf only modifies ah, zero out the rest
 	return result & 0xFF00;
 }
 
 int DISABLE_OPT RCT2_CALLFUNC_X(int address, int *_eax, int *_ebx, int *_ecx, int *_edx, int *_esi, int *_edi, int *_ebp)
 {
-	int result;
+	int result = 0;
+#ifdef PLATFORM_X86
 	#ifdef _MSC_VER
 	__asm {
 		// Store C's base pointer
@@ -207,6 +210,7 @@ int DISABLE_OPT RCT2_CALLFUNC_X(int address, int *_eax, int *_ebx, int *_ecx, in
 		: "eax","ecx","edx","esi","edi","memory"
 	);
 	#endif
+#endif // PLATFORM_X86
 	// lahf only modifies ah, zero out the rest
 	return result & 0xFF00;
 }

commit 9714c3e77ce5601a86f8fec23c7066a24ae95b0b

Fix typo introduced in 288d11a, fixes #3376
See https://sourceforge.net/p/predef/wiki/Architectures/

This reverts commit 67a86fe22f9654f39f6c6197f94fd9857cc29abd.

openrct2.vcxproj
@@ -361,7 +361,7 @@
       <Optimization>Disabled</Optimization>
       <SDLCheck>true</SDLCheck>
       <StructMemberAlignment>1Byte</StructMemberAlignment>
-      <PreprocessorDefinitions>$(OpenRCT2_DEFINES);PLATFORM_X86;DEBUG;_CRT_SECURE_NO_WARNINGS;_USE_MATH_DEFINES;CURL_STATICLIB;SDL_MAIN_HANDLED;_WINSOCK_DEPRECATED_NO_WARNINGS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>$(OpenRCT2_DEFINES);DEBUG;_CRT_SECURE_NO_WARNINGS;_USE_MATH_DEFINES;CURL_STATICLIB;SDL_MAIN_HANDLED;_WINSOCK_DEPRECATED_NO_WARNINGS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
       <MultiProcessorCompilation>true</MultiProcessorCompilation>
       <ObjectFileName>$(IntDir)\%(RelativeDir)</ObjectFileName>
@@ -388,7 +388,7 @@
       <TreatSpecificWarningsAsErrors>4013</TreatSpecificWarningsAsErrors>
       <OmitFramePointers />
       <BufferSecurityCheck>false</BufferSecurityCheck>
-      <PreprocessorDefinitions>$(OpenRCT2_DEFINES);PLATFORM_X86;_CRT_SECURE_NO_WARNINGS;_USE_MATH_DEFINES;CURL_STATICLIB;SDL_MAIN_HANDLED;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>$(OpenRCT2_DEFINES);_CRT_SECURE_NO_WARNINGS;_USE_MATH_DEFINES;CURL_STATICLIB;SDL_MAIN_HANDLED;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <ObjectFileName>$(IntDir)\%(RelativeDir)</ObjectFileName>
       <MultiProcessorCompilation>true</MultiProcessorCompilation>
       <FavorSizeOrSpeed>Speed</FavorSizeOrSpeed>

src/common.h
@@ -34,7 +34,7 @@
 #endif
 #define abstract = 0
 
-#if defined(__i386__) || defined(_M_I86)
+#if defined(__i386__) || defined(_M_IX86)
 #define PLATFORM_X86
 #endif
 