c53d25922efa85351b2f91152cb6aaf72f5eebc7
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-04-25 10:35:35 +0200

    Move surface painting to seperate file

---
+++ src/interface/paint_surface.h
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ * Copyright (c) 2014 Ted John
+ * OpenRCT2, an open source clone of Roller Coaster Tycoon 2.
+ *
+ * This file is part of OpenRCT2.
+ *
+ * OpenRCT2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#ifndef _PAINT_SURFACE_H
+#define _PAINT_SURFACE_H
+
+#include "../world/map.h"
+
+void viewport_surface_paint_setup(uint8 direction, uint16 height, rct_map_element *mapElement);
+
+#endif //_PAINT_SURFACE_H
