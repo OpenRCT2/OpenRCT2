ccf922245eea46ad7b83f0d37a401f4ad1b9c81e
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-04-27 23:13:32 +0200

    Update headers

--- src/interface/paint_surface.c
+++ src/interface/paint_surface.c
@@ -1,28 +1,7 @@
-/*****************************************************************************
- * Copyright (c) 2014 Ted John
- * OpenRCT2, an open source clone of Roller Coaster Tycoon 2.
- *
- * This file is part of OpenRCT2.
- *
- * OpenRCT2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *****************************************************************************/
-
+#include "../common.h"
 #include "paint_surface.h"
 #include "viewport.h"
 #include "../config.h"
-#include "../addresses.h"
-#include "../world/map.h"
 
 const uint8 byte_97B444[] = {
 	0, 2, 1, 3, 8, 10, 9, 11, 4, 6,
