871ed67a78ac8b41fa0dc94253021fe1df69e287
zsilencer <zsilencergame@gmail.com>
2016-05-08 18:26:13 -0600

    Decompile vehicle_visual_reverser

--- src/ride/coaster/reverser_roller_coaster.c
+++ src/ride/coaster/reverser_roller_coaster.c
@@ -13,3 +13,27 @@
  * A full copy of the GNU General Public License can be found in licence.txt
  *****************************************************************************/
 #pragma endregion
+
+#include "../../addresses.h"
+#include "../../config.h"
+#include "../../interface/viewport.h"
+#include "../../world/sprite.h"
+#include "../../paint/paint.h"
+#include "../vehicle_paint.h"
+
+/**
+ *
+ *  rct2: 0x006D4453
+ */
+void vehicle_visual_reverser(int x, int imageDirection, int y, int z, rct_vehicle *vehicle, const rct_ride_entry_vehicle *vehicleEntry)
+{
+	rct_vehicle *v1 = &g_sprite_list[vehicle->prev_vehicle_on_ride].vehicle;
+	rct_vehicle *v2 = &g_sprite_list[vehicle->next_vehicle_on_ride].vehicle;
+	x = (v1->x + v2->x) / 2;
+	y = (v1->y + v2->y) / 2;
+	z = (v1->z + v2->z) / 2;
+	RCT2_GLOBAL(0x9DE568, sint16) = x;
+	RCT2_GLOBAL(0x9DE56C, sint16) = y;
+	uint32 rct2VehiclePtrFormat = ((uint32)vehicleEntry) - offsetof(rct_ride_entry, vehicles);
+	RCT2_CALLPROC_X(0x006D45F8, x, imageDirection, y, z, (int)vehicle, rct2VehiclePtrFormat, 0);
+}
