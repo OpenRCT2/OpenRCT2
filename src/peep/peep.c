6db3828f32069b472348ba65887e68f7fc0483e6
zsilencer <zsilencergame@gmail.com>
2016-05-15 06:21:19 -0600

    Fix #3609: Crash from invalid peep

--- src/peep/peep.c
+++ src/peep/peep.c
@@ -1578,7 +1578,8 @@ void remove_peep_from_queue(rct_peep* peep)
 		return;
 	}
 
-	for (rct_peep* other_peep = GET_PEEP(ride->last_peep_in_queue[cur_station]);;
+	for (rct_peep* other_peep = GET_PEEP(ride->last_peep_in_queue[cur_station]);
+		ride->last_peep_in_queue[cur_station] != 0xFFFF;
 		other_peep = GET_PEEP(other_peep->next_in_queue)){
 		if (peep->sprite_index == other_peep->next_in_queue){
 			other_peep->next_in_queue = peep->next_in_queue;
