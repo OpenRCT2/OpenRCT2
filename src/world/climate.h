cfb8db84982480305e3a1774523612893f6406d7
brettpenzer123 <brettpenzer123@live.co.uk>
2016-05-15 14:45:28 +0100

    New weather type snow.
    I think this is correct.

--- src/world/climate.h
+++ src/world/climate.h
@@ -26,13 +26,14 @@ enum {
 	CLIMATE_COLD
 };
 
-enum{
+enum {
 	WEATHER_SUNNY,
 	WEATHER_PARTIALLY_CLOUDY,
 	WEATHER_CLOUDY,
 	WEATHER_RAIN,
 	WEATHER_HEAVY_RAIN,
-	WEATHER_THUNDER
+	WEATHER_THUNDER,
+	WEATHER_SNOW
 };
 
 typedef struct rct_weather {
@@ -40,6 +41,7 @@ typedef struct rct_weather {
 	sint8 effect_level;
 	sint8 gloom_level;
 	sint8 rain_level;
+	sint8 snow_level;
 	uint32 sprite_id;
 } rct_weather;
 
@@ -49,14 +51,16 @@ typedef struct rct_weather {
 #define gClimateCurrentWeatherEffect	RCT2_GLOBAL(RCT2_ADDRESS_CURRENT_WEATHER_EFFECT, uint8)
 #define gClimateCurrentWeatherGloom		RCT2_GLOBAL(RCT2_ADDRESS_CURRENT_WEATHER_GLOOM, uint8)
 #define gClimateCurrentRainLevel		RCT2_GLOBAL(RCT2_ADDRESS_CURRENT_RAIN_LEVEL, uint8)
+#define gClimateCurrentSnowLevel		RCT2_GLOBAL(RCT2_ADDRESS_CURRENT_SNOW_LEVEL, uint8)
 #define gClimateNextWeather				RCT2_GLOBAL(RCT2_ADDRESS_NEXT_WEATHER, uint8)
 #define gClimateNextTemperature			RCT2_GLOBAL(RCT2_ADDRESS_NEXT_TEMPERATURE, sint8)
 #define gClimateNextWeatherEffect		RCT2_GLOBAL(RCT2_ADDRESS_NEXT_WEATHER_EFFECT, uint8)
 #define gClimateNextWeatherGloom		RCT2_GLOBAL(RCT2_ADDRESS_NEXT_WEATHER_GLOOM, uint8)
 #define gClimateNextRainLevel			RCT2_GLOBAL(RCT2_ADDRESS_NEXT_RAIN_LEVEL, uint8)
+#define gClimateNextSnowLevel			RCT2_GLOBAL(RCT2_ADDRESS_NEXT_SNOW_LEVEL, uint8)
 #define gClimateUpdateTimer				RCT2_GLOBAL(RCT2_ADDRESS_CLIMATE_UPDATE_TIMER, uint16)
 
-extern const rct_weather climate_weather_data[6];
+extern const rct_weather climate_weather_data[7];
 
 extern uint16 gClimateLightningFlash;
 
