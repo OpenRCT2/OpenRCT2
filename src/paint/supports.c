588296bcb795042fa626153e789a44731995ee83
Duncan <duncans_pumpkin@hotmail.co.uk>
2016-05-19 12:37:04 +0100

    Fix metal a supports on slopes.

--- src/paint/supports.c
+++ src/paint/supports.c
@@ -493,7 +493,7 @@ bool metal_a_supports_paint_setup(int supportType, int segment, int special, int
 		image_id |= imageColourFlags;
 
 
-		sub_98197C(image_id, xOffset, yOffset, 0, 0, z - 1, height, boundBoxOffsetX, boundBoxOffsetY, boundBoxOffsetZ, rotation);
+		sub_98197C(image_id, xOffset, yOffset, 0, 0, 0, height, boundBoxOffsetX, boundBoxOffsetY, boundBoxOffsetZ, rotation);
 
 		height += z;
 	}
