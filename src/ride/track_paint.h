6a8c99a73db88195e7fd00ac2295e8d964090865
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-05-19 23:15:54 +0200

    Draw Roto Drop

--- src/ride/track_paint.h
+++ src/ride/track_paint.h
@@ -90,6 +90,7 @@ TRACK_PAINT_FUNCTION get_track_paint_function_merry_go_round(int trackType, int 
 TRACK_PAINT_FUNCTION get_track_paint_function_facility(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_haunted_house(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_circus_show(int trackType, int direction);
+TRACK_PAINT_FUNCTION get_track_paint_function_roto_drop(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_flying_saucers(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_crooked_house(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_submarine_ride(int trackType, int direction);
