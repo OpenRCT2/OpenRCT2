193d6f06f16e95140c7fd1c2cde8dc816a435b22
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-05-20 14:15:19 +0200

    Compile assets into apk

--- distribution/android/app/src/main/java/org/openrct/android/MainActivity.java
+++ distribution/android/app/src/main/java/org/openrct/android/MainActivity.java
@@ -2,13 +2,23 @@ package org.openrct.android;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import org.apache.commons.io.IOUtils;
 import org.libsdl.app.SDLActivity;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends SDLActivity {
+
+    public static final String TAG = "OpenRCT2";
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -17,7 +27,6 @@ public class MainActivity extends SDLActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         boolean canRead = false;
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             canRead = true;
@@ -33,7 +42,43 @@ public class MainActivity extends SDLActivity {
             return;
         }
 
+        File dataDir = new File("/sdcard/openrct2/");
+        try {
+            copyAsset(getAssets(), "openrct2_load", dataDir, "openrct2_load");
+            copyAsset(getAssets(), "data", dataDir, "");
+        } catch (IOException e) {
+            Log.e(TAG, "Error extracting files", e);
+            finish();
+            return;
+        }
+
+
         super.onCreate(savedInstanceState);
+    }
+
+    private void copyAsset(AssetManager assets, String srcPath, File dataDir, String destPath) throws IOException {
+        String[] list = assets.list(srcPath);
+
+        if (list.length == 0) {
+            InputStream input = assets.open(srcPath);
+            File extractedFile = new File(dataDir, destPath);
+            File parentFile = extractedFile.getParentFile();
+            if (!parentFile.exists()) {
+                boolean success = parentFile.mkdirs();
+                if (!success) {
+                    Log.d(TAG, String.format("Error creating folder '%s'", parentFile));
+                }
+            }
+            FileOutputStream output = new FileOutputStream(extractedFile);
+            IOUtils.copyLarge(input, output);
+            output.close();
+            input.close();
+            return;
+        }
+
+        for (String fileName : list) {
+            copyAsset(assets, srcPath + File.separator + fileName, dataDir, destPath + File.separator + fileName);
+        }
     }
 
     public float getDefaultScale() {
