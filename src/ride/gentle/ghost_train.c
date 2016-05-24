75493a543356da9c76ef0fe351378d9184543d19
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-05-24 21:01:30 +0200

    Draw flat to 25 deg up

--- src/ride/gentle/ghost_train.c
+++ src/ride/gentle/ghost_train.c
@@ -27,11 +27,19 @@ enum
 {
 	SPR_GHOST_TRAIN_TRACK_FLAT_SW_NE = 28821,
 	SPR_GHOST_TRAIN_TRACK_FLAT_NW_SE = 28822,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_SW_NE = 28823,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_NW_SE = 28824,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_NE_SW = 28825,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_SE_NW = 28826,
 
 	SPR_GHOST_TRAIN_TRACK_25_DEG_UP_SW_NE = 28831,
 	SPR_GHOST_TRAIN_TRACK_25_DEG_UP_NW_SE = 28832,
 	SPR_GHOST_TRAIN_TRACK_25_DEG_UP_NE_SW = 28833,
 	SPR_GHOST_TRAIN_TRACK_25_DEG_UP_SE_NW = 28834,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_SW_NE = 28835,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_NW_SE = 28836,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_NE_SW = 28837,
+	SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_SE_NW = 28838,
 
 	SPR_GHOST_TRAIN_TRACK_25_DEG_UP_FRONT_SW_NE = 28843,
 	SPR_GHOST_TRAIN_TRACK_25_DEG_UP_FRONT_NW_SE = 28844,
@@ -47,6 +55,13 @@ static const uint32 ghost_train_track_pieces_flat[4] = {
 	SPR_GHOST_TRAIN_TRACK_FLAT_NW_SE,
 	SPR_GHOST_TRAIN_TRACK_FLAT_SW_NE,
 	SPR_GHOST_TRAIN_TRACK_FLAT_NW_SE,
+};
+
+static const uint32 ghost_train_track_pieces_flat_to_25_deg_up[4][2] = {
+	{SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_SW_NE, SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_SW_NE},
+	{SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_NW_SE, SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_NW_SE},
+	{SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_NE_SW, SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_NE_SW},
+	{SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_SE_NW, SPR_GHOST_TRAIN_TRACK_FLAT_TO_25_DEG_UP_FRONT_SE_NW},
 };
 
 static const uint32 ghost_train_track_pieces_25_deg_up[4][2] = {
@@ -133,9 +148,46 @@ static void paint_ghost_train_track_25_deg_up(uint8 rideIndex, uint8 trackSequen
 	paint_util_set_general_support_height(height + 56, 0x20);
 }
 
-/** rct2: 0x */
+/** rct2: 0x00770C0C */
 static void paint_ghost_train_track_flat_to_25_deg_up(uint8 rideIndex, uint8 trackSequence, uint8 direction, int height, rct_map_element * mapElement)
 {
+	rct_xy16 position = {RCT2_GLOBAL(0x009DE56A, sint16), RCT2_GLOBAL(0x009DE56E, sint16)};
+
+	uint32 imageId = ghost_train_track_pieces_flat_to_25_deg_up[direction][0] | RCT2_GLOBAL(0x00F44198, uint32);
+	if (direction == 0 || direction == 2) {
+		sub_98197C(imageId, 0, 0, 32, 20, 3, height, 0, 6, height, get_current_rotation());
+	} else {
+		sub_98197C(imageId, 0, 0, 20, 32, 3, height, 6, 0, height, get_current_rotation());
+	}
+
+	imageId = ghost_train_track_pieces_flat_to_25_deg_up[direction][1] | RCT2_GLOBAL(0x00F44198, uint32);
+	if (direction == 0 || direction == 2) {
+		sub_98197C(imageId, 0, 0, 32, 1, 15, height, 0, 27, height, get_current_rotation());
+	} else {
+		sub_98197C(imageId, 0, 0, 1, 32, 15, height, 27, 0, height, get_current_rotation());
+	}
+
+	if (track_paint_util_should_paint_supports(position)) {
+		metal_a_supports_paint_setup(3, 4, 3, height, RCT2_GLOBAL(0x00F4419C, uint32));
+	}
+
+	switch (direction) {
+		case 0:
+			paint_util_push_tunnel_left(height, TUNNEL_0);
+			break;
+		case 1:
+			paint_util_push_tunnel_right(height, TUNNEL_2);
+			break;
+		case 2:
+			paint_util_push_tunnel_left(height, TUNNEL_2);
+			break;
+		case 3:
+			paint_util_push_tunnel_right(height, TUNNEL_0);
+			break;
+	}
+
+	paint_util_set_segment_support_height(paint_util_rotate_segments(SEGMENT_D0 | SEGMENT_C4 | SEGMENT_CC, direction), 0xFFFF, 0);
+	paint_util_set_general_support_height(height + 48, 0x20);
 }
 
 /** rct2: 0x */
