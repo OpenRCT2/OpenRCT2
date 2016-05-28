1aa8f54083a10a8d3dd7ab4ed567c3fdcfeff5e4
JeroenDStout <none@none>
2016-05-28 21:09:30 +0200

    Fixed effect of STOUT_PEEPS_EXPANDED_EXPERIMENT with padding

--- src/peep/peep.h
+++ src/peep/peep.h
@@ -400,16 +400,15 @@ typedef struct rct_peep {
 	sint16 sprite_right;			// 0x1A
 	sint16 sprite_bottom;			// 0x1C
 	uint8 sprite_direction;			// 0x1E
-#ifdef STOUT_PEEPS_EXPANDED_EXPERIMENT
 	union {
 		uint8 pad_1F[3];
+#ifdef STOUT_PEEPS_EXPANDED_EXPERIMENT
 		struct {
 			uint8 peeps_ex_direction_preference;
-			uint8 pad_1;
-			uint8 pad_2;
+			uint8 pad_1F_[2];
 		};
-	};
 #endif
+	};
 	uint16 name_string_idx;			// 0x22
 	uint16 next_x;					// 0x24
 	uint16 next_y;					// 0x26
