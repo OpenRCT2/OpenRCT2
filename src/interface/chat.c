d8cdc16f856d104799ccf3bb6a0b950f66ea79b7
Matte A <matte_med_latte@hotmail.com>
2016-06-09 23:08:03 +0200

    Adding the basic background and changing size

--- src/interface/chat.c
+++ src/interface/chat.c
@@ -81,8 +81,8 @@ void chat_draw(rct_drawpixelinfo * dpi)
 		return;
 	}
 	_chatLeft = 10;
-	_chatTop = gScreenHeight - 40 - ((CHAT_HISTORY_SIZE + 1) * 10);
-	_chatRight = gScreenWidth - 10;
+	_chatTop = gScreenHeight - 100 - ((CHAT_HISTORY_SIZE + 1) * 10);
+	_chatRight = gScreenWidth - 500;
 	_chatBottom = gScreenHeight - 45;
 	char lineBuffer[CHAT_INPUT_SIZE + 10];
 	char* lineCh = lineBuffer;
@@ -99,6 +99,8 @@ void chat_draw(rct_drawpixelinfo * dpi)
 		gfx_draw_string(dpi, lineBuffer, 255, x, y);
 	}
 	if (gChatOpen) {
+		gfx_fill_rect(dpi, _chatLeft, _chatTop, _chatRight, _chatBottom, 0x2000000 | 50); 	// Background
+		
 		lineCh = utf8_write_codepoint(lineCh, FORMAT_OUTLINE);
 		lineCh = utf8_write_codepoint(lineCh, FORMAT_CELADON);
 		safe_strcpy(lineCh, _chatCurrentLine, CHAT_INPUT_SIZE);
