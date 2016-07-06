f6ded42f82589267c8dbe5eb4a4d3c2746fd1b40
Sijmen Schoon <sijmenschoon@gmail.com>
2016-07-06 21:04:46 +0200

    Make path_get_directory const

--- src/util/util.c
+++ src/util/util.c
@@ -62,7 +62,7 @@ bool filename_valid_characters(const utf8 *filename)
 	return true;
 }
 
-utf8 *path_get_directory(const utf8 *path)
+const utf8 *path_get_directory(const utf8 *path)
 {
 	// Find the last slash or backslash in the path
 	char *filename = strrchr(path, platform_get_path_separator());
