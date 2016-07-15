8f4ba85b896b9debf83c175c743575dd6d881be5
Ted John <ted@brambles.org>
2016-07-15 22:55:19 +0100

    import unk_982134

--- src/peep/peep.c
+++ src/peep/peep.c
@@ -332,6 +332,10 @@ static uint8 unk_981D8F[] = {
 	34, 35, 36, 0, 0
 };
 
+static uint8 unk_982134[] = {
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1,
+};
+
 // These arrays contain the base minimum and maximum nausea ratings for peeps, based on their nausea tolerance level.
 static const ride_rating NauseaMinimumThresholds[] = {
 	0, 0, 200, 400
@@ -586,7 +590,7 @@ static void sub_68F41A(rct_peep *peep, int index)
 			peep->action = PEEP_ACTION_NONE_2;
 
 		peep->peep_flags &= ~PEEP_FLAGS_SLOW_WALK;
-		if (RCT2_ADDRESS(0x00982134, uint8)[sprite_type] & 1){
+		if (unk_982134[sprite_type] & 1) {
 			peep->peep_flags |= PEEP_FLAGS_SLOW_WALK;
 		}
 
@@ -1287,7 +1291,7 @@ static void set_sprite_type(rct_peep* peep, uint8 type){
 		peep->action = PEEP_ACTION_NONE_2;
 
 	peep->peep_flags &= ~PEEP_FLAGS_SLOW_WALK;
-	if (RCT2_ADDRESS(0x00982134, uint8)[type] & 1){
+	if (unk_982134[type] & 1) {
 		peep->peep_flags |= PEEP_FLAGS_SLOW_WALK;
 	}
 
