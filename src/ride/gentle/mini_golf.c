215ed5e14d0b906001c23fe77559f782c43a6810
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-07-25 22:11:35 +0200

    Attempt to name animations

--- src/ride/gentle/mini_golf.c
+++ src/ride/gentle/mini_golf.c
@@ -375,7 +375,7 @@ static const uint32 mini_golf_track_sprites_hole_e[][3][2] = {
 static const uint8 mini_golf_peep_animation_frames_walk[] = {0, 1, 2, 3, 4, 5};
 
 /** rct2: 0x00933478 */
-static const uint8 mini_golf_peep_animation_frames_move_down[] = {12, 13, 14, 15};
+static const uint8 mini_golf_peep_animation_frames_place_ball_downwards[] = {12, 13, 14, 15};
 
 /** rct2: 0x009334B5 */
 static const uint8 mini_golf_peep_animation_frames_swing[] = {31, 31, 31, 31, 31, 31, 31, 31, 31, 32, 33, 33, 33, 33, 34};
@@ -384,13 +384,13 @@ static const uint8 mini_golf_peep_animation_frames_swing[] = {31, 31, 31, 31, 31
 static const uint8 mini_golf_peep_animation_frames_swing_left[] = {6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 8, 8, 8, 9};
 
 /** rct2: 0x0093348D */
-static const uint8 mini_golf_peep_animation_frames_3[] = {12, 13, 14, 15, 14, 13, 12};
+static const uint8 mini_golf_peep_animation_frames_place_ball_upwards[] = {12, 13, 14, 15, 14, 13, 12};
 
 /** rct2: 0x00933495 */
 static const uint8 mini_golf_peep_animation_frames_jump[] = {16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
 /** rct2: 0x009334A5 */
-static const uint8 mini_golf_peep_animation_frames_move_up[] = {15, 14, 13, 12};
+static const uint8 mini_golf_peep_animation_frames_pickup_ball[] = {15, 14, 13, 12};
 
 /** rct2: 0x009334C5 */
 static const uint8 mini_golf_peep_animation_frames_put[] = {35, 36, 36, 36, 36, 36, 35, 35, 35, 35};
@@ -401,11 +401,11 @@ static const uint8 mini_golf_peep_animation_frames_put_left[] = {10, 11, 11, 11,
 /** rct2: 0x008B8F74 */
 static const uint8 * mini_golf_peep_animation_frames[] = {
 	mini_golf_peep_animation_frames_walk,
-	mini_golf_peep_animation_frames_move_down,
+	mini_golf_peep_animation_frames_place_ball_downwards,
 	mini_golf_peep_animation_frames_swing_left,
-	mini_golf_peep_animation_frames_3,
+	mini_golf_peep_animation_frames_place_ball_upwards,
 	mini_golf_peep_animation_frames_jump,
-	mini_golf_peep_animation_frames_move_up,
+	mini_golf_peep_animation_frames_pickup_ball,
 	mini_golf_peep_animation_frames_put_left,
 	mini_golf_peep_animation_frames_swing,
 	mini_golf_peep_animation_frames_put,
@@ -413,11 +413,11 @@ static const uint8 * mini_golf_peep_animation_frames[] = {
 
 const uint8 mini_golf_peep_animation_lengths[] = {
 	countof(mini_golf_peep_animation_frames_walk),
-	countof(mini_golf_peep_animation_frames_move_down),
+	countof(mini_golf_peep_animation_frames_place_ball_downwards),
 	countof(mini_golf_peep_animation_frames_swing_left),
-	countof(mini_golf_peep_animation_frames_3),
+	countof(mini_golf_peep_animation_frames_place_ball_upwards),
 	countof(mini_golf_peep_animation_frames_jump),
-	countof(mini_golf_peep_animation_frames_move_up),
+	countof(mini_golf_peep_animation_frames_pickup_ball),
 	countof(mini_golf_peep_animation_frames_put_left),
 	countof(mini_golf_peep_animation_frames_swing),
 	countof(mini_golf_peep_animation_frames_put),
