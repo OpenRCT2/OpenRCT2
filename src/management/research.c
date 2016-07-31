OpenRCT2/OpenRCT2

commit aa929e1593e7a2adcc0c5886c2693c3e2ec407fa

add globals for researched things, cleanup

src/management/research.c
@@ -25,6 +25,7 @@
 #include "../rct1.h"
 #include "../ride/ride.h"
 #include "../ride/ride_data.h"
+#include "../ride/track_data.h"
 #include "../world/scenery.h"
 #include "news_item.h"
 #include "research.h"
@@ -37,7 +38,15 @@ rct_research_item *gResearchItems = (rct_research_item*)RCT2_RESEARCH_ITEMS;
 // 0x00EE787C
 uint8 gResearchUncompletedCategories;
 
+uint32 *gResearchedRideTypes = RCT2_ADDRESS(RCT2_ADDRESS_RESEARCHED_RIDE_TYPES, uint32);
+uint32 *gResearchedRideEntries = RCT2_ADDRESS(RCT2_ADDRESS_RESEARCHED_RIDE_ENTRIES, uint32);
+uint32 *gResearchedTrackTypesA = RCT2_ADDRESS(RCT2_ADDRESS_RESEARCHED_TRACK_TYPES_A, uint32);
+uint32 *gResearchedTrackTypesB = RCT2_ADDRESS(RCT2_ADDRESS_RESEARCHED_TRACK_TYPES_B, uint32);
+
 bool gSilentResearch = false;
+
+static void ride_type_set_invented(int rideType);
+static void ride_entry_set_invented(int rideEntryIndex);
 
 /**
  *
@@ -171,15 +180,15 @@ void research_finish_item(sint32 entryIndex)
 		base_ride_type = (entryIndex >> 8) & 0xFF;
 		rideEntryIndex = entryIndex & 0xFF;
 		rideEntry = get_ride_entry(rideEntryIndex);
-		RCT2_ADDRESS(0x01357404, uint32)[base_ride_type >> 5] |= (1u << (base_ride_type & 0x1F));
-		RCT2_ADDRESS(0x01357444, uint32)[base_ride_type] = RCT2_ADDRESS(0x0097C468, uint32)[base_ride_type];
-		RCT2_ADDRESS(0x01357644, uint32)[base_ride_type] = RCT2_ADDRESS(0x0097C5D4, uint32)[base_ride_type];
+		ride_type_set_invented(base_ride_type);
+		gResearchedTrackTypesA[base_ride_type] = (RideTypePossibleTrackConfigurations[base_ride_type]         ) & 0xFFFFFFFFULL;
+		gResearchedTrackTypesB[base_ride_type] = (RideTypePossibleTrackConfigurations[base_ride_type] >> 32ULL) & 0xFFFFFFFFULL;
 		if (RideData4[base_ride_type].flags & RIDE_TYPE_FLAG4_3) {
 			ebx = RCT2_GLOBAL(0x0097D4F5 + (base_ride_type * 8), uint8);
-			RCT2_ADDRESS(0x01357444, uint32)[ebx] = RCT2_ADDRESS(0x0097C468, uint32)[ebx];
-			RCT2_ADDRESS(0x01357644, uint32)[ebx] = RCT2_ADDRESS(0x0097C5D4, uint32)[ebx];
+			gResearchedTrackTypesA[ebx] = (RideTypePossibleTrackConfigurations[ebx]         ) & 0xFFFFFFFFULL;
+			gResearchedTrackTypesB[ebx] = (RideTypePossibleTrackConfigurations[ebx] >> 32ULL) & 0xFFFFFFFFULL;
 		}
-		RCT2_ADDRESS(0x001357424, uint32)[rideEntryIndex >> 5] |= 1u << (rideEntryIndex & 0x1F);
+		ride_entry_set_invented(rideEntryIndex);
 		if (!(rideEntry->flags & RIDE_ENTRY_FLAG_SEPARATE_RIDE)) {
 			for (i = 0; i < 128; i++) {
 				rideEntry2 = get_ride_entry(i);
@@ -188,8 +197,12 @@ void research_finish_item(sint32 entryIndex)
 				if ((rideEntry2->flags & RIDE_ENTRY_FLAG_SEPARATE_RIDE))
 					continue;
 
-				if (rideEntry2->ride_type[0] == base_ride_type || rideEntry2->ride_type[1] == base_ride_type || rideEntry2->ride_type[2] == base_ride_type)
-					RCT2_ADDRESS(0x001357424, uint32)[i >> 5] |= 1u << (i & 0x1F);
+				if (rideEntry2->ride_type[0] == base_ride_type ||
+					rideEntry2->ride_type[1] == base_ride_type ||
+					rideEntry2->ride_type[2] == base_ride_type
+				) {
+					ride_entry_set_invented(rideEntryIndex);
+				}
 			}
 		}
 
@@ -307,8 +320,8 @@ void sub_684AC3(){
 	}
 
 	for (int i = 0; i < 8; ++i){
-		RCT2_ADDRESS(0x01357404, uint32)[i] = 0;
-		RCT2_ADDRESS(0x01357424, uint32)[i] = 0;
+		gResearchedRideTypes[i] = 0;
+		gResearchedRideEntries[i] = 0;
 	}
 
 	for (int i = 0; i < 56; i++){
@@ -586,4 +599,46 @@ void research_insert_scenery_group_entry(uint8 entryIndex, bool researched)
 {
 	rct_scenery_set_entry *scenerySetEntry = g_scenerySetEntries[entryIndex];
 	research_insert(researched, entryIndex, RESEARCH_CATEGORY_SCENERYSET);
+}
+
+bool ride_type_is_invented(int rideType)
+{
+	int quadIndex = rideType >> 5;
+	int bitIndex = rideType & 0x1F;
+	bool invented = (gResearchedRideTypes[quadIndex] & (1 << bitIndex));
+	return invented;
+}
+
+bool ride_entry_is_invented(int rideEntryIndex)
+{
+	int quadIndex = rideEntryIndex >> 5;
+	int bitIndex = rideEntryIndex & 0x1F;
+	bool invented = (gResearchedRideEntries[quadIndex] & (1 << bitIndex));
+	return invented;
+}
+
+bool track_type_is_invented(uint8 rideType, int trackType)
+{
+	if (trackType < 32) {
+		bool invented = (gResearchedTrackTypesA[rideType] & (1UL << trackType));
+		return invented;
+	} else {
+		trackType -= 32;
+		bool invented = (gResearchedTrackTypesB[rideType] & (1UL << trackType));
+		return invented;
+	}
+}
+
+static void ride_type_set_invented(int rideType)
+{
+	int quadIndex = rideType >> 5;
+	int bitIndex = rideType & 0x1F;
+	gResearchedRideTypes[quadIndex] |= 1 << bitIndex;
+}
+
+static void ride_entry_set_invented(int rideEntryIndex)
+{
+	int quadIndex = rideEntryIndex >> 5;
+	int bitIndex = rideEntryIndex & 0x1F;
+	gResearchedRideEntries[quadIndex] |= 1 << bitIndex;
 }

src/addresses.h
@@ -361,6 +361,11 @@
 
 #define RCT2_ADDRESS_CURRENT_TICKS					0x013628F4
 #define RCT2_ADDRESS_RIDE_LIST						0x013628F8
+#define RCT2_ADDRESS_RESEARCHED_RIDE_TYPES			0x01357404
+#define RCT2_ADDRESS_RESEARCHED_RIDE_ENTRIES		0x01357424
+#define RCT2_ADDRESS_RESEARCHED_TRACK_TYPES_A		0x01357444
+#define RCT2_ADDRESS_RESEARCHED_TRACK_TYPES_B		0x01357644
+
 #define RCT2_ADDRESS_RIDE_COUNT						0x013587C8
 #define RCT2_ADDRESS_RIDE_FLAGS						0x0097CF40
 

src/management/research.h
@@ -74,6 +74,10 @@ enum {
 
 extern rct_research_item *gResearchItems;
 extern uint8 gResearchUncompletedCategories;
+extern uint32 *gResearchedRideTypes;
+extern uint32 *gResearchedRideEntries;
+extern uint32 *gResearchedTrackTypesA;
+extern uint32 *gResearchedTrackTypesB;
 extern bool gSilentResearch;
 
 void research_reset_items();
@@ -93,5 +97,9 @@ void research_remove(sint32 entryIndex);
 
 void research_insert_ride_entry(uint8 entryIndex, bool researched);
 void research_insert_scenery_group_entry(uint8 entryIndex, bool researched);
+
+bool ride_type_is_invented(int rideType);
+bool ride_entry_is_invented(int rideEntryIndex);
+bool track_type_is_invented(uint8 rideType, int trackType);
 
 #endif

src/rct2/S6Exporter.cpp
@@ -273,10 +273,10 @@ void S6Exporter::Export()
     _s6.guest_count_change_modifier = gGuestChangeModifier;
     _s6.current_research_level = gResearchFundingLevel;
     // pad_01357400
-    memcpy(_s6.ride_types_researched, RCT2_ADDRESS(0x01357404, uint32), sizeof(_s6.ride_types_researched));
-    memcpy(_s6.ride_entries_researched, RCT2_ADDRESS(0x01357424, uint32), sizeof(_s6.ride_entries_researched));
-    memcpy(_s6.dword_01357444, RCT2_ADDRESS(0x01357444, uint32), sizeof(_s6.dword_01357444));
-    memcpy(_s6.dword_01357644, RCT2_ADDRESS(0x01357644, uint32), sizeof(_s6.dword_01357644));
+    memcpy(_s6.researched_ride_types, gResearchedRideTypes, sizeof(_s6.researched_ride_types));
+    memcpy(_s6.researched_ride_entries, gResearchedRideEntries, sizeof(_s6.researched_ride_entries));
+    memcpy(_s6.researched_track_types_a, gResearchedTrackTypesA, sizeof(_s6.researched_track_types_a));
+    memcpy(_s6.researched_track_types_b, gResearchedTrackTypesB, sizeof(_s6.researched_track_types_b));
 
     _s6.guests_in_park = gNumGuestsInPark;
     _s6.guests_heading_for_park = gNumGuestsHeadingForPark;

src/rct2/S6Importer.cpp
@@ -205,10 +205,10 @@ void S6Importer::Import()
     gGuestChangeModifier = _s6.guest_count_change_modifier;
     gResearchFundingLevel = _s6.current_research_level;
     // pad_01357400
-    memcpy(RCT2_ADDRESS(0x01357404, uint32), _s6.ride_types_researched, sizeof(_s6.ride_types_researched));
-    memcpy(RCT2_ADDRESS(0x01357424, uint32), _s6.ride_entries_researched, sizeof(_s6.ride_entries_researched));
-    memcpy(RCT2_ADDRESS(0x01357444, uint32), _s6.dword_01357444, sizeof(_s6.dword_01357444));
-    memcpy(RCT2_ADDRESS(0x01357644, uint32), _s6.dword_01357644, sizeof(_s6.dword_01357644));
+    memcpy(gResearchedRideTypes, _s6.researched_ride_types, sizeof(_s6.researched_ride_types));
+    memcpy(gResearchedRideEntries, _s6.researched_ride_entries, sizeof(_s6.researched_ride_entries));
+    memcpy(gResearchedTrackTypesA, _s6.researched_track_types_a, sizeof(_s6.researched_track_types_a));
+    memcpy(gResearchedTrackTypesB, _s6.researched_track_types_b, sizeof(_s6.researched_track_types_b));
 
     gNumGuestsInPark = _s6.guests_in_park;
     gNumGuestsHeadingForPark = _s6.guests_heading_for_park;

src/ride/ride.c
@@ -7652,14 +7652,6 @@ void ride_set_num_cars_per_vehicle(int rideIndex, int numCarsPerVehicle)
 	);
 }
 
-static bool ride_entry_is_invented(int rideEntryIndex)
-{
-	int quadIndex = rideEntryIndex >> 5;
-	int bitIndex = rideEntryIndex & 0x1F;
-	bool invented = (RCT2_ADDRESS(0x01357424, uint32)[quadIndex] & (1 << bitIndex));
-	return invented;
-}
-
 static bool ride_is_vehicle_type_valid(rct_ride *ride, uint8 inputRideEntryIndex)
 {
 	bool selectionShouldBeExpanded;

src/ride/track.c
@@ -617,9 +617,9 @@ static money32 track_place(int rideIndex, int type, int originX, int originY, in
 	RCT2_GLOBAL(RCT2_ADDRESS_ABOVE_GROUND_FLAGS, uint8) = 0;
 
 	uint64 enabledTrackPieces = 0;
-	enabledTrackPieces |= rideEntry->enabledTrackPiecesB & RCT2_ADDRESS(0x01357644, uint32)[ride->type];
+	enabledTrackPieces |= rideEntry->enabledTrackPiecesB & gResearchedTrackTypesB[ride->type];
 	enabledTrackPieces <<= 32;
-	enabledTrackPieces |= rideEntry->enabledTrackPiecesA & RCT2_ADDRESS(0x01357444, uint32)[ride->type];
+	enabledTrackPieces |= rideEntry->enabledTrackPiecesA & gResearchedTrackTypesA[ride->type];
 	uint32 rideTypeFlags = RCT2_GLOBAL(RCT2_ADDRESS_RIDE_FLAGS + (ride->type * 8), uint32);
 	RCT2_GLOBAL(0x00F44068, uint32) = rideTypeFlags;
 

commit 05ac1a49b6505eec20a8548b0b3076decad9eb72

Fix #3806: Other Coaster Vehicle options not showing in Ride Window
This error was introduced with aa929e1 (see line 204 in src/management/research.c in that commit).

src/management/research.c
@@ -203,7 +203,7 @@ void research_finish_item(sint32 entryIndex)
 					rideEntry2->ride_type[1] == base_ride_type ||
 					rideEntry2->ride_type[2] == base_ride_type
 				) {
-					ride_entry_set_invented(rideEntryIndex);
+					ride_entry_set_invented(i);
 				}
 			}
 		}