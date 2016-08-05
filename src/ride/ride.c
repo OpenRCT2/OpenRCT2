d556281a5a17d65442dc9975a1c42387ec1c2e40
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-08-05 11:33:04 +0200

    Include RideColourPresets

--- src/ride/ride.c
+++ src/ride/ride.c
@@ -5837,7 +5837,7 @@ static int ride_get_random_colour_preset_index(uint8 ride_type)
 		return 0;
 	}
 
-	colourPresets = RCT2_ADDRESS(0x0097D934, track_colour_preset_list*)[ride_type];
+	colourPresets = &RideColourPresets[ride_type];
 
 	// 200 attempts to find a colour preset that hasn't already been used in the park for this ride type
 	for (int i = 0; i < 200; i++) {
@@ -5859,7 +5859,7 @@ static void ride_set_colour_preset(rct_ride *ride, uint8 index) {
 	const track_colour_preset_list *colourPresets;
 	const track_colour *colours;
 
-	colourPresets = RCT2_ADDRESS(0x0097D934, track_colour_preset_list*)[ride->type];
+	colourPresets = &RideColourPresets[ride->type];
 	colours = &colourPresets->list[index];
 	for (int i = 0; i < 4; i++) {
 		ride->track_colour_main[i] = colours->main;
