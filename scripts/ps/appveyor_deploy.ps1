bd006de77b793cc51ea2bc753f1a97e7b42c6c11
Ted John <ted@brambles.org>
2016-08-26 18:50:52 +0100

    Fix appveyor deployment script

--- scripts/ps/appveyor_deploy.ps1
+++ scripts/ps/appveyor_deploy.ps1
@@ -6,7 +6,7 @@
 if (${env:OPENRCT2_ORG_TOKEN})
 {
     # Only upload tagged builds, develop branch or push/ branches
-    if (${env:APPVEYOR_REPO_TAG} -or ${env:APPVEYOR_REPO_BRANCH} -match "^develop$|^push/")
+    if (${env:APPVEYOR_REPO_TAG} -eq "true" -or ${env:APPVEYOR_REPO_BRANCH} -match "^develop$|^push/")
     {
         msbuild openrct2.proj /t:UploadArtifacts /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
     }
