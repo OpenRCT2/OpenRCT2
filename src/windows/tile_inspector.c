e45470ad89728c83e63bfcdd7b26a85a1c18f8ce
Broxzier <hielke.morsink@gmail.com>
2016-09-12 20:45:19 +0200

    Fix 'RCT2_GLOBAL undefined' build error

--- src/windows/tile_inspector.c
+++ src/windows/tile_inspector.c
@@ -14,6 +14,7 @@
  *****************************************************************************/
 #pragma endregion
 
+#include "../addresses.h"
 #include "../input.h"
 #include "../interface/themes.h"
 #include "../interface/viewport.h"
