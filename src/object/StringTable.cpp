f31bf03ee9dc08d80412c0ad5706000031cb82f9
Ted John <ted@brambles.org>
2016-09-20 12:29:38 +0100

    Refactor LanguagePack to interface / implementation
    This reduces the required amount of includes and definitions necessary in LanguagePack.h and having to declare private signatures twice.

--- src/object/StringTable.cpp
+++ src/object/StringTable.cpp
@@ -21,6 +21,11 @@
 #include "Object.h"
 #include "StringTable.h"
 
+extern "C"
+{
+    #include "../localisation/localisation.h"
+}
+
 constexpr uint8 RCT2_LANGUAGE_ID_ENGLISH_UK = 0;
 constexpr uint8 RCT2_LANGUAGE_ID_BLANK = 254;
 constexpr uint8 RCT2_LANGUAGE_ID_END = 255;
