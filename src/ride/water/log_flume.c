209ec19c8ceedbaad0e69d87ccb5a43411c0e5b3
Ted John <ted@brambles.org>
2016-09-25 13:09:30 +0100

    Implement track flat

--- src/ride/water/log_flume.c
+++ src/ride/water/log_flume.c
@@ -21,9 +21,45 @@
 #include "../track.h"
 #include "../track_paint.h"
 
+enum {
+	SPR_LOG_FLUME_FLAT_SW_NE = 20996,
+	SPR_LOG_FLUME_FLAT_NW_SE = 20997,
+	SPR_LOG_FLUME_FLAT_NE_SW = 20998,
+	SPR_LOG_FLUME_FLAT_SE_NW = 20999,
+	SPR_LOG_FLUME_FLAT_FRONT_SW_NE = 21000,
+	SPR_LOG_FLUME_FLAT_FRONT_NW_SE = 21001,
+	SPR_LOG_FLUME_FLAT_FRONT_NE_SW = 21002,
+	SPR_LOG_FLUME_FLAT_FRONT_SE_NW = 21003,
+};
+
+static void paint_log_flume_track_flat(uint8 rideIndex, uint8 trackSequence, uint8 direction, int height, rct_map_element * mapElement)
+{
+	static const uint32 imageIds[4][2] =
+	{
+		{ SPR_LOG_FLUME_FLAT_SW_NE, SPR_LOG_FLUME_FLAT_FRONT_SW_NE },
+		{ SPR_LOG_FLUME_FLAT_NW_SE, SPR_LOG_FLUME_FLAT_FRONT_NW_SE },
+		{ SPR_LOG_FLUME_FLAT_NE_SW, SPR_LOG_FLUME_FLAT_FRONT_NE_SW },
+		{ SPR_LOG_FLUME_FLAT_SE_NW, SPR_LOG_FLUME_FLAT_FRONT_SE_NW },
+	};
+
+	uint32 imageId = imageIds[direction][0] | gTrackColours[SCHEME_TRACK];
+	uint32 frontImageId = imageIds[direction][1] | gTrackColours[SCHEME_TRACK];
+
+	sub_98197C_rotated(direction, imageId, 0, 0, 32, 20, 2, height, 0, 6, height);
+	sub_98197C_rotated(direction, frontImageId, 0, 0, 32, 1, 26, height, 0, 27, height);
+
+	metal_a_supports_paint_setup(3, 4, 0, height, gTrackColours[SCHEME_SUPPORTS]);
+
+	paint_util_push_tunnel_rotated(direction, height, TUNNEL_0);
+	paint_util_set_segment_support_height(paint_util_rotate_segments(SEGMENT_C4 | SEGMENT_CC | SEGMENT_D0, direction), 0xFFFF, 0);
+	paint_util_set_general_support_height(height + 32, 0x20);
+}
+
 TRACK_PAINT_FUNCTION get_track_paint_function_log_flume(int trackType, int direction)
 {
 	switch (trackType) {
+	case TRACK_ELEM_FLAT:
+		return paint_log_flume_track_flat;
 	}
 
 	return NULL;
