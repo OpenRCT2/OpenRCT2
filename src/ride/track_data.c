3e5d445b79a4976eaea0bce7d93e72ed3a6f9ff9
Michał Janiszewski <janisozaur@gmail.com>
2016-10-09 14:43:49 +0200

    Add generated Lay down RC

--- src/ride/track_data.c
+++ src/ride/track_data.c
@@ -6303,7 +6303,7 @@ const TRACK_PAINT_FUNCTION_GETTER RideTypeTrackPaintFunctions[91] = {
 	get_track_paint_function_virginia_reel,		// RIDE_TYPE_VIRGINIA_REEL
 	get_track_paint_function_splash_boats,		// RIDE_TYPE_SPLASH_BOATS
 	get_track_paint_function_mini_helicopters,	// RIDE_TYPE_MINI_HELICOPTERS
-	0,						// RIDE_TYPE_LAY_DOWN_ROLLER_COASTER
+	get_track_paint_function_lay_down_rc,						// RIDE_TYPE_LAY_DOWN_ROLLER_COASTER
 	0,						// RIDE_TYPE_SUSPENDED_MONORAIL
 	0,						// RIDE_TYPE_LAY_DOWN_ROLLER_COASTER_ALT
 	0,						// RIDE_TYPE_REVERSER_ROLLER_COASTER
