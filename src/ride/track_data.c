c8ae40332c56aa1bcbd5707d5327969409580c6f
Michał Janiszewski <janisozaur@gmail.com>
2016-10-09 14:55:48 +0200

    Add generated Reverser RC
    Includes previous function `vehicle_visual_reverser`

--- src/ride/track_data.c
+++ src/ride/track_data.c
@@ -6306,7 +6306,7 @@ const TRACK_PAINT_FUNCTION_GETTER RideTypeTrackPaintFunctions[91] = {
 	get_track_paint_function_lay_down_rc,						// RIDE_TYPE_LAY_DOWN_ROLLER_COASTER
 	get_track_paint_function_suspended_monorail,						// RIDE_TYPE_SUSPENDED_MONORAIL
 	0,						// RIDE_TYPE_LAY_DOWN_ROLLER_COASTER_ALT
-	0,						// RIDE_TYPE_REVERSER_ROLLER_COASTER
+	get_track_paint_function_reverser_rc,						// RIDE_TYPE_REVERSER_ROLLER_COASTER
 	0,						// RIDE_TYPE_HEARTLINE_TWISTER_COASTER
 	get_track_paint_function_mini_golf,		// RIDE_TYPE_MINI_GOLF
 	0,						// RIDE_TYPE_GIGA_COASTER
