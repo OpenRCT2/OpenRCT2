9e946bce8eae5c4a2af4b7d6b0ec25c075f500e8
Michał Janiszewski <janisozaur@gmail.com>
2016-10-09 14:20:04 +0200

    Add generated Suspended swinging RC

--- src/ride/track_paint.h
+++ src/ride/track_paint.h
@@ -262,6 +262,7 @@ typedef TRACK_PAINT_FUNCTION (*TRACK_PAINT_FUNCTION_GETTER)(int trackType, int d
 
 TRACK_PAINT_FUNCTION get_track_paint_function_spiral_rc(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_stand_up_rc(int trackType, int direction);
+TRACK_PAINT_FUNCTION get_track_paint_function_suspended_swinging_rc(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_junior_rc(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_monorail(int trackType, int direction);
 TRACK_PAINT_FUNCTION get_track_paint_function_boat_ride(int trackType, int direction);
