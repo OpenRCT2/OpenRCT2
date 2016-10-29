OpenRCT2/OpenRCT2

commit e5ff7412e4913407b7ba2e7a6c9b3186a0947655

Refactor/Improve String and Path Handling

src/platform/windows.c
@@ -178,16 +178,6 @@ void platform_get_time_local(rct2_time *out_time)
 	out_time->second = systime.wSecond;
 }
 
-char platform_get_path_separator()
-{
-	return '\\';
-}
-
-const char *platform_get_new_line()
-{
-	return "\r\n";
-}
-
 bool platform_file_exists(const utf8 *path)
 {
 	wchar_t *wPath = utf8_to_widechar(path);
@@ -208,7 +198,9 @@ bool platform_directory_exists(const utf8 *path)
 bool platform_original_game_data_exists(const utf8 *path)
 {
 	utf8 checkPath[MAX_PATH];
-	sprintf(checkPath, "%s%c%s%c%s", path, platform_get_path_separator(), "Data", platform_get_path_separator(), "g1.dat");
+	safe_strcpy(checkPath, path, MAX_PATH);
+	safe_strcat_path(checkPath, "Data", MAX_PATH);
+	safe_strcat_path(checkPath, "g1.dat", MAX_PATH);
 	return platform_file_exists(checkPath);
 }
 
@@ -465,8 +457,8 @@ bool platform_enumerate_directories_next(int handle, utf8 *path)
 	}
 
 	utf8 *filename = widechar_to_utf8(enumFileInfo->data.cFileName);
-	strncpy(path, filename, MAX_PATH);
-	strncat(path, "\\", MAX_PATH);
+	safe_strcpy(path, filename, MAX_PATH);
+	path_end_with_separator(path, MAX_PATH);
 	free(filename);
 	return true;
 }
@@ -519,7 +511,6 @@ bool platform_file_delete(const utf8 *path)
 void platform_resolve_openrct_data_path()
 {
 	wchar_t wOutPath[MAX_PATH];
-	const char separator[2] = { platform_get_path_separator(), 0 };
 
 	if (gCustomOpenrctDataPath[0] != 0) {
 		wchar_t *customUserDataPathW = utf8_to_widechar(gCustomOpenrctDataPath);
@@ -532,18 +523,13 @@ void platform_resolve_openrct_data_path()
 		free(outPathTemp);
 		free(customUserDataPathW);
 
-		// Ensure path ends with separator
-		size_t len = strlen(_userDataDirectoryPath);
-		if (_userDataDirectoryPath[len - 1] != separator[0]) {
-			strcat(_userDataDirectoryPath, separator);
-		}
+		path_end_with_separator(_userDataDirectoryPath, sizeof(_userDataDirectoryPath));
 		return;
 	}
-	char buffer[MAX_PATH] = { 0 };
-	platform_get_exe_path(buffer);
+	char buffer[MAX_PATH];
+	platform_get_exe_path(buffer, sizeof(buffer));
 
-	strncat(buffer, separator, MAX_PATH - strnlen(buffer, MAX_PATH) - 1);
-	strncat(buffer, "data", MAX_PATH - strnlen(buffer, MAX_PATH) - 1);
+	safe_strcat_path(buffer, "data", MAX_PATH);
 
 	if (platform_directory_exists(buffer))
 	{
@@ -556,9 +542,9 @@ void platform_resolve_openrct_data_path()
 	}
 }
 
-void platform_get_openrct_data_path(utf8 *outPath)
+void platform_get_openrct_data_path(utf8 *outPath, size_t outSize)
 {
-	safe_strcpy(outPath, _openrctDataDirectoryPath, sizeof(_openrctDataDirectoryPath));
+	safe_strcpy(outPath, _openrctDataDirectoryPath, outSize);
 }
 
 /**
@@ -569,7 +555,6 @@ void platform_get_openrct_data_path(utf8 *outPath)
 void platform_resolve_user_data_path()
 {
 	wchar_t wOutPath[MAX_PATH];
-	const char separator[2] = { platform_get_path_separator(), 0 };
 
 	if (gCustomUserDataPath[0] != 0) {
 		wchar_t *customUserDataPathW = utf8_to_widechar(gCustomUserDataPath);
@@ -582,11 +567,7 @@ void platform_resolve_user_data_path()
 		free(outPathTemp);
 		free(customUserDataPathW);
 
-		// Ensure path ends with separator
-		size_t len = strlen(_userDataDirectoryPath);
-		if (_userDataDirectoryPath[len - 1] != separator[0]) {
-			strcat(_userDataDirectoryPath, separator);
-		}
+		path_end_with_separator(_userDataDirectoryPath, sizeof(_userDataDirectoryPath));
 		return;
 	}
 
@@ -595,23 +576,20 @@ void platform_resolve_user_data_path()
 		safe_strcpy(_userDataDirectoryPath, outPathTemp, sizeof(_userDataDirectoryPath));
 		free(outPathTemp);
 
-		strcat(_userDataDirectoryPath, separator);
-		strcat(_userDataDirectoryPath, "OpenRCT2");
-		strcat(_userDataDirectoryPath, separator);
+		safe_strcat_path(_userDataDirectoryPath, "OpenRCT2", sizeof(_userDataDirectoryPath));
+		path_end_with_separator(_userDataDirectoryPath, sizeof(_userDataDirectoryPath));
 	} else {
 		log_fatal("Unable to resolve user data path.");
 		exit(-1);
 	}
 }
 
-void platform_get_user_directory(utf8 *outPath, const utf8 *subDirectory)
+void platform_get_user_directory(utf8 *outPath, const utf8 *subDirectory, size_t outSize)
 {
-	const char separator[2] = { platform_get_path_separator(), 0 };
-
-	strcpy(outPath, _userDataDirectoryPath);
+	safe_strcpy(outPath, _userDataDirectoryPath, outSize);
 	if (subDirectory != NULL && subDirectory[0] != 0) {
-		strcat(outPath, subDirectory);
-		strcat(outPath, separator);
+		safe_strcat_path(outPath, subDirectory, outSize);
+		path_end_with_separator(outPath, outSize);
 	}
 }
 
@@ -624,7 +602,7 @@ void platform_show_messagebox(utf8 *message)
  *
  *  rct2: 0x004080EA
  */
-bool platform_open_common_file_dialog(utf8 *outFilename, file_dialog_desc *desc)
+bool platform_open_common_file_dialog(utf8 *outFilename, file_dialog_desc *desc, size_t outSize)
 {
 	OPENFILENAMEW openFileName;
 	wchar_t wcFilename[MAX_PATH];
@@ -651,9 +629,9 @@ bool platform_open_common_file_dialog(utf8 *outFilename, file_dialog_desc *desc)
 	utf8 *ch = filters;
 	for (int i = 0; i < countof(desc->filters); i++) {
 		if (desc->filters[i].name != NULL) {
-			strcpy(ch, desc->filters[i].name);
+			safe_strcpy(ch, desc->filters[i].name, sizeof(filters) - (ch - filters));
 			ch = strchr(ch, 0) + 1;
-			strcpy(ch, desc->filters[i].pattern);
+			safe_strcpy(ch, desc->filters[i].pattern, sizeof(filters) - (ch - filters));
 			ch = strchr(ch, 0) + 1;
 		}
 	}
@@ -695,7 +673,7 @@ bool platform_open_common_file_dialog(utf8 *outFilename, file_dialog_desc *desc)
 
 	if (result) {
 		utf8 *resultFilename = widechar_to_utf8(openFileName.lpstrFile);
-		strcpy(outFilename, resultFilename);
+		safe_strcpy(outFilename, resultFilename, outSize);
 		free(resultFilename);
 
 		// If there is no extension, append the pattern
@@ -709,7 +687,7 @@ bool platform_open_common_file_dialog(utf8 *outFilename, file_dialog_desc *desc)
 			const utf8 *pattern = desc->filters[filterIndex].pattern;
 			const utf8 *patternExtension = path_get_extension(pattern);
 			if (!str_is_null_or_empty(patternExtension)) {
-				strcat(outFilename, patternExtension);
+				safe_strcat_path(outFilename, patternExtension, outSize);
 			}
 		}
 	}
@@ -775,10 +753,10 @@ int windows_get_registry_install_info(rct2_install_info *installInfo, char *sour
 	HKEY hKey;
 	DWORD type, size;
 
-	strcpy(subkeyInfogrames, "Software\\Infogrames\\");
-	strcat(subkeyInfogrames, source);
-	strcpy(subkeyFishTechGroup, "Software\\Fish Technology Group\\");
-	strcat(subkeyFishTechGroup, source);
+	safe_strcpy(subkeyInfogrames, "Software\\Infogrames\\", sizeof(subkeyInfogrames));
+	safe_strcat(subkeyInfogrames, source, sizeof(subkeyInfogrames));
+	safe_strcpy(subkeyFishTechGroup, "Software\\Fish Technology Group\\", sizeof(subkeyFishTechGroup));
+	safe_strcat(subkeyFishTechGroup, source, sizeof(subkeyFishTechGroup));
 
 	if (RegOpenKeyA(HKEY_LOCAL_MACHINE, subkeyInfogrames, &hKey) != ERROR_SUCCESS)
 		return 0;
@@ -798,7 +776,7 @@ int windows_get_registry_install_info(rct2_install_info *installInfo, char *sour
 	size = 4;
 	RegQueryValueExA(hKey, "InstallLevel", 0, &type, (LPBYTE)&installInfo->installLevel, &size);
 	for (int i = 0; i <= 15; i++) {
-		sprintf(keyName, "AddonPack%d", i);
+		snprintf(keyName, 100, "AddonPack%d", i);
 		size = sizeof(installInfo->expansionPackNames[i]);
 		if (RegQueryValueExA(hKey, keyName, 0, &type, installInfo->expansionPackNames[i], &size) == ERROR_SUCCESS)
 			installInfo->activeExpansionPacks |= (1 << i);
@@ -992,23 +970,21 @@ char *strndup(const char *src, size_t size)
 	return (char *)dst;
 }
 
-void platform_get_exe_path(utf8 *outPath)
+void platform_get_exe_path(utf8 *outPath, size_t outSize)
 {
 	wchar_t exePath[MAX_PATH];
 	wchar_t tempPath[MAX_PATH];
 	wchar_t *exeDelimiter;
-	int exeDelimiterIndex;
 
 	GetModuleFileNameW(NULL, exePath, MAX_PATH);
-	exeDelimiter = wcsrchr(exePath, platform_get_path_separator());
-	exeDelimiterIndex = (int)(exeDelimiter - exePath);
-	lstrcpynW(tempPath, exePath, exeDelimiterIndex + 1);
-	tempPath[exeDelimiterIndex] = L'\0';
+	exeDelimiter = wcsrchr(exePath, *PATH_SEPARATOR);
+	*exeDelimiter = L'\0';
+	wcscpy_s(tempPath, MAX_PATH, exePath);
 	_wfullpath(exePath, tempPath, MAX_PATH);
-	WideCharToMultiByte(CP_UTF8, 0, exePath, countof(exePath), outPath, MAX_PATH, NULL, NULL);
+	WideCharToMultiByte(CP_UTF8, 0, exePath, MAX_PATH, outPath, (int) outSize, NULL, NULL);
 }
 
-bool platform_get_font_path(TTFFontDescriptor *font, utf8 *buffer)
+bool platform_get_font_path(TTFFontDescriptor *font, utf8 *buffer, size_t size)
 {
 #if !defined(__MINGW32__) && ((NTDDI_VERSION >= NTDDI_VISTA) && !defined(_USING_V110_SDK71_) && !defined(_ATL_XP_TARGETING))
 	wchar_t *fontFolder;
@@ -1016,15 +992,13 @@ bool platform_get_font_path(TTFFontDescriptor *font, utf8 *buffer)
 	{
 		// Convert wchar to utf8, then copy the font folder path to the buffer.
 		utf8 *outPathTemp = widechar_to_utf8(fontFolder);
-		strcpy(buffer, outPathTemp);
+		safe_strcpy(buffer, outPathTemp, size);
 		free(outPathTemp);
 
 		CoTaskMemFree(fontFolder);
 
 		// Append the requested font's file name.
-		const char separator[2] = { platform_get_path_separator(), 0 };
-		strcat(buffer, separator);
-		strcat(buffer, font->filename);
+		safe_strcat_path(buffer, font->filename, size);
 		return true;
 	}
 	else
@@ -1033,8 +1007,8 @@ bool platform_get_font_path(TTFFontDescriptor *font, utf8 *buffer)
 	}
 #else
 	log_warning("Compatibility hack: falling back to C:\\Windows\\Fonts");
-	strcpy(buffer, "C:\\Windows\\Fonts\\");
-	strcat(buffer, font->filename);
+	safe_strcpy(buffer, "C:\\Windows\\Fonts\\", size);
+	safe_strcat_path(buffer, font->filename, size);
 	return true;
 #endif
 }
@@ -1090,6 +1064,8 @@ static bool windows_setup_file_association(
 ) {
 	wchar_t exePathW[MAX_PATH];
 	wchar_t dllPathW[MAX_PATH];
+	
+	int printResult;
 
 	GetModuleFileNameW(NULL, exePathW, sizeof(exePathW));
 	GetModuleFileNameW(_dllModule, dllPathW, sizeof(dllPathW));
@@ -1126,7 +1102,8 @@ static bool windows_setup_file_association(
 	}
 	// [hRootKey\OpenRCT2.ext\DefaultIcon]
 	wchar_t szIconW[MAX_PATH];
-	wsprintfW(szIconW, L"\"%s\",%d", dllPathW, iconIndex);
+	printResult = swprintf_s(szIconW, MAX_PATH, L"\"%s\",%d", dllPathW, iconIndex);
+	assert(printResult >= 0);
 	if (RegSetValueW(hKey, L"DefaultIcon", REG_SZ, szIconW, 0) != ERROR_SUCCESS) {
 		goto fail;
 	}
@@ -1143,7 +1120,8 @@ static bool windows_setup_file_association(
 
 	// [hRootKey\OpenRCT2.sv6\shell\open\command]
 	wchar_t szCommandW[MAX_PATH];
-	wsprintfW(szCommandW, L"\"%s\" %s", exePathW, commandArgsW);
+	printResult = swprintf_s(szCommandW, MAX_PATH, L"\"%s\" %s", exePathW, commandArgsW);
+	assert(printResult >= 0);
 	if (RegSetValueW(hKey, L"shell\\open\\command", REG_SZ, szCommandW, 0) != ERROR_SUCCESS) {
 		goto fail;
 	}

commit 43ec7febff6e030b651f82255e3e3fcbea370ade

Fix #4711: Cannot open System Dialogue Window when saving game
Another regression from e5ff7412e4913407b7ba2e7a6c9b3186a0947655, we want to append an extension, not a directory.

src/platform/windows.c
@@ -716,7 +716,7 @@ bool platform_open_common_file_dialog(utf8 *outFilename, file_dialog_desc *desc,
 			const utf8 *pattern = desc->filters[filterIndex].pattern;
 			const utf8 *patternExtension = path_get_extension(pattern);
 			if (!str_is_null_or_empty(patternExtension)) {
-				safe_strcat_path(outFilename, patternExtension, outSize);
+				safe_strcat(outFilename, patternExtension, outSize);
 			}
 		}
 	}