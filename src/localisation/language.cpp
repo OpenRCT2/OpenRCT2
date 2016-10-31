c506f2730d446ca74e24602dd24714b35def1f2d
Ted John <ted@brambles.org>
2016-10-31 23:44:21 +0000

    Reformat language table

--- src/localisation/language.cpp
+++ src/localisation/language.cpp
@@ -92,26 +92,26 @@ static TTFFontSetDescriptor TTFFontArial =
 
 const language_descriptor LanguagesDescriptors[LANGUAGE_COUNT] =
 {
-    { "",            "",                         "",                        FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_UNDEFINED
-    { "en-GB",      "English (UK)",             "English (UK)",             FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_ENGLISH_UK
-    { "en-US",      "English (US)",             "English (US)",             FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_US             },  // LANGUAGE_ENGLISH_US
-    { "de-DE",      "German",                   "Deutsch",                  FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_GERMAN                 },  // LANGUAGE_GERMAN
-    { "nl-NL",      "Dutch",                    "Nederlands",               FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_DUTCH                  },  // LANGUAGE_DUTCH
-    { "fr-FR",      "French",                 u8"Français",                 FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_FRENCH                 },  // LANGUAGE_FRENCH
-    { "hu-HU",      "Hungarian",                "Magyar",                   FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_HUNGARIAN
-    { "pl-PL",      "Polish",                   "Polski",                   &TTFFontArial,          RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_POLISH
-    { "es-ES",      "Spanish",                u8"Español",                  FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_SPANISH                },  // LANGUAGE_SPANISH
-    { "sv-SE",      "Swedish",                  "Svenska",                  FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_SWEDISH                },  // LANGUAGE_SWEDISH
-    { "it-IT",      "Italian",                  "Italiano",                 FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ITALIAN                },  // LANGUAGE_ITALIAN
-    { "pt-BR",      "Portuguese (BR)",        u8"Português (BR)",           FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_PORTUGUESE             },  // LANGUAGE_PORTUGUESE_BR
-    { "zh-TW",      "Chinese (Traditional)",    "Chinese (Traditional)",    &TTFFontMingLiu,        RCT2_LANGUAGE_ID_CHINESE_TRADITIONAL    },  // LANGUAGE_CHINESE_TRADITIONAL
-    { "zh-CN",      "Chinese (Simplified)",     "Chinese (Simplified)",     &TTFFontSimSun,         RCT2_LANGUAGE_ID_CHINESE_SIMPLIFIED     },  // LANGUAGE_CHINESE_SIMPLIFIED
-    { "fi-FI",      "Finnish",                  "Suomi",                    FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_FINNISH
-    { "ko-KR",      "Korean",                   "Korean",                   &TTFFontGulim,          RCT2_LANGUAGE_ID_KOREAN                 },  // LANGUAGE_KOREAN
-    { "ru-RU",      "Russian",                  "Russian",                  &TTFFontArial,          RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_RUSSIAN
-    { "cs-CZ",      "Czech",                    "Czech",                    &TTFFontArial,          RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_CZECH
-    { "ja-JP",      "Japanese",                 "Japanese",                 &TTFFontMSGothic,       RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_JAPANESE
-    { "nb-NO",      "Norwegian",                "Norsk",                    FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK,            },  // LANGUAGE_NORWEGIAN
+    { "",      "",                        "",                       FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_UNDEFINED
+    { "en-GB", "English (UK)",            "English (UK)",           FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_ENGLISH_UK
+    { "en-US", "English (US)",            "English (US)",           FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_US             },  // LANGUAGE_ENGLISH_US
+    { "de-DE", "German",                  "Deutsch",                FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_GERMAN                 },  // LANGUAGE_GERMAN
+    { "nl-NL", "Dutch",                   "Nederlands",             FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_DUTCH                  },  // LANGUAGE_DUTCH
+    { "fr-FR", "French",                u8"Français",               FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_FRENCH                 },  // LANGUAGE_FRENCH
+    { "hu-HU", "Hungarian",               "Magyar",                 FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_HUNGARIAN
+    { "pl-PL", "Polish",                  "Polski",                 &TTFFontArial,          RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_POLISH
+    { "es-ES", "Spanish",               u8"Español",                FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_SPANISH                },  // LANGUAGE_SPANISH
+    { "sv-SE", "Swedish",                 "Svenska",                FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_SWEDISH                },  // LANGUAGE_SWEDISH
+    { "it-IT", "Italian",                 "Italiano",               FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ITALIAN                },  // LANGUAGE_ITALIAN
+    { "pt-BR", "Portuguese (BR)",       u8"Português (BR)",         FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_PORTUGUESE             },  // LANGUAGE_PORTUGUESE_BR
+    { "zh-TW", "Chinese (Traditional)",   "Chinese (Traditional)",  &TTFFontMingLiu,        RCT2_LANGUAGE_ID_CHINESE_TRADITIONAL    },  // LANGUAGE_CHINESE_TRADITIONAL
+    { "zh-CN", "Chinese (Simplified)",    "Chinese (Simplified)",   &TTFFontSimSun,         RCT2_LANGUAGE_ID_CHINESE_SIMPLIFIED     },  // LANGUAGE_CHINESE_SIMPLIFIED
+    { "fi-FI", "Finnish",                 "Suomi",                  FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_FINNISH
+    { "ko-KR", "Korean",                  "Korean",                 &TTFFontGulim,          RCT2_LANGUAGE_ID_KOREAN                 },  // LANGUAGE_KOREAN
+    { "ru-RU", "Russian",                 "Russian",                &TTFFontArial,          RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_RUSSIAN
+    { "cs-CZ", "Czech",                   "Czech",                  &TTFFontArial,          RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_CZECH
+    { "ja-JP", "Japanese",                "Japanese",               &TTFFontMSGothic,       RCT2_LANGUAGE_ID_ENGLISH_UK             },  // LANGUAGE_JAPANESE
+    { "nb-NO", "Norwegian",               "Norsk",                  FONT_OPENRCT2_SPRITE,   RCT2_LANGUAGE_ID_ENGLISH_UK,            },  // LANGUAGE_NORWEGIAN
 };
 
 int gCurrentLanguage = LANGUAGE_UNDEFINED;
