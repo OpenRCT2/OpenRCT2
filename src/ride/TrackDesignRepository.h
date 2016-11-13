552497928904d4bd180c815415d2fcceb1b54dad
Michał Janiszewski <janisozaur@gmail.com>
2016-11-13 14:08:41 +0100

    Add include guards to headers missing it

--- src/ride/TrackDesignRepository.h
+++ src/ride/TrackDesignRepository.h
@@ -14,6 +14,8 @@
  *****************************************************************************/
 #pragma endregion
 
+#pragma once
+
 #include "../common.h"
 
 typedef struct track_design_file_ref
