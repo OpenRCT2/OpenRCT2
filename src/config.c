afdc1457d511d0a2fb3f1cd5d3ccf01caaf20d52
Michał Janiszewski <janisozaur@gmail.com>
2016-11-30 11:50:43 +0100

    Move font-related code out from language.cpp

--- src/config.c
+++ src/config.c
@@ -334,7 +334,6 @@ sound_configuration gConfigSound;
 twitch_configuration gConfigTwitch;
 network_configuration gConfigNetwork;
 notification_configuration gConfigNotifications;
-font_configuration gConfigFonts;
 title_sequences_configuration gConfigTitleSequences;
 
 static bool config_open(const utf8string path);
