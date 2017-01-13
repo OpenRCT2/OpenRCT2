e496271859e6ca1deadb8aed279c6b5e907f4cc9
X7123M3-256 <X123M3-256@users.noreply.github.com>
2017-01-13 23:06:12 +0000

    Add apparently missing commands to this list

--- src/openrct2/cmdline/SpriteCommands.cpp
+++ src/openrct2/cmdline/SpriteCommands.cpp
@@ -40,12 +40,12 @@ static exitcode_t HandleSprite(CommandLineArgEnumerator *argEnumerator);
 const CommandLineCommand CommandLine::SpriteCommands[]
 {
     // Main commands
-    DefineCommand("append",    "<spritefile> <input>",                SpriteOptions, HandleSprite),
-    DefineCommand("build",     "<spritefile> <resourcedir> [silent]", SpriteOptions, HandleSprite),
-    DefineCommand("create",    "<spritefile>",                        SpriteOptions, HandleSprite),
-    DefineCommand("details",   "<spritefile> [idx]",                  SpriteOptions, HandleSprite),
-    DefineCommand("export",    "<spritefile> <idx> <output>",         SpriteOptions, HandleSprite),
-    DefineCommand("exportall", "<spritefile> <outputdir>",            SpriteOptions, HandleSprite),
+    DefineCommand("append",    "<spritefile> <input> [x_offset y_offset]", SpriteOptions, HandleSprite),
+    DefineCommand("build",     "<spritefile> <resourcedir> [silent]",      SpriteOptions, HandleSprite),
+    DefineCommand("create",    "<spritefile>",                             SpriteOptions, HandleSprite),
+    DefineCommand("details",   "<spritefile> [idx]",                       SpriteOptions, HandleSprite),
+    DefineCommand("export",    "<spritefile> <idx> <output>",              SpriteOptions, HandleSprite),
+    DefineCommand("exportall", "<spritefile> <output directory>",          SpriteOptions, HandleSprite),
     CommandTableEnd
 };
 
