6b3767ce3768b380a76d98b77ce6125401987928
wolfreak99 <jbminor1991@gmail.com>
2017-01-14 09:42:23 -0500

    move assignment below overflow check

--- src/openrct2/management/finance.c
+++ src/openrct2/management/finance.c
@@ -73,13 +73,15 @@ uint8 gCommandExpenditureType;
 void finance_payment(money32 amount, rct_expenditure_type type)
 {
 	money32 cur_money = DECRYPT_MONEY(gCashEncrypted);
-	money32 new_money = cur_money - amount;
+	money32 new_money;
 
 	//overflow check
 	if (amount < 0 && cur_money >= INT_MAX + amount)
 		new_money = INT_MAX;
 	else if (amount >= 0 && cur_money < INT_MIN + amount)
 		new_money = INT_MIN;
+	else
+		new_money = cur_money - amount;
 
 	gCashEncrypted = ENCRYPT_MONEY(new_money);
 	gExpenditureTable[type] -= amount;
