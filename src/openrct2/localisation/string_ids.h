5bdf2cac75a3365da7c70c201ee62b4e67f5fc49
wolfreak99 <jbminor1991@gmail.com>
2017-02-08 13:05:50 -0500

    Add permissions for MODIFY_TILE command

--- src/openrct2/localisation/string_ids.h
+++ src/openrct2/localisation/string_ids.h
@@ -3667,6 +3667,7 @@ enum {
 	STR_RIDE_INCOME_ADMISSION_PAY_FOR_ENTRY_TIP = 6013,
 	STR_ADMISSION_PRICE_PAY_PER_RIDE_TIP = 6014,
 	STR_TILE_INSPECTOR_PATH_SLOPED = 6015,
+	STR_ACTION_MODIFY_TILE = 6016,
 
 	// Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
 	STR_COUNT = 32768
