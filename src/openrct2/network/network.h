86d056b816316e0f2c848adff82bd37f941ab3e6
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2017-03-21 19:05:53 +0000

    Start adding network code

--- src/openrct2/network/network.h
+++ src/openrct2/network/network.h
@@ -147,6 +147,8 @@ public:
     void Server_Send_CHAT(const char* text);
     void Client_Send_GAMECMD(uint32 eax, uint32 ebx, uint32 ecx, uint32 edx, uint32 esi, uint32 edi, uint32 ebp, uint8 callback);
     void Server_Send_GAMECMD(uint32 eax, uint32 ebx, uint32 ecx, uint32 edx, uint32 esi, uint32 edi, uint32 ebp, uint8 playerid, uint8 callback);
+    void Client_Send_GAME_ACTION(const IGameAction * action);
+    void Server_Send_GAME_ACTION(const IGameAction * action);
     void Server_Send_TICK();
     void Server_Send_PLAYERLIST();
     void Client_Send_PING();
@@ -326,6 +328,9 @@ sint32 network_get_pickup_peep_old_x(uint8 playerid);
 void network_send_map();
 void network_send_chat(const char* text);
 void network_send_gamecmd(uint32 eax, uint32 ebx, uint32 ecx, uint32 edx, uint32 esi, uint32 edi, uint32 ebp, uint8 callback);
+#ifdef __cplusplus
+void network_send_game_action(const IGameAction * action);
+#endif
 void network_send_password(const char* password);
 
 void network_set_password(const char* password);
