f7f5cb754b1212532811ab175eb8fde2153087c8
Richard Jenkins <rwj@serenity.uk.net>
2017-05-12 23:57:54 +0100

    Add macOS target for CLI build

---
+++ OpenRCT2-cli/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenRCT2-cli
+//
+//  Created by Richard Jenkins on 12/05/2017.
+//  Copyright © 2017 OpenRCT2. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
