0662f1630a6b4cf60029e1bf66af2645e55cf18c
Ted John <ted@brambles.org>
2017-06-03 13:57:40 +0100

    Fix ride ratings test to work with new Context flow

--- test/tests/RideRatings.cpp
+++ test/tests/RideRatings.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include <gtest/gtest.h>
+#include <openrct2/audio/AudioContext.h>
+#include <openrct2/Context.h>
 #include <openrct2/core/File.h>
 #include <openrct2/core/Path.hpp>
 #include <openrct2/core/String.hpp>
 #include <openrct2/OpenRCT2.h>
+#include <openrct2/ui/UiContext.h>
 #include "TestData.h"
 
 extern "C"
@@ -11,6 +14,10 @@ extern "C"
     #include <openrct2/platform/platform.h>
     #include <openrct2/game.h>
 }
+
+using namespace OpenRCT2;
+using namespace OpenRCT2::Audio;
+using namespace OpenRCT2::Ui;
 
 class RideRatings : public testing::Test
 {
@@ -57,8 +64,12 @@ TEST_F(RideRatings, all)
     std::string path = TestData::GetParkPath("bpb.sv6");
 
     gOpenRCT2Headless = true;
+
     core_init();
-    openrct2_initialise();
+    auto context = CreateContext();
+    bool initialised = context->Initialise();
+    ASSERT_TRUE(initialised);
+
     game_load_sv6_path(path.c_str());
 
     // Check ride count to check load was successful
@@ -85,5 +96,5 @@ TEST_F(RideRatings, all)
         }
     }
 
-    openrct2_dispose();
+    delete context;
 }
