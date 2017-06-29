5d1ffd76b5845c7305efd295b597cc8ea7da6385
wolfreak99 <jbminor1991@gmail.com>
2017-06-29 14:44:46 -0400

    Move windows/cheats.c/MONEY_DIGITS to localisation.h/MONEY_STRING_MAXLENGTH

--- src/openrct2/windows/cheats.c
+++ src/openrct2/windows/cheats.c
@@ -36,8 +36,7 @@
 
 #define CHEATS_MONEY_DEFAULT MONEY(5000,00)
 #define CHEATS_MONEY_INCREMENT_DIV MONEY(1000,00)
-#define MONEY_DIGITS 14
-static utf8 _moneySpinnerText[MONEY_DIGITS];
+static utf8 _moneySpinnerText[MONEY_STRING_MAXLENGTH];
 static money32 _moneySpinnerValue = CHEATS_MONEY_DEFAULT;
 
 enum {
@@ -583,8 +582,8 @@ static void window_cheats_money_mouseup(rct_window *w, rct_widgetindex widgetInd
         game_do_command(0, GAME_COMMAND_FLAG_APPLY, CHEAT_NOMONEY, gParkFlags & PARK_FLAGS_NO_MONEY ? 0 : 1, GAME_COMMAND_CHEAT, 0, 0);
         break;
     case WIDX_MONEY_SPINNER:
-        money_to_string(_moneySpinnerValue, _moneySpinnerText, MONEY_DIGITS);
-        window_text_input_raw_open(w, WIDX_MONEY_SPINNER, STR_ENTER_NEW_VALUE, STR_ENTER_NEW_VALUE, _moneySpinnerText, MONEY_DIGITS);
+        money_to_string(_moneySpinnerValue, _moneySpinnerText, MONEY_STRING_MAXLENGTH);
+        window_text_input_raw_open(w, WIDX_MONEY_SPINNER, STR_ENTER_NEW_VALUE, STR_ENTER_NEW_VALUE, _moneySpinnerText, MONEY_STRING_MAXLENGTH);
         break;
     case WIDX_SET_MONEY:
         game_do_command(0, GAME_COMMAND_FLAG_APPLY, CHEAT_SETMONEY, _moneySpinnerValue, GAME_COMMAND_CHEAT, 0, 0);
