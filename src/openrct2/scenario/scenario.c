3fa742a3fa49bb605e1a8ea1aeee99abd6a7d17d
CharlesLove <crashbanito@hotmail.com>
2017-07-22 14:36:53 -0500

    Reverted to Code that set gCheatsUnlockAllPrices in S4/S6Importer
    Removed dirty code that set this cheat for saved games. During import,
    gCheatsUnlockAllPrices will be set according to the scenarios' base
    game.

--- src/openrct2/scenario/scenario.c
+++ src/openrct2/scenario/scenario.c
@@ -95,8 +95,6 @@ ParkLoadResult * scenario_load_and_play_from_path(const char * path)
     uint32 extension = get_file_extension_type(path);
     ParkLoadResult * result = NULL;
 
-	cheats_reset();
-
     if (extension == FILE_EXTENSION_SC6) {
         result = scenario_load(path);
         if (ParkLoadResult_GetError(result) != PARK_LOAD_ERROR_OK) {
