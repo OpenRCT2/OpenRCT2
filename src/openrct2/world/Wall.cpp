OpenRCT2/OpenRCT2

commit db438a27b79d3c29b5f7f9100995612e69f798dd

Use map_element_get_direction in more places, create map_element_get_direction_with_offset()

src/openrct2/world/footpath.c
@@ -808,9 +808,7 @@ void footpath_bridge_get_info_from_pos(sint32 screenX, sint32 screenY, sint32 *x
     if (interactionType == VIEWPORT_INTERACTION_ITEM_RIDE && map_element_get_type(*mapElement) == MAP_ELEMENT_TYPE_ENTRANCE) {
         sint32 directions = entrance_get_directions(*mapElement);
         if (directions & 0x0F) {
-            sint32 bx = bitscanforward(directions);
-            bx += (*mapElement)->type; // First two bits seem to contain the direction of entrance/exit
-            bx &= 3;
+            sint32 bx = map_element_get_direction_with_offset(*mapElement, bitscanforward(directions));
             if (direction != NULL) *direction = bx;
             return;
         }
@@ -888,7 +886,7 @@ bool fence_in_the_way(sint32 x, sint32 y, sint32 z0, sint32 z1, sint32 direction
             continue;
         if (z1 <= mapElement->base_height)
             continue;
-        if ((mapElement->type & MAP_ELEMENT_DIRECTION_MASK) != direction)
+        if ((map_element_get_direction(mapElement)) != direction)
             continue;
 
         return true;
@@ -1198,7 +1196,7 @@ static void loc_6A6D7E(
                     if (!(FlatRideTrackSequenceProperties[trackType][trackSequence] & TRACK_SEQUENCE_FLAG_CONNECTS_TO_PATH)) {
                         return;
                     }
-                    uint16 dx = ((direction - mapElement->type) & 3) ^ 2;
+                    uint16 dx = (direction - map_element_get_direction(mapElement)) ^ 2;
                     if (!(FlatRideTrackSequenceProperties[trackType][trackSequence] & (1 << dx))) {
                         return;
                     }
@@ -1210,7 +1208,7 @@ static void loc_6A6D7E(
                 break;
             case MAP_ELEMENT_TYPE_ENTRANCE:
                 if (z == mapElement->base_height) {
-                    if (entrance_has_direction(mapElement, ((direction - mapElement->type) & 3) ^ 2)) {
+                    if (entrance_has_direction(mapElement, ((direction - map_element_get_direction(mapElement)) % 4) ^ 2)) {
                         if (query) {
                             neighbour_list_push(neighbourList, 8, direction, mapElement->properties.entrance.ride_index,  mapElement->properties.entrance.index);
                         } else {
@@ -1290,7 +1288,7 @@ static void loc_6A6C85(
         if (!(FlatRideTrackSequenceProperties[trackType][trackSequence] & TRACK_SEQUENCE_FLAG_CONNECTS_TO_PATH)) {
             return;
         }
-        uint16 dx = (direction - mapElement->type) & 3;
+        uint16 dx = (direction - map_element_get_direction(mapElement)) % 4;
         if (!(FlatRideTrackSequenceProperties[trackType][trackSequence] & (1 << dx))) {
             return;
         }
@@ -1516,7 +1514,7 @@ void footpath_update_queue_chains()
                 if (mapElement->base_height != z) continue;
                 if (mapElement->properties.entrance.type != ENTRANCE_TYPE_RIDE_ENTRANCE) continue;
 
-                uint8 direction = (mapElement->type & 3) ^ 2;
+                uint8 direction = map_element_get_direction_with_offset(mapElement, 2);
                 footpath_chain_ride_queue(rideIndex, i, x << 5, y << 5, mapElement, direction);
             } while (!map_element_is_last_for_tile(mapElement++));
         }
@@ -2003,7 +2001,7 @@ void footpath_update_queue_entrance_banner(sint32 x, sint32 y, rct_map_element *
     case MAP_ELEMENT_TYPE_ENTRANCE:
         if (mapElement->properties.entrance.type == ENTRANCE_TYPE_RIDE_ENTRANCE) {
             footpath_queue_chain_push(mapElement->properties.entrance.ride_index);
-            footpath_chain_ride_queue(255, 0, x, y, mapElement, (mapElement->type & MAP_ELEMENT_DIRECTION_MASK) ^ 2);
+            footpath_chain_ride_queue(255, 0, x, y, mapElement, map_element_get_direction_with_offset(mapElement, 2));
         }
         break;
     }

src/openrct2/world/map.c
@@ -279,6 +279,11 @@ sint32 map_element_get_direction(const rct_map_element *element)
     return element->type & MAP_ELEMENT_DIRECTION_MASK;
 }
 
+sint32 map_element_get_direction_with_offset(const rct_map_element *element, uint8 offset)
+{
+    return ((element->type & MAP_ELEMENT_DIRECTION_MASK) + offset) % 4;
+}
+
 sint32 map_element_get_terrain(const rct_map_element *element)
 {
     sint32 terrain = (element->properties.surface.terrain >> 5) & 7;
@@ -888,7 +893,7 @@ void game_command_remove_large_scenery(sint32* eax, sint32* ebx, sint32* ecx, si
         if ((map_element->properties.scenerymultiple.type >> 10) != tileIndex)
             continue;
 
-        if ((map_element->type & MAP_ELEMENT_DIRECTION_MASK) != map_element_direction)
+        if ((map_element_get_direction(map_element)) != map_element_direction)
             continue;
 
         // If we are removing ghost elements
@@ -959,7 +964,7 @@ void game_command_remove_large_scenery(sint32* eax, sint32* ebx, sint32* ecx, si
             if (map_element_get_type(sceneryElement) != MAP_ELEMENT_TYPE_SCENERY_MULTIPLE)
                 continue;
 
-            if ((sceneryElement->type & MAP_ELEMENT_DIRECTION_MASK) != map_element_direction)
+            if (map_element_get_direction(sceneryElement) != map_element_direction)
                 continue;
 
             if ((sceneryElement->properties.scenerymultiple.type >> 10) != i)
@@ -1142,7 +1147,7 @@ restart_from_beginning:
                 sint32 eax = x * 32;
                 sint32 ebx = flags;
                 sint32 ecx = y * 32;
-                sint32 edx = (mapElement->base_height << 8) | (mapElement->type & MAP_ELEMENT_DIRECTION_MASK);
+                sint32 edx = (mapElement->base_height << 8) | (map_element_get_direction(mapElement));
                 sint32 edi = 0, ebp = 0;
                 cost = game_do_command(eax, ebx, ecx, edx, GAME_COMMAND_REMOVE_WALL, edi, ebp);
 
@@ -1157,7 +1162,7 @@ restart_from_beginning:
         case MAP_ELEMENT_TYPE_SCENERY_MULTIPLE:
             if (clear & (1 << 1)) {
                 sint32 eax = x * 32;
-                sint32 ebx = flags | ((mapElement->type & MAP_ELEMENT_DIRECTION_MASK) << 8);
+                sint32 ebx = flags | ((map_element_get_direction(mapElement)) << 8);
                 sint32 ecx = y * 32;
                 sint32 edx = mapElement->base_height | ((mapElement->properties.scenerymultiple.type >> 10) << 8);
                 sint32 edi = 0, ebp = 0;
@@ -1315,7 +1320,7 @@ static money32 map_change_surface_style(sint32 x0, sint32 y0, sint32 x1, sint32 
 
             if (surfaceStyle != 0xFF){
                 uint8 cur_terrain = (
-                    (mapElement->type&MAP_ELEMENT_DIRECTION_MASK) << 3) |
+                    map_element_get_direction(mapElement) << 3) |
                     (mapElement->properties.surface.terrain >> 5);
 
                 if (surfaceStyle != cur_terrain) {
@@ -1368,10 +1373,9 @@ static money32 map_change_surface_style(sint32 x0, sint32 y0, sint32 x1, sint32 
             {
                 if (!(mapElement->properties.surface.terrain & MAP_ELEMENT_SURFACE_TERRAIN_MASK))
                 {
-                    if (!(mapElement->type & MAP_ELEMENT_DIRECTION_MASK))
+                    if (!(map_element_get_direction(mapElement)))
                     {
-                        if ((mapElement->properties.surface.grass_length & 7) != GRASS_LENGTH_CLEAR_0)
-                        {
+                        if ((mapElement->properties.surface.grass_length & 7) != GRASS_LENGTH_CLEAR_0) {
                             mapElement->properties.surface.grass_length = GRASS_LENGTH_CLEAR_0;
                             map_invalidate_tile_full(x, y);
                         }
@@ -3561,7 +3565,7 @@ static void clear_element_at(sint32 x, sint32 y, rct_map_element **elementPtr)
                 x,
                 GAME_COMMAND_FLAG_APPLY,
                 y,
-                (element->type & MAP_ELEMENT_DIRECTION_MASK) | (element->base_height << 8),
+                map_element_get_direction(element) | (element->base_height << 8),
                 GAME_COMMAND_REMOVE_WALL,
                 0,
                 0
@@ -3571,7 +3575,7 @@ static void clear_element_at(sint32 x, sint32 y, rct_map_element **elementPtr)
         gGameCommandErrorTitle = STR_CANT_REMOVE_THIS;
         game_do_command(
                 x,
-                (GAME_COMMAND_FLAG_APPLY) | ((element->type & MAP_ELEMENT_DIRECTION_MASK) << 8),
+                (GAME_COMMAND_FLAG_APPLY) | (map_element_get_direction(element) << 8),
                 y,
                 (element->base_height) | (((element->properties.scenerymultiple.type >> 8) >> 2) << 8),
                 GAME_COMMAND_REMOVE_LARGE_SCENERY,
@@ -3666,7 +3670,7 @@ rct_map_element *map_get_large_scenery_segment(sint32 x, sint32 y, sint32 z, sin
             continue;
         if ((mapElement->properties.scenerymultiple.type >> 10) != sequence)
             continue;
-        if ((mapElement->type & MAP_ELEMENT_DIRECTION_MASK) != direction)
+        if ((map_element_get_direction(mapElement)) != direction)
             continue;
 
         return mapElement;
@@ -4106,7 +4110,7 @@ void game_command_set_sign_style(sint32* eax, sint32* ebx, sint32* ecx, sint32* 
             banner->x * 32,
             banner->y * 32,
             mapElement->base_height,
-            mapElement->type & 3,
+            map_element_get_direction(mapElement),
             mapElement->properties.scenerymultiple.type >> 10,
             mainColour,
             textColour

test/testpaint/compat.c
@@ -129,6 +129,10 @@ int map_element_get_direction(const rct_map_element *element) {
     return element->type & MAP_ELEMENT_DIRECTION_MASK;
 }
 
+int map_element_get_direction_with_offset(const rct_map_element *element, uint8 offset) {
+    return ((element->type & MAP_ELEMENT_DIRECTION_MASK) + offset) % 4;
+}
+
 rct_map_element *map_get_first_element_at(int x, int y) {
     if (x < 0 || y < 0 || x > 255 || y > 255) {
         log_error("Trying to access element outside of range");

src/openrct2/interface/viewport_interaction.c
@@ -469,7 +469,7 @@ static void viewport_interaction_remove_footpath_item(rct_map_element *mapElemen
  */
 void viewport_interaction_remove_park_entrance(rct_map_element *mapElement, sint32 x, sint32 y)
 {
-    sint32 rotation = (mapElement->type + 1) & 3;
+    sint32 rotation = map_element_get_direction_with_offset(mapElement, 1);
     switch (mapElement->properties.entrance.index & 0x0F) {
     case 1:
         x += TileDirectionDelta[rotation].x;
@@ -499,7 +499,7 @@ static void viewport_interaction_remove_park_wall(rct_map_element *mapElement, s
             x,
             GAME_COMMAND_FLAG_APPLY,
             y,
-            (mapElement->type & 0x3) | (mapElement->base_height << 8),
+            map_element_get_direction(mapElement) | (mapElement->base_height << 8),
             GAME_COMMAND_REMOVE_WALL,
             0,
             0
@@ -524,7 +524,7 @@ static void viewport_interaction_remove_large_scenery(rct_map_element *mapElemen
         gGameCommandErrorTitle = STR_CANT_REMOVE_THIS;
         game_do_command(
             x,
-            1 | ((mapElement->type & 0x3) << 8),
+            1 | (map_element_get_direction(mapElement) << 8),
             y,
             mapElement->base_height | ((mapElement->properties.scenerymultiple.type >> 10) << 8),
             GAME_COMMAND_REMOVE_LARGE_SCENERY,

src/openrct2/paint/map_element/entrance.c
@@ -48,7 +48,7 @@ static void ride_entrance_exit_paint(uint8 direction, sint32 height, rct_map_ele
             lightfx_add_3d_light_magic_from_drawing_tile(0, 0, height + 45, LIGHTFX_LIGHT_TYPE_LANTERN_3);
         }
 
-        switch (map_element->type & MAP_ELEMENT_DIRECTION_MASK) {
+        switch (map_element_get_direction(map_element)) {
         case 0:
             lightfx_add_3d_light_magic_from_drawing_tile(16, 0, height + 16, LIGHTFX_LIGHT_TYPE_LANTERN_2);
             break;

src/openrct2/paint/map_element/map_element.c
@@ -251,7 +251,7 @@ static void sub_68B3FB(sint32 x, sint32 y)
         // Only paint map_elements below the clip height.
         if ((gCurrentViewportFlags & VIEWPORT_FLAG_PAINT_CLIP_TO_HEIGHT) && (map_element->base_height > gClipHeight)) break;
 
-        sint32 direction = (map_element->type + rotation) & MAP_ELEMENT_DIRECTION_MASK;
+        sint32 direction = map_element_get_direction_with_offset(map_element, rotation);
         sint32 height = map_element->base_height * 8;
 
         rct_xy16 dword_9DE574 = gPaintMapPosition;

src/openrct2/paint/map_element/surface.c
@@ -1076,7 +1076,7 @@ void surface_paint(uint8 direction, uint16 height, rct_map_element * mapElement)
 
         sint32 branch = -1;
         if ((mapElement->properties.surface.terrain & 0xE0) == 0) {
-            if ((mapElement->type & 0x3) == 0) {
+            if (map_element_get_direction(mapElement) == 0) {
                 if (zoomLevel == 0) {
                     if ((gCurrentViewportFlags & (VIEWPORT_FLAG_HIDE_BASE | VIEWPORT_FLAG_UNDERGROUND_INSIDE)) == 0) {
                         branch = mapElement->properties.surface.grass_length & 0x7;

src/openrct2/peep/peep.c
@@ -2393,7 +2393,7 @@ static void peep_go_to_ride_entrance(rct_peep* peep, rct_ride* ride){
 
     rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-    uint8 direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+    uint8 direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
     x *= 32;
     y *= 32;
@@ -2654,7 +2654,7 @@ static void peep_update_ride_sub_state_1(rct_peep* peep){
 
         rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-        uint8 direction_entrance = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+        uint8 direction_entrance = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
         if (ride->type == RIDE_TYPE_MAZE){
             peep->maze_last_edge = direction_entrance + 1;
@@ -2693,7 +2693,7 @@ static void peep_update_ride_sub_state_1(rct_peep* peep){
 
         map_element = ride_get_station_start_track_element(ride, peep->current_ride_station);
 
-        uint8 direction_track = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+        uint8 direction_track = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
         peep->var_37 = (direction_entrance << 2) | (direction_track << 4);
 
@@ -2731,14 +2731,14 @@ static void peep_update_ride_sub_state_1(rct_peep* peep){
 
         rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-        uint8 direction_entrance = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+        uint8 direction_entrance = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
         x = ride->station_starts[peep->current_ride_station].x;
         y = ride->station_starts[peep->current_ride_station].y;
 
         map_element = ride_get_station_start_track_element(ride, peep->current_ride_station);
 
-        uint8 direction_track = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+        uint8 direction_track = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
         vehicle = GET_VEHICLE(ride->vehicles[peep->current_train]);
         ride_entry = get_ride_entry(vehicle->ride_subtype);
@@ -2940,7 +2940,7 @@ static void peep_update_ride_sub_state_2_rejoin_queue(rct_peep* peep, rct_ride* 
 
     rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-    uint8 direction_entrance = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+    uint8 direction_entrance = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
     x *= 32;
     y *= 32;
@@ -3159,7 +3159,7 @@ static void peep_update_ride_sub_state_7(rct_peep* peep){
 
         rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-        uint8 exit_direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+        uint8 exit_direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
         exit_direction ^= (1 << 1);
 
         if (!ride_type_has_flag(ride->type, RIDE_TYPE_FLAG_16)){
@@ -3248,14 +3248,14 @@ static void peep_update_ride_sub_state_7(rct_peep* peep){
 
     rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-    uint8 exit_direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+    uint8 exit_direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
     x = ride->station_starts[peep->current_ride_station].x;
     y = ride->station_starts[peep->current_ride_station].y;
 
     map_element = ride_get_station_start_track_element(ride, peep->current_ride_station);
 
-    uint8 station_direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+    uint8 station_direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
     vehicle = GET_VEHICLE(ride->vehicles[peep->current_train]);
 
@@ -3323,7 +3323,7 @@ static void peep_update_ride_prepare_for_state_9(rct_peep* peep){
 
     rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-    uint8 exit_direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+    uint8 exit_direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
     x *= 32;
     y *= 32;
@@ -3540,7 +3540,7 @@ static void peep_update_ride_sub_state_13(rct_peep* peep){
 
     rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-    uint8 exit_direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+    uint8 exit_direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
     exit_direction ^= (1 << 1);
 
     x *= 32;
@@ -3610,7 +3610,7 @@ static void peep_update_ride_sub_state_14(rct_peep* peep){
 
             rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-            uint8 exit_direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+            uint8 exit_direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
 
             peep->var_37 = (exit_direction * 4) | (peep->var_37 & 0x30) |  1;
             x = ride->station_starts[peep->current_ride_station].x;
@@ -3801,7 +3801,7 @@ static void peep_update_ride_sub_state_16(rct_peep* peep){
 
     rct_map_element* map_element = ride_get_station_exit_element(ride, x, y, z);
 
-    uint8 exit_direction = (map_element == NULL ? 0 : map_element->type & MAP_ELEMENT_DIRECTION_MASK);
+    uint8 exit_direction = (map_element == NULL ? 0 : map_element_get_direction(map_element));
     exit_direction ^= (1 << 1);
 
     x *= 32;
@@ -4588,7 +4588,7 @@ static bool peep_update_fixing_sub_state_9(bool firstRun, rct_peep *peep, rct_ri
                 input.y = trackBeginEnd.begin_y;
                 input.element = trackBeginEnd.begin_element;
 
-                direction = trackBeginEnd.begin_element->type & 3;
+                direction = map_element_get_direction(trackBeginEnd.begin_element);
                 continue;
             }
 
@@ -5947,7 +5947,7 @@ static void peep_update_heading_to_inspect(rct_peep* peep){
             }
         }
 
-        uint8 direction = map_element->type & MAP_ELEMENT_DIRECTION_MASK;
+        uint8 direction = map_element_get_direction(map_element);
         peep->direction = direction;
 
         sint32 x = peep->next_x + 16 + word_981D6C[direction].x * 53;
@@ -6067,7 +6067,7 @@ static void peep_update_answering(rct_peep* peep){
             }
         }
 
-        uint8 direction = map_element->type & MAP_ELEMENT_DIRECTION_MASK;
+        uint8 direction = map_element_get_direction(map_element);
         peep->direction = direction;
 
         sint32 x = peep->next_x + 16 + word_981D6C[direction].x * 53;
@@ -8066,7 +8066,7 @@ static sint32 peep_interact_with_entrance(rct_peep* peep, sint16 x, sint16 y, rc
         if (map_element->properties.entrance.index & 0xF)
             return peep_return_to_centre_of_tile(peep);
 
-        uint8 entranceDirection = map_element->type & MAP_ELEMENT_DIRECTION_MASK;
+        uint8 entranceDirection = map_element_get_direction(map_element);
         if (entranceDirection != peep->direction){
             if ((entranceDirection ^ (1 << 1)) != peep->direction)
                 return peep_return_to_centre_of_tile(peep);
@@ -8777,14 +8777,14 @@ static uint8 footpath_element_dest_in_dir(
             if (z != mapElement->base_height) continue;
             switch (mapElement->properties.entrance.type) {
             case ENTRANCE_TYPE_RIDE_ENTRANCE:
-                direction = mapElement->type & MAP_ELEMENT_DIRECTION_MASK;
+                direction = map_element_get_direction(mapElement);
                 if (direction == chosenDirection) {
                     *outRideIndex = mapElement->properties.entrance.ride_index;
                     return PATH_SEARCH_RIDE_ENTRANCE;
                 }
                 break;
             case ENTRANCE_TYPE_RIDE_EXIT:
-                direction = mapElement->type & MAP_ELEMENT_DIRECTION_MASK;
+                direction = map_element_get_direction(mapElement);
                 if (direction == chosenDirection) {
                     *outRideIndex = mapElement->properties.entrance.ride_index;
                     return PATH_SEARCH_RIDE_EXIT;
@@ -9099,7 +9099,7 @@ static void peep_pathfind_heuristic_search(sint16 x, sint16 y, uint8 z, rct_peep
                  * For mechanics heading for the ride entrance
                  * (in the case when the station has no exit),
                  * the goal is the ride entrance tile. */
-                direction = mapElement->type & MAP_ELEMENT_DIRECTION_MASK;
+                direction = map_element_get_direction(mapElement);
                 if (direction == test_edge) {
                     /* The rideIndex will be useful for
                      * adding transport rides later. */
@@ -9118,7 +9118,7 @@ static void peep_pathfind_heuristic_search(sint16 x, sint16 y, uint8 z, rct_peep
             case ENTRANCE_TYPE_RIDE_EXIT:
                 /* For mechanics heading for the ride exit, the
                  * goal is the ride exit tile. */
-                direction = mapElement->type & MAP_ELEMENT_DIRECTION_MASK;
+                direction = map_element_get_direction(mapElement);
                 if (direction == test_edge) {
                     searchResult = PATH_SEARCH_RIDE_EXIT;
                     found = true;
@@ -10063,7 +10063,7 @@ static void get_ride_queue_end(sint16 *x, sint16 *y, sint16 *z){
     if (!found)
         return;
 
-    uint8 direction = (mapElement->type & 3) ^ (1 << 1);
+    uint8 direction = map_element_get_direction_with_offset(mapElement, 2);
     rct_map_element* lastPathElement = NULL;
     rct_map_element* firstPathElement = NULL;
 
@@ -11427,7 +11427,7 @@ static bool peep_find_ride_to_look_at(rct_peep *peep, uint8 edge, uint8 *rideToV
             if (map_element_is_ghost(mapElement)) continue;
         }
         if (map_element_get_type(mapElement) != MAP_ELEMENT_TYPE_WALL) continue;
-        if ((map_element_get_direction(mapElement) ^ 0x2) != edge) continue;
+        if (map_element_get_direction_with_offset(mapElement, 2) != edge) continue;
         if (get_wall_entry(mapElement->properties.wall.type)->wall.flags2 & WALL_SCENERY_2_FLAG4) continue;
         // TODO: Check whether this shouldn't be <=, as the other loops use. If so, also extract as loop A.
         if (peep->next_z + 4 >= mapElement->base_height) continue;
@@ -11513,7 +11513,7 @@ static bool peep_find_ride_to_look_at(rct_peep *peep, uint8 edge, uint8 *rideToV
             if (map_element_is_ghost(mapElement)) continue;
         }
         if (map_element_get_type(mapElement) != MAP_ELEMENT_TYPE_WALL) continue;
-        if ((map_element_get_direction(mapElement) ^ 0x2) != edge) continue;
+        if (map_element_get_direction_with_offset(mapElement, 2) != edge) continue;
         if (get_wall_entry(mapElement->properties.wall.type)->wall.flags2 & WALL_SCENERY_2_FLAG4) continue;
         if (peep->next_z + 6 <= mapElement->base_height) continue;
         if (peep->next_z >= mapElement->clearance_height) continue;
@@ -11596,7 +11596,7 @@ static bool peep_find_ride_to_look_at(rct_peep *peep, uint8 edge, uint8 *rideToV
             if (map_element_is_ghost(mapElement)) continue;
         }
         if (map_element_get_type(mapElement) != MAP_ELEMENT_TYPE_WALL) continue;
-        if ((map_element_get_direction(mapElement) ^ 0x2) != edge) continue;
+        if (map_element_get_direction_with_offset(mapElement, 2) != edge) continue;
         if (get_wall_entry(mapElement->properties.wall.type)->wall.flags2 & WALL_SCENERY_2_FLAG4) continue;
         if (peep->next_z + 8 <= mapElement->base_height) continue;
         if (peep->next_z >= mapElement->clearance_height) continue;

commit 6b48f4344e6abc5fd26198e74d0a07def5b6b95d

Fix #6100: Paths don't connect to exit
Regression from db438a27b79d3c29b5f7f9100995612e69f798dd, % does not work on negative numbers.

src/openrct2/world/Wall.cpp
@@ -71,7 +71,7 @@ static bool WallCheckObstructionWithTrack(rct_scenery_entry * wall,
 {
     sint32 trackType = trackElement->properties.track.type;
     sint32 sequence = map_element_get_track_sequence(trackElement);
-    sint32 direction = (edge - map_element_get_direction(trackElement)) % 4;
+    sint32 direction = (edge - map_element_get_direction(trackElement)) & MAP_ELEMENT_DIRECTION_MASK;
     rct_ride * ride = get_ride(trackElement->properties.track.ride_index);
 
     if (TrackIsAllowedWallEdges(ride->type, trackType, sequence, direction))
@@ -223,7 +223,7 @@ static bool WallCheckObstruction(rct_scenery_entry * wall,
             entry = get_large_scenery_entry(entryType);
             tile = &entry->large_scenery.tiles[sequence];
             {
-                sint32 direction = ((edge - map_element_get_direction(mapElement)) % 4) + 8;
+                sint32 direction = ((edge - map_element_get_direction(mapElement)) & MAP_ELEMENT_DIRECTION_MASK) + 8;
                 if (!(tile->var_7 & (1 << direction)))
                 {
                     map_obstruction_set_error_text(mapElement);

src/openrct2/world/footpath.c
@@ -1208,7 +1208,7 @@ static void loc_6A6D7E(
                 break;
             case MAP_ELEMENT_TYPE_ENTRANCE:
                 if (z == mapElement->base_height) {
-                    if (entrance_has_direction(mapElement, ((direction - map_element_get_direction(mapElement)) % 4) ^ 2)) {
+                    if (entrance_has_direction(mapElement, ((direction - map_element_get_direction(mapElement)) & MAP_ELEMENT_DIRECTION_MASK) ^ 2)) {
                         if (query) {
                             neighbour_list_push(neighbourList, 8, direction, mapElement->properties.entrance.ride_index,  mapElement->properties.entrance.index);
                         } else {
@@ -1288,7 +1288,7 @@ static void loc_6A6C85(
         if (!(FlatRideTrackSequenceProperties[trackType][trackSequence] & TRACK_SEQUENCE_FLAG_CONNECTS_TO_PATH)) {
             return;
         }
-        uint16 dx = (direction - map_element_get_direction(mapElement)) % 4;
+        uint16 dx = (direction - map_element_get_direction(mapElement)) & MAP_ELEMENT_DIRECTION_MASK;
         if (!(FlatRideTrackSequenceProperties[trackType][trackSequence] & (1 << dx))) {
             return;
         }

src/openrct2/world/map.c
@@ -280,7 +280,7 @@ sint32 map_element_get_direction(const rct_map_element *element)
 
 sint32 map_element_get_direction_with_offset(const rct_map_element *element, uint8 offset)
 {
-    return ((element->type & MAP_ELEMENT_DIRECTION_MASK) + offset) % 4;
+    return ((element->type & MAP_ELEMENT_DIRECTION_MASK) + offset) & MAP_ELEMENT_DIRECTION_MASK;
 }
 
 sint32 map_element_get_terrain(const rct_map_element *element)

test/testpaint/compat.c
@@ -130,7 +130,7 @@ int map_element_get_direction(const rct_map_element *element) {
 }
 
 int map_element_get_direction_with_offset(const rct_map_element *element, uint8 offset) {
-    return ((element->type & MAP_ELEMENT_DIRECTION_MASK) + offset) % 4;
+    return ((element->type & MAP_ELEMENT_DIRECTION_MASK) + offset) & MAP_ELEMENT_DIRECTION_MASK;
 }
 
 rct_map_element *map_get_first_element_at(int x, int y) {