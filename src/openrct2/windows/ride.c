96665926a6d5a35f8443743ede9376e179a575cd
linkinpark9812 <30772334+linkinpark9812@users.noreply.github.com>
2017-08-06 15:48:06 -0500

    Added 1 more ride type

--- src/openrct2/windows/ride.c
+++ src/openrct2/windows/ride.c
@@ -1315,7 +1315,8 @@ static void window_ride_draw_tab_vehicle(rct_drawpixelinfo *dpi, rct_window *w)
 			|| ride->type == RIDE_TYPE_INVERTED_IMPULSE_COASTER	
 			|| ride->type == RIDE_TYPE_SUSPENDED_SWINGING_COASTER
 			|| ride->type == RIDE_TYPE_CHAIRLIFT
-			|| ride->type == RIDE_TYPE_MINI_SUSPENDED_COASTER) {
+			|| ride->type == RIDE_TYPE_MINI_SUSPENDED_COASTER
+			|| ride->type == RIDE_TYPE_SUSPENDED_MONORAIL) {
 			y /= 4;
 		}
 
