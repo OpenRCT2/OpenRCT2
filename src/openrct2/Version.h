1f9ae4d4ec1f3afd9fba2c306879d60fcf40c288
Michał Janiszewski <janisozaur+signed@gmail.com>
2017-08-10 00:17:26 +0200

    Start v0.1.2

--- src/openrct2/Version.h
+++ src/openrct2/Version.h
@@ -19,7 +19,7 @@
 #include "common.h"
 
 #define OPENRCT2_NAME               "OpenRCT2"
-#define OPENRCT2_VERSION            "0.1.1"
+#define OPENRCT2_VERSION            "0.1.2"
 
 #if defined(__amd64__) || defined(_M_AMD64)
     #define OPENRCT2_ARCHITECTURE       "x86-64"
