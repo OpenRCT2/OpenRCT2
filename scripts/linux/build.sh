a80287a4d323e2d28349989a83f223801fee4a3a
Michał Janiszewski <janisozaur+signed@gmail.com>
2017-09-17 23:09:00 +0200

    Use ninja for building on Travis

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -27,28 +27,28 @@ pushd build
 		chmod a+rwx "$(pwd)"
 		chmod g+s "$(pwd)"
 		# CMAKE and MAKE opts from environment
-		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:64bit-only bash -c "cmake ../ -DWITH_TESTS=on $OPENRCT2_CMAKE_OPTS && make $OPENRCT_MAKE_OPTS && make test ARGS=\"-V\""
+		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:64bit-only bash -c "cmake ../ -DWITH_TESTS=on $OPENRCT2_CMAKE_OPTS && ninja $OPENRCT_MAKE_OPTS && ninja test ARGS=\"-V\""
 	elif [[ $TARGET == "ubuntu_i686" ]]
 	then
 		PARENT=$(readlink -f ../)
 		chmod a+rwx "$(pwd)"
 		chmod g+s "$(pwd)"
 		# CMAKE and MAKE opts from environment
-		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:ubuntu_i686 bash -c "cmake ../ -DWITH_TESTS=on $OPENRCT2_CMAKE_OPTS && make all testpaint install $OPENRCT_MAKE_OPTS && make test ARGS=\"-V\" && ( ./testpaint --quiet ||  if [[ \$? -eq 1 ]] ; then echo Allowing failed tests to pass ; else echo here ; false; fi )"
+		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:ubuntu_i686 bash -c "cmake ../ -DWITH_TESTS=on $OPENRCT2_CMAKE_OPTS && ninja all testpaint install $OPENRCT_MAKE_OPTS && ninja test ARGS=\"-V\" && ( ./testpaint --quiet ||  if [[ \$? -eq 1 ]] ; then echo Allowing failed tests to pass ; else echo here ; false; fi )"
 	elif [[ $TARGET == "ubuntu_amd64" ]]
 	then
 		PARENT=$(readlink -f ../)
 		chmod a+rwx "$(pwd)"
 		chmod g+s "$(pwd)"
 		# CMAKE and MAKE opts from environment
-		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:ubuntu_amd64 bash -c "cmake ../ -DWITH_TESTS=on $OPENRCT2_CMAKE_OPTS && make $OPENRCT_MAKE_OPTS install && make test ARGS=\"-V\""
+		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:ubuntu_amd64 bash -c "cmake ../ -DWITH_TESTS=on $OPENRCT2_CMAKE_OPTS && ninja $OPENRCT_MAKE_OPTS install && ninja test ARGS=\"-V\""
 	elif [[ $TARGET == "windows" ]]
 	then
 		PARENT=$(readlink -f ../)
 		chmod a+rwx "$(pwd)"
 		chmod g+s "$(pwd)"
 		# CMAKE and MAKE opts from environment
-		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:mingw-arch bash -c "cmake ../ $OPENRCT2_CMAKE_OPTS && make $OPENRCT_MAKE_OPTS"
+		docker run -v "$PARENT":"$PARENT" -w "$PARENT"/build -i -t openrct2/openrct2:mingw-arch bash -c "cmake ../ $OPENRCT2_CMAKE_OPTS && ninja $OPENRCT_MAKE_OPTS"
 	else
 		echo "Unkown target $TARGET"
 		exit 1
@@ -74,7 +74,7 @@ fi
 if [[ -z "$DISABLE_G2_BUILD" ]]; then
     echo Building: g2.dat
 	pushd build
-	    make g2
+	    ninja g2
 	popd
 fi
 
