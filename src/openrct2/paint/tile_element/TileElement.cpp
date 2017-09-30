79fa16138ad042bada15e79963ce81d9877e9f1a
Jeroen D Stout <jeroenstout@gmail.com>
2017-09-30 21:28:44 +0200

    Move virtual_floor_paint to own file / add edge highlighting

--- src/openrct2/paint/tile_element/TileElement.cpp
+++ src/openrct2/paint/tile_element/TileElement.cpp
@@ -358,64 +358,6 @@ static void sub_68B3FB(paint_session * session, sint32 x, sint32 y)
     }
 }
 
-void virtual_floor_paint(paint_session * session)
-{
-    if (!input_test_place_object_modifier(PLACE_OBJECT_MODIFIER_COPY_Z | PLACE_OBJECT_MODIFIER_SHIFT_Z))
-    {
-        return;
-    }
-
-    bool    showFade = false;
-    bool    showLit = false;
-
-    if ((gMapSelectFlags & MAP_SELECT_FLAG_ENABLE) &&
-        session->MapPosition.x >= gMapSelectPositionA.x - gMapVirtualFloorBaseSize &&
-        session->MapPosition.y >= gMapSelectPositionA.y - gMapVirtualFloorBaseSize &&
-        session->MapPosition.x <= gMapSelectPositionB.x + gMapVirtualFloorBaseSize &&
-        session->MapPosition.y <= gMapSelectPositionB.y + gMapVirtualFloorBaseSize)
-    {
-        showFade = true;
-    }
-
-    if (!showFade && !(gMapSelectFlags & MAP_SELECT_FLAG_ENABLE_CONSTRUCT))
-    {
-        return;
-    }
-
-    LocationXY16 * tile;
-    for (tile = gMapSelectionTiles; tile->x != -1; tile++) {
-        if (session->MapPosition.x >= tile->x - gMapVirtualFloorBaseSize &&
-            session->MapPosition.y >= tile->y - gMapVirtualFloorBaseSize &&
-            session->MapPosition.x <= tile->x + gMapVirtualFloorBaseSize &&
-            session->MapPosition.y <= tile->y + gMapVirtualFloorBaseSize)
-        {
-            showFade = true;
-        }
-    }
-
-    if (!showFade)
-    {
-        return;
-    }
-
-    for (tile = gMapSelectionTiles; tile->x != -1; tile++) {
-        if (session->MapPosition.x == tile->x &&
-            session->MapPosition.y == tile->y)
-        {
-            showLit = true;
-            break;
-        }
-    }
-
-    session->InteractionType = VIEWPORT_INTERACTION_ITEM_NONE;
-    uint32 image_id = SPR_TERRAIN_SELECTION_SQUARE | COLOUR_GREY << 24 | COLOUR_DARK_PURPLE << 19 | IMAGE_TYPE_REMAP;
-    if (showLit)
-    {
-        image_id = SPR_TERRAIN_SELECTION_PATROL_AREA | COLOUR_YELLOW << 19 | IMAGE_TYPE_REMAP;
-    }
-    sub_98196C(session, image_id, 0, 0, 10, 10, -1, gMapVirtualFloorHeight, get_current_rotation());
-}
-
 void paint_util_push_tunnel_left(paint_session * session, uint16 height, uint8 type)
 {
     session->LeftTunnels[session->LeftTunnelCount] = {static_cast<uint8>((height / 16)), type};
