54f222db4ca3a55995cd9add436dde31d000f7ad
vector-of-bool <vectorofbool@gmail.com>
2017-10-09 20:59:00 -0600

    Specify sysroot when cross compiling with MinGW

--- cmake/toolchains/MinGW.cmake
+++ cmake/toolchains/MinGW.cmake
@@ -2,13 +2,11 @@ set(CMAKE_SYSTEM_NAME Windows)
 
 set(COMPILER_PREFIX i686-w64-mingw32)
 set(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
-set(CMAKE_C_FLAGS -m32)
+set(CMAKE_C_FLAGS_INIT -m32)
 set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-c++)
 set(CMAKE_CXX_FLAGS -m32)
 set(CMAKE_RC_COMPILER ${COMPILER_PREFIX}-windres)
-set(CMAKE_RC_FLAGS -m32)
-set(CMAKE_PKGCONFIG_EXECUTABLE ${COMPILER_PREFIX}-pkg-config)
-set(PKG_CONFIG_EXECUTABLE ${COMPILER_PREFIX}-pkg-config)
+set(CMAKE_RC_FLAGS_INIT -m32)
 
 if(APPLE)
      set(TARGET_ENVIRONMENT /usr/local/mingw-w32-bin_i686-darwin/i686-w64-mingw32)
@@ -18,6 +16,7 @@ endif()
 
 # here is the target environment located
 set(CMAKE_FIND_ROOT_PATH ${TARGET_ENVIRONMENT})
+set(CMAKE_SYSROOT ${CMAKE_FIND_ROOT_PATH})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
