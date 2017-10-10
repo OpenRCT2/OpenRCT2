1279a2fb386e640091e5b0e5aec357880e241e6b
vector-of-bool <vectorofbool@gmail.com>
2017-10-10 00:58:11 -0600

    MinGW Freetype wants BZip2

--- cmake/FindFreetype.cmake
+++ cmake/FindFreetype.cmake
@@ -6,6 +6,10 @@ include(FindPackageHandleStandardArgs)
 find_library(FREETYPE_LIBRARY NAMES libfreetype.a Freetype)
 find_path(FREETYPE_INCLUDE_DIR ft2build.h PATH_SUFFIXES freetype2)
 
+if(MINGW)
+    find_dependency(BZip2)
+endif()
+
 add_library(Freetype::Freetype IMPORTED UNKNOWN)
 
 set_target_properties(Freetype::Freetype PROPERTIES
@@ -17,3 +21,7 @@ find_package_handle_standard_args(Freetype
     "Unable to find required Freetype library"
     FREETYPE_LIBRARY FREETYPE_INCLUDE_DIR
     )
+
+if(MINGW)
+    set_property(TARGET Freetype::Freetype APPEND PROPERTY INTERFACE_LINK_LIBRARIES BZip2::BZip2)
+endif()
