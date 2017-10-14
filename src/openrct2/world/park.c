1561a7c5c9070cd65b78a2d1f158484f39d09c80
Hris7 <christopher.koeck91@gmail.com>
2017-10-14 22:35:00 +0200

    Double closing Park forces guests to leave

--- src/openrct2/world/park.c
+++ src/openrct2/world/park.c
@@ -642,6 +642,21 @@ void park_update_histories()
 
 void park_set_open(sint32 open)
 {
+    if (!park_is_open() && open == 0)
+    {
+        sint32 sprite_index;
+        rct_peep *peep;
+
+        FOR_ALL_GUESTS(sprite_index, peep)
+        {
+            // force them to leave queues
+            if (peep->state == PEEP_STATE_QUEUING)
+            {
+                remove_peep_from_ride(peep);
+            }
+            peep_leave_park(peep);
+        }
+    }
     game_do_command(0, GAME_COMMAND_FLAG_APPLY, 0, open << 8, GAME_COMMAND_SET_PARK_OPEN, 0, 0);
 }
 
