42236f3a1d113de0a06e1815f2d4d3e3657b9860
Christian F. Coors <ccoors@uni-bremen.de>
2017-10-15 21:38:46 +0200

    Fix saving without dialog in new scenario

--- src/openrct2-ui/windows/LoadSave.cpp
+++ src/openrct2-ui/windows/LoadSave.cpp
@@ -765,6 +765,7 @@ static void window_loadsave_select(rct_window *w, const char *path)
         save_path(&gConfigGeneral.last_save_game_directory, pathBuffer);
         safe_strcpy(gScenarioSavePath, pathBuffer, MAX_PATH);
         safe_strcpy(gCurrentLoadedPath, pathBuffer, MAX_PATH);
+        gFirstTimeSaving = true;
         window_loadsave_invoke_callback(MODAL_RESULT_OK, pathBuffer);
         window_close_by_class(WC_LOADSAVE);
         gfx_invalidate_screen();
