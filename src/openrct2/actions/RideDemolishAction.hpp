b27860a43b4913bf444286d84cc365401070c638
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2017-10-17 17:52:53 +0100

    Fix #6478. Rides can no longer be destroyed during pause mode.
    Mistake made during refactoring into GameAction

--- src/openrct2/actions/RideDemolishAction.hpp
+++ src/openrct2/actions/RideDemolishAction.hpp
@@ -39,7 +39,7 @@ public:
 
     uint16 GetActionFlags() const override
     {
-        return GameAction::GetActionFlags() | GA_FLAGS::ALLOW_WHILE_PAUSED;
+        return GameAction::GetActionFlags();
     }
 
     void Serialise(DataSerialiser& stream) override
