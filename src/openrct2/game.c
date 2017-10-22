36e1a540e8cbe1cab188e08ddcb2d5b24cf32f2f
zaxcav <daveanti@gmail.com>
2017-10-22 21:42:00 +0200

    Fix gEntrance locations after loading from file

--- src/openrct2/game.c
+++ src/openrct2/game.c
@@ -1121,6 +1121,22 @@ void game_fix_save_vars()
 
     // Fix invalid vehicle sprite sizes, thus preventing visual corruption of sprites
     fix_invalid_vehicle_sprite_sizes();
+
+    // Fix gEntrance locations for which the map_element no longer exists
+    uint8 entranceNum;
+    for (entranceNum = 0; entranceNum < MAX_PARK_ENTRANCES; ++entranceNum)
+    {
+        if (gParkEntrances[entranceNum].x == LOCATION_NULL)
+            continue;
+
+        if (map_get_park_entrance_element_at(
+            gParkEntrances[entranceNum].x,
+            gParkEntrances[entranceNum].y,
+            gParkEntrances[entranceNum].z >> 3, false) == NULL)
+        {
+            gParkEntrances[entranceNum].x = LOCATION_NULL;
+        }
+    }
 }
 
 void handle_park_load_failure_with_title_opt(const ParkLoadResult * result, const utf8 * path, bool loadTitleFirst)
