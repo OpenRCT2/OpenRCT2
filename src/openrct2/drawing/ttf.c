f144e59cd358d724d51e0950067eca2a66c6c26f
Aaron van Geffen <aaron@aaronweb.net>
2017-10-30 15:39:36 +0100

    Reduce console verbosity for TTF loading.

--- src/openrct2/drawing/ttf.c
+++ src/openrct2/drawing/ttf.c
@@ -78,13 +78,13 @@ bool ttf_initialise()
 
             utf8 fontPath[MAX_PATH];
             if (!platform_get_font_path(fontDesc, fontPath, sizeof(fontPath))) {
-                log_error("Unable to load font '%s'", fontDesc->font_name);
+                log_verbose("Unable to load font '%s'", fontDesc->font_name);
                 return false;
             }
 
             fontDesc->font = ttf_open_font(fontPath, fontDesc->ptSize);
             if (fontDesc->font == NULL) {
-                log_error("Unable to load '%s'", fontPath);
+                log_verbose("Unable to load '%s'", fontPath);
                 return false;
             }
 
