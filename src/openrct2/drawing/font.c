9fcef9ed0773eef035e50d8673dc91906d889f89
Michał Janiszewski <janisozaur+signed@gmail.com>
2017-11-26 21:48:54 +0100

    Only guard font access in RCT2-less builds

--- src/openrct2/drawing/font.c
+++ src/openrct2/drawing/font.c
@@ -124,7 +124,9 @@ sint32 font_sprite_get_codepoint_offset(sint32 codepoint)
 sint32 font_sprite_get_codepoint_width(sint32 fontSpriteBase, sint32 codepoint)
 {
     uint32 spriteFontIdx = fontSpriteBase + font_sprite_get_codepoint_offset(codepoint);
+#ifdef NO_RCT2
     openrct2_assert(spriteFontIdx < countof(_spriteFontCharacterWidths), "Invalid font index %u", spriteFontIdx);
+#endif
     return _spriteFontCharacterWidths[spriteFontIdx];
 }
 
