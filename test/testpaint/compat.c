7f5567ebd88f0d3b721fb293b2449412c54383ad
Hielke Morsink <hielke.morsink@gmail.com>
2017-12-29 01:41:19 +0100

    Replace water height mask define in testpaint

--- test/testpaint/compat.c
+++ test/testpaint/compat.c
@@ -210,7 +210,7 @@ void tile_element_decrement_onride_photo_timout(rct_tile_element * tileElement)
 
 sint32 map_get_water_height(const rct_tile_element * tileElement)
 {
-    return tileElement->properties.surface.terrain & TILE_ELEMENT_WATER_HEIGHT_MASK;
+    return tileElement->properties.surface.terrain & TILE_ELEMENT_SURFACE_WATER_HEIGHT_MASK;
 }
 
 bool ride_type_has_flag(int rideType, int flag)
