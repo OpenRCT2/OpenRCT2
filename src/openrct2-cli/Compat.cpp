a662143a1c3b9cb11617256d3c42f335a6a6218c
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2018-01-03 11:20:48 +0100

    fix widget

--- src/openrct2-cli/Compat.cpp
+++ src/openrct2-cli/Compat.cpp
@@ -1,10 +1,12 @@
-void widget_scroll_update_thumbs(){ }
-void widget_draw(){ }
-void widget_is_enabled(){ }
-void widget_is_disabled(){ }
-void widget_is_pressed(){ }
-void widget_is_highlighted(){ }
-void widget_is_active_tool(){ }
-void widget_scroll_get_part(){ }
-void widget_set_enabled(){ }
-void widget_set_checkbox_value(){ }
+#include <openrct2/interface/widget.h>
+
+void widget_scroll_update_thumbs(rct_window *w, rct_widgetindex widget_index) { }
+void widget_draw(rct_drawpixelinfo *dpi, rct_window *w, rct_widgetindex widgetIndex){ }
+bool widget_is_enabled(rct_window *w, rct_widgetindex widgetIndex) { }
+bool widget_is_disabled(rct_window *w, rct_widgetindex widgetIndex) { }
+bool widget_is_pressed(rct_window *w, rct_widgetindex widgetIndex) { }
+bool widget_is_highlighted(rct_window *w, rct_widgetindex widgetIndex) { }
+bool widget_is_active_tool(rct_window *w, rct_widgetindex widgetIndex) { }
+void widget_scroll_get_part(rct_window *w, rct_widget* widget, sint32 x, sint32 y, sint32 *output_x, sint32 *output_y, sint32 *output_scroll_area, sint32 *scroll_id) { }
+void widget_set_enabled(rct_window *w, rct_widgetindex widgetIndex, bool enabled) { }
+void widget_set_checkbox_value(rct_window *w, rct_widgetindex widgetIndex, sint32 value) { }
