0252ce21a9b64a1198ef1f9fb19cd35035f90c9d
Michał Janiszewski <janisozaur+signed@gmail.com>
2018-01-07 22:01:44 +0100

    Gracefully handle some nullptrs

--- src/openrct2/interface/Window.cpp
+++ src/openrct2/interface/Window.cpp
@@ -2026,7 +2026,7 @@ void window_event_scroll_mouseover_call(rct_window *w, sint32 scrollIndex, sint3
 
 void window_event_textinput_call(rct_window *w, rct_widgetindex widgetIndex, char *text)
 {
-    if (w->event_handlers->text_input != nullptr)
+    if (w != nullptr && w->event_handlers->text_input != nullptr)
         w->event_handlers->text_input(w, widgetIndex, text);
 }
 
