163ffb50fff656b6cb1b011d3dd4f2b01fad8d0a
Jacob Wallraff <jwallra@gmail.com>
2018-02-11 15:23:08 -0800

    Adding missing semicolon

--- src/openrct2/scenario/ScenarioRepository.h
+++ src/openrct2/scenario/ScenarioRepository.h
@@ -101,7 +101,5 @@ void    scenario_repository_scan();
 size_t  scenario_repository_get_count();
 const   scenario_index_entry *scenario_repository_get_by_index(size_t index);
 bool    scenario_repository_try_record_highscore(const utf8 * scenarioFileName, money32 companyValue, const utf8 * name);
-bool    scenario_repository_try_record_speedrun_highscore(const utf8 * scenarioFileName, uint32 daysValue)
+bool    scenario_repository_try_record_speedrun_highscore(const utf8 * scenarioFileName, uint32 daysValue);
 void    scenario_translate(scenario_index_entry * scenarioEntry, const struct rct_object_entry * stexObjectEntry);
-
-
