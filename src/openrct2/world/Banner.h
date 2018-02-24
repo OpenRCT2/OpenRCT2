86d498be2a1c8ab7fc1ef5eba201e0c575ce2c49
Hielke Morsink <hielke.morsink@gmail.com>
2018-02-24 16:33:11 +0100

    Fix #5261 Deleting the sign after copy/pasting it will crash the game
    Prior to the previous commit, signs could be duplicated by pasting them using the tile inspector, which results in some funky behaviour. A duplicated sign refers to the same banner index (which in turn refers to the same user string ID), making the player unable to modify it. Deleting it would previously even crash the game.
    This commit looks for such signs when loading a scenario, and attempts to fix them, by creating a new banner entry (and user string if one is used).

--- src/openrct2/world/Banner.h
+++ src/openrct2/world/Banner.h
@@ -50,6 +50,7 @@ sint32 create_new_banner(uint8 flags);
 rct_tile_element *banner_get_tile_element(sint32 bannerIndex);
 sint32 banner_get_closest_ride_index(sint32 x, sint32 y, sint32 z);
 void banner_reset_broken_index();
+void fix_duplicated_banners();
 void game_command_callback_place_banner(sint32 eax, sint32 ebx, sint32 ecx, sint32 edx, sint32 esi, sint32 edi, sint32 ebp);
 
 #endif
