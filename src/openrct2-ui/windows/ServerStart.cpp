ca4e1a36fb51074dce89ff1d8b9b8b5cd331483e
Aaron van Geffen <aaron@aaronweb.net>
2018-03-09 20:49:59 +0100

    Fix widget dimensions for 'Start server' window.

--- src/openrct2-ui/windows/ServerStart.cpp
+++ src/openrct2-ui/windows/ServerStart.cpp
@@ -51,10 +51,10 @@ enum {
 };
 
 #define WW 300
-#define WH 152
+#define WH 154
 
 static rct_widget window_server_start_widgets[] = {
-    { WWT_FRAME,            0,  0,      WW-1,   0,          WH-1,   0xFFFFFFFF,                     STR_NONE },                 // panel / background
+    { WWT_FRAME,            0,  0,      WW-1,   0,          WH-1,   STR_NONE,                       STR_NONE },                 // panel / background
     { WWT_CAPTION,          0,  1,      WW-2,   1,          14,     STR_START_SERVER,               STR_WINDOW_TITLE_TIP },     // title bar
     { WWT_CLOSEBOX,         0,  WW-13,  WW-3,   2,          13,     STR_CLOSE_X,                    STR_CLOSE_WINDOW_TIP },     // close x button
     { WWT_TEXT_BOX,         1,  120,    WW-8,   20,         32,     STR_NONE,                       STR_NONE },                 // port text box
@@ -62,12 +62,12 @@ static rct_widget window_server_start_widgets[] = {
     { WWT_TEXT_BOX,         1,  120,    WW-8,   52,         64,     STR_NONE,                       STR_NONE },                 // description text box
     { WWT_TEXT_BOX,         1,  120,    WW-8,   68,         80,     STR_NONE,                       STR_NONE },                 // greeting text box
     { WWT_TEXT_BOX,         1,  120,    WW-8,   84,         96,     STR_NONE,                       STR_NONE },                 // password text box
-    { WWT_SPINNER,          1,  120,    WW-8,   100,        109,    STR_SERVER_MAX_PLAYERS_VALUE,   STR_NONE },                 // max players
-    { WWT_BUTTON,           1,  WW-18,  WW-8,   100,        104,    STR_NUMERIC_UP,                 STR_NONE },
-    { WWT_BUTTON,           1,  WW-18,  WW-8,   104,        108,    STR_NUMERIC_DOWN,               STR_NONE },
-    { WWT_CHECKBOX,         1,  6,      WW-8,   117,        123,    STR_ADVERTISE,                  STR_ADVERTISE_SERVER_TIP }, // advertise checkbox
-    { WWT_BUTTON,           1,  6,      106,    WH-6-11,    WH-6,   STR_NEW_GAME,                   STR_NONE },                 // start server button
-    { WWT_BUTTON,           1,  112,    212,    WH-6-11,    WH-6,   STR_LOAD_GAME,                  STR_NONE },
+    { WWT_SPINNER,          1,  120,    WW-8,   100,        111,    STR_SERVER_MAX_PLAYERS_VALUE,   STR_NONE },                 // max players
+    { WWT_BUTTON,           1,  WW-18,  WW-8,   101,        105,    STR_NUMERIC_UP,                 STR_NONE },
+    { WWT_BUTTON,           1,  WW-18,  WW-8,   106,        110,    STR_NUMERIC_DOWN,               STR_NONE },
+    { WWT_CHECKBOX,         1,  6,      WW-8,   117,        130,    STR_ADVERTISE,                  STR_ADVERTISE_SERVER_TIP }, // advertise checkbox
+    { WWT_BUTTON,           1,  6,      106,    WH-6-13,    WH-6,   STR_NEW_GAME,                   STR_NONE },                 // start server button
+    { WWT_BUTTON,           1,  112,    212,    WH-6-13,    WH-6,   STR_LOAD_GAME,                  STR_NONE },
     { WIDGETS_END },
 };
 
