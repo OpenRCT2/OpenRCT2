125819581ffaa10308250b1e5188c8e10734256d
Michał Janiszewski <janisozaur+signed@gmail.com>
2018-03-12 23:54:00 +0100

    Fix MSVC build

--- src/openrct2/CmdlineSprite.cpp
+++ src/openrct2/CmdlineSprite.cpp
@@ -20,6 +20,7 @@
 #include "CmdlineSprite.h"
 #include "drawing/Drawing.h"
 #include "Imaging.h"
+#include "localisation/Language.h"
 #include "OpenRCT2.h"
 #include "platform/platform.h"
 #include "util/Util.h"
