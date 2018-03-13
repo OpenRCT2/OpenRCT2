45a92a73564df4dc54a4eb725512f432f459d7e8
Hielke Morsink <hielke.morsink@gmail.com>
2018-03-13 19:52:18 +0100

    Fix #5210 Unable to open system dialog for saving
    The reason the file dialog failed to open (on Windows at least) is that the dialog would be used to save a file, yet a folder was provided as the default path. This commit attempts to append a logical filename to the path, including an extension.

--- src/openrct2-ui/windows/LoadSave.cpp
+++ src/openrct2-ui/windows/LoadSave.cpp
@@ -27,6 +27,7 @@
 #include <openrct2/core/String.hpp>
 #include <openrct2/core/Util.hpp>
 #include <openrct2/Editor.h>
+#include <openrct2/FileClassifier.h>
 #include <openrct2/Game.h>
 #include <openrct2/localisation/Localisation.h>
 #include <openrct2/platform/platform.h>
@@ -313,37 +314,39 @@ static void window_loadsave_resize(rct_window *w)
 
 static bool browse(bool isSave, char *path, size_t pathSize)
 {
-    safe_strcpy(path, _directory, pathSize);
-    if (isSave)
-        safe_strcat_path(path, _defaultName, pathSize);
-
     file_dialog_desc desc = { 0 };
-    desc.initial_directory = _directory;
-    desc.type = isSave ? FD_SAVE : FD_OPEN;
-    desc.default_filename = isSave ? path : nullptr;
-
+    const utf8 * extension = "";
+    uint32 fileType = FILE_EXTENSION_UNKNOWN;
     rct_string_id title = STR_NONE;
     switch (_type & 0x0E)
     {
     case LOADSAVETYPE_GAME:
+        extension = ".sv6";
+        fileType = FILE_EXTENSION_SV6;
         title = isSave ? STR_FILE_DIALOG_TITLE_SAVE_GAME : STR_FILE_DIALOG_TITLE_LOAD_GAME;
         desc.filters[0].name = language_get_string(STR_OPENRCT2_SAVED_GAME);
         desc.filters[0].pattern = isSave ? "*.sv6" : "*.sv6;*.sc6;*.sv4;*.sc4";
         break;
 
     case LOADSAVETYPE_LANDSCAPE:
+        extension = ".sc6";
+        fileType = FILE_EXTENSION_SC6;
         title = isSave ? STR_FILE_DIALOG_TITLE_SAVE_LANDSCAPE : STR_FILE_DIALOG_TITLE_LOAD_LANDSCAPE;
         desc.filters[0].name = language_get_string(STR_OPENRCT2_LANDSCAPE_FILE);
         desc.filters[0].pattern = isSave ? "*.sc6" : "*.sc6;*.sv6;*.sc4;*.sv4";
         break;
 
     case LOADSAVETYPE_SCENARIO:
+        extension = ".sc6";
+        fileType = FILE_EXTENSION_SC6;
         title = STR_FILE_DIALOG_TITLE_SAVE_SCENARIO;
         desc.filters[0].name = language_get_string(STR_OPENRCT2_SCENARIO_FILE);
         desc.filters[0].pattern = "*.sc6";
         break;
 
     case LOADSAVETYPE_TRACK:
+        extension = ".td6";
+        fileType = FILE_EXTENSION_TD6;
         title = isSave ? STR_FILE_DIALOG_TITLE_SAVE_TRACK : STR_FILE_DIALOG_TITLE_INSTALL_NEW_TRACK_DESIGN;
         desc.filters[0].name = language_get_string(STR_OPENRCT2_TRACK_DESIGN_FILE);
         desc.filters[0].pattern = isSave ? "*.td6" : "*.td6;*.td4";
@@ -356,12 +359,48 @@ static bool browse(bool isSave, char *path, size_t pathSize)
         break;
     }
 
+    safe_strcpy(path, _directory, pathSize);
+    if (isSave)
+    {
+        // The file browser requires a file path instead of just a directory
+        if (String::SizeOf(_defaultName) > 0)
+        {
+            safe_strcat_path(path, _defaultName, pathSize);
+        }
+        else
+        {
+            utf8 buffer[USER_STRING_MAX_LENGTH]{};
+            if (gParkName != STR_NONE)
+                format_string(buffer, pathSize, gParkName, nullptr);
+
+            // Use localized "Unnamed Park" if park name was empty
+            if (String::SizeOf(buffer) == 0)
+                format_string(buffer, pathSize, STR_UNNAMED_PARK, nullptr);
+
+            safe_strcat_path(path, buffer, pathSize);
+        }
+    }
+
+    desc.initial_directory = _directory;
+    desc.type = isSave ? FD_SAVE : FD_OPEN;
+    desc.default_filename = isSave ? path : nullptr;
+
     // Add 'all files' filter. If the number of filters is increased, this code will need to be adjusted.
     desc.filters[1].name = language_get_string(STR_ALL_FILES);
     desc.filters[1].pattern = "*";
 
     desc.title = language_get_string(title);
-    return platform_open_common_file_dialog(path, &desc, pathSize);
+    if (platform_open_common_file_dialog(path, &desc, pathSize))
+    {
+        // When the given save type was given, Windows still interprets a filename with a dot in its name as a custom extension,
+        // meaning files like "My Coaster v1.2" will not get the .td6 extension by default.
+        if (get_file_extension_type(path) != fileType)
+            path_append_extension(path, extension, pathSize);
+
+        return true;
+    }
+
+    return false;
 }
 
 static void window_loadsave_mouseup(rct_window *w, rct_widgetindex widgetIndex)
