OpenRCT2/OpenRCT2

commit fe59637823bc7ae468e6a88de3c0cb8ad48c430c

Adds constructors to convert from Coords to TileCoords

src/openrct2/peep/Peep.cpp
@@ -10762,7 +10762,7 @@ sint32 peep_pathfind_choose_direction(sint16 x, sint16 y, uint8 z, rct_peep * pe
              * In the future these could be used to visualise the
              * pathfinding on the map. */
             uint8          endJunctions         = 0;
-            TileCoordsXYZ endJunctionList[16]  = { 0 };
+            TileCoordsXYZ endJunctionList[16];
             uint8          endDirectionList[16] = { 0 };
 
             bool inPatrolArea = false;

src/openrct2/world/Location.hpp
@@ -52,24 +52,30 @@ constexpr sint32 COORDS_NULL = -1;
  * Tile coordinates use 1 x/y increment per tile.
  * Regular ('big', 'sprite') coordinates use 32 x/y increments per tile.
  * */
-struct TileCoordsXY
-{
-    sint32 x, y;
-};
-
 struct CoordsXY
 {
     sint32 x, y;
 };
 
-struct TileCoordsXYZ
+struct TileCoordsXY
 {
-    sint32 x, y, z;
+    TileCoordsXY() = default;
+    TileCoordsXY(sint32 x_, sint32 y_) : x(x_), y(y_) {}
+    explicit TileCoordsXY(CoordsXY c) : x(c.x / 32), y(c.y / 32) {}
+    sint32 x = 0, y = 0;
 };
 
 struct CoordsXYZ
 {
     sint32 x, y, z;
+};
+
+struct TileCoordsXYZ
+{
+    TileCoordsXYZ() = default;
+    TileCoordsXYZ(sint32 x_, sint32 y_, sint32 z_) : x(x_), y(y_), z(z_) {}
+    explicit TileCoordsXYZ(CoordsXYZ c) : x(c.x / 32), y(c.y / 32), z(c.z / 8) {}
+    sint32 x = 0, y = 0, z = 0;
 };
 
 struct TileCoordsXYZD

commit 55d053417dc7e18a2844782cee90b1a62a9fa2d4

Fix debug builds
Regression from #7261.

src/openrct2/peep/Peep.cpp
@@ -10699,10 +10699,10 @@ sint32 peep_pathfind_choose_direction(sint16 x, sint16 y, uint8 z, rct_peep * pe
         uint8  best_sub   = 0xFF;
 
 #if defined(DEBUG_LEVEL_1) && DEBUG_LEVEL_1
-        uint8          bestJunctions         = 0;
-        TileCoordsXYZ bestJunctionList[16]  = { 0 };
-        uint8          bestDirectionList[16] = { 0 };
-        TileCoordsXYZ bestXYZ               = { 0, 0, 0 };
+        uint8         bestJunctions = 0;
+        TileCoordsXYZ bestJunctionList[16];
+        uint8         bestDirectionList[16];
+        TileCoordsXYZ bestXYZ;
 
         if (gPathFindDebug)
         {