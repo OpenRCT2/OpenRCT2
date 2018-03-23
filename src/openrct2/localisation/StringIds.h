5492b3b127f78bcb88dce226a01e80fcbdf23847
Michael Steenbeek <m.o.steenbeek@gmail.com>
2018-03-23 10:36:14 +0100

    Change order 2 [ci skip]

--- src/openrct2/localisation/StringIds.h
+++ src/openrct2/localisation/StringIds.h
@@ -3897,8 +3897,8 @@ enum {
 
     STR_FROZEN = 6232,
 
-    STR_SHORTCUT_HIGHLIGHT_PATH_ISSUES_TOGGLE = 6233,
-    STR_SHORTCUT_VIEW_CLIPPING = 6234,
+    STR_SHORTCUT_VIEW_CLIPPING = 6233,
+    STR_SHORTCUT_HIGHLIGHT_PATH_ISSUES_TOGGLE = 6234,
 
     // Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
     STR_COUNT = 32768
