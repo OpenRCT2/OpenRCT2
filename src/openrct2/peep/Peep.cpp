a70301c1146f6a421989c393ae50c8755ad99b28
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2018-04-02 18:21:38 +0100

    Make it compile

--- src/openrct2/peep/Peep.cpp
+++ src/openrct2/peep/Peep.cpp
@@ -8422,7 +8422,7 @@ static sint32 guest_path_finding(rct_peep * peep)
 sint32 rct_peep::PerformNextAction(uint8 & pathing_result)
 {
     rct_tile_element * tmpTile;
-    PerformNextAction(pathing_result, tmpTile);
+    return PerformNextAction(pathing_result, tmpTile);
 }
 
 /**
