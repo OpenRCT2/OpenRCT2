e4fd2cd748491fc5d9c36ec9314a7dc70d558696
Hielke Morsink <hielke.morsink@gmail.com>
2018-04-25 14:04:12 +0200

    Fix memset byte value too big
    `memset` takes an integer as argument which gets converted to an unsigned byte. `SPRITE_INDEX_NULL` has a value of 0xFFFF, essentially cutting it to 0xFF. Calling `std::fill_n` instead of `memcpy` fills the memory of each element rather than each byte. In this case, the result is the same.
    
    None of the other calls to `memset` in the project use values bigger than 0xFF.

--- src/openrct2/world/Sprite.cpp
+++ src/openrct2/world/Sprite.cpp
@@ -14,11 +14,13 @@
  *****************************************************************************/
 #pragma endregion
 
+#include <algorithm>
 #include <cmath>
 #include "../audio/audio.h"
 #include "../Cheats.h"
 #include "../core/Guard.hpp"
 #include "../core/Math.hpp"
+#include "../core/Util.hpp"
 #include "../Game.h"
 #include "../interface/Viewport.h"
 #include "../localisation/Date.h"
@@ -176,7 +178,7 @@ void reset_sprite_list()
  */
 void reset_sprite_spatial_index()
 {
-    memset(gSpriteSpatialIndex, SPRITE_INDEX_NULL, sizeof(gSpriteSpatialIndex));
+    std::fill_n(gSpriteSpatialIndex, Util::CountOf(gSpriteSpatialIndex), SPRITE_INDEX_NULL);
     for (size_t i = 0; i < MAX_SPRITES; i++) {
         rct_sprite *spr = get_sprite(i);
         if (spr->unknown.sprite_identifier != SPRITE_IDENTIFIER_NULL) {
