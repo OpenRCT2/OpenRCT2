34b5aab6c06c4c0af7d48b3aecb75b8135605d88
Hielke Morsink <hielke.morsink@gmail.com>
2018-05-24 23:01:16 +0200

    Add new tile element classes for each type
    This introduces some template code  to get around the problem of properties not being available for child-elements. I've tried to make clear what it's for by adding a comment.
    
    So far only the Track element has an extra property (rideIndex), but it's fairly easy to add new ones.

--- src/openrct2/scripting/ScTile.hpp
+++ src/openrct2/scripting/ScTile.hpp
@@ -9,7 +9,7 @@ namespace OpenRCT2::Scripting
 {
     class ScTileElement
     {
-    private:
+    protected:
         rct_tile_element * _element;
 
     public:
@@ -18,16 +18,33 @@ namespace OpenRCT2::Scripting
         {
         }
 
-        std::string type_get()
+        std::string type_get() const
         {
-            if (_element->GetType() == TILE_ELEMENT_TYPE_PATH)
+            switch (_element->GetType())
             {
-                return "footpath";
+                case TILE_ELEMENT_TYPE_SURFACE: return "surface";
+                case TILE_ELEMENT_TYPE_PATH: return "footpath";
+                case TILE_ELEMENT_TYPE_TRACK: return "track";
+                case TILE_ELEMENT_TYPE_SMALL_SCENERY: return "small-scenery";
+                case TILE_ELEMENT_TYPE_ENTRANCE: return "entrance";
+                case TILE_ELEMENT_TYPE_WALL: return "wall";
+                case TILE_ELEMENT_TYPE_LARGE_SCENERY: return "large-scenery";
+                case TILE_ELEMENT_TYPE_BANNER: return "banner";
+                case TILE_ELEMENT_TYPE_CORRUPT: return "openrct2-corrupt";
+                default: return "unknown";
             }
-            return "unknown";
         }
 
-        bool broken_get() { return _element->flags & TILE_ELEMENT_FLAG_BROKEN; }
+        template<typename T>
+        T* getAs()
+        {
+            return static_cast<T*>(this);
+        }
+
+        bool broken_get() const
+        {
+            return _element->flags & TILE_ELEMENT_FLAG_BROKEN;
+        }
         void broken_set(bool value)
         {
             if (value)
@@ -40,12 +57,153 @@ namespace OpenRCT2::Scripting
             }
         }
 
-        static void Register(duk_context * ctx)
+        auto baseHeight_get() const
         {
-            dukglue_register_property(ctx, &ScTileElement::type_get, nullptr, "type");
-            dukglue_register_property(ctx, &ScTileElement::broken_get, &ScTileElement::broken_set, "broken");
+            return _element->base_height;
+        }
+        void baseHeight_set(uint8 newBaseHeight)
+        {
+            _element->base_height = newBaseHeight;
+        }
+
+        auto clearanceHeight_get() const
+        {
+            return _element->clearance_height;
+        }
+        void clearanceHeight_set(uint8 newClearanceHeight)
+        {
+            _element->clearance_height = newClearanceHeight;
+        }
+
+        static void Register(duk_context* ctx);
+        template<typename T>
+        static void RegisterSharedProperties(duk_context* ctx);
+    };
+
+    class ScSurfaceElement final : public ScTileElement
+    {
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScSurfaceElement>(ctx);
         }
     };
+
+    class ScFootpathElement final : public ScTileElement
+    {
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScFootpathElement>(ctx);
+        }
+    };
+
+    class ScTrackElement final : public ScTileElement
+    {
+        auto track_ride_index_get() const
+        {
+            return _element->properties.track.ride_index;
+        }
+
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScTrackElement>(ctx);
+            dukglue_register_property(ctx, &ScTrackElement::track_ride_index_get, nullptr, "rideIndex");
+        }
+    };
+
+    class ScSmallSceneryElement final : public ScTileElement
+    {
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScSmallSceneryElement>(ctx);
+        }
+    };
+
+    class ScEntranceElement final : public ScTileElement
+    {
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScEntranceElement>(ctx);
+        }
+    };
+
+    class ScWallElement final : public ScTileElement
+    {
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScWallElement>(ctx);
+        }
+    };
+
+    class ScLargeSceneryElement final : public ScTileElement
+    {
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScLargeSceneryElement>(ctx);
+        }
+    };
+
+    class ScBannerElement final : public ScTileElement
+    {
+    public:
+        static void Register(duk_context* ctx)
+        {
+            RegisterSharedProperties<ScBannerElement>(ctx);
+        }
+    };
+
+    void ScTileElement::Register(duk_context* ctx)
+    {
+        dukglue_register_method(ctx, &ScTileElement::getAs<ScSurfaceElement>, "asSurface");
+        dukglue_register_method(ctx, &ScTileElement::getAs<ScTrackElement>, "asTrack");
+        dukglue_register_method(ctx, &ScTileElement::getAs<ScSmallSceneryElement>, "asSmallScenery");
+        dukglue_register_method(ctx, &ScTileElement::getAs<ScEntranceElement>, "asEntrance");
+        dukglue_register_method(ctx, &ScTileElement::getAs<ScWallElement>, "asWall");
+        dukglue_register_method(ctx, &ScTileElement::getAs<ScLargeSceneryElement>, "asLargeScenery");
+        dukglue_register_method(ctx, &ScTileElement::getAs<ScBannerElement>, "asBanner");
+
+        // Shared properties
+        RegisterSharedProperties<ScTileElement>(ctx);
+
+        // Register type specific properties
+        ScSurfaceElement ::Register(ctx);
+        ScTrackElement ::Register(ctx);
+        ScSmallSceneryElement ::Register(ctx);
+        ScEntranceElement ::Register(ctx);
+        ScWallElement ::Register(ctx);
+        ScLargeSceneryElement ::Register(ctx);
+        ScBannerElement ::Register(ctx);
+    }
+
+    template<typename T>
+    void ScTileElement::RegisterSharedProperties(duk_context* ctx)
+    {
+        // When you register a member function of a parent class, it belongs to the parent and cannot be accessed by its
+        // children. To get around this issue, we need to force the function pointer types to be of the class we want them to be
+        // available for. This needs to be done for every child.
+        dukglue_register_property(ctx, static_cast<std::string (T::*)() const>(&ScTileElement::type_get), nullptr, "type");
+        dukglue_register_property(
+            ctx,
+            static_cast<bool (T::*)() const>(&ScTileElement::broken_get),
+            static_cast<void (T::*)(bool)>(&ScTileElement::broken_set),
+            "broken");
+        dukglue_register_property(
+            ctx,
+            static_cast<uint8 (T::*)() const>(&ScTileElement::baseHeight_get),
+            static_cast<void (T::*)(uint8)>(&ScTileElement::baseHeight_set),
+            "baseHeight");
+        dukglue_register_property(
+            ctx,
+            static_cast<uint8 (T::*)() const>(&ScTileElement::clearanceHeight_get),
+            static_cast<void (T::*)(uint8)>(&ScTileElement::clearanceHeight_set),
+            "clearanceHeight");
+    }
 
     class ScTile
     {
