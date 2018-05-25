b59ce9a96c7911891fc4e841e0e1e51a40117b6a
Aaron van Geffen <aaron@aaronweb.net>
2018-05-25 11:55:06 +0200

    Prevent peeps from spawning in the scenario editor.
    Regression from 721dc007784734c550c8e0364c2d3b2e4dfa572c.

--- src/openrct2/world/Park.cpp
+++ src/openrct2/world/Park.cpp
@@ -579,6 +579,10 @@ void Park::Initialise()
 
 void Park::Update()
 {
+    // TODO: move when GameState class is introduced.
+    if (gScreenFlags & (SCREEN_FLAGS_SCENARIO_EDITOR | SCREEN_FLAGS_TRACK_DESIGNER | SCREEN_FLAGS_TRACK_MANAGER))
+        return;
+
     // Every 5 seconds approximately
     if (gCurrentTicks % 512 == 0)
     {
