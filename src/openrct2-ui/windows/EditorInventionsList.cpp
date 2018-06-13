3461f1e1819cb80e3963d6abe1d48bb2f6b21b95
Andy Ford <TheHungryPredator@hotmail.co.uk>
2018-06-13 18:22:22 +0100

    Fix #7673: Vehicle names cut in invention list
    The string formatting was formatting based on the size of the
    pointer to the buffer, not the buffer itself, this truncated all
    vehicle names to 8 characters.

--- src/openrct2-ui/windows/EditorInventionsList.cpp
+++ src/openrct2-ui/windows/EditorInventionsList.cpp
@@ -802,7 +802,7 @@ static void window_editor_inventions_list_scrollpaint(rct_window *w, rct_drawpix
         {
             const rct_string_id rideGroupName = get_ride_naming(researchItem->baseRideType, get_ride_entry(researchItem->entryIndex)).name;
             format_string(groupNamePtr, sizeof(groupNameBuffer), STR_INVENTIONS_LIST_RIDE_AND_VEHICLE_NAME, (void*) &rideGroupName);
-            format_string(vehicleNamePtr, sizeof(vehicleNamePtr), itemNameId, nullptr);
+            format_string(vehicleNamePtr, sizeof(vehicleNameBuffer), itemNameId, nullptr);
         }
         else
         {
