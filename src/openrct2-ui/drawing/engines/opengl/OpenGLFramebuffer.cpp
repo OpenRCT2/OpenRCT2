e59c7ee7b88ac1c66e56ffbef4fe0fec41cba47b
Hielke Morsink <hielke.morsink@gmail.com>
2018-06-15 13:24:52 +0200

    Add full stop

--- src/openrct2-ui/drawing/engines/opengl/OpenGLFramebuffer.cpp
+++ src/openrct2-ui/drawing/engines/opengl/OpenGLFramebuffer.cpp
@@ -4,7 +4,7 @@
  * For a complete list of all authors, please refer to contributors.md
  * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
  *
- * OpenRCT2 is licensed under the GNU General Public License version 3
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
 
 #ifndef DISABLE_OPENGL
--- src/openrct2/object/SmallSceneryObject.cpp
+++ src/openrct2/object/SmallSceneryObject.cpp
@@ -4,7 +4,7 @@
  * For a complete list of all authors, please refer to contributors.md
  * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
  *
- * OpenRCT2 is licensed under the GNU General Public License version 3
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
 
 #pragma warning(disable : 4706) // assignment within conditional expression
