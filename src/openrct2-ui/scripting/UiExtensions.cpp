c0646142803ec46e8ab1078a6d345d791512707f
Hielke Morsink <hielke.morsink@gmail.com>
2018-06-17 15:48:42 +0200

    Use new copyright/license notice

--- src/openrct2-ui/scripting/UiExtensions.cpp
+++ src/openrct2-ui/scripting/UiExtensions.cpp
@@ -1,3 +1,12 @@
+/*****************************************************************************
+ * Copyright (c) 2014-2018 OpenRCT2 developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
+ *
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
 #include <openrct2/scripting/ScriptEngine.h>
 #include "UiExtensions.h"
 #include "ScUi.hpp"
