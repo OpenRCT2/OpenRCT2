e567c568bbfc9bb57ff797849042e5545560fcd4
TheAssassin <theassassin@assassinate-you.net>
2018-07-01 14:08:18 +0200

    Remove deprecated icu build

--- scripts/linux/build-appimage.sh
+++ scripts/linux/build-appimage.sh
@@ -26,37 +26,6 @@ OLD_CWD=$(readlink -f .)
 
 pushd "$BUILD_DIR"
 
-#pushd "$REPO_ROOT"
-#wget -c http://download.icu-project.org/files/icu4c/59.1/icu4c-59_1-src.tgz
-#echo "54923fa9fab5b2b83f235fb72523de37  icu4c-59_1-src.tgz" | md5sum -c
-#popd
-#
-#tar xf "$REPO_ROOT"/icu4c-59_1-src.tgz
-#
-#if which clang &>/dev/null; then
-#    export CC=clang
-#    if which clang++ &>/dev/null; then
-#        export CXX=clang++
-#    else
-#        export CXX=clang
-#    fi
-#fi
-#
-#if which ccache &>/dev/null; then
-#    export CC="ccache $CC" CXX="ccache $CXX"
-#fi
-#
-#
-#pushd icu/source
-#./runConfigureICU Linux --enable-static --enable-shared --prefix="$BUILD_DIR"/ext
-#unset CC CXX
-#make install -j$(nproc)
-#popd
-#
-#export PKG_CONFIG_PATH=$(readlink -f ext/lib/pkgconfig)
-#
-#cmake "$REPO_ROOT" -DCMAKE_PREFIX_PATH=$(readlink -f ext)
-
 #wget https://raw.githubusercontent.com/wheybags/glibc_version_header/master/version_headers/force_link_glibc_2.27.h
 
 #flags="-include "$(readlink -f "force_link_glibc_2.27.h")
