4b4510b34747911618a9798e9fd7127ad1130c77
Aaron van Geffen <aaron@aaronweb.net>
2018-08-15 16:34:11 +0200

    Refactor common code to their own functions.

--- src/openrct2-ui/windows/LoadSave.cpp
+++ src/openrct2-ui/windows/LoadSave.cpp
@@ -161,12 +161,85 @@ static void window_loadsave_sort_list();
 
 static rct_window* window_overwrite_prompt_open(const char* name, const char* path);
 
-static int32_t window_loadsave_get_dir(utf8* last_save, char* path, const char* subdir, size_t pathSize)
+static utf8* getLastDirectoryByType(int32_t type)
 {
+    switch (type & 0x0E)
+    {
+        case LOADSAVETYPE_GAME:
+            return gConfigGeneral.last_save_game_directory;
+
+        case LOADSAVETYPE_LANDSCAPE:
+            return gConfigGeneral.last_save_landscape_directory;
+
+        case LOADSAVETYPE_SCENARIO:
+            return gConfigGeneral.last_save_scenario_directory;
+
+        case LOADSAVETYPE_TRACK:
+            return gConfigGeneral.last_save_track_directory;
+
+        default:
+            return nullptr;
+    }
+}
+
+static void getInitialDirectoryByType(const int32_t type, char* path, size_t pathSize)
+{
+    const char* subdir = nullptr;
+    switch (type & 0x0E)
+    {
+        case LOADSAVETYPE_GAME:
+            subdir = "save";
+            break;
+
+        case LOADSAVETYPE_LANDSCAPE:
+            subdir = "landscape";
+            break;
+
+        case LOADSAVETYPE_SCENARIO:
+            subdir = "scenario";
+            break;
+
+        case LOADSAVETYPE_TRACK:
+            subdir = "track";
+            break;
+    }
+
+    platform_get_user_directory(path, subdir, pathSize);
+}
+
+static const char* getFilterPatternByType(const int32_t type, const bool isSave)
+{
+    switch (type & 0x0E)
+    {
+        case LOADSAVETYPE_GAME:
+            return isSave ? "*.sv6" : "*.sv6;*.sc6;*.sc4;*.sv4;*.sv7";
+
+        case LOADSAVETYPE_LANDSCAPE:
+            return isSave ? "*.sc6" : "*.sc6;*.sv6;*.sc4;*.sv4;*.sv7";
+
+        case LOADSAVETYPE_SCENARIO:
+            return "*.sc6";
+
+        case LOADSAVETYPE_TRACK:
+            return isSave ? "*.td6" : "*.td6;*.td4";
+
+        case LOADSAVETYPE_IMAGE:
+            return "*.bmp;*.jpg;*.png";
+
+        default:
+            openrct2_assert(true, "Unsupported load/save directory type.");
+    }
+
+    return "";
+}
+
+static int32_t window_loadsave_get_dir(const int32_t type, char* path, size_t pathSize)
+{
+    const char* last_save = getLastDirectoryByType(type);
     if (last_save && platform_ensure_directory_exists(last_save))
         safe_strcpy(path, last_save, pathSize);
     else
-        platform_get_user_directory(path, subdir, pathSize);
+        getInitialDirectoryByType(type, path, pathSize);
 
     if (!platform_ensure_directory_exists(path))
     {
@@ -191,8 +264,10 @@ rct_window* window_loadsave_open(int32_t type, const char* defaultName, loadsave
     }
 
     bool isSave = (type & 0x01) == LOADSAVETYPE_SAVE;
-    bool success = false;
     char path[MAX_PATH];
+    bool success = window_loadsave_get_dir(type, path, sizeof(path));
+    if (!success)
+        return nullptr;
 
     // Bypass the lot?
     if (gConfigGeneral.use_native_browse_dialog)
@@ -218,69 +293,38 @@ rct_window* window_loadsave_open(int32_t type, const char* defaultName, loadsave
         w->max_height = WH * 2;
     }
 
-    w->no_list_items = 0;
+    window_loadsave_populate_list(w, false, path, getFilterPatternByType(type, isSave));
+    w->no_list_items = static_cast<uint16_t>(_listItems.size());
     w->selected_list_item = -1;
 
     switch (type & 0x0E)
     {
         case LOADSAVETYPE_GAME:
             w->widgets[WIDX_TITLE].text = isSave ? STR_FILE_DIALOG_TITLE_SAVE_GAME : STR_FILE_DIALOG_TITLE_LOAD_GAME;
-            if (window_loadsave_get_dir(gConfigGeneral.last_save_game_directory, path, "save", sizeof(path)))
-            {
-                window_loadsave_populate_list(w, isSave, path, isSave ? ".sv6" : ".sv6;.sc6;.sv4;.sc4;.sv7");
-                success = true;
-            }
             break;
 
         case LOADSAVETYPE_LANDSCAPE:
             w->widgets[WIDX_TITLE].text = isSave ? STR_FILE_DIALOG_TITLE_SAVE_LANDSCAPE : STR_FILE_DIALOG_TITLE_LOAD_LANDSCAPE;
-            if (window_loadsave_get_dir(gConfigGeneral.last_save_landscape_directory, path, "landscape", sizeof(path)))
-            {
-                window_loadsave_populate_list(w, isSave, path, isSave ? ".sc6" : ".sc6;.sv6;.sc4;.sv4;.sv7");
-                success = true;
-            }
             break;
 
         case LOADSAVETYPE_SCENARIO:
             w->widgets[WIDX_TITLE].text = STR_FILE_DIALOG_TITLE_SAVE_SCENARIO;
-            if (window_loadsave_get_dir(gConfigGeneral.last_save_scenario_directory, path, "scenario", sizeof(path)))
-            {
-                window_loadsave_populate_list(w, isSave, path, ".sc6");
-                success = true;
-            }
             break;
 
         case LOADSAVETYPE_TRACK:
             w->widgets[WIDX_TITLE].text = isSave ? STR_FILE_DIALOG_TITLE_SAVE_TRACK
                                                  : STR_FILE_DIALOG_TITLE_INSTALL_NEW_TRACK_DESIGN;
-            if (window_loadsave_get_dir(gConfigGeneral.last_save_track_directory, path, "track", sizeof(path)))
-            {
-                window_loadsave_populate_list(w, isSave, path, isSave ? ".td6" : ".td6;.td4");
-                success = true;
-            }
             break;
 
         case LOADSAVETYPE_IMAGE:
             openrct2_assert(!isSave, "Cannot save images through loadsave window");
             w->widgets[WIDX_TITLE].text = STR_FILE_DIALOG_TITLE_LOAD_HEIGHTMAP;
-            if (window_loadsave_get_dir(gConfigGeneral.last_save_track_directory, path, "", sizeof(path)))
-            {
-                window_loadsave_populate_list(w, false, path, ".bmp;.png");
-                success = true;
-            }
             break;
 
         default:
-            log_error("Unsupported load/save type: %d", type & 0x0F);
+            openrct2_assert(true, "Unsupported load/save type: %d", type & 0x0F);
     }
 
-    if (!success)
-    {
-        window_close(w);
-        return nullptr;
-    }
-
-    w->no_list_items = static_cast<uint16_t>(_listItems.size());
     window_init_scroll_widgets(w);
     window_loadsave_compute_max_date_width();
 
@@ -320,7 +364,7 @@ static bool browse(bool isSave, char* path, size_t pathSize)
             fileType = FILE_EXTENSION_SV6;
             title = isSave ? STR_FILE_DIALOG_TITLE_SAVE_GAME : STR_FILE_DIALOG_TITLE_LOAD_GAME;
             desc.filters[0].name = language_get_string(STR_OPENRCT2_SAVED_GAME);
-            desc.filters[0].pattern = isSave ? "*.sv6" : "*.sv6;*.sc6;*.sv4;*.sc4;*.sv7";
+            desc.filters[0].pattern = getFilterPatternByType(_type, isSave);
             break;
 
         case LOADSAVETYPE_LANDSCAPE:
@@ -328,7 +372,7 @@ static bool browse(bool isSave, char* path, size_t pathSize)
             fileType = FILE_EXTENSION_SC6;
             title = isSave ? STR_FILE_DIALOG_TITLE_SAVE_LANDSCAPE : STR_FILE_DIALOG_TITLE_LOAD_LANDSCAPE;
             desc.filters[0].name = language_get_string(STR_OPENRCT2_LANDSCAPE_FILE);
-            desc.filters[0].pattern = isSave ? "*.sc6" : "*.sc6;*.sv6;*.sc4;*.sv4;*.sv7";
+            desc.filters[0].pattern = getFilterPatternByType(_type, isSave);
             break;
 
         case LOADSAVETYPE_SCENARIO:
@@ -336,7 +380,7 @@ static bool browse(bool isSave, char* path, size_t pathSize)
             fileType = FILE_EXTENSION_SC6;
             title = STR_FILE_DIALOG_TITLE_SAVE_SCENARIO;
             desc.filters[0].name = language_get_string(STR_OPENRCT2_SCENARIO_FILE);
-            desc.filters[0].pattern = "*.sc6";
+            desc.filters[0].pattern = getFilterPatternByType(_type, isSave);
             break;
 
         case LOADSAVETYPE_TRACK:
@@ -344,13 +388,13 @@ static bool browse(bool isSave, char* path, size_t pathSize)
             fileType = FILE_EXTENSION_TD6;
             title = isSave ? STR_FILE_DIALOG_TITLE_SAVE_TRACK : STR_FILE_DIALOG_TITLE_INSTALL_NEW_TRACK_DESIGN;
             desc.filters[0].name = language_get_string(STR_OPENRCT2_TRACK_DESIGN_FILE);
-            desc.filters[0].pattern = isSave ? "*.td6" : "*.td6;*.td4";
+            desc.filters[0].pattern = getFilterPatternByType(_type, isSave);
             break;
 
         case LOADSAVETYPE_IMAGE:
             title = STR_FILE_DIALOG_TITLE_LOAD_HEIGHTMAP;
             desc.filters[0].name = language_get_string(STR_OPENRCT2_HEIGHTMAP_FILE);
-            desc.filters[0].pattern = "*.jpg;*.png;*.bmp";
+            desc.filters[0].pattern = getFilterPatternByType(_type, isSave);
             break;
     }
 
@@ -469,22 +513,7 @@ static void window_loadsave_mouseup(rct_window* w, rct_widgetindex widgetIndex)
             break;
 
         case WIDX_DEFAULT:
-            switch (_type & 0x0E)
-            {
-                case LOADSAVETYPE_GAME:
-                    platform_get_user_directory(path, "save", sizeof(path));
-                    break;
-                case LOADSAVETYPE_LANDSCAPE:
-                    platform_get_user_directory(path, "landscape", sizeof(path));
-                    break;
-                case LOADSAVETYPE_SCENARIO:
-                    platform_get_user_directory(path, "scenario", sizeof(path));
-                    break;
-                case LOADSAVETYPE_TRACK:
-                    platform_get_user_directory(path, "track", sizeof(path));
-                    break;
-            }
-
+            getInitialDirectoryByType(_type, path, sizeof(path));
             window_loadsave_populate_list(w, isSave, path, _extension);
             window_init_scroll_widgets(w);
             w->no_list_items = static_cast<uint16_t>(_listItems.size());
@@ -869,14 +898,12 @@ static void window_loadsave_populate_list(rct_window* w, int32_t includeNewItem,
         char filter[MAX_PATH];
         char extCopy[64];
         safe_strcpy(extCopy, extension, Util::CountOf(extCopy));
-        char* extToken;
         bool showExtension = false;
-        extToken = strtok(extCopy, ";");
+        char* extToken = strtok(extCopy, ";");
         while (extToken != nullptr)
         {
             safe_strcpy(filter, directory, Util::CountOf(filter));
-            safe_strcat_path(filter, "*", Util::CountOf(filter));
-            path_append_extension(filter, extToken, Util::CountOf(filter));
+            safe_strcat_path(filter, extToken, Util::CountOf(filter));
 
             auto scanner = std::unique_ptr<IFileScanner>(Path::ScanDirectory(filter, false));
             while (scanner->Next())
