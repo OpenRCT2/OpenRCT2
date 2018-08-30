03cd16220e54733b40b12bc57463d49a364654a5
pkubaj <pkubaj@users.noreply.github.com>
2018-08-30 21:19:46 +0200

    Change ordering of includes (#7941)
    It fixes build with LLVM 6.0.1 and GCC 7.3.0 on FreeBSD 11.2-RELEASE

--- src/openrct2/platform/Platform.Linux.cpp
+++ src/openrct2/platform/Platform.Linux.cpp
@@ -13,8 +13,8 @@
 #    include <pwd.h>
 #    if defined(__FreeBSD__)
 #        include <stddef.h>
+#        include <sys/param.h>
 #        include <sys/sysctl.h>
-#        include <sys/types.h>
 #    endif // __FreeBSD__
 #    if defined(__linux__)
 // for PATH_MAX
