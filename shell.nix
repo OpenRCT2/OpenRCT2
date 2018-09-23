766e1f1b6c57933b060621c49ec58071f6b9debe
Jappie Klooster <jappieklooster@hotmail.com>
2018-09-23 11:59:17 +0200

    Delete comment from wihc unstalbe this is

--- shell.nix
+++ shell.nix
@@ -4,7 +4,6 @@
     pinnedPkgs = hostPkgs.fetchFromGitHub {
       owner = "NixOS";
       repo = "nixpkgs-channels";
-      # nixos-unstable as of 2017-11-13T08:53:10-00:00
       rev = "08d245eb31a3de0ad73719372190ce84c1bf3aee";
       sha256 = "1g22f8r3l03753s67faja1r0dq0w88723kkfagskzg9xy3qs8yw8";
     };
