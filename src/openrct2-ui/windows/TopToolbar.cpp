687fbc2a0a7c14ab3cd32c3054b0e01bdd8a2e20
Michael Steenbeek <m.o.steenbeek@gmail.com>
2018-10-05 13:40:05 +0200

    Modify game_command_set_scenery_colour() to only take the scenery quadrant

--- src/openrct2-ui/windows/TopToolbar.cpp
+++ src/openrct2-ui/windows/TopToolbar.cpp
@@ -992,9 +992,10 @@ static void repaint_scenery_tool_down(int16_t x, int16_t y, rct_widgetindex widg
                     scenery_entry, SMALL_SCENERY_FLAG_HAS_PRIMARY_COLOUR | SMALL_SCENERY_FLAG_HAS_GLASS))
                 return;
 
+            uint8_t quadrant = tile_element->AsSmallScenery()->GetSceneryQuadrant();
             gGameCommandErrorTitle = STR_CANT_REPAINT_THIS;
             game_do_command(
-                grid_x, 1 | (tile_element->type << 8), grid_y,
+                grid_x, GAME_COMMAND_FLAG_APPLY | quadrant, grid_y,
                 tile_element->base_height | (tile_element->AsSmallScenery()->GetEntryIndex() << 8),
                 GAME_COMMAND_SET_SCENERY_COLOUR, 0, gWindowSceneryPrimaryColour | (gWindowScenerySecondaryColour << 8));
             break;
