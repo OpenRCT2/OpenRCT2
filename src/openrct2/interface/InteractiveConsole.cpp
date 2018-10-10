8bf3f58ed0c064992764c85cec8510edbd963ec1
tombomp <tombomp@gmail.com>
2018-10-10 21:15:27 +0100

    accidental comma

--- src/openrct2/interface/InteractiveConsole.cpp
+++ src/openrct2/interface/InteractiveConsole.cpp
@@ -1374,7 +1374,7 @@ static constexpr const console_command console_command_table[] = {
     { "remove_unused_objects", cc_remove_unused_objects, "Removes all the unused objects from the object selection.", "remove_unused_objects" },
     { "remove_park_fences", cc_remove_park_fences, "Removes all park fences from the surface", "remove_park_fences"},
     { "show_limits", cc_show_limits, "Shows the map data counts and limits.", "show_limits" },
-    { "date", cc_for_date, "Sets the date to a given date.", "Format <year>[ <month>[ <day>]]."},
+    { "date", cc_for_date, "Sets the date to a given date.", "Format <year>[ <month>[ <day>]]."}
 };
 // clang-format on
 
