73dae036013c476db56ef881fd6aeb7a3ab8960a
Ted John <ted@brambles.org>
2018-10-28 12:57:57 +0000

    Update appveyor_deploy.ps1

--- scripts/ps/appveyor_deploy.ps1
+++ scripts/ps/appveyor_deploy.ps1
@@ -15,8 +15,8 @@ if ($nottesting -and $notvs2015)
         {
             msbuild openrct2.proj /t:UploadArtifacts /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
             # curl is sometimes aliased so be explicit
-            $zipPath = (Get-ChildItem bin/openrct2-symbols-*.zip).FullName
-            Write-Host $zipPath
+            $zipPath = (Get-ChildItem artifacts/openrct2-symbols-*.zip).FullName
+            Write-Host "Uploading $zipPath to backtrace.io..."
             curl.exe --data-binary "@$zipPath" 'https://openrct2.sp.backtrace.io:6098/post?format=symbols&token=e9e6d681fafdeac9f6131b4b59a155d54bebad567a8c0380d70643f4414819f5'
         }
         else
