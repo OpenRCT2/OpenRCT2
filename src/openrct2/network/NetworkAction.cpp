b45fabdc7deb93db3edcfc5133ac55c0b867c6f5
Hielke Morsink <hielke.morsink@gmail.com>
2018-11-08 23:44:51 +0100

    Add new action to 'edit scenario options' permission

--- src/openrct2/network/NetworkAction.cpp
+++ src/openrct2/network/NetworkAction.cpp
@@ -216,7 +216,8 @@ const std::vector<NetworkAction> NetworkActions::Actions =
     }, {
         STR_ACTION_EDIT_SCENARIO_OPTIONS, "PERMISSION_EDIT_SCENARIO_OPTIONS",
         {
-            GAME_COMMAND_EDIT_SCENARIO_OPTIONS
+            GAME_COMMAND_EDIT_SCENARIO_OPTIONS,
+            GAME_COMMAND_SET_CLIMATE
         }
     }
 };
