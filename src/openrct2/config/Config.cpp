de05ddbd1b927bf73dd8b0c4379e338945eb7d5d
Lars Toenning <1716810+ltoenning@users.noreply.github.com>
2019-01-18 22:39:16 +0100

    Adding define for DEFAULT_NUM_AUTOSAVES_TO_KEEP

--- src/openrct2/config/Config.cpp
+++ src/openrct2/config/Config.cpp
@@ -34,6 +34,8 @@
 
 #include <memory>
 
+#define DEFAULT_NUM_AUTOSAVES_TO_KEEP 10
+
 using namespace OpenRCT2;
 using namespace OpenRCT2::Ui;
 
@@ -137,7 +139,7 @@ namespace Config
             auto model = &gConfigGeneral;
             model->always_show_gridlines = reader->GetBoolean("always_show_gridlines", false);
             model->autosave_frequency = reader->GetInt32("autosave", AUTOSAVE_EVERY_5MINUTES);
-            model->autosave_amount = reader->GetInt32("autosave_amount", 10);
+            model->autosave_amount = reader->GetInt32("autosave_amount", DEFAULT_NUM_AUTOSAVES_TO_KEEP);
             model->confirmation_prompt = reader->GetBoolean("confirmation_prompt", false);
             model->currency_format = reader->GetEnum<int32_t>("currency_format", platform_get_locale_currency(), Enum_Currency);
             model->custom_currency_rate = reader->GetInt32("custom_currency_rate", 10);
