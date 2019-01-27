f6a0eb24c375ffb5e7cb7fc044685fc4a355f1fa
Tom Lankhorst <hello@tomlankhorst.nl>
2019-01-27 16:05:49 +0100

    CS

--- src/openrct2/management/Marketing.cpp
+++ src/openrct2/management/Marketing.cpp
@@ -46,16 +46,16 @@ int32_t marketing_get_campaign_guest_generation_probability(int32_t campaign)
     switch (campaign)
     {
         case ADVERTISING_CAMPAIGN_PARK_ENTRY_FREE:
-            if (park_get_entrance_fee() < MONEY(4,0))
+            if (park_get_entrance_fee() < MONEY(4, 0))
                 probability /= 8;
             break;
         case ADVERTISING_CAMPAIGN_PARK_ENTRY_HALF_PRICE:
-            if (park_get_entrance_fee() < MONEY(6,0))
+            if (park_get_entrance_fee() < MONEY(6, 0))
                 probability /= 8;
             break;
         case ADVERTISING_CAMPAIGN_RIDE_FREE:
             ride = get_ride(gMarketingCampaignRideIndex[campaign]);
-            if (ride->price < MONEY(3,0))
+            if (ride->price < MONEY(3, 0))
                 probability /= 8;
             break;
     }
