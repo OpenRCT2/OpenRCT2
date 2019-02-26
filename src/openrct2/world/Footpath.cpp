584c4d513476e1d9dd83b5d8036e3ce350c9a7eb
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2019-02-26 13:02:29 +0000

    Prevent a dereference of nullptr

--- src/openrct2/world/Footpath.cpp
+++ src/openrct2/world/Footpath.cpp
@@ -2433,8 +2433,8 @@ void footpath_update_path_wide_flags(int32_t x, int32_t y)
 
 bool footpath_is_blocked_by_vehicle(const TileCoordsXYZ& position)
 {
-    auto pathElement = map_get_path_element_at(position.x, position.y, position.z)->AsPath();
-    return pathElement != nullptr && pathElement->IsBlockedByVehicle();
+    auto pathElement = map_get_path_element_at(position.x, position.y, position.z);
+    return pathElement != nullptr && pathElement->AsPath()->IsBlockedByVehicle();
 }
 
 /**
