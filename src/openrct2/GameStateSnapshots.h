9065126241097b1e2c1da33d86f0250df708a3ba
Matt <m.moninger.h@gmail.com>
2019-03-03 05:13:01 +0100

    Add initial interface.

---
+++ src/openrct2/GameStateSnapshots.h
@@ -0,0 +1,58 @@
+/*****************************************************************************
+ * Copyright (c) 2014-2019 OpenRCT2 developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
+ *
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#pragma once
+
+#include "common.h"
+
+#include <memory>
+#include <set>
+#include <string>
+
+struct GameStateSnapshot_t;
+struct GameStateCompareData_t;
+
+/*
+ * Interface to create and capture game states. It only allows to have 32 active snapshots
+ * the oldest snapshot will be removed from the buffer. Never store the snapshot pointer
+ * as it may become invalid at any time when a snapshot is created, rather Link the snapshot
+ * to a specific tick which can be obtained by that later again assuming its still valid.
+ */
+interface IGameStateSnapshots
+{
+    /*
+     * Creates a new empty snapshot, oldest snapshot will be removed.
+     */
+    virtual GameStateSnapshot_t* CreateSnapshot() = 0;
+
+    /*
+     * Links the snapshot to a specific game tick.
+     */
+    virtual void LinkSnapshot(GameStateSnapshot_t * snapshot, uint32_t tick) = 0;
+
+    /*
+     * This will fill the snapshot with the current game state in a compact form.
+     */
+    virtual void Capture(GameStateSnapshot_t * snapshot) = 0;
+
+    /*
+     * Returns the snapshot for a given tick in the history, nullptr if not found.
+     */
+    virtual const GameStateSnapshot_t* GetLinkedSnapshot(uint32_t tick) const = 0;
+
+    /*
+     * Serialisation of GameStateSnapshot_t
+     */
+    virtual void SerialiseSnapshot(GameStateSnapshot_t * snapshot, DataSerialiser & serialiser) const = 0;
+
+    /*
+     * Compares two states resulting GameStateCompareData_t with all mismatches stored.
+     */
+    virtual GameStateCompareData_t Compare(const GameStateSnapshot_t* left, const GameStateSnapshot_t* right) const = 0;
+};
