0db6a8a7953a301d1d64a91b749a33ba92311736
Joshua Tucker <joshtucker579@Joshuas-MacBook-Air.local>
2019-03-24 10:32:43 -0400

    Removed conditional statement incorrectly increasing loc.z height

--- src/openrct2-ui/windows/TopToolbar.cpp
+++ src/openrct2-ui/windows/TopToolbar.cpp
@@ -1425,11 +1425,7 @@ static void sub_6E1F34(
 
                 gSceneryPlaceZ = 0;
                 uint16_t water_height = tile_element->AsSurface()->GetWaterHeight();
-                if (water_height != 0)
-                {
-                    gSceneryPlaceZ = water_height * 16;
-                }
-
+                
                 // If SHIFT pressed
                 if (gSceneryShiftPressed)
                 {
