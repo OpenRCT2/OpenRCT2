c7e7cc2bea5fd32199ac5ce573e11e13d8aa7125
Hussein Okasha <hokasha2016@my.fit.edu>
2019-03-27 14:46:29 -0400

    Fix #8219: Faulty folder recreation in save

--- src/openrct2-ui/windows/LoadSave.cpp
+++ src/openrct2-ui/windows/LoadSave.cpp
@@ -240,16 +240,11 @@ static const char* getFilterPatternByType(const int32_t type, const bool isSave)
 static int32_t window_loadsave_get_dir(const int32_t type, char* path, size_t pathSize)
 {
     const char* last_save = getLastDirectoryByType(type);
-    if (last_save && platform_ensure_directory_exists(last_save))
+    // checks for path existance
+    if (platform_directory_exists(last_save))
         safe_strcpy(path, last_save, pathSize);
     else
         getInitialDirectoryByType(type, path, pathSize);
-
-    if (!platform_ensure_directory_exists(path))
-    {
-        log_error("Unable to create save directory.");
-        return 0;
-    }
 
     return 1;
 }
