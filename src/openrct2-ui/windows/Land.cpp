50ae1214617f18f41f3a477eb81ae9f9671352d7
Liam <parker.liam5@gmail.com>
2019-04-18 00:06:43 +1000

    Removed rendering of prices on water/land/clear ui

--- src/openrct2-ui/windows/Land.cpp
+++ src/openrct2-ui/windows/Land.cpp
@@ -360,15 +360,18 @@ static void window_land_paint(rct_window* w, rct_drawpixelinfo* dpi)
     x = w->x + (previewWidget->left + previewWidget->right) / 2;
     y = w->y + previewWidget->bottom + 5;
 
-    // Draw raise cost amount
-    if (gLandToolRaiseCost != MONEY32_UNDEFINED && gLandToolRaiseCost != 0)
-        gfx_draw_string_centred(dpi, STR_RAISE_COST_AMOUNT, x, y, COLOUR_BLACK, &gLandToolRaiseCost);
-    y += 10;
+    if (!(gParkFlags & PARK_FLAGS_NO_MONEY))
+    {
+        // Draw raise cost amount
+        if (gLandToolRaiseCost != MONEY32_UNDEFINED && gLandToolRaiseCost != 0)
+            gfx_draw_string_centred(dpi, STR_RAISE_COST_AMOUNT, x, y, COLOUR_BLACK, &gLandToolRaiseCost);
+        y += 10;
 
-    // Draw lower cost amount
-    if (gLandToolLowerCost != MONEY32_UNDEFINED && gLandToolLowerCost != 0)
-        gfx_draw_string_centred(dpi, STR_LOWER_COST_AMOUNT, x, y, COLOUR_BLACK, &gLandToolLowerCost);
-    y += 50;
+        // Draw lower cost amount
+        if (gLandToolLowerCost != MONEY32_UNDEFINED && gLandToolLowerCost != 0)
+            gfx_draw_string_centred(dpi, STR_LOWER_COST_AMOUNT, x, y, COLOUR_BLACK, &gLandToolLowerCost);
+        y += 50;
+    }
 
     // Draw paint price
     numTiles = gLandToolSize * gLandToolSize;
