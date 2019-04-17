3efc21fff9461b00d0090b1f2d756d97d337fb5f
lwatson2016 <38050085+lwatson2016@users.noreply.github.com>
2019-04-17 12:12:07 -0400

    Update Map.h

--- src/openrct2/world/Map.h
+++ src/openrct2/world/Map.h
@@ -153,8 +153,6 @@ EntranceElement* map_get_ride_entrance_element_at(int32_t x, int32_t y, int32_t 
 EntranceElement* map_get_ride_exit_element_at(int32_t x, int32_t y, int32_t z, bool ghost);
 int16_t tile_element_height(int32_t x, int32_t y);
 int16_t tile_element_water_height(int32_t x, int32_t y);
-uint8_t map_get_highest_land_height(int32_t xMin, int32_t xMax, int32_t yMin, int32_t yMax);
-uint8_t map_get_lowest_land_height(int32_t xMin, int32_t xMax, int32_t yMin, int32_t yMax);
 bool map_coord_is_connected(int32_t x, int32_t y, int32_t z, uint8_t faceDirection);
 void map_remove_provisional_elements();
 void map_restore_provisional_elements();
