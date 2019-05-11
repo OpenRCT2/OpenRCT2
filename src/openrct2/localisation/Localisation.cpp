891e34a97fa8d94d90bd2f075d444cfb0db045e1
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2019-05-11 18:04:22 +0100

    Undo unrelated changes

--- src/openrct2/localisation/Localisation.cpp
+++ src/openrct2/localisation/Localisation.cpp
@@ -30,7 +30,6 @@
 #include "Localisation.h"
 
 #include <algorithm>
-#include <cmath>
 #include <cstring>
 #include <ctype.h>
 #include <iterator>
@@ -1513,8 +1512,8 @@ money32 string_to_money(const char* string_to_monetise)
 
     auto number = std::stod(processedString, nullptr);
     number /= (currencyDesc->rate / 10.0);
-    auto whole = static_cast<int32_t>(number);
-    auto fraction = static_cast<uint8_t>(ceil((number - whole) * 100.0));
+    auto whole = static_cast<uint16_t>(number);
+    auto fraction = static_cast<uint8_t>((number - whole) * 100);
 
     money32 result = MONEY(whole, fraction);
     // Check if MONEY resulted in overflow
