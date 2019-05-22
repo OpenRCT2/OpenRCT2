e9549e88daa9710e7b16d5218be31c8c2d37802c
NexGenration <jbgamer99@gmail.com>
2019-05-22 02:55:19 -0400

    Fix typo: gCommonFromatArgs -> gCommonFormatArgs

--- src/openrct2-ui/windows/Guest.cpp
+++ src/openrct2-ui/windows/Guest.cpp
@@ -2069,7 +2069,7 @@ void window_guest_debug_paint(rct_window* w, rct_drawpixelinfo* dpi)
     auto y = w->y + window_guest_debug_widgets[WIDX_PAGE_BACKGROUND].top + 4;
     {
         set_format_arg(0, uint32_t, peep->sprite_index);
-        gfx_draw_string_left(dpi, STR_PEEP_DEBUG_SPRITE_INDEX, gCommonFromatArgs, 0 x, y);
+        gfx_draw_string_left(dpi, STR_PEEP_DEBUG_SPRITE_INDEX, gCommonFormatArgs, 0 x, y);
     }
     y += LIST_ROW_HEIGHT;
     {
