9123fa74d3e4e342a2581abbc126ec5624521de3
Richard Fine <richardf@unity3d.com>
2019-08-26 18:50:37 +0100

    Add ALL_DIRECTIONS array

--- src/openrct2/world/Location.hpp
+++ src/openrct2/world/Location.hpp
@@ -270,6 +270,11 @@ typedef uint8_t Direction;
 const Direction INVALID_DIRECTION = 0xFF;
 
 /**
+ * Array of all valid cardinal directions, to make it easy to write range-based for loops like: for (Direction d : ALL_DIRECTIONS)
+ */
+constexpr Direction ALL_DIRECTIONS[] = { 0, 1, 2, 3 };
+
+/**
  * Given a direction, return the direction that points the other way,
  * on the same axis.
  */
