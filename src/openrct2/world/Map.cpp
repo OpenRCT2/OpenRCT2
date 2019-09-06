7f54ba0be7dfc8553a17c9c105ca37686fe5f969
crorvig <crorvig@gmail.com>
2019-09-05 23:25:29 -0400

    Set BuildInPauseMode to true during map_remove_out_of_range_elements()
    Set BuildInPauseMode to true during map_remove_out_of_range_elements()

--- src/openrct2/world/Map.cpp
+++ src/openrct2/world/Map.cpp
@@ -1543,6 +1543,7 @@ void map_remove_out_of_range_elements()
     int32_t mapMaxXY = gMapSizeMaxXY;
     //#9955 ensure that we can remove elements
     bool buildState = gCheatsBuildInPauseMode;
+    CheatsSet(CheatType::BuildInPauseMode, true);
 
     for (int32_t y = 0; y < (MAXIMUM_MAP_SIZE_TECHNICAL * 32); y += 32)
     {
