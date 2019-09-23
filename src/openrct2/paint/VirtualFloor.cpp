5b07abfa54794d49c5ef35919ed94492df9d8a4d
Tulio Leao <tupaschoal@gmail.com>
2019-09-22 23:22:12 -0300

    Avoid dereferencing map_get_first_element_at nullptr on VirtualFloor.cpp

--- src/openrct2/paint/VirtualFloor.cpp
+++ src/openrct2/paint/VirtualFloor.cpp
@@ -243,6 +243,8 @@ static void virtual_floor_get_tile_properties(
     //  * Walls / banners, which are displayed as occupied edges
     //  * Ghost objects, which are displayed as lit squares
     TileElement* tileElement = map_get_first_element_at(x >> 5, y >> 5);
+    if (tileElement == nullptr)
+        return;
     do
     {
         int32_t elementType = tileElement->GetType();
