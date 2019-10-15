ac30e35dfa5bb8f32437b6902b0e6593070de0e2
Matt <m.moninger.h@gmail.com>
2019-10-15 17:32:44 +0200

    Fix crash when ducks try to navigate out of bounds

--- src/openrct2/world/Duck.cpp
+++ src/openrct2/world/Duck.cpp
@@ -124,7 +124,7 @@ void rct_duck::UpdateFlyToWater()
     int32_t manhattanDistanceN = abs(target_x - newX) + abs(target_y - newY);
 
     auto surfaceElement = map_get_surface_element_at({ target_x, target_y });
-    int32_t waterHeight = surfaceElement->GetWaterHeight();
+    int32_t waterHeight = surfaceElement != nullptr ? surfaceElement->GetWaterHeight() : 0;
     if (waterHeight == 0)
     {
         state = DUCK_STATE::FLY_AWAY;
