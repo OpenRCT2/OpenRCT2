8f7c95e425c8751e0728e20f4d65a8cfcf671497
Krisztián Csordás <cskrisztianster@gmail.com>
2019-10-28 20:43:22 +0100

    Fix SDL_Texture double free (#10156)
    Double freeing of SDL_Texture caused assertion failure when calling
    HardwareDisplayDrawingEngine::SetVSync. According to SDL documentation,
    SDL_DestroyRenderer frees the associated textures.

--- src/openrct2-ui/drawing/engines/HardwareDisplayDrawingEngine.cpp
+++ src/openrct2-ui/drawing/engines/HardwareDisplayDrawingEngine.cpp
@@ -62,14 +62,6 @@ public:
 
     ~HardwareDisplayDrawingEngine() override
     {
-        if (_screenTexture != nullptr)
-        {
-            SDL_DestroyTexture(_screenTexture);
-        }
-        if (_scaledScreenTexture != nullptr)
-        {
-            SDL_DestroyTexture(_scaledScreenTexture);
-        }
         SDL_FreeFormat(_screenTextureFormat);
         SDL_DestroyRenderer(_sdlRenderer);
     }
@@ -85,6 +77,8 @@ public:
         {
             _useVsync = vsync;
             SDL_DestroyRenderer(_sdlRenderer);
+            _screenTexture = nullptr;
+            _scaledScreenTexture = nullptr;
             Initialise();
             Resize(_uiContext->GetWidth(), _uiContext->GetHeight());
         }
