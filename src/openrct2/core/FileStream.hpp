2b2a03c2e1c33f2fcedf964413c6caddc872abca
Michael Steenbeek <m.o.steenbeek@gmail.com>
2019-12-19 18:07:05 +0100

    Fix formatting

--- src/openrct2/core/FileStream.hpp
+++ src/openrct2/core/FileStream.hpp
@@ -20,13 +20,13 @@
 #endif
 
 #if defined(__linux__) && !defined(__ANDROID__)
-#define ftello ftello64
-#define fseeko fseeko64
+#    define ftello ftello64
+#    define fseeko fseeko64
 #endif
 
 #ifdef _MSC_VER
-#define ftello _ftelli64
-#define fseeko _fseeki64
+#    define ftello _ftelli64
+#    define fseeko _fseeki64
 #endif
 
 enum
