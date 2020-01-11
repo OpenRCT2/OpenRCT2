1b0a7f330a257d1286a15d20c828cfd41f64da1b
Gymnasiast <m.o.steenbeek@gmail.com>
2020-01-11 23:46:48 +0100

    Fix #7784: Vehicle tab takes 1st car colour instead of tab_vehicle's colour

--- src/openrct2/ride/Ride.cpp
+++ src/openrct2/ride/Ride.cpp
@@ -3108,8 +3108,16 @@ vehicle_colour ride_get_vehicle_colour(Ride* ride, int32_t vehicleIndex)
 {
     vehicle_colour result;
 
-    // Prevent indexing array out of bounds
-    vehicleIndex = std::min(vehicleIndex, MAX_CARS_PER_TRAIN);
+    if (ride->colour_scheme_type == VEHICLE_COLOUR_SCHEME_PER_VEHICLE)
+    {
+        // Prevent indexing array out of bounds
+        vehicleIndex = std::min(vehicleIndex, MAX_CARS_PER_TRAIN);
+    }
+    else
+    {
+        // In this case, only the first car will be set and the rest will be either black or some residual colour.
+        vehicleIndex = 0;
+    }
 
     result.main = ride->vehicle_colours[vehicleIndex].Body;
     result.additional_1 = ride->vehicle_colours[vehicleIndex].Trim;
