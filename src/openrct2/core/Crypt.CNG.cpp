0cc02f397e32579c4baa08b68e04c15edf3e865a
Ted John <ted@brambles.org>
2020-02-05 08:04:05 +0000

    Fix guards on crypto implementations

--- src/openrct2/core/Crypt.CNG.cpp
+++ src/openrct2/core/Crypt.CNG.cpp
@@ -1,20 +1,13 @@
-#pragma region Copyright(c) 2018 OpenRCT2 Developers
 /*****************************************************************************
- * OpenRCT2, an open source clone of Roller Coaster Tycoon 2.
+ * Copyright (c) 2014-2020 OpenRCT2 developers
  *
- * OpenRCT2 is the work of many authors, a full list can be found in contributors.md
- * For more information, visit https://github.com/OpenRCT2/OpenRCT2
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
  *
- * OpenRCT2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * A full copy of the GNU General Public License can be found in licence.txt
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
-#pragma endregion
 
-#ifdef __USE_CNG__
+#if !defined(DISABLE_NETWORK) && defined(_WIN32)
 
 #    include "../platform/Platform2.h"
 #    include "Crypt.h"
--- src/openrct2/core/Crypt.OpenSSL.cpp
+++ src/openrct2/core/Crypt.OpenSSL.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2014-2019 OpenRCT2 developers
+ * Copyright (c) 2014-2020 OpenRCT2 developers
  *
  * For a complete list of all authors, please refer to contributors.md
  * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
@@ -7,7 +7,7 @@
  * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
 
-#if defined(DISABLE_NETWORK) && !defined(__USE_CNG__)
+#if !defined(DISABLE_NETWORK) && !defined(_WIN32)
 
 #    include "Crypt.h"
 
