OpenRCT2/OpenRCT2

commit 09fad0ef7f15ce074c387d1bd9c27789ff827828

Change rct_viewport to use ScreenCoordsXY

src/openrct2/audio/Audio.cpp
@@ -220,9 +220,9 @@ static AudioParams audio_get_params_from_location(SoundId soundId, const CoordsX
     {
         if (viewport->flags & VIEWPORT_FLAG_SOUND_ON)
         {
-            int16_t vy = pos2.y - viewport->view_y;
-            int16_t vx = pos2.x - viewport->view_x;
-            params.pan = viewport->x + (vx >> viewport->zoom);
+            int16_t vy = pos2.y - viewport->viewPos.x;
+            int16_t vx = pos2.x - viewport->viewPos.y;
+            params.pan = viewport->pos.x + (vx >> viewport->zoom);
             params.volume = SoundVolumeAdjust[static_cast<uint8_t>(soundId)]
                 + ((-1024 * viewport->zoom - 1) * (1 << volumeDown)) + 1;
 

src/openrct2-ui/UiContext.cpp
@@ -794,10 +794,10 @@ private:
                 auto vp = original_w->viewport;
                 if (vp != nullptr)
                 {
-                    left = std::max<int16_t>(left, vp->x);
-                    right = std::min<int16_t>(right, vp->x + vp->width);
-                    top = std::max<int16_t>(top, vp->y);
-                    bottom = std::min<int16_t>(bottom, vp->y + vp->height);
+                    left = std::max<int16_t>(left, vp->pos.x);
+                    right = std::min<int16_t>(right, vp->pos.x + vp->width);
+                    top = std::max<int16_t>(top, vp->pos.y);
+                    bottom = std::min<int16_t>(bottom, vp->pos.y + vp->height);
                     if (left < right && top < bottom)
                     {
                         auto width = right - left;

src/openrct2-ui/interface/InGameConsole.cpp
@@ -242,10 +242,10 @@ void InGameConsole::Update()
             rct_viewport* mainViewport = window_get_viewport(mainWindow);
             if (mainViewport != nullptr)
             {
-                if (_lastMainViewportX != mainViewport->view_x || _lastMainViewportY != mainViewport->view_y)
+                if (_lastMainViewportX != mainViewport->viewPos.x || _lastMainViewportY != mainViewport->viewPos.y)
                 {
-                    _lastMainViewportX = mainViewport->view_x;
-                    _lastMainViewportY = mainViewport->view_y;
+                    _lastMainViewportX = mainViewport->viewPos.x;
+                    _lastMainViewportY = mainViewport->viewPos.y;
 
                     gfx_invalidate_screen();
                 }

src/openrct2-ui/interface/ViewportInteraction.cpp
@@ -627,8 +627,8 @@ static Peep* viewport_interaction_get_closest_peep(ScreenCoordsXY screenCoords, 
     if (viewport == nullptr || viewport->zoom >= 2)
         return nullptr;
 
-    screenCoords.x = ((screenCoords.x - viewport->x) << viewport->zoom) + viewport->view_x;
-    screenCoords.y = ((screenCoords.y - viewport->y) << viewport->zoom) + viewport->view_y;
+    screenCoords.x = ((screenCoords.x - viewport->pos.x) << viewport->zoom) + viewport->viewPos.x;
+    screenCoords.y = ((screenCoords.y - viewport->pos.y) << viewport->zoom) + viewport->viewPos.y;
 
     closestPeep = nullptr;
     closestDistance = 0xFFFF;

src/openrct2-ui/windows/Map.cpp
@@ -926,8 +926,8 @@ static void window_map_centre_on_view_point()
 
     // calculate centre view point of viewport and transform it to minimap coordinates
 
-    cx = ((w->viewport->view_width >> 1) + w->viewport->view_x) >> 5;
-    dx = ((w->viewport->view_height >> 1) + w->viewport->view_y) >> 4;
+    cx = ((w->viewport->view_width >> 1) + w->viewport->viewPos.x) >> 5;
+    dx = ((w->viewport->view_height >> 1) + w->viewport->viewPos.y) >> 4;
     cx += offset.x;
     dx += offset.y;
 
@@ -1132,10 +1132,10 @@ static void window_map_paint_hud_rectangle(rct_drawpixelinfo* dpi)
         return;
 
     auto offset = MiniMapOffsets[get_current_rotation()];
-    int16_t left = (viewport->view_x >> 5) + offset.x;
-    int16_t right = ((viewport->view_x + viewport->view_width) >> 5) + offset.x;
-    int16_t top = (viewport->view_y >> 4) + offset.y;
-    int16_t bottom = ((viewport->view_y + viewport->view_height) >> 4) + offset.y;
+    int16_t left = (viewport->viewPos.x >> 5) + offset.x;
+    int16_t right = ((viewport->viewPos.x + viewport->view_width) >> 5) + offset.x;
+    int16_t top = (viewport->viewPos.y >> 4) + offset.y;
+    int16_t bottom = ((viewport->viewPos.y + viewport->view_height) >> 4) + offset.y;
 
     // top horizontal lines
     gfx_fill_rect(dpi, left, top, left + 3, top, PALETTE_INDEX_56);

src/openrct2-ui/windows/Player.cpp
@@ -456,8 +456,7 @@ void window_player_overview_invalidate(rct_window* w)
     {
         rct_widget* viewportWidget = &window_player_overview_widgets[WIDX_VIEWPORT];
 
-        viewport->x = w->x + viewportWidget->left;
-        viewport->y = w->y + viewportWidget->top;
+        viewport->pos = { w->x + viewportWidget->left, w->y + viewportWidget->top };
         viewport->width = viewportWidget->right - viewportWidget->left;
         viewport->height = viewportWidget->bottom - viewportWidget->top;
         viewport->view_width = viewport->width << viewport->zoom;
@@ -667,8 +666,7 @@ static void window_player_update_viewport(rct_window* w, bool scroll)
                 w->saved_view_y = centreLoc->y;
                 if (!scroll)
                 {
-                    w->viewport->view_x = centreLoc->x;
-                    w->viewport->view_y = centreLoc->y;
+                    w->viewport->viewPos = *centreLoc;
                 }
                 widget_invalidate(w, WIDX_VIEWPORT);
             }

src/openrct2-ui/windows/Viewport.cpp
@@ -99,8 +99,8 @@ rct_window* window_viewport_open()
     if (mainWindow != nullptr)
     {
         rct_viewport* mainViewport = mainWindow->viewport;
-        int32_t x = mainViewport->view_x + (mainViewport->view_width / 2);
-        int32_t y = mainViewport->view_y + (mainViewport->view_height / 2);
+        int32_t x = mainViewport->viewPos.x + (mainViewport->view_width / 2);
+        int32_t y = mainViewport->viewPos.y + (mainViewport->view_height / 2);
         w->saved_view_x = x - (w->viewport->view_width / 2);
         w->saved_view_y = y - (w->viewport->view_height / 2);
     }
@@ -211,8 +211,7 @@ static void window_viewport_invalidate(rct_window* w)
     if (viewport->zoom >= 3)
         w->disabled_widgets |= 1 << WIDX_ZOOM_OUT;
 
-    viewport->x = w->x + viewportWidget->left;
-    viewport->y = w->y + viewportWidget->top;
+    viewport->pos = ScreenCoordsXY{ w->x + viewportWidget->left, w->y + viewportWidget->top };
     viewport->width = viewportWidget->right - viewportWidget->left;
     viewport->height = viewportWidget->bottom - viewportWidget->top;
     viewport->view_width = viewport->width << viewport->zoom;

src/openrct2/cmdline/BenchSpriteSort.cpp
@@ -92,8 +92,7 @@ static std::vector<paint_session> extract_paint_session(const std::string parkFi
         resolutionHeight += 128;
 
         rct_viewport viewport;
-        viewport.x = 0;
-        viewport.y = 0;
+        viewport.pos = { 0, 0 };
         viewport.width = resolutionWidth;
         viewport.height = resolutionHeight;
         viewport.view_width = viewport.width;
@@ -109,8 +108,7 @@ static std::vector<paint_session> extract_paint_session(const std::string parkFi
         x = customY - customX;
         y = ((customX + customY) / 2) - z;
 
-        viewport.view_x = x - ((viewport.view_width) / 2);
-        viewport.view_y = y - ((viewport.view_height) / 2);
+        viewport.viewPos = { x - ((viewport.view_width) / 2), y - ((viewport.view_height) / 2) };
         viewport.zoom = 0;
         gCurrentRotation = 0;
 

commit d3df5539bb039adf2919b87d8cc44b2d02688c06

Fix #10914: Missing sounds, including guest purchase sound.
Regression from 09fad0ef7f15ce074c387d1bd9c27789ff827828 (part of #10854)

src/openrct2/audio/Audio.cpp
@@ -218,8 +218,8 @@ static AudioParams audio_get_params_from_location(SoundId soundId, const CoordsX
     {
         if (viewport->flags & VIEWPORT_FLAG_SOUND_ON)
         {
-            int16_t vy = pos2.y - viewport->viewPos.x;
-            int16_t vx = pos2.x - viewport->viewPos.y;
+            int16_t vy = pos2.y - viewport->viewPos.y;
+            int16_t vx = pos2.x - viewport->viewPos.x;
             params.pan = viewport->pos.x + (vx >> viewport->zoom);
             params.volume = SoundVolumeAdjust[static_cast<uint8_t>(soundId)]
                 + ((-1024 * viewport->zoom - 1) * (1 << volumeDown)) + 1;