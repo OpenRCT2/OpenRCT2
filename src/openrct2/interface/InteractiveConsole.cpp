96e7cccb235ed2678bdabff3de0e3d3bffde67c0
Julian <juli199696@web.de>
2020-04-03 22:39:30 +0200

    Load_Park feature
    New feature "load_park name" from "save" folder.

--- src/openrct2/interface/InteractiveConsole.cpp
+++ src/openrct2/interface/InteractiveConsole.cpp
@@ -39,6 +39,7 @@
 #include "../object/ObjectManager.h"
 #include "../object/ObjectRepository.h"
 #include "../peep/Staff.h"
+#include "../platform/platform.h"
 #include "../ride/Ride.h"
 #include "../ride/RideData.h"
 #include "../util/Util.h"
@@ -1316,6 +1317,19 @@ static int32_t cc_for_date([[maybe_unused]] InteractiveConsole& console, [[maybe
     return 1;
 }
 
+static int32_t cc_load_park([[maybe_unused]] InteractiveConsole& console, [[maybe_unused]] const arguments_t& argv)
+{
+    if (argv.size() > 0)
+    {
+        char savePath[MAX_PATH];
+        platform_get_user_directory(savePath, "save", sizeof(savePath));
+        safe_strcat_path(savePath, argv[0].c_str(), sizeof(savePath));
+        path_append_extension(savePath, ".sv6", sizeof(savePath));
+		context_load_park_from_file(savePath);
+    }
+    return 1;
+}
+
 static int32_t cc_save_park([[maybe_unused]] InteractiveConsole& console, [[maybe_unused]] const arguments_t& argv)
 {
     if (argv.size() < 1)
@@ -1687,6 +1701,8 @@ static constexpr const console_command console_command_table[] = {
                                     "Loading a scenery group will not load its associated objects.\n"
                                     "This is a safer method opposed to \"open object_selection\".",
                                     "load_object <objectfilenodat>" },
+	{ "load_park", cc_load_park, "Load park", "load_park [name]" },
+ 								
     { "object_count", cc_object_count, "Shows the number of objects of each type in the scenario.", "object_count" },
     { "open", cc_open, "Opens the window with the give name.", "open <window>." },
     { "quit", cc_close, "Closes the console.", "quit" },
