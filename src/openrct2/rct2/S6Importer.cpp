174a480870a3107139f95877aaef7b54d7767648
Breno Rodrigues Guimarães <brenorg@gmail.com>
2020-04-15 02:17:43 -0300

    Use utf8_remove_format_codes to remove format codes from user strings (#11289)
    The previous code used RCT12::RemoveFormatCodes to remove format codes from RCT strings. But that function uses utf8 methods to identify format codes. In this new version, rct2_to_utf8 is called first to obtain an utf8 string, and then utf8_remove_format_codes can be used.

--- src/openrct2/rct2/S6Importer.cpp
+++ src/openrct2/rct2/S6Importer.cpp
@@ -1631,8 +1631,9 @@ public:
     {
         const auto originalString = _s6.custom_strings[(stringId - USER_STRING_START) % 1024];
         std::string_view originalStringView(originalString, USER_STRING_MAX_LENGTH);
-        auto withoutFormatCodes = RCT12::RemoveFormatCodes(originalStringView);
-        return rct2_to_utf8(withoutFormatCodes, RCT2_LANGUAGE_ID_ENGLISH_UK);
+        auto asUtf8 = rct2_to_utf8(originalStringView, RCT2_LANGUAGE_ID_ENGLISH_UK);
+        utf8_remove_format_codes(asUtf8.data(), /*allow colour*/ false);
+        return asUtf8.data();
     }
 
     std::vector<rct_object_entry> GetRequiredObjects()
