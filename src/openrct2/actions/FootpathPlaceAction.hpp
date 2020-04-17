98199d1ccc6f519582f8e5301c92b0080994afb3
frutiemax <lucas.malo.belanger@gmail.com>
2020-04-17 19:39:08 -0400

    Lamps won't get removed if on a queue line.

--- src/openrct2/actions/FootpathPlaceAction.hpp
+++ src/openrct2/actions/FootpathPlaceAction.hpp
@@ -191,8 +191,8 @@ private:
         {
             if (isQueue)
             {
-                // remove any addition that isn't a TV
-                if ((elem->path_bit.flags & PATH_BIT_FLAG_IS_QUEUE_SCREEN) == 0)
+                // remove any addition that isn't a TV or a lamp
+                if ((elem->path_bit.flags & PATH_BIT_FLAG_IS_QUEUE_SCREEN) == 0 && (elem->path_bit.flags & PATH_BIT_FLAG_LAMP) == 0)
                 {
                     pathElement->SetIsBroken(false);
                     pathElement->SetAddition(0);
