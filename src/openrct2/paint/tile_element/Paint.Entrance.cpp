OpenRCT2/OpenRCT2

commit a0619d04cfa9035d0b513766e7aeba9a7ae57224

Replace usage of set_format_arg by Formatter::Common() (#11412)

src/openrct2/paint/tile_element/Paint.Entrance.cpp
@@ -165,16 +165,17 @@ static void ride_entrance_exit_paint(paint_session* session, uint8_t direction, 
     if (!is_exit && !(tile_element->IsGhost()) && tile_element->AsEntrance()->GetRideIndex() != RIDE_ID_NULL
         && stationObj->ScrollingMode != SCROLLING_MODE_NONE)
     {
-        set_format_arg(0, rct_string_id, STR_RIDE_ENTRANCE_NAME);
-        set_format_arg(4, uint32_t, 0);
+        auto ft = Formatter::Common();
+        ft.Add<rct_string_id>(STR_RIDE_ENTRANCE_NAME);
+        ft.Add<uint32_t>(0);
 
         if (ride->status == RIDE_STATUS_OPEN && !(ride->lifecycle_flags & RIDE_LIFECYCLE_BROKEN_DOWN))
         {
-            ride->FormatNameTo(gCommonFormatArgs + 2);
+            ride->FormatNameTo(ft);
         }
         else
         {
-            set_format_arg(2, rct_string_id, STR_RIDE_ENTRANCE_CLOSED);
+            ft.Add<rct_string_id>(STR_RIDE_ENTRANCE_CLOSED);
         }
 
         utf8 entrance_string[256];
@@ -270,20 +271,20 @@ static void park_entrance_paint(paint_session* session, uint8_t direction, int32
                 break;
 
             {
-                set_format_arg(0, uint32_t, 0);
-                set_format_arg(4, uint32_t, 0);
+                Formatter::Common().Add<uint32_t>(0).Add<uint32_t>(0);
+                auto ft = Formatter::Common();
 
                 if (gParkFlags & PARK_FLAGS_PARK_OPEN)
                 {
                     const auto& park = OpenRCT2::GetContext()->GetGameState()->GetPark();
                     auto name = park.Name.c_str();
-                    set_format_arg(0, rct_string_id, STR_STRING);
-                    set_format_arg(2, const char*, name);
+                    ft.Add<rct_string_id>(STR_STRING);
+                    ft.Add<const char*>(name);
                 }
                 else
                 {
-                    set_format_arg(0, rct_string_id, STR_BANNER_TEXT_CLOSED);
-                    set_format_arg(2, uint32_t, 0);
+                    ft.Add<rct_string_id>(STR_BANNER_TEXT_CLOSED);
+                    ft.Add<uint32_t>(0);
                 }
 
                 utf8 park_name[256];

src/openrct2-ui/windows/TrackList.cpp
@@ -453,7 +453,7 @@ static void window_track_list_invalidate(rct_window* w)
         stringId = rideName.name;
     }
 
-    set_format_arg(0, rct_string_id, stringId);
+    Formatter::Common().Add<rct_string_id>(stringId);
     if (gScreenFlags & SCREEN_FLAGS_TRACK_MANAGER)
     {
         window_track_list_widgets[WIDX_TITLE].text = STR_TRACK_DESIGNS;
@@ -622,8 +622,9 @@ static void window_track_list_paint(rct_window* w, rct_drawpixelinfo* dpi)
             }
 
             // Ride length
-            set_format_arg(0, rct_string_id, STR_RIDE_LENGTH_ENTRY);
-            set_format_arg(2, uint16_t, _loadedTrackDesign->ride_length);
+            auto ft = Formatter::Common();
+            ft.Add<rct_string_id>(STR_RIDE_LENGTH_ENTRY);
+            ft.Add<uint16_t>(_loadedTrackDesign->ride_length);
             gfx_draw_string_left_clipped(dpi, STR_TRACK_LIST_RIDE_LENGTH, gCommonFormatArgs, COLOUR_BLACK, x, y, 214);
             y += LIST_ROW_HEIGHT;
         }
@@ -683,15 +684,16 @@ static void window_track_list_paint(rct_window* w, rct_drawpixelinfo* dpi)
     if (_loadedTrackDesign->space_required_x != 0xFF)
     {
         // Space required
-        set_format_arg(0, uint16_t, _loadedTrackDesign->space_required_x);
-        set_format_arg(2, uint16_t, _loadedTrackDesign->space_required_y);
+        auto ft = Formatter::Common();
+        ft.Add<uint16_t>(_loadedTrackDesign->space_required_x);
+        ft.Add<uint16_t>(_loadedTrackDesign->space_required_y);
         gfx_draw_string_left(dpi, STR_TRACK_LIST_SPACE_REQUIRED, gCommonFormatArgs, COLOUR_BLACK, x, y);
         y += LIST_ROW_HEIGHT;
     }
 
     if (_loadedTrackDesign->cost != 0)
     {
-        set_format_arg(0, uint32_t, _loadedTrackDesign->cost);
+        Formatter::Common().Add<uint32_t>(_loadedTrackDesign->cost);
         gfx_draw_string_left(dpi, STR_TRACK_LIST_COST_AROUND, gCommonFormatArgs, COLOUR_BLACK, x, y);
     }
 }
@@ -755,8 +757,9 @@ static void window_track_list_scrollpaint(rct_window* w, rct_drawpixelinfo* dpi,
             }
 
             // Draw track name
-            set_format_arg(0, rct_string_id, STR_TRACK_LIST_NAME_FORMAT);
-            set_format_arg(2, utf8*, _trackDesigns[i].name);
+            auto ft = Formatter::Common();
+            ft.Add<rct_string_id>(STR_TRACK_LIST_NAME_FORMAT);
+            ft.Add<utf8*>(_trackDesigns[i].name);
             gfx_draw_string_left(dpi, stringId, gCommonFormatArgs, COLOUR_BLACK, x, y - 1);
         }
 

src/openrct2-ui/windows/Viewport.cpp
@@ -201,7 +201,7 @@ static void window_viewport_invalidate(rct_window* w)
     }
 
     // Set title
-    set_format_arg(0, uint32_t, w->number);
+    Formatter::Common().Add<uint32_t>(w->number);
 
     // Set disabled widgets
     w->disabled_widgets = 0;

src/openrct2/Context.cpp
@@ -638,7 +638,7 @@ namespace OpenRCT2
                         }
 
                         auto windowManager = _uiContext->GetWindowManager();
-                        set_format_arg(0, uint16_t, e.Flag);
+                        Formatter::Common().Add<uint16_t>(e.Flag);
                         windowManager->ShowError(STR_FAILED_TO_LOAD_IMCOMPATIBLE_RCTC_FLAG, STR_NONE);
                     }
                     catch (const std::exception& e)

src/openrct2/EditorObjectSelectionSession.cpp
@@ -506,7 +506,8 @@ int32_t window_editor_object_selection_select_object(uint8_t bh, int32_t flags, 
         {
             char objectName[64];
             object_create_identifier_name(objectName, 64, &item->ObjectEntry);
-            set_format_arg(0, const char*, objectName);
+            auto ft = Formatter::Common();
+            ft.Add<const char*>(objectName);
             set_object_selection_error(bh, STR_OBJECT_SELECTION_ERR_SHOULD_SELECT_X_FIRST);
             return 0;
         }

src/openrct2/Game.cpp
@@ -462,7 +462,8 @@ void game_fix_save_vars()
                 peep->current_ride = RIDE_ID_NULL;
                 continue;
             }
-            set_format_arg(0, uint32_t, peep->id);
+            auto ft = Formatter::Common();
+            ft.Add<uint32_t>(peep->id);
             auto curName = peep->GetName();
             log_warning(
                 "Peep %u (%s) has invalid ride station = %u for ride %u.", spriteIndex, curName.c_str(), srcStation, rideIdx);

src/openrct2/interface/Screenshot.cpp
@@ -429,8 +429,9 @@ void screenshot_giant()
         WriteDpiToFile(path->c_str(), &dpi, renderedPalette);
 
         // Show user that screenshot saved successfully
-        set_format_arg(0, rct_string_id, STR_STRING);
-        set_format_arg(2, char*, path_get_filename(path->c_str()));
+        auto ft = Formatter::Common();
+        ft.Add<rct_string_id>(STR_STRING);
+        ft.Add<char*>(path_get_filename(path->c_str()));
         context_show_error(STR_SCREENSHOT_SAVED_AS, STR_NONE);
     }
     catch (const std::exception& e)

src/openrct2/management/Marketing.cpp
@@ -70,18 +70,19 @@ static void marketing_raise_finished_notification(const MarketingCampaign& campa
 {
     if (gConfigNotifications.park_marketing_campaign_finished)
     {
+        auto ft = Formatter::Common();
         // This sets the string parameters for the marketing types that have an argument.
         if (campaign.Type == ADVERTISING_CAMPAIGN_RIDE_FREE || campaign.Type == ADVERTISING_CAMPAIGN_RIDE)
         {
             auto ride = get_ride(campaign.RideId);
             if (ride != nullptr)
             {
-                ride->FormatNameTo(gCommonFormatArgs);
+                ride->FormatNameTo(ft);
             }
         }
         else if (campaign.Type == ADVERTISING_CAMPAIGN_FOOD_OR_DRINK_FREE)
         {
-            set_format_arg(0, rct_string_id, ShopItems[campaign.ShopItemType].Naming.Plural);
+            ft.Add<rct_string_id>(ShopItems[campaign.ShopItemType].Naming.Plural);
         }
 
         news_item_add_to_queue(NEWS_ITEM_MONEY, MarketingCampaignNames[campaign.Type][2], 0);

commit 305b4d49576e093efce0d7fd650fb8265435fb87

Fix #11469: Clear memory but dont increment buffer position (#11472)
Fix a regression from #11412 that did:

set_format_arg(0, rct_string_id, ...)
set_format_arg(4, uint32_t, 0)

ride->FormatNameTo(gCommonFormatArgs + 2)

Notice it continues from byte 2, and not 8.
So the `set_format_arg(4, uint32_t... )` was just
for clearing memory, not for adding a new argument

src/openrct2/paint/tile_element/Paint.Entrance.cpp
@@ -165,9 +165,11 @@ static void ride_entrance_exit_paint(paint_session* session, uint8_t direction, 
     if (!is_exit && !(tile_element->IsGhost()) && tile_element->AsEntrance()->GetRideIndex() != RIDE_ID_NULL
         && stationObj->ScrollingMode != SCROLLING_MODE_NONE)
     {
+        // clear next 8 bytes
+        Formatter::Common().Add<uint32_t>(0).Add<uint32_t>(0);
+
         auto ft = Formatter::Common();
         ft.Add<rct_string_id>(STR_RIDE_ENTRANCE_NAME);
-        ft.Add<uint32_t>(0);
 
         if (ride->status == RIDE_STATUS_OPEN && !(ride->lifecycle_flags & RIDE_LIFECYCLE_BROKEN_DOWN))
         {