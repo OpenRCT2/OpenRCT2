1b5253494c647900fb1e55a1f897692ac04dfb5a
modfort <mehdi-098@outlook.fr>
2020-05-07 17:00:17 +0200

    Part of #11571: comment the last function Part of #11571

--- src/openrct2/drawing/Drawing.h
+++ src/openrct2/drawing/Drawing.h
@@ -14,7 +14,6 @@
 #include "../interface/Colour.h"
 #include "../interface/ZoomLevel.hpp"
 
-
 #include <vector>
 struct ScreenCoordsXY;
 
@@ -535,7 +534,7 @@ void gfx_draw_string(rct_drawpixelinfo* dpi, const_utf8string buffer, uint8_t co
 
 void gfx_draw_string_left(
     rct_drawpixelinfo* dpi, rct_string_id format, void* args, uint8_t colour, const ScreenCoordsXY& coords);
-void gfx_draw_string_left(rct_drawpixelinfo* dpi, rct_string_id format, void* args, uint8_t colour, int32_t x, int32_t y);
+// void gfx_draw_string_left(rct_drawpixelinfo* dpi, rct_string_id format, void* args, uint8_t colour, int32_t x, int32_t y);
 void gfx_draw_string_centred(
     rct_drawpixelinfo* dpi, rct_string_id format, int32_t x, int32_t y, uint8_t colour, const void* args);
 void gfx_draw_string_right(rct_drawpixelinfo* dpi, rct_string_id format, void* args, uint8_t colour, int32_t x, int32_t y);
