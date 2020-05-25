3fdab8e7d9563cf6d2a413109a4c869aba2ab57c
Michiel Bos <michielbos97@hotmail.com>
2020-05-25 20:18:39 +0200

    Added icon to Cheats button to indicate clearance checks are disabled

--- src/openrct2/localisation/StringIds.h
+++ src/openrct2/localisation/StringIds.h
@@ -3907,6 +3907,7 @@ enum
     STR_PATH_TO_RCT1_IS_WRONG_VERSION = 6372,
 
     STR_SHORTCUT_TOGGLE_CLEARANCE_CHECKS = 6373,
+    STR_ICON_CLEARANCE_CHECKS_DISABLED = 6374,
 
     // Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
     /* MAX_STR_COUNT = 32768 */ // MAX_STR_COUNT - upper limit for number of strings, not the current count strings
