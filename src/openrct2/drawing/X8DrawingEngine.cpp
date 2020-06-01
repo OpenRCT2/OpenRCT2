3552694d78bbb442038116786da5195daa237ac8
Ted John <ted@brambles.org>
2020-06-01 19:45:01 +0100

    Fix 11845: Disabled icons are not shown as disabled

--- src/openrct2/drawing/X8DrawingEngine.cpp
+++ src/openrct2/drawing/X8DrawingEngine.cpp
@@ -740,8 +740,11 @@ void X8DrawingContext::DrawSpriteRawMasked(int32_t x, int32_t y, uint32_t maskIm
 
 void X8DrawingContext::DrawSpriteSolid(uint32_t image, int32_t x, int32_t y, uint8_t colour)
 {
+    uint8_t palette[256];
+    std::fill_n(palette, sizeof(palette), colour);
+    palette[0] = 0;
     gfx_draw_sprite_palette_set_software(
-        _dpi, ImageId::FromUInt32((image & 0x7FFFF) | IMAGE_TYPE_REMAP), x, y, PaletteMap::GetDefault());
+        _dpi, ImageId::FromUInt32((image & 0x7FFFF) | IMAGE_TYPE_REMAP), x, y, PaletteMap(palette));
 }
 
 void X8DrawingContext::DrawGlyph(uint32_t image, int32_t x, int32_t y, const PaletteMap& paletteMap)
