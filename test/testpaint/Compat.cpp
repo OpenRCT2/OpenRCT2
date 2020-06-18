22166db30d6ae1fab0167d43664fe05147451c39
Gymnasiast <m.o.steenbeek@gmail.com>
2020-06-18 16:25:51 +0200

    Fix compilation on TestPaint

--- test/testpaint/Compat.cpp
+++ test/testpaint/Compat.cpp
@@ -528,3 +528,319 @@ bool track_type_is_station(track_type_t trackType)
             return false;
     }
 }
+
+void ride_ratings_calculate_spiral_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_stand_up_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_suspended_swinging_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_inverted_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_junior_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_miniature_railway([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_monorail([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_mini_suspended_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_boat_hire([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_wooden_wild_mouse([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_steeplechase([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_car_ride([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_launched_freefall([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_bobsleigh_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_observation_tower([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_looping_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_dinghy_slide([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_mine_train_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_chairlift([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_corkscrew_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_maze([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_spiral_slide([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_go_karts([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_log_flume([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_river_rapids([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_dodgems([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_pirate_ship([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_inverter_ship([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_food_stall([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_shop([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_merry_go_round([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_information_kiosk([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_toilets([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_ferris_wheel([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_motion_simulator([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_3d_cinema([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_top_spin([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_space_rings([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_reverse_freefall_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_lift([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_vertical_drop_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_cash_machine([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_twist([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_haunted_house([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_first_aid([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_circus_show([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_ghost_train([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_twister_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_wooden_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_side_friction_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_wild_mouse([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_multi_dimension_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_flying_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_virginia_reel([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_splash_boats([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_mini_helicopters([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_lay_down_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_suspended_monorail([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_reverser_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_heartline_twister_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_mini_golf([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_giga_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_roto_drop([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_flying_saucers([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_crooked_house([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_monorail_cycles([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_compact_inverted_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_water_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_air_powered_vertical_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_inverted_hairpin_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_magic_carpet([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_submarine_ride([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_river_rafts([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_enterprise([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_inverted_impulse_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_mini_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_mine_ride([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_lim_launched_roller_coaster([[maybe_unused]] Ride* ride)
+{
+}
+
+void ride_ratings_calculate_drink_stall([[maybe_unused]] Ride* ride)
+{
+}
