ea9dd60b91a98be9b4291957f905831043866254
Hudson Oliveira <35645457+hdpoliveira@users.noreply.github.com>
2020-06-19 14:06:21 -0300

    Part of #9473: Create Vehicle::UpdateGoKartAttemptSwitchLanes (#11995)

--- src/openrct2/ride/Vehicle.h
+++ src/openrct2/ride/Vehicle.h
@@ -425,6 +425,7 @@ private:
     void UpdateTrackMotionMiniGolfVehicle(Ride* curRide, rct_ride_entry* rideEntry, rct_ride_entry_vehicle* vehicleEntry);
     bool UpdateTrackMotionForwardsGetNewTrack(uint16_t trackType, Ride* curRide, rct_ride_entry* rideEntry);
     bool UpdateTrackMotionBackwardsGetNewTrack(uint16_t trackType, Ride* curRide, uint16_t* progress);
+    void UpdateGoKartAttemptSwitchLanes();
 };
 
 struct train_ref
