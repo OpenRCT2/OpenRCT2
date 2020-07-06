9b6f258a42551a7a62a54d876b1483afc005b056
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2020-07-06 21:57:32 +0100

    Use ternary

--- src/openrct2/world/Sprite.h
+++ src/openrct2/world/Sprite.h
@@ -196,17 +196,13 @@ rct_sprite* get_sprite(size_t sprite_idx);
 template<typename T = SpriteBase> T* GetEntity(size_t sprite_idx)
 {
     auto spr = reinterpret_cast<SpriteBase*>(get_sprite(sprite_idx));
-    if (spr == nullptr)
-        return nullptr;
-    return spr->As<T>();
+    return spr != nullptr ? spr->As<T>() : nullptr;
 }
 
 template<typename T = SpriteBase> T* TryGetEntity(size_t sprite_idx)
 {
     auto spr = reinterpret_cast<SpriteBase*>(try_get_sprite(sprite_idx));
-    if (spr == nullptr)
-        return nullptr;
-    return spr->As<T>();
+    return spr != nullptr ? spr->As<T>() : nullptr;
 }
 
 uint16_t GetEntityListCount(EntityListId list);
