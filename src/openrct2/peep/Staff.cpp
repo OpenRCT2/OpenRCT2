3f037ba401d7598f5052a4d3636089830aa1d128
Jimver <jim.verheijde@hotmail.com>
2020-07-07 13:56:26 +0200

    Refactor hex variable probabilities

--- src/openrct2/peep/Staff.cpp
+++ src/openrct2/peep/Staff.cpp
@@ -643,9 +643,9 @@ bool Staff::DoHandymanPathFinding()
                 bool chooseRandom = true;
                 if (litterDirection != INVALID_DIRECTION && pathDirections & (1 << litterDirection))
                 {
-                    /// When in a queue path make the probability of following litter much lower
+                    /// When in a queue path make the probability of following litter much lower (10% instead of 90%)
                     /// as handymen often get stuck when there is litter on a normal path next to a queue they are in
-                    uint32_t chooseRandomProbability = pathElement->IsQueue() ? (0xFFFF - 0x1999) : 0x1999;
+                    uint32_t chooseRandomProbability = pathElement->IsQueue() ? 0xE666 : 0x1999;
                     if ((scenario_rand() & 0xFFFF) >= chooseRandomProbability)
                     {
                         chooseRandom = false;
