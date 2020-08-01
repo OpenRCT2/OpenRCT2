ddd21fa87a1f4cfc420e91a79d32370212adab27
annoyingdroid <48995374+annoyingdroid@users.noreply.github.com>
2020-08-01 13:40:01 -0400

    Part of #12098: Use ScreenRect on gfx_filter_rect (#12134)

--- src/openrct2-ui/interface/Window.cpp
+++ src/openrct2-ui/interface/Window.cpp
@@ -587,7 +587,7 @@ void window_draw_widgets(rct_window* w, rct_drawpixelinfo* dpi)
 
     if ((w->flags & WF_TRANSPARENT) && !(w->flags & WF_NO_BACKGROUND))
         gfx_filter_rect(
-            dpi, w->windowPos.x, w->windowPos.y, w->windowPos.x + w->width - 1, w->windowPos.y + w->height - 1, PALETTE_51);
+            dpi, { w->windowPos, w->windowPos + ScreenCoordsXY{ w->width - 1, w->windowPos.y + w->height - 1 } }, PALETTE_51);
 
     // todo: some code missing here? Between 006EB18C and 006EB260
 
