d98838430c141a937c04d298be4e04fedb73b27a
ceeac <ceeac@users.noreply.github.com>
2020-08-14 17:03:08 +0200

    Fix wrong CCache version string (#12679)
    Use CMAKE_MATCH_1 instead of CMAKE_MATCH_0 which holds the entire match.

--- cmake/FindCCache.cmake
+++ cmake/FindCCache.cmake
@@ -9,8 +9,9 @@ if (CCache_EXECUTABLE)
         OUTPUT_VARIABLE CCache_VERSION_OUTPUT
     )
 
-    string(REGEX MATCH "version ([0-9]+\\.[0-9]+\\.[0-9]+)" CCache_VERSION_TEMP ${CCache_VERSION_OUTPUT})
-    set(CCache_VERSION "${CMAKE_MATCH_0}")
+    if (CCache_VERSION_OUTPUT MATCHES "version ([0-9]+\\.[0-9]+\\.[0-9]+)")
+        set(CCache_VERSION "${CMAKE_MATCH_1}")
+    endif ()
 endif (CCache_EXECUTABLE)
 
 find_package_handle_standard_args(CCache
