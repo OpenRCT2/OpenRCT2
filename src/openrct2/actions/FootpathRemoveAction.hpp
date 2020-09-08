e0a6e0b832cf3d41d0f632a44451739287de0eaa
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2020-09-08 18:44:38 +0100

    Add visitors for all scenery placement and removes
    This should allow greater flexibility for plugin developers

--- src/openrct2/actions/FootpathRemoveAction.hpp
+++ src/openrct2/actions/FootpathRemoveAction.hpp
@@ -34,6 +34,11 @@ public:
     {
     }
 
+    void AcceptParameters(GameActionParameterVisitor & visitor) override
+    {
+        visitor.Visit(_loc);
+    }
+
     uint16_t GetActionFlags() const override
     {
         return GameAction::GetActionFlags();
--- src/openrct2/actions/WallPlaceAction.hpp
+++ src/openrct2/actions/WallPlaceAction.hpp
@@ -84,6 +84,23 @@ public:
         }
     }
 
+    void AcceptParameters(GameActionParameterVisitor & visitor) override
+    {
+        visitor.Visit(_loc);
+        visitor.Visit("object", _wallType);
+        visitor.Visit("edge", _edge);
+        visitor.Visit("primaryColour", _primaryColour);
+        visitor.Visit("secondaryColour", _secondaryColour);
+        visitor.Visit("tertiaryColour", _tertiaryColour);
+        rct_scenery_entry* sceneryEntry = get_large_scenery_entry(_wallType);
+        if (sceneryEntry != nullptr)
+        {
+            if (sceneryEntry->large_scenery.scrolling_mode != SCROLLING_MODE_NONE)
+            {
+                _bannerId = create_new_banner(0);
+            }
+        }
+    }
     uint16_t GetActionFlags() const override
     {
         return GameAction::GetActionFlags();
