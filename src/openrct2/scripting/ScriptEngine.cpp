21b9dee249ee7c5a49aaecb0c3d92f290563f7f0
tombomp <tombomp@gmail.com>
2020-09-09 12:45:20 +0100

    Increment OPENRCT2_PLUGIN_API_VERSION

--- src/openrct2/scripting/ScriptEngine.cpp
+++ src/openrct2/scripting/ScriptEngine.cpp
@@ -42,7 +42,7 @@
 using namespace OpenRCT2;
 using namespace OpenRCT2::Scripting;
 
-static constexpr int32_t OPENRCT2_PLUGIN_API_VERSION = 4;
+static constexpr int32_t OPENRCT2_PLUGIN_API_VERSION = 5;
 
 struct ExpressionStringifier final
 {
