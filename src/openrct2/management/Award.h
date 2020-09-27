e9443ff49c152ec02bc223ce4fa45edcb9a8973a
gsckoco <gsckmp4@outlook.com>
2020-09-27 23:20:02 +0100

    Refactor PARK_AWARD to use strong enum

--- src/openrct2/management/Award.h
+++ src/openrct2/management/Award.h
@@ -17,26 +17,25 @@ struct Award
     uint16_t Type;
 };
 
-enum PARK_AWARD
-{
-    PARK_AWARD_MOST_UNTIDY,
-    PARK_AWARD_MOST_TIDY,
-    PARK_AWARD_BEST_ROLLERCOASTERS,
-    PARK_AWARD_BEST_VALUE,
-    PARK_AWARD_MOST_BEAUTIFUL,
-    PARK_AWARD_WORST_VALUE,
-    PARK_AWARD_SAFEST,
-    PARK_AWARD_BEST_STAFF,
-    PARK_AWARD_BEST_FOOD,
-    PARK_AWARD_WORST_FOOD,
-    PARK_AWARD_BEST_RESTROOMS,
-    PARK_AWARD_MOST_DISAPPOINTING,
-    PARK_AWARD_BEST_WATER_RIDES,
-    PARK_AWARD_BEST_CUSTOM_DESIGNED_RIDES,
-    PARK_AWARD_MOST_DAZZLING_RIDE_COLOURS,
-    PARK_AWARD_MOST_CONFUSING_LAYOUT,
-    PARK_AWARD_BEST_GENTLE_RIDES,
-    PARK_AWARD_COUNT
+enum class ParkAward : uint8_t {
+    MostUntidy,
+    MostTidy,
+    BestRollercoasters, // BestRollercoasters or BestRollerCoasters?
+    BestValue,
+    MostBeautiful,
+    WorstValue,
+    Safest,
+    BestStaff,
+    BestFood,
+    WorstFood,
+    BestRestrooms,
+    MostDisappointing,
+    BestWaterRides,
+    BestCustomDesignedRides,
+    MostDazzlingRideColours,
+    MostConfusingLayout,
+    BestGentleRides,
+    Count
 };
 
 #define MAX_AWARDS 4
