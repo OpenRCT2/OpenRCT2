e3e5e90420cae1e28c28b5f484abbee97b45e445
Tulio Leao <tupaschoal@gmail.com>
2020-10-07 23:56:27 -0300

    Fix #4041: Garbled park option on scenario editor with custom theme

--- src/openrct2-ui/windows/EditorScenarioOptions.cpp
+++ src/openrct2-ui/windows/EditorScenarioOptions.cpp
@@ -1351,7 +1351,6 @@ static void window_editor_scenario_options_park_paint(rct_window* w, rct_drawpix
         // Pay for park or rides label
         screenCoords = w->windowPos
             + ScreenCoordsXY{ w->widgets[WIDX_PAY_FOR_PARK_OR_RIDES].left + 1, w->widgets[WIDX_PAY_FOR_PARK_OR_RIDES].top };
-        gfx_draw_string_left(dpi, STR_FREE_PARK_ENTER, nullptr, COLOUR_BLACK, screenCoords);
 
         // Pay for park and/or rides value
         if (gParkFlags & PARK_FLAGS_UNLOCK_ALL_PRICES)
@@ -1361,6 +1360,7 @@ static void window_editor_scenario_options_park_paint(rct_window* w, rct_drawpix
         else
             stringId = STR_PAY_PARK_ENTER;
 
+        gfx_draw_string_left(dpi, stringId, nullptr, COLOUR_BLACK, screenCoords);
         gfx_draw_string_left(dpi, STR_WINDOW_COLOUR_2_STRINGID, &stringId, COLOUR_BLACK, screenCoords);
     }
 
