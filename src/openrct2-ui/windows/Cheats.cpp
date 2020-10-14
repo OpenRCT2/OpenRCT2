8d53cc63390705a41baaef449c447a787cd2ddbf
Aaron van Geffen <aaron@aaronweb.net>
2020-10-14 18:13:46 +0200

    WIP: split off objective group

--- src/openrct2-ui/windows/Cheats.cpp
+++ src/openrct2-ui/windows/Cheats.cpp
@@ -133,13 +133,14 @@ enum WINDOW_CHEATS_WIDGET_IDX
     WIDX_CREATE_DUCKS,
     WIDX_OWN_ALL_LAND,
     WIDX_REMOVE_DUCKS,
+    WIDX_OBJECTIVE_GROUP,
+    WIDX_NEVERENDING_MARKETING,
     WIDX_FORCE_PARK_RATING,
     WIDX_PARK_RATING_SPINNER,
     WIDX_INCREASE_PARK_RATING,
     WIDX_DECREASE_PARK_RATING,
     WIDX_WIN_SCENARIO,
     WIDX_HAVE_FUN,
-    WIDX_NEVERENDING_MARKETING,
     WIDX_WEATHER_GROUP,
     WIDX_FREEZE_WEATHER,
     WIDX_WEATHER,
@@ -258,16 +259,17 @@ static rct_widget window_cheats_guests_widgets[] =
 static rct_widget window_cheats_misc_widgets[] =
 {
     MAIN_CHEATS_WIDGETS,
-    MakeWidget        ({  5,  48}, {238, 169},   WWT_GROUPBOX, WindowColour::Secondary, STR_CHEAT_GENERAL_GROUP                                             ), // General group
+    MakeWidget        ({  5,  48}, {238,  60},   WWT_GROUPBOX, WindowColour::Secondary, STR_CHEAT_GENERAL_GROUP                                             ), // General group
     MakeWidget        ({ 11,  62}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CHEAT_OPEN_PARK,             STR_CHEAT_OPEN_PARK_TIP            ), // open / close park
     MakeWidget        ({ 11,  83}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CREATE_DUCKS,                STR_CREATE_DUCKS_TIP               ), // Create ducks
     MakeWidget        ({127,  62}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CHEAT_OWN_ALL_LAND,          STR_CHEAT_OWN_ALL_LAND_TIP         ), // Own all land
     MakeWidget        ({127,  83}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_REMOVE_DUCKS,                STR_REMOVE_DUCKS_TIP               ), // Remove ducks
-    MakeWidget        ({ 11, 153}, CHEAT_BUTTON, WWT_CHECKBOX, WindowColour::Secondary, STR_FORCE_PARK_RATING                                               ), // Force park rating
-    MakeSpinnerWidgets({126, 155}, {111,  12},   WWT_SPINNER,  WindowColour::Secondary                                                                      ), // park rating (3 widgets)
-    MakeWidget        ({ 11, 174}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CHEAT_WIN_SCENARIO                                              ), // Win scenario
-    MakeWidget        ({127, 174}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CHEAT_HAVE_FUN                                                  ), // Have fun!
-    MakeWidget        ({ 11, 195}, CHEAT_CHECK,  WWT_CHECKBOX, WindowColour::Secondary, STR_CHEAT_NEVERENDING_MARKETING, STR_CHEAT_NEVERENDING_MARKETING_TIP), // never ending marketing campaigns
+    MakeWidget        ({  5, 112}, {238,  75},   WWT_GROUPBOX, WindowColour::Secondary, STR_CHEAT_GENERAL_GROUP                                             ), // General group
+    MakeWidget        ({ 11, 127}, CHEAT_CHECK,  WWT_CHECKBOX, WindowColour::Secondary, STR_CHEAT_NEVERENDING_MARKETING, STR_CHEAT_NEVERENDING_MARKETING_TIP), // never ending marketing campaigns
+    MakeWidget        ({ 11, 141}, CHEAT_BUTTON, WWT_CHECKBOX, WindowColour::Secondary, STR_FORCE_PARK_RATING                                               ), // Force park rating
+    MakeSpinnerWidgets({126, 142}, {111,  14},   WWT_SPINNER,  WindowColour::Secondary                                                                      ), // park rating (3 widgets)
+    MakeWidget        ({ 11, 162}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CHEAT_WIN_SCENARIO                                              ), // Win scenario
+    MakeWidget        ({127, 162}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CHEAT_HAVE_FUN                                                  ), // Have fun!
     MakeWidget        ({  5, 221}, {238,  64},   WWT_GROUPBOX, WindowColour::Secondary, STR_CHEAT_WEATHER_GROUP                                             ), // Weather group
     MakeWidget        ({ 11, 237}, CHEAT_BUTTON, WWT_BUTTON,   WindowColour::Secondary, STR_CHEAT_FREEZE_WEATHER,        STR_CHEAT_FREEZE_WEATHER_TIP       ), // Freeze weather
     MakeWidget        ({126, 258}, {111,  14},   WWT_DROPDOWN, WindowColour::Secondary, STR_NONE,                        STR_FORCE_WEATHER_TOOLTIP          ), // Force weather
@@ -1144,8 +1146,10 @@ static void window_cheats_paint(rct_window* w, rct_drawpixelinfo* dpi)
         gfx_draw_string_left(dpi, STR_FORCE_WEATHER, nullptr, COLOUR_BLACK, w->windowPos + ScreenCoordsXY{ X_LCOL, 261 });
         auto ft = Formatter();
         ft.Add<int32_t>(_parkRatingSpinnerValue);
+
+        auto& widget = w->widgets[WIDX_PARK_RATING_SPINNER];
         DrawTextBasic(
-            dpi, w->windowPos + ScreenCoordsXY{ X_RCOL, 156 }, STR_FORMAT_INTEGER, ft, w->colours[1], TextAlignment::RIGHT);
+            dpi, w->windowPos + ScreenCoordsXY{ widget.left + 1, widget.top + 1 }, STR_FORMAT_INTEGER, ft, w->colours[1]);
     }
     else if (w->page == WINDOW_CHEATS_PAGE_GUESTS)
     {
