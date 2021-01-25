4d8faee062c704adaeadcf6c6f4c1fd9771dae7f
Ted John <ted@brambles.org>
2021-01-25 02:32:42 +0000

    Increment plugin API version

--- src/openrct2/scripting/ScriptEngine.cpp
+++ src/openrct2/scripting/ScriptEngine.cpp
@@ -44,7 +44,7 @@
 using namespace OpenRCT2;
 using namespace OpenRCT2::Scripting;
 
-static constexpr int32_t OPENRCT2_PLUGIN_API_VERSION = 18;
+static constexpr int32_t OPENRCT2_PLUGIN_API_VERSION = 19;
 
 struct ExpressionStringifier final
 {
