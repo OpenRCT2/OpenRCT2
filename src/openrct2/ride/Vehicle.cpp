11349cb4da09cbf56cba988aff6674bf89c38916
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2021-02-02 16:43:44 +0000

    Increment network version

--- src/openrct2/ride/Vehicle.cpp
+++ src/openrct2/ride/Vehicle.cpp
@@ -823,7 +823,6 @@ void Vehicle::ApplyMass(int16_t appliedMass)
     mass = std::clamp<int32_t>(mass + appliedMass, 1, std::numeric_limits<decltype(mass)>::max());
 }
 
-
 void Vehicle::MoveRelativeDistance(int32_t distance)
 {
     remaining_distance += distance;
