8968dd227444e9dc60a2bd7d56a6edbe65869c92
Gymnasiast <m.o.steenbeek@gmail.com>
2021-02-18 17:53:30 +0100

    Apply Duncan's request

--- src/openrct2/rct2/S6Importer.cpp
+++ src/openrct2/rct2/S6Importer.cpp
@@ -803,13 +803,10 @@ public:
         dst.ProximityStart = { src.proximity_start_x, src.proximity_start_y, src.proximity_start_z };
         dst.CurrentRide = src.current_ride;
         dst.State = src.state;
-        if (src.proximity_track_type == 0xFF)
-            dst.ProximityTrackType = TrackElemType::None;
-        else if (
-            src.current_ride < RCT12_MAX_RIDES_IN_PARK && _s6.rides[src.current_ride].type < std::size(RideTypeDescriptors))
+        if (src.current_ride < RCT12_MAX_RIDES_IN_PARK && _s6.rides[src.current_ride].type < std::size(RideTypeDescriptors))
             dst.ProximityTrackType = RCT2TrackTypeToOpenRCT2(src.proximity_track_type, _s6.rides[src.current_ride].type);
         else
-            dst.ProximityTrackType = src.proximity_track_type;
+            dst.ProximityTrackType = 0xFF;
         dst.ProximityBaseHeight = src.proximity_base_height;
         dst.ProximityTotal = src.proximity_total;
         for (size_t i = 0; i < std::size(src.proximity_scores); i++)
