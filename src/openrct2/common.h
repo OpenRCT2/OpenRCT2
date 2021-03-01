0fab4956eda0fe289b72d08a4b62f4458175f307
Geoff Banh <38046772+gbanh1@users.noreply.github.com>
2021-03-01 11:59:06 -0700

    Fix #14093: Remove useless MONEY_FREE def

--- src/openrct2/common.h
+++ src/openrct2/common.h
@@ -146,7 +146,6 @@ constexpr auto MONEY(int32_t whole, int32_t fraction)
     return (whole)*10 + ((fraction) / 10);
 }
 
-#define MONEY_FREE 0.00_GBP
 #define MONEY16_UNDEFINED static_cast<money16>(static_cast<uint16_t>(0xFFFF))
 #define MONEY32_UNDEFINED (static_cast<money32>(0x80000000))
 
