6dab8ef583f872888425b42f4d74767dbd6b0292
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2021-04-02 07:36:27 +0100

    Use correct null value

--- src/openrct2/scripting/ScTile.hpp
+++ src/openrct2/scripting/ScTile.hpp
@@ -440,7 +440,7 @@ namespace OpenRCT2::Scripting
                 case TILE_ELEMENT_TYPE_PATH:
                 {
                     auto el = _element->AsPath();
-                    if (el->IsQueue() && el->GetRideIndex() != 0xFF)
+                    if (el->IsQueue() && el->GetRideIndex() != RIDE_ID_NULL)
                         duk_push_int(ctx, el->GetRideIndex());
                     else
                         duk_push_null(ctx);
@@ -466,7 +466,7 @@ namespace OpenRCT2::Scripting
             }
             return DukValue::take_from_stack(ctx);
         }
-        void ride_set(uint8_t value)
+        void ride_set(ride_id_t value)
         {
             ThrowIfGameStateNotMutable();
             switch (_element->GetType())
@@ -506,7 +506,7 @@ namespace OpenRCT2::Scripting
                 case TILE_ELEMENT_TYPE_PATH:
                 {
                     auto el = _element->AsPath();
-                    if (el->IsQueue() && el->GetRideIndex() != 0xFF)
+                    if (el->IsQueue() && el->GetRideIndex() != RIDE_ID_NULL)
                         duk_push_int(ctx, el->GetStationIndex());
                     else
                         duk_push_null(ctx);
