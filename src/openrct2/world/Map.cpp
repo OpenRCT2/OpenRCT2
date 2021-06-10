3e5f962cf74ef2332f7b3efd268f29c823b8ffdd
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2021-06-10 07:21:06 +0100

    Allow reaching the limit

--- src/openrct2/world/Map.cpp
+++ src/openrct2/world/Map.cpp
@@ -207,7 +207,7 @@ void ReorganiseTileElements()
 static bool map_check_free_elements_and_reorganise(size_t numElementsOnTile, size_t numNewElements)
 {
     // Check hard cap on num in use tiles (this would be the size of _tileElements immediately after a reorg)
-    if (_tileElementsInUse + numNewElements >= MAX_TILE_ELEMENTS)
+    if (_tileElementsInUse + numNewElements > MAX_TILE_ELEMENTS)
     {
         gGameCommandErrorText = STR_ERR_LANDSCAPE_DATA_AREA_FULL;
         return false;
