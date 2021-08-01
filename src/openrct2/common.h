114eff46f09e6411e593b5a4a90cf09d1b6edd4c
Gymnasiast <m.o.steenbeek@gmail.com>
2021-08-01 10:12:37 +0200

    Remove static attribute from ToMoney64/32/16

--- src/openrct2/common.h
+++ src/openrct2/common.h
@@ -143,22 +143,22 @@ using money64 = fixed64_1dp;
 #define MONEY32_UNDEFINED (static_cast<money32>(0x80000000))
 #define MONEY64_UNDEFINED (static_cast<money64>(0x8000000000000000))
 
-static constexpr money64 ToMoney64(money32 value)
+constexpr money64 ToMoney64(money32 value)
 {
     return value == MONEY32_UNDEFINED ? MONEY64_UNDEFINED : value;
 }
 
-static constexpr money64 ToMoney64(money16 value)
+constexpr money64 ToMoney64(money16 value)
 {
     return value == MONEY16_UNDEFINED ? MONEY64_UNDEFINED : value;
 }
 
-static constexpr money32 ToMoney32(money64 value)
+constexpr money32 ToMoney32(money64 value)
 {
     return value == MONEY64_UNDEFINED ? MONEY32_UNDEFINED : static_cast<money32>(value);
 }
 
-static constexpr money16 ToMoney16(money64 value)
+constexpr money16 ToMoney16(money64 value)
 {
     return value == MONEY64_UNDEFINED ? MONEY16_UNDEFINED : static_cast<money16>(value);
 }
