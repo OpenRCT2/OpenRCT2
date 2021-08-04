af102e391e7b9894099691bdbb651c6831023a9b
ZehMatt <m.moninger.h@gmail.com>
2021-08-04 18:04:25 +0300

    Return correct entity type, API breaking change

--- src/openrct2/scripting/ScEntity.hpp
+++ src/openrct2/scripting/ScEntity.hpp
@@ -56,8 +56,9 @@ namespace OpenRCT2::Scripting
                     case EntityType::Vehicle:
                         return "car";
                     case EntityType::Guest:
+                        return "guest";
                     case EntityType::Staff:
-                        return "peep";
+                        return "staff";
                     case EntityType::SteamParticle:
                         return "steam_particle";
                     case EntityType::MoneyEffect:
