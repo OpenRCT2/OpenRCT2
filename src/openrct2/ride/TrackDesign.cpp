2597c04eb4348f80549e898c3a2351f500598e72
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2021-10-02 00:19:39 +0300

    Refactor RideCreateAction result data passing

--- src/openrct2/ride/TrackDesign.cpp
+++ src/openrct2/ride/TrackDesign.cpp
@@ -1866,8 +1866,7 @@ static money32 track_design_ride_create_command(int32_t type, int32_t subType, i
     auto gameAction = RideCreateAction(type, subType, 0, 0);
     gameAction.SetFlags(flags);
 
-    auto r = GameActions::ExecuteNested(&gameAction);
-    const RideCreateGameActionResult* res = static_cast<RideCreateGameActionResult*>(r.get());
+    auto res = GameActions::ExecuteNested(&gameAction);
 
     // Callee's of this function expect MONEY32_UNDEFINED in case of failure.
     if (res->Error != GameActions::Status::Ok)
@@ -1875,7 +1874,7 @@ static money32 track_design_ride_create_command(int32_t type, int32_t subType, i
         return MONEY32_UNDEFINED;
     }
 
-    *outRideIndex = res->rideIndex;
+    *outRideIndex = res->GetData<ride_id_t>();
 
     return res->Cost;
 }
