fda4476288d28fca3b4b0b00e6a874ca4920bd29
Soham Roy <72430659+sohamroy19@users.noreply.github.com>
2021-10-04 01:51:08 +0530

    fix: Coding style (#11354)
    Removed extra underscores.

--- src/openrct2/drawing/Font.h
+++ src/openrct2/drawing/Font.h
@@ -33,8 +33,8 @@ enum class FontSpriteBase : int16_t
 
 #ifndef NO_TTF
 
-struct Internal_TTF_Font;
-using TTF_Font = Internal_TTF_Font;
+struct InternalTTFFont;
+using TTF_Font = InternalTTFFont;
 struct TTFFontDescriptor
 {
     const utf8* filename;
