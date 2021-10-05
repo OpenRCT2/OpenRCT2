dd4d07136a5d57a786c464c0be8ebe432cf433a9
Sebastian Flinck Lindström <sebastianflincklindstrom@gmail.com>
2021-10-05 14:42:08 +0200

    Update to clang format

--- src/openrct2-ui/windows/ClearScenery.cpp
+++ src/openrct2-ui/windows/ClearScenery.cpp
@@ -16,8 +16,8 @@
 #include <openrct2/world/Park.h>
 #include <openrct2/world/Scenery.h>
 
-// clang-format off
-enum WINDOW_CLEAR_SCENERY_WIDGET_IDX {
+enum WINDOW_CLEAR_SCENERY_WIDGET_IDX
+{
     WIDX_BACKGROUND,
     WIDX_TITLE,
     WIDX_CLOSE,
@@ -28,162 +28,170 @@ enum WINDOW_CLEAR_SCENERY_WIDGET_IDX {
     WIDX_LARGE_SCENERY,
     WIDX_FOOTPATH
 };
-
+// clang-format on
 static constexpr const rct_string_id WINDOW_TITLE = STR_CLEAR_SCENERY;
 static constexpr const int32_t WW = 98;
 static constexpr const int32_t WH = 94;
 
-static constexpr ScreenSize CLEAR_SCENERY_BUTTON = {24, 24};
+static constexpr ScreenSize CLEAR_SCENERY_BUTTON = { 24, 24 };
 
 static rct_widget window_clear_scenery_widgets[] = {
     WINDOW_SHIM(WINDOW_TITLE, WW, WH),
-    MakeWidget     ({27, 17}, {44, 32}, WindowWidgetType::ImgBtn,  WindowColour::Primary  , SPR_LAND_TOOL_SIZE_0,        STR_NONE),                                   // preview box
-    MakeRemapWidget({28, 18}, {16, 16}, WindowWidgetType::TrnBtn,  WindowColour::Secondary, SPR_LAND_TOOL_DECREASE,      STR_ADJUST_SMALLER_LAND_TIP),                // decrement size
-    MakeRemapWidget({54, 32}, {16, 16}, WindowWidgetType::TrnBtn,  WindowColour::Secondary, SPR_LAND_TOOL_INCREASE,      STR_ADJUST_LARGER_LAND_TIP),                 // increment size
-    MakeRemapWidget({ 7, 53}, CLEAR_SCENERY_BUTTON, WindowWidgetType::FlatBtn, WindowColour::Secondary, SPR_G2_BUTTON_TREES,         STR_CLEAR_SCENERY_REMOVE_SMALL_SCENERY_TIP), // small scenery
-    MakeRemapWidget({37, 53}, CLEAR_SCENERY_BUTTON, WindowWidgetType::FlatBtn, WindowColour::Secondary, SPR_G2_BUTTON_LARGE_SCENERY, STR_CLEAR_SCENERY_REMOVE_LARGE_SCENERY_TIP), // large scenery
-    MakeRemapWidget({67, 53}, CLEAR_SCENERY_BUTTON, WindowWidgetType::FlatBtn, WindowColour::Secondary, SPR_G2_BUTTON_FOOTPATH,      STR_CLEAR_SCENERY_REMOVE_FOOTPATHS_TIP),     // footpaths
+    MakeWidget(
+        { 27, 17 }, { 44, 32 }, WindowWidgetType::ImgBtn, WindowColour::Primary, SPR_LAND_TOOL_SIZE_0, STR_NONE), // preview box
+    MakeRemapWidget(
+        { 28, 18 }, { 16, 16 }, WindowWidgetType::TrnBtn, WindowColour::Secondary, SPR_LAND_TOOL_DECREASE,
+        STR_ADJUST_SMALLER_LAND_TIP), // decrement size
+    MakeRemapWidget(
+        { 54, 32 }, { 16, 16 }, WindowWidgetType::TrnBtn, WindowColour::Secondary, SPR_LAND_TOOL_INCREASE,
+        STR_ADJUST_LARGER_LAND_TIP), // increment size
+    MakeRemapWidget(
+        { 7, 53 }, CLEAR_SCENERY_BUTTON, WindowWidgetType::FlatBtn, WindowColour::Secondary, SPR_G2_BUTTON_TREES,
+        STR_CLEAR_SCENERY_REMOVE_SMALL_SCENERY_TIP), // small scenery
+    MakeRemapWidget(
+        { 37, 53 }, CLEAR_SCENERY_BUTTON, WindowWidgetType::FlatBtn, WindowColour::Secondary, SPR_G2_BUTTON_LARGE_SCENERY,
+        STR_CLEAR_SCENERY_REMOVE_LARGE_SCENERY_TIP), // large scenery
+    MakeRemapWidget(
+        { 67, 53 }, CLEAR_SCENERY_BUTTON, WindowWidgetType::FlatBtn, WindowColour::Secondary, SPR_G2_BUTTON_FOOTPATH,
+        STR_CLEAR_SCENERY_REMOVE_FOOTPATHS_TIP), // footpaths
     WIDGETS_END,
 };
 
 class CleanSceneryWindow final : public Window
 {
-    public:
-        void OnOpen() override
+public:
+    void OnOpen() override
+    {
+        widgets = window_clear_scenery_widgets;
+        enabled_widgets = (1ULL << WIDX_CLOSE) | (1ULL << WIDX_INCREMENT) | (1ULL << WIDX_DECREMENT) | (1ULL << WIDX_PREVIEW)
+            | (1ULL << WIDX_SMALL_SCENERY) | (1ULL << WIDX_LARGE_SCENERY) | (1ULL << WIDX_FOOTPATH);
+        hold_down_widgets = (1ULL << WIDX_INCREMENT) | (1ULL << WIDX_DECREMENT);
+        WindowInitScrollWidgets(this);
+        window_push_others_below(this);
+
+        gLandToolSize = 2;
+        gClearSceneryCost = MONEY64_UNDEFINED;
+
+        gClearSmallScenery = true;
+        gClearLargeScenery = false;
+        gClearFootpath = false;
+    }
+
+    void OnClose() override
+    {
+        if (clear_scenery_tool_is_active())
+            tool_cancel();
+    }
+
+    void OnMouseUp(const rct_widgetindex widgetIndex) override
+    {
+        switch (widgetIndex)
         {
-            widgets = window_clear_scenery_widgets;
-            enabled_widgets = (1ULL << WIDX_CLOSE) | (1ULL << WIDX_INCREMENT) | (1ULL << WIDX_DECREMENT)
-                | (1ULL << WIDX_PREVIEW) | (1ULL << WIDX_SMALL_SCENERY) | (1ULL << WIDX_LARGE_SCENERY) | (1ULL << WIDX_FOOTPATH);
-            hold_down_widgets = (1ULL << WIDX_INCREMENT) | (1ULL << WIDX_DECREMENT);
-            WindowInitScrollWidgets(this);
-            window_push_others_below(this);
-
-            gLandToolSize = 2;
-            gClearSceneryCost = MONEY64_UNDEFINED;
-
-            gClearSmallScenery = true;
-            gClearLargeScenery = false;
-            gClearFootpath = false;
-        }
-
-        void OnClose() override
-        {
-            if (clear_scenery_tool_is_active())
-                tool_cancel();
-        }
-
-        void OnMouseUp(const rct_widgetindex widgetIndex) override
-        {
-            
-            
-            switch (widgetIndex)
-                {
-                case WIDX_CLOSE:
-                    Close();
-                    break;
-                case WIDX_PREVIEW:
-                    {
-                        Formatter ft;
-                        ft.Add<int16_t>(MINIMUM_TOOL_SIZE);
-                        ft.Add<int16_t>(MAXIMUM_TOOL_SIZE);
-                        TextInputOpen(WIDX_PREVIEW, STR_SELECTION_SIZE, STR_ENTER_SELECTION_SIZE, ft, STR_NONE, STR_NONE, 3);
-                        break;
-                    }
-                case WIDX_SMALL_SCENERY:
-                    gClearSmallScenery ^= 1;
-                    Invalidate();
-                    break;
-                case WIDX_LARGE_SCENERY:
-                    gClearLargeScenery ^= 1;
-                    Invalidate();
-                    break;
-                case WIDX_FOOTPATH:
-                    gClearFootpath ^= 1;
-                    Invalidate();
-                    break;
-            }
-        }
-
-        void OnMouseDown(const rct_widgetindex widgetIndex) override
-        {
-            switch(widgetIndex)
-                {
-                    case WIDX_DECREMENT:
-                        // Decrement land tool size, if it stays within the limit
-                        gLandToolSize = std::max(MINIMUM_TOOL_SIZE, gLandToolSize - 1);
-
-                        // Invalidate the window
-                        Invalidate();
-                        break;
-                    case WIDX_INCREMENT:
-                        // Increment land tool size, if it stays within the limit
-                        gLandToolSize = std::min(MAXIMUM_TOOL_SIZE, gLandToolSize + 1);
-
-                        // Invalidate the window
-                        Invalidate();
-                        break;
-                }
-        }
-
-        void OnTextInput(const rct_widgetindex widgetIndex, const std::string_view text) override
-        {
-            if (widgetIndex != WIDX_PREVIEW || text.empty())
-                return;
-
-            char* end;
-            int32_t size = strtol(std::string(text).c_str(), &end, 10);
-            if (*end == '\0')
-            {
-                size = std::clamp(size, MINIMUM_TOOL_SIZE, MAXIMUM_TOOL_SIZE);
-                gLandToolSize = size;
-                Invalidate();
-            }
-        }
-
-        void OnUpdate() override
-        {
-            frame_no++;
-            // Close window if another tool is open
-            if (!clear_scenery_tool_is_active())
+            case WIDX_CLOSE:
                 Close();
-        }
-
-        void Invalidate()
-        {
-            // Set the preview image button to be pressed down
-            pressed_widgets = (1ULL << WIDX_PREVIEW) | (gClearSmallScenery ? (1ULL << WIDX_SMALL_SCENERY) : 0)
-                | (gClearLargeScenery ? (1ULL << WIDX_LARGE_SCENERY) : 0) | (gClearFootpath ? (1ULL << WIDX_FOOTPATH) : 0);
-
-            // Update the preview image (for tool sizes up to 7)
-            window_clear_scenery_widgets[WIDX_PREVIEW].image = LandTool::SizeToSpriteIndex(gLandToolSize);
-        }
-
-        void OnDraw(rct_drawpixelinfo& dpi) override
-        {
-            DrawWidgets(dpi);
-
-            // Draw number for tool sizes bigger than 7
-            ScreenCoordsXY screenCoords = { windowPos.x + window_clear_scenery_widgets[WIDX_PREVIEW].midX(),
-                                            windowPos.y + window_clear_scenery_widgets[WIDX_PREVIEW].midY() };
-            if (gLandToolSize > MAX_TOOL_SIZE_WITH_SPRITE)
+                break;
+            case WIDX_PREVIEW:
             {
-                auto ft = Formatter();
-                ft.Add<uint16_t>(gLandToolSize);
-                DrawTextBasic(&dpi, screenCoords - ScreenCoordsXY{ 0, 2 }, STR_LAND_TOOL_SIZE_VALUE, ft, { TextAlignment::CENTRE });
+                Formatter ft;
+                ft.Add<int16_t>(MINIMUM_TOOL_SIZE);
+                ft.Add<int16_t>(MAXIMUM_TOOL_SIZE);
+                TextInputOpen(WIDX_PREVIEW, STR_SELECTION_SIZE, STR_ENTER_SELECTION_SIZE, ft, STR_NONE, STR_NONE, 3);
+                break;
             }
-
-            // Draw cost amount
-            if (gClearSceneryCost != MONEY64_UNDEFINED && gClearSceneryCost != 0 && !(gParkFlags & PARK_FLAGS_NO_MONEY))
-            {
-                auto ft = Formatter();
-                ft.Add<money64>(gClearSceneryCost);
-                screenCoords.x = window_clear_scenery_widgets[WIDX_PREVIEW].midX() + windowPos.x;
-                screenCoords.y = window_clear_scenery_widgets[WIDX_PREVIEW].bottom + windowPos.y + 5 + 27;
-                DrawTextBasic(&dpi, screenCoords, STR_COST_AMOUNT, ft, { TextAlignment::CENTRE });
-            }
+            case WIDX_SMALL_SCENERY:
+                gClearSmallScenery ^= 1;
+                Invalidate();
+                break;
+            case WIDX_LARGE_SCENERY:
+                gClearLargeScenery ^= 1;
+                Invalidate();
+                break;
+            case WIDX_FOOTPATH:
+                gClearFootpath ^= 1;
+                Invalidate();
+                break;
         }
+    }
+
+    void OnMouseDown(const rct_widgetindex widgetIndex) override
+    {
+        switch (widgetIndex)
+        {
+            case WIDX_DECREMENT:
+                // Decrement land tool size, if it stays within the limit
+                gLandToolSize = std::max(MINIMUM_TOOL_SIZE, gLandToolSize - 1);
+
+                // Invalidate the window
+                Invalidate();
+                break;
+            case WIDX_INCREMENT:
+                // Increment land tool size, if it stays within the limit
+                gLandToolSize = std::min(MAXIMUM_TOOL_SIZE, gLandToolSize + 1);
+
+                // Invalidate the window
+                Invalidate();
+                break;
+        }
+    }
+
+    void OnTextInput(const rct_widgetindex widgetIndex, const std::string_view text) override
+    {
+        if (widgetIndex != WIDX_PREVIEW || text.empty())
+            return;
+
+        char* end;
+        int32_t size = strtol(std::string(text).c_str(), &end, 10);
+        if (*end == '\0')
+        {
+            size = std::clamp(size, MINIMUM_TOOL_SIZE, MAXIMUM_TOOL_SIZE);
+            gLandToolSize = size;
+            Invalidate();
+        }
+    }
+
+    void OnUpdate() override
+    {
+        frame_no++;
+        // Close window if another tool is open
+        if (!clear_scenery_tool_is_active())
+            Close();
+    }
+
+    void Invalidate()
+    {
+        // Set the preview image button to be pressed down
+        pressed_widgets = (1ULL << WIDX_PREVIEW) | (gClearSmallScenery ? (1ULL << WIDX_SMALL_SCENERY) : 0)
+            | (gClearLargeScenery ? (1ULL << WIDX_LARGE_SCENERY) : 0) | (gClearFootpath ? (1ULL << WIDX_FOOTPATH) : 0);
+
+        // Update the preview image (for tool sizes up to 7)
+        window_clear_scenery_widgets[WIDX_PREVIEW].image = LandTool::SizeToSpriteIndex(gLandToolSize);
+    }
+
+    void OnDraw(rct_drawpixelinfo& dpi) override
+    {
+        DrawWidgets(dpi);
+
+        // Draw number for tool sizes bigger than 7
+        ScreenCoordsXY screenCoords = { windowPos.x + window_clear_scenery_widgets[WIDX_PREVIEW].midX(),
+                                        windowPos.y + window_clear_scenery_widgets[WIDX_PREVIEW].midY() };
+        if (gLandToolSize > MAX_TOOL_SIZE_WITH_SPRITE)
+        {
+            auto ft = Formatter();
+            ft.Add<uint16_t>(gLandToolSize);
+            DrawTextBasic(&dpi, screenCoords - ScreenCoordsXY{ 0, 2 }, STR_LAND_TOOL_SIZE_VALUE, ft, { TextAlignment::CENTRE });
+        }
+
+        // Draw cost amount
+        if (gClearSceneryCost != MONEY64_UNDEFINED && gClearSceneryCost != 0 && !(gParkFlags & PARK_FLAGS_NO_MONEY))
+        {
+            auto ft = Formatter();
+            ft.Add<money64>(gClearSceneryCost);
+            screenCoords.x = window_clear_scenery_widgets[WIDX_PREVIEW].midX() + windowPos.x;
+            screenCoords.y = window_clear_scenery_widgets[WIDX_PREVIEW].bottom + windowPos.y + 5 + 27;
+            DrawTextBasic(&dpi, screenCoords, STR_COST_AMOUNT, ft, { TextAlignment::CENTRE });
+        }
+    }
 };
-
 
 rct_window* window_clear_scenery_open()
 {
