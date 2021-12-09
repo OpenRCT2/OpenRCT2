88c43b63a3415d114c047782706d1d0b4b8a7879
Ted John <ted@brambles.org>
2021-12-09 17:17:28 +0000

    Fix #16155: Paint issue on Swinging Ship

--- src/openrct2/ride/thrill/SwingingShip.cpp
+++ src/openrct2/ride/thrill/SwingingShip.cpp
@@ -108,6 +108,11 @@ static void PaintSwingingShipStructure(
         session->CurrentlyDrawnItem = vehicle;
     }
 
+    const auto& bounds = SwingingShipData[direction];
+    CoordsXYZ offset((direction & 1) ? 0 : axisOffset, (direction & 1) ? axisOffset : 0, height + 7);
+    CoordsXYZ bbLength(bounds.length_x, bounds.length_y, 80);
+    CoordsXYZ bbOffset(bounds.offset_x, bounds.offset_y, height + 7);
+
     auto baseImageId = rideEntry->vehicles[0].base_image_id + SwingingShipBaseSpriteOffset[direction];
     if (vehicle != nullptr)
     {
@@ -127,22 +132,20 @@ static void PaintSwingingShipStructure(
         }
     }
 
-    auto imageTemplate = ImageId(0, ride.vehicle_colours[0].Body, ride.vehicle_colours[0].Trim);
+    auto supportsImageTemplate = ImageId::FromUInt32(session->TrackColours[SCHEME_TRACK]);
+    auto vehicleImageTemplate = ImageId(0, ride.vehicle_colours[0].Body, ride.vehicle_colours[0].Trim);
     auto imageFlags = session->TrackColours[SCHEME_MISC];
     if (imageFlags != IMAGE_TYPE_REMAP)
     {
-        imageTemplate = ImageId::FromUInt32(imageFlags);
+        vehicleImageTemplate = ImageId::FromUInt32(imageFlags);
     }
 
-    const auto& bounds = SwingingShipData[direction];
-    CoordsXYZ offset((direction & 1) ? 0 : axisOffset, (direction & 1) ? axisOffset : 0, height + 7);
-    CoordsXYZ bbLength(bounds.length_x, bounds.length_y, 80);
-    CoordsXYZ bbOffset(bounds.offset_x, bounds.offset_y, height + 7);
-
-    auto imageId = imageTemplate.WithIndex(SwingingShipFrameSprites[(direction & 1)][0]);
+    // Supports (back)
+    auto imageId = supportsImageTemplate.WithIndex(SwingingShipFrameSprites[(direction & 1)][0]);
     PaintAddImageAsParent(session, imageId, offset, bbLength, bbOffset);
 
-    imageId = imageTemplate.WithIndex(baseImageId);
+    // Ship
+    imageId = vehicleImageTemplate.WithIndex(baseImageId);
     PaintAddImageAsChild(session, imageId, offset, bbLength, bbOffset);
 
     if (vehicle != nullptr)
@@ -150,7 +153,8 @@ static void PaintSwingingShipStructure(
         PaintSwingingShipRiders(session, ride, *vehicle, baseImageId, direction, offset, bbLength, bbOffset);
     }
 
-    imageId = imageTemplate.WithIndex(SwingingShipFrameSprites[(direction & 1)][1]);
+    // Supports (front)
+    imageId = supportsImageTemplate.WithIndex(SwingingShipFrameSprites[(direction & 1)][1]);
     PaintAddImageAsChild(session, imageId, offset, bbLength, bbOffset);
 
     session->CurrentlyDrawnItem = savedTileElement;
