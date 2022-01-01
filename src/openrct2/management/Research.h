9dec23039084d7b829a40f6d313ac050d91fdab3
Gymnasiast <m.o.steenbeek@gmail.com>
2022-01-01 14:40:29 +0100

    Remove unused symbols in Research.h

--- src/openrct2/management/Research.h
+++ src/openrct2/management/Research.h
@@ -87,34 +87,10 @@ struct ResearchItem
     {
     }
 
-    RCT12ResearchItem ToRCT12ResearchItem() const
-    {
-        RCT12ResearchItem retItem = {};
-        if (IsNull())
-        {
-            retItem.rawValue = RCT12_RESEARCHED_ITEMS_SEPARATOR;
-        }
-        else
-        {
-            retItem.entryIndex = OpenRCT2EntryIndexToRCTEntryIndex(entryIndex);
-            retItem.baseRideType = RCT2::OpenRCT2RideTypeToRCT2RideType(baseRideType);
-            retItem.type = static_cast<uint8_t>(type);
-            retItem.flags = (flags & ~RESEARCH_ENTRY_FLAG_FIRST_OF_TYPE);
-            retItem.category = EnumValue(category);
-        }
-
-        return retItem;
-    }
-
     ResearchItem(const RCT12ResearchItem& oldResearchItem);
 
     bool operator==(const ResearchItem& rhs) const;
 };
-
-// Only used to mark as null nowadays. Deprecated. TODO: remove.
-#define RESEARCH_ITEM_NULL 0xFFFFFFFF
-
-#define MAX_RESEARCH_ITEMS 500
 
 enum
 {
