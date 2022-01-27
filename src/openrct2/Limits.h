2c0a49e4063ca4ee31948c96cc10a37073b7defb
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-01-27 20:21:02 +0000

    Switch style

--- src/openrct2/Limits.h
+++ src/openrct2/Limits.h
@@ -14,17 +14,17 @@ namespace OpenRCT2::Limits
 {
     constexpr const uint16_t MaxRidesInPark = 1000;
     constexpr const uint16_t MaxStationsPerRide = 255;
-    using RCT12::Limits::CustomerHistorySize;
-    using RCT12::Limits::MaxGolfHoles;
-    using RCT12::Limits::MaxHelices;
-    using RCT12::Limits::MaxInversions;
+    constexpr const uint8_t CustomerHistorySize = RCT12::Limits::CustomerHistorySize;
+    constexpr const uint16_t MaxGolfHoles = RCT12::Limits::MaxGolfHoles;
+    constexpr const uint16_t MaxHelices = RCT12::Limits::MaxHelices;
+    constexpr const uint16_t MaxInversions = RCT12::Limits::MaxInversions;
     constexpr const uint16_t MaxTrainsPerRide = 255;
     constexpr const uint16_t MaxCarsPerTrain = 255;
     constexpr const uint16_t MaxVehicleColours = MaxTrainsPerRide; // this should really be MaxTrainsPerRide * MaxCarsPerTrain
     constexpr const uint8_t MaxCircuitsPerRide = 20;
-    using RCT12::Limits::MaxAwards;
-    using RCT12::Limits::NumColourSchemes;
-    using RCT2::Limits::DowntimeHistorySize;
+    constexpr const uint8_t MaxAwards = RCT12::Limits::MaxAwards;
+    constexpr const uint8_t NumColourSchemes = RCT12::Limits::NumColourSchemes;
+    constexpr const uint8_t DowntimeHistorySize = RCT2::Limits::DowntimeHistorySize;
     constexpr const uint16_t MaxPeepSpawns = 256;
     constexpr const uint16_t MaxParkEntrances = 256;
 } // namespace OpenRCT2::Limits
