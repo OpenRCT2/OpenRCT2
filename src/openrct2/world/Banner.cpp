OpenRCT2/OpenRCT2

commit 63a2e9d716b48235bdb71c6392462ee2b9f18504

Refactor uses of old BannerIndex

src/openrct2/world/Banner.cpp
@@ -113,19 +113,19 @@ static ride_id_t banner_get_ride_index_at(const CoordsXYZ& bannerCoords)
 
 static BannerIndex BannerGetNewIndex()
 {
-    for (BannerIndex bannerIndex = 0; bannerIndex < MAX_BANNERS; bannerIndex++)
+    for (uint16_t bannerIndex = 0; bannerIndex < MAX_BANNERS; bannerIndex++)
     {
         if (bannerIndex < _banners.size())
         {
             if (_banners[bannerIndex].IsNull())
             {
-                return bannerIndex;
+                return BannerIndex::FromUnderlying(bannerIndex);
             }
         }
         else
         {
             _banners.emplace_back();
-            return static_cast<BannerIndex>(_banners.size() - 1);
+            return BannerIndex::FromUnderlying(bannerIndex);
         }
     }
     return BANNER_INDEX_NULL;
@@ -238,12 +238,13 @@ ride_id_t banner_get_closest_ride_index(const CoordsXYZ& mapPos)
 
 void banner_reset_broken_index()
 {
-    for (BannerIndex bannerIndex = 0; bannerIndex < _banners.size(); bannerIndex++)
+    for (uint16_t index = 0; index < _banners.size(); index++)
     {
-        auto tileElement = banner_get_tile_element(bannerIndex);
+        const auto bannerId = BannerIndex::FromUnderlying(index);
+        auto tileElement = banner_get_tile_element(bannerId);
         if (tileElement == nullptr)
         {
-            auto banner = GetBanner(bannerIndex);
+            auto banner = GetBanner(bannerId);
             if (banner != nullptr)
             {
                 banner->type = BANNER_NULL;
@@ -269,10 +270,11 @@ void fix_duplicated_banners()
                 if (bannerIndex == BANNER_INDEX_NULL)
                     continue;
 
-                if (activeBanners[bannerIndex])
+                const auto index = bannerIndex.ToUnderlying();
+                if (activeBanners[index])
                 {
                     log_info(
-                        "Duplicated banner with index %d found at x = %d, y = %d and z = %d.", bannerIndex, x, y,
+                        "Duplicated banner with index %d found at x = %d, y = %d and z = %d.", index, x, y,
                         bannerElement->base_height);
 
                     // Banner index is already in use by another banner, so duplicate it
@@ -282,7 +284,7 @@ void fix_duplicated_banners()
                         log_error("Failed to create new banner.");
                         continue;
                     }
-                    Guard::Assert(!activeBanners[newBanner->id]);
+                    Guard::Assert(!activeBanners[index]);
 
                     // Copy over the original banner, but update the location
                     const auto* oldBanner = GetBanner(bannerIndex);
@@ -299,7 +301,7 @@ void fix_duplicated_banners()
                 }
 
                 // Mark banner index as in-use
-                activeBanners[bannerIndex] = true;
+                activeBanners[index] = true;
             }
         }
     }
@@ -383,9 +385,10 @@ void UnlinkAllBannersForRide(ride_id_t rideId)
 
 Banner* GetBanner(BannerIndex id)
 {
-    if (id < _banners.size())
+    const auto index = id.ToUnderlying();
+    if (index < _banners.size())
     {
-        auto banner = &_banners[id];
+        auto banner = &_banners[index];
         if (banner != nullptr && !banner->IsNull())
         {
             return banner;
@@ -396,14 +399,15 @@ Banner* GetBanner(BannerIndex id)
 
 Banner* GetOrCreateBanner(BannerIndex id)
 {
-    if (id < MAX_BANNERS)
+    const auto index = id.ToUnderlying();
+    if (index < MAX_BANNERS)
     {
-        if (id >= _banners.size())
+        if (index >= _banners.size())
         {
-            _banners.resize(id + 1);
+            _banners.resize(index + 1);
         }
         // Create the banner
-        auto& banner = _banners[id];
+        auto& banner = _banners[index];
         banner.id = id;
         return &banner;
     }

src/openrct2-ui/windows/Banner.cpp
@@ -88,9 +88,14 @@ private:
         Invalidate();
     }
 
+    BannerIndex GetBannerIndex() const
+    {
+        return BannerIndex::FromUnderlying(number);
+    }
+
     BannerElement* GetBannerElement()
     {
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(GetBannerIndex());
         if (banner == nullptr)
         {
             return nullptr;
@@ -109,7 +114,7 @@ private:
             {
                 continue;
             }
-            if (bannerElement->GetIndex() == number)
+            if (bannerElement->GetIndex() == GetBannerIndex())
             {
                 return bannerElement;
             }
@@ -131,7 +136,7 @@ public:
     void Initialise(rct_windownumber _number)
     {
         number = _number;
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(BannerIndex::FromUnderlying(number));
 
         auto* bannerElement = GetBannerElement();
         if (bannerElement == nullptr)
@@ -144,7 +149,7 @@ public:
     void OnMouseDown(rct_widgetindex widgetIndex) override
     {
         rct_widget* widget = &widgets[widgetIndex];
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(GetBannerIndex());
         if (banner == nullptr)
         {
             Close();
@@ -177,7 +182,7 @@ public:
 
     void OnMouseUp(rct_widgetindex widgetIndex) override
     {
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(GetBannerIndex());
         if (banner == nullptr)
         {
             Close();
@@ -207,7 +212,7 @@ public:
             {
                 textinput_cancel();
                 auto bannerSetStyle = BannerSetStyleAction(
-                    BannerSetStyleType::NoEntry, number, banner->flags ^ BANNER_FLAG_NO_ENTRY);
+                    BannerSetStyleType::NoEntry, GetBannerIndex(), banner->flags ^ BANNER_FLAG_NO_ENTRY);
                 GameActions::Execute(&bannerSetStyle);
                 break;
             }
@@ -223,7 +228,7 @@ public:
                 if (dropdownIndex == -1)
                     break;
 
-                auto bannerSetStyle = BannerSetStyleAction(BannerSetStyleType::PrimaryColour, number, dropdownIndex);
+                auto bannerSetStyle = BannerSetStyleAction(BannerSetStyleType::PrimaryColour, GetBannerIndex(), dropdownIndex);
                 GameActions::Execute(&bannerSetStyle);
                 break;
             }
@@ -231,7 +236,7 @@ public:
             {
                 if (dropdownIndex == -1)
                     break;
-                auto bannerSetStyle = BannerSetStyleAction(BannerSetStyleType::TextColour, number, dropdownIndex + 1);
+                auto bannerSetStyle = BannerSetStyleAction(BannerSetStyleType::TextColour, GetBannerIndex(), dropdownIndex + 1);
                 GameActions::Execute(&bannerSetStyle);
                 break;
             }
@@ -242,7 +247,7 @@ public:
     {
         if (widgetIndex == WIDX_BANNER_TEXT)
         {
-            auto bannerSetNameAction = BannerSetNameAction(number, std::string(text));
+            auto bannerSetNameAction = BannerSetNameAction(GetBannerIndex(), std::string(text));
             GameActions::Execute(&bannerSetNameAction);
         }
     }
@@ -265,7 +270,7 @@ public:
 
     void OnPrepareDraw() override
     {
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(GetBannerIndex());
         if (banner == nullptr)
         {
             return;

src/openrct2-ui/interface/ViewportInteraction.cpp
@@ -582,7 +582,7 @@ bool ViewportInteractionRightClick(const ScreenCoordsXY& screenCoords)
             ViewportInteractionRemoveLargeScenery(info.Element, info.Loc);
             break;
         case ViewportInteractionItem::Banner:
-            context_open_detail_window(WD_BANNER, info.Element->AsBanner()->GetIndex());
+            context_open_detail_window(WD_BANNER, info.Element->AsBanner()->GetIndex().ToUnderlying());
             break;
     }
 
@@ -669,7 +669,7 @@ static void ViewportInteractionRemoveParkWall(TileElement* tileElement, const Co
     auto* wallEntry = tileElement->AsWall()->GetEntry();
     if (wallEntry->scrolling_mode != SCROLLING_MODE_NONE)
     {
-        context_open_detail_window(WD_SIGN_SMALL, tileElement->AsWall()->GetBannerIndex());
+        context_open_detail_window(WD_SIGN_SMALL, tileElement->AsWall()->GetBannerIndex().ToUnderlying());
     }
     else
     {
@@ -690,7 +690,7 @@ static void ViewportInteractionRemoveLargeScenery(TileElement* tileElement, cons
     if (sceneryEntry->scrolling_mode != SCROLLING_MODE_NONE)
     {
         auto bannerIndex = tileElement->AsLargeScenery()->GetBannerIndex();
-        context_open_detail_window(WD_SIGN, bannerIndex);
+        context_open_detail_window(WD_SIGN, bannerIndex.ToUnderlying());
     }
     else
     {

src/openrct2-ui/windows/Sign.cpp
@@ -61,7 +61,7 @@ private:
 
     void ShowTextInput()
     {
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(BannerIndex::FromUnderlying(number));
         if (banner != nullptr)
         {
             auto bannerText = banner->GetText();
@@ -87,14 +87,14 @@ public:
     {
         number = windowNumber;
         _isSmall = isSmall;
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(BannerIndex::FromUnderlying(number));
         if (banner == nullptr)
         {
             return false;
         }
 
         auto signViewPosition = banner->position.ToCoordsXY().ToTileCentre();
-        auto* tileElement = banner_get_tile_element(number);
+        auto* tileElement = banner_get_tile_element(BannerIndex::FromUnderlying(number));
         if (tileElement == nullptr)
             return false;
 
@@ -138,7 +138,7 @@ public:
 
     void OnMouseUp(rct_widgetindex widgetIndex) override
     {
-        auto* banner = GetBanner(number);
+        auto* banner = GetBanner(BannerIndex::FromUnderlying(number));
         if (banner == nullptr)
         {
             Close();
@@ -151,7 +151,7 @@ public:
                 break;
             case WIDX_SIGN_DEMOLISH:
             {
-                auto* tileElement = banner_get_tile_element(number);
+                auto* tileElement = banner_get_tile_element(BannerIndex::FromUnderlying(number));
                 if (tileElement == nullptr)
                 {
                     Close();
@@ -203,7 +203,8 @@ public:
                 if (dropdownIndex == -1)
                     return;
                 list_information_type = dropdownIndex;
-                auto signSetStyleAction = SignSetStyleAction(number, dropdownIndex, var_492, !_isSmall);
+                auto signSetStyleAction = SignSetStyleAction(
+                    BannerIndex::FromUnderlying(number), dropdownIndex, var_492, !_isSmall);
                 GameActions::Execute(&signSetStyleAction);
                 break;
             }
@@ -212,7 +213,8 @@ public:
                 if (dropdownIndex == -1)
                     return;
                 var_492 = dropdownIndex;
-                auto signSetStyleAction = SignSetStyleAction(number, list_information_type, dropdownIndex, !_isSmall);
+                auto signSetStyleAction = SignSetStyleAction(
+                    BannerIndex::FromUnderlying(number), list_information_type, dropdownIndex, !_isSmall);
                 GameActions::Execute(&signSetStyleAction);
                 break;
             }
@@ -227,7 +229,7 @@ public:
     {
         if (widgetIndex == WIDX_SIGN_TEXT && !text.empty())
         {
-            auto signSetNameAction = SignSetNameAction(number, std::string(text));
+            auto signSetNameAction = SignSetNameAction(BannerIndex::FromUnderlying(number), std::string(text));
             GameActions::Execute(&signSetNameAction);
         }
     }
@@ -288,7 +290,7 @@ public:
     {
         RemoveViewport();
 
-        auto banner = GetBanner(number);
+        auto banner = GetBanner(BannerIndex::FromUnderlying(number));
         if (banner == nullptr)
         {
             return;

src/openrct2-ui/windows/TileInspector.cpp
@@ -2328,8 +2328,8 @@ static void WindowTileInspectorScrollpaint(rct_window* w, rct_drawpixelinfo* dpi
                 break;
             case TILE_ELEMENT_TYPE_BANNER:
                 snprintf(
-                    buffer, sizeof(buffer), "%s (%d)", language_get_string(STR_BANNER_WINDOW_TITLE),
-                    tileElement->AsBanner()->GetIndex());
+                    buffer, sizeof(buffer), "%s (%u)", language_get_string(STR_BANNER_WINDOW_TITLE),
+                    tileElement->AsBanner()->GetIndex().ToUnderlying());
                 typeName = buffer;
                 break;
             default:

src/openrct2-ui/windows/TopToolbar.cpp
@@ -2005,7 +2005,7 @@ static void WindowTopToolbarSceneryToolDown(const ScreenCoordsXY& windowPos, rct
                 {
                     auto data = result->GetData<BannerPlaceActionResult>();
                     OpenRCT2::Audio::Play3D(OpenRCT2::Audio::SoundId::PlaceItem, result->Position);
-                    context_open_detail_window(WD_BANNER, data.bannerId);
+                    context_open_detail_window(WD_BANNER, data.bannerId.ToUnderlying());
                 }
             });
             GameActions::Execute(&bannerPlaceAction);

src/openrct2/ParkFile.cpp
@@ -972,9 +972,9 @@ namespace OpenRCT2
                     cs.Write(static_cast<uint32_t>(numBanners));
 
                     [[maybe_unused]] size_t numWritten = 0;
-                    for (BannerIndex i = 0; i < MAX_BANNERS; i++)
+                    for (uint16_t i = 0; i < MAX_BANNERS; i++)
                     {
-                        auto banner = GetBanner(i);
+                        auto banner = GetBanner(BannerIndex::FromUnderlying(i));
                         if (banner != nullptr)
                         {
                             ReadWriteBanner(version, cs, *banner);
@@ -990,9 +990,9 @@ namespace OpenRCT2
                     {
                         std::vector<Banner> banners;
                         cs.ReadWriteVector(banners, [version, &cs](Banner& banner) { ReadWriteBanner(version, cs, banner); });
-                        for (size_t i = 0; i < banners.size(); i++)
+                        for (uint16_t i = 0; i < banners.size(); i++)
                         {
-                            auto bannerIndex = static_cast<BannerIndex>(i);
+                            auto bannerIndex = BannerIndex::FromUnderlying(i);
                             auto banner = GetOrCreateBanner(bannerIndex);
                             if (banner != nullptr)
                             {

src/openrct2/core/DataSerialiserTraits.h
@@ -843,3 +843,24 @@ template<> struct DataSerializerTraits_t<TileCoordsXYZD>
         stream->Write(msg, strlen(msg));
     }
 };
+
+template<typename T, T TNull, typename TTag> struct DataSerializerTraits_t<TIdentifier<T, TNull, TTag>>
+{
+    static void encode(OpenRCT2::IStream* stream, const TIdentifier<T, TNull, TTag>& id)
+    {
+        stream->WriteValue(ByteSwapBE(id.ToUnderlying()));
+    }
+
+    static void decode(OpenRCT2::IStream* stream, TIdentifier<T, TNull, TTag>& id)
+    {
+        auto temp = ByteSwapBE(stream->ReadValue<T>());
+        id = TIdentifier<T, TNull, TTag>::FromUnderlying(temp);
+    }
+
+    static void log(OpenRCT2::IStream* stream, const TIdentifier<T, TNull, TTag>& id)
+    {
+        char msg[128] = {};
+        snprintf(msg, sizeof(msg), "Id(%u)", static_cast<uint32_t>(id.ToUnderlying()));
+        stream->Write(msg, strlen(msg));
+    }
+};

commit 59d287c466250fea13d124be65206aa8d08cd289

Fix #16227: Assertion hit in fix_duplicated_banners()
Refactor error, introduced in https://github.com/OpenRCT2/OpenRCT2/commit/63a2e9d716b48235bdb71c6392462ee2b9f18504#

src/openrct2/world/Banner.cpp
@@ -285,7 +285,7 @@ void fix_duplicated_banners()
                         log_error("Failed to create new banner.");
                         continue;
                     }
-                    Guard::Assert(!activeBanners[index]);
+                    Guard::Assert(!activeBanners[newBanner->id.ToUnderlying()]);
 
                     // Copy over the original banner, but update the location
                     const auto* oldBanner = GetBanner(bannerIndex);