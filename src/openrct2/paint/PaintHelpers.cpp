e258d38b77dea08dc4e1c8b5e00c02828bfaddd2
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-03-11 11:27:28 +0000

    Remove unused function

--- src/openrct2/paint/PaintHelpers.cpp
+++ src/openrct2/paint/PaintHelpers.cpp
@@ -13,21 +13,6 @@
 
 paint_struct* PaintAddImageAsParentRotated(
     paint_session& session, uint8_t direction, uint32_t image_id, int32_t x_offset, int32_t y_offset,
-    int32_t bound_box_length_x, int32_t bound_box_length_y, int32_t bound_box_length_z, int32_t z_offset)
-{
-    if (direction & 1)
-    {
-        return PaintAddImageAsParent(
-            session, image_id, { y_offset, x_offset, z_offset },
-            { bound_box_length_y, bound_box_length_x, bound_box_length_z });
-    }
-
-    return PaintAddImageAsParent(
-        session, image_id, { x_offset, y_offset, z_offset }, { bound_box_length_x, bound_box_length_y, bound_box_length_z });
-}
-
-paint_struct* PaintAddImageAsParentRotated(
-    paint_session& session, uint8_t direction, uint32_t image_id, int32_t x_offset, int32_t y_offset,
     int32_t bound_box_length_x, int32_t bound_box_length_y, int32_t bound_box_length_z, int32_t z_offset,
     int32_t bound_box_offset_x, int32_t bound_box_offset_y, int32_t bound_box_offset_z)
 {
