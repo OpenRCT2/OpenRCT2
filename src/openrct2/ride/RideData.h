f5507c42e0c731e83cc421de6cdd9c66b1bfdfe9
X123M3-256 <X123M3-256@users.noreply.github.com>
2022-03-28 20:32:42 +0100

    Add RIDE_TYPE_FLAG_UP_INCLINE_REQUIRES_LIFT

--- src/openrct2/ride/RideData.h
+++ src/openrct2/ride/RideData.h
@@ -274,6 +274,7 @@ enum ride_type_flags : uint64_t
     RIDE_TYPE_FLAG_SUPPORTS_LEVEL_CROSSINGS = (1ULL << 49),
     RIDE_TYPE_FLAG_IS_SUSPENDED = (1ULL << 50),
     RIDE_TYPE_FLAG_HAS_LANDSCAPE_DOORS = (1ULL << 51),
+    RIDE_TYPE_FLAG_UP_INCLINE_REQUIRES_LIFT = (1ULL << 52),
 };
 
 // Set on ride types that have a main colour, additional colour and support colour.
