daeaa989277b35560a2455f919c910d154baf3d8
Hielke Morsink <hielke.morsink@gmail.com>
2022-04-24 15:10:27 +0200

    Update downloaded title sequence to v0.4.0

--- shell.nix
+++ shell.nix
@@ -22,8 +22,8 @@ let
   title-sequences-src = pkgs.fetchFromGitHub {
     owner = "OpenRCT2";
     repo = "title-sequences";
-    rev = "v0.1.2b";
-    sha256 = "a220b4c3cd3180bebb261cc59141b7fb290b433631ba9c7587a4f2c9f7dc4e4c";
+    rev = "v0.4.0";
+    sha256 = "6e7c7b554717072bfc7acb96fd0101dc8e7f0ea0ea316367a05c2e92950c9029";
 };
 in
 pkgs.stdenv.mkDerivation {
