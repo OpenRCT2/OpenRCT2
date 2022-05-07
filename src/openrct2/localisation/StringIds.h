5f2bde9375a014d33c32e2f77e82efbfc488d1ff
Hielke Morsink <hielke.morsink@gmail.com>
2022-05-07 15:42:24 +0200

    Show Tile Inspector hint when nothing is selected

--- src/openrct2/localisation/StringIds.h
+++ src/openrct2/localisation/StringIds.h
@@ -3874,6 +3874,8 @@ enum : uint16_t
     STR_GROUP_BY_TRACK_TYPE_TIP = 6495,
     STR_NEW_RIDE_VEHICLE_NAME = 6496,
 
+    STR_TILE_INSPECTOR_SELECT_TILE_HINT = 6497,
+
     // Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
     /* MAX_STR_COUNT = 32768 */ // MAX_STR_COUNT - upper limit for number of strings, not the current count strings
 };
