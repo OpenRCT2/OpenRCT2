65b2ac479bc61f1fb05aa72be1a68087d19afc13
guljam <cube4none@gmail.com>
2022-05-19 23:27:24 +0900

    added miss LandRights file

--- src/openrct2-ui/windows/LandRights.cpp
+++ src/openrct2-ui/windows/LandRights.cpp
@@ -237,7 +237,9 @@ public:
     void OnToolUpdate(rct_widgetindex widgetIndex, const ScreenCoordsXY& screenCoords) override
     {
         if (input_get_state() == InputState::ViewportRight)
+        {
             return;
+        }
 
         map_invalidate_selection_rect();
         gMapSelectFlags &= ~MAP_SELECT_FLAG_ENABLE;
