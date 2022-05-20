1bf57071a387896358c3012870839d7d870c525a
guljam <cube4none@gmail.com>
2022-05-21 00:37:28 +0900

    Revert "Add Classification field to RTD (#17131)"
    This reverts commit c1065db88f1d08f0417aeca484459e5006bd9cff.

--- src/openrct2/ride/shops/meta/CashMachine.h
+++ src/openrct2/ride/shops/meta/CashMachine.h
@@ -46,6 +46,5 @@ constexpr const RideTypeDescriptor CashMachineRTD =
     SET_FIELD(ColourPresets, DEFAULT_FLAT_RIDE_COLOUR_PRESET),
     SET_FIELD(ColourPreview, { 0, 0 }),
     SET_FIELD(ColourKey, RideColourKey::CashMachine),
-    SET_FIELD(Classification, RideClassification::KioskOrFacility),
 };
 // clang-format on
--- src/openrct2/ride/shops/meta/Toilets.h
+++ src/openrct2/ride/shops/meta/Toilets.h
@@ -47,6 +47,5 @@ constexpr const RideTypeDescriptor ToiletsRTD =
     SET_FIELD(ColourPresets, DEFAULT_FLAT_RIDE_COLOUR_PRESET),
     SET_FIELD(ColourPreview, { 0, 0 }),
     SET_FIELD(ColourKey, RideColourKey::Toilets),
-    SET_FIELD(Classification, RideClassification::KioskOrFacility),
 };
 // clang-format on
