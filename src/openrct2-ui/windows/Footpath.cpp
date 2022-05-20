8636e9a4be7c294aa630cc42d115e106a0fc52d1
guljam <cube4none@gmail.com>
2022-05-20 19:41:03 +0900

    apply clang format

--- src/openrct2-ui/windows/Footpath.cpp
+++ src/openrct2-ui/windows/Footpath.cpp
@@ -391,7 +391,7 @@ static void WindowFootpathToolupdate(rct_window* w, rct_widgetindex widgetIndex,
     {
         return;
     }
-    
+
     if (widgetIndex == WIDX_CONSTRUCT_ON_LAND)
     {
         WindowFootpathSetProvisionalPathAtPoint(screenCoords);
