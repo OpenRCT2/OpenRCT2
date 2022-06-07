bde81c1d34a4aca0901e46e714048a84478adf23
Duncan <duncans_pumpkin@hotmail.co.uk>
2022-06-07 12:06:57 +0100

    Move MainWindow into window class (#17351)

--- src/openrct2-ui/windows/Main.cpp
+++ src/openrct2-ui/windows/Main.cpp
@@ -16,19 +16,56 @@
 #include <openrct2/world/Footpath.h>
 
 // clang-format off
-static rct_widget window_main_widgets[] = {
+static rct_widget _mainWidgets[] = {
     MakeWidget({0, 0}, {0, 0}, WindowWidgetType::Viewport, WindowColour::Primary, STR_VIEWPORT),
     WIDGETS_END,
 };
-
-static void WindowMainSetViewportFlags(rct_viewport& viewport);
-static void WindowMainPaint(rct_window *w, rct_drawpixelinfo *dpi);
-
-static rct_window_event_list window_main_events([](auto& events)
-{
-    events.paint = &WindowMainPaint;
-});
 // clang-format on
+
+class MainWindow final : public Window
+{
+public:
+    void OnOpen() override
+    {
+        _mainWidgets[0].right = width;
+        _mainWidgets[0].bottom = height;
+        widgets = _mainWidgets;
+
+        viewport_create(this, windowPos, width, height, Focus(CoordsXYZ(0x0FFF, 0x0FFF, 0)));
+        if (viewport != nullptr)
+        {
+            SetViewportFlags();
+        }
+        gCurrentRotation = 0;
+        gShowGridLinesRefCount = 0;
+        gShowLandRightsRefCount = 0;
+        gShowConstuctionRightsRefCount = 0;
+        WindowFootpathResetSelectedPath();
+    }
+
+    void OnDraw(rct_drawpixelinfo& dpi) override
+    {
+        viewport_render(&dpi, viewport, { { dpi.x, dpi.y }, { dpi.x + dpi.width, dpi.y + dpi.height } });
+    }
+
+private:
+    void SetViewportFlags()
+    {
+        viewport->flags |= VIEWPORT_FLAG_SOUND_ON;
+        if (gConfigGeneral.invisible_rides)
+            viewport->flags |= VIEWPORT_FLAG_INVISIBLE_RIDES;
+        if (gConfigGeneral.invisible_vehicles)
+            viewport->flags |= VIEWPORT_FLAG_INVISIBLE_VEHICLES;
+        if (gConfigGeneral.invisible_trees)
+            viewport->flags |= VIEWPORT_FLAG_INVISIBLE_VEGETATION;
+        if (gConfigGeneral.invisible_scenery)
+            viewport->flags |= VIEWPORT_FLAG_INVISIBLE_SCENERY;
+        if (gConfigGeneral.invisible_paths)
+            viewport->flags |= VIEWPORT_FLAG_INVISIBLE_PATHS;
+        if (gConfigGeneral.invisible_supports)
+            viewport->flags |= VIEWPORT_FLAG_INVISIBLE_SUPPORTS;
+    }
+};
 
 /**
  * Creates the main window that holds the main viewport.
@@ -36,51 +73,5 @@ static rct_window_event_list window_main_events([](auto& events)
  */
 rct_window* WindowMainOpen()
 {
-    window_main_widgets[0].right = context_get_width();
-    window_main_widgets[0].bottom = context_get_height();
-    rct_window* window = WindowCreate(
-        ScreenCoordsXY(0, 0), window_main_widgets[0].right, window_main_widgets[0].bottom, &window_main_events, WC_MAIN_WINDOW,
-        WF_STICK_TO_BACK);
-    window->widgets = window_main_widgets;
-
-    viewport_create(window, window->windowPos, window->width, window->height, Focus(CoordsXYZ(0x0FFF, 0x0FFF, 0)));
-    if (window->viewport != nullptr)
-    {
-        WindowMainSetViewportFlags(*window->viewport);
-    }
-    gCurrentRotation = 0;
-    gShowGridLinesRefCount = 0;
-    gShowLandRightsRefCount = 0;
-    gShowConstuctionRightsRefCount = 0;
-    WindowFootpathResetSelectedPath();
-
-    return window;
-}
-
-static void WindowMainSetViewportFlags(rct_viewport& viewport)
-{
-    viewport.flags |= VIEWPORT_FLAG_SOUND_ON;
-    if (gConfigGeneral.invisible_rides)
-        viewport.flags |= VIEWPORT_FLAG_INVISIBLE_RIDES;
-    if (gConfigGeneral.invisible_vehicles)
-        viewport.flags |= VIEWPORT_FLAG_INVISIBLE_VEHICLES;
-    if (gConfigGeneral.invisible_trees)
-        viewport.flags |= VIEWPORT_FLAG_INVISIBLE_VEGETATION;
-    if (gConfigGeneral.invisible_scenery)
-        viewport.flags |= VIEWPORT_FLAG_INVISIBLE_SCENERY;
-    if (gConfigGeneral.invisible_paths)
-        viewport.flags |= VIEWPORT_FLAG_INVISIBLE_PATHS;
-    if (gConfigGeneral.invisible_supports)
-        viewport.flags |= VIEWPORT_FLAG_INVISIBLE_SUPPORTS;
-}
-
-/**
- *
- *  rct2: 0x66CCAE
- * This function immediately jumps to 0x00685BE1 this is the second function
- * decompiled.
- */
-static void WindowMainPaint(rct_window* w, rct_drawpixelinfo* dpi)
-{
-    viewport_render(dpi, w->viewport, { { dpi->x, dpi->y }, { dpi->x + dpi->width, dpi->y + dpi->height } });
+    return WindowCreate<MainWindow>(WC_MAIN_WINDOW, context_get_width(), context_get_height(), WF_STICK_TO_BACK);
 }
