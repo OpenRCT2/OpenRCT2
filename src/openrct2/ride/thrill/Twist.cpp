2d9e2de6036bfc1ff0b2080879c974813c6469d7
ywnico <96934073+ywnico@users.noreply.github.com>
2022-06-23 02:07:59 -0400

    Fix: Wrong T-shirt colors when riding Twist (#17433)
    A typo in the code had caused the wrong ImageId object to be used when
    painting the peeps, resulting in their T-shirts being set to the ride
    colors. The peeps are now painted with the correct T-shirt colors.

--- src/openrct2/ride/thrill/Twist.cpp
+++ src/openrct2/ride/thrill/Twist.cpp
@@ -67,7 +67,7 @@ static void paint_twist_structure(
         {
             imageTemplate = ImageId(0, vehicle->peep_tshirt_colours[i], vehicle->peep_tshirt_colours[i + 1]);
             auto peepFrameNum = (frameNum + i * 12) % 216;
-            imageId = imageId.WithIndex(baseImageId + 24 + peepFrameNum);
+            imageId = imageTemplate.WithIndex(baseImageId + 24 + peepFrameNum);
             PaintAddImageAsChild(
                 session, imageId, { xOffset, yOffset, height }, { 24, 24, 48 }, { xOffset + 16, yOffset + 16, height });
         }
