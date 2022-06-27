83758de2843a80612e345412f16567db59bc5fc0
Ted John <ted@brambles.org>
2022-06-28 00:16:40 +0100

    Prevent ride music popping in loudly

--- src/openrct2/ride/RideAudio.cpp
+++ src/openrct2/ride/RideAudio.cpp
@@ -202,7 +202,7 @@ namespace OpenRCT2::RideAudio
                 auto source = audioObj->GetSample(0);
                 if (source != nullptr)
                 {
-                    auto channel = CreateAudioChannel(source, MixerGroup::Sound, false);
+                    auto channel = CreateAudioChannel(source, MixerGroup::Sound, false, 0);
                     if (channel != nullptr)
                     {
                         _musicChannels.emplace_back(instance, channel, nullptr);
@@ -226,7 +226,7 @@ namespace OpenRCT2::RideAudio
                         if (source != nullptr)
                         {
                             auto shouldLoop = musicObj->GetTrackCount() == 1;
-                            auto channel = CreateAudioChannel(source, MixerGroup::RideMusic, shouldLoop);
+                            auto channel = CreateAudioChannel(source, MixerGroup::RideMusic, shouldLoop, 0);
                             if (channel != nullptr)
                             {
                                 _musicChannels.emplace_back(instance, channel, source);
