2d03b5ebd2a1925236c2a56235f99013989a0bac
Karst <karstvangalenlast@gmail.com>
2022-07-17 21:40:15 -0500

    Progress 17 july
    I am probably gonna try modifying the necessary sprites for this PR, I'll probably eventually swap these out for X's upcoming accelerator and blitz sprites. I originally didn't intend to expand this as much as I have, but I noticed sprites aren't lining up how I want them to and I think if I put in the effort it'll end up looking alot cleaner.
    
    This commit adds sprites for selecting square track on straight up to 60 degree pieces.

--- src/openrct2/localisation/StringIds.h
+++ src/openrct2/localisation/StringIds.h
@@ -3882,6 +3882,9 @@ enum : uint16_t
 
     STR_RANDOM_COLOUR = 6501,
 
+    STR_TRIANGLE_TRACK_TOOLTIP = 6502,
+    STR_SQUARE_TRACK_TOOLTIP = 6503,
+
     // Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
     /* MAX_STR_COUNT = 32768 */ // MAX_STR_COUNT - upper limit for number of strings, not the current count strings
 };
