71ad3af93051c89fe93e38a153a70f47506f902d
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-07-22 03:48:43 -0700

    Use more smart pointers in openrct2-ui (#17582)
    * Use more smart ptrs
    
    * Use move instead of copy ctor

--- src/openrct2/Game.h
+++ src/openrct2/Game.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2014-2020 OpenRCT2 developers
+ * Copyright (c) 2014-2022 OpenRCT2 developers
  *
  * For a complete list of all authors, please refer to contributors.md
  * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
@@ -12,8 +12,10 @@
 #include "common.h"
 #include "core/String.hpp"
 
+#include <memory>
 #include <string>
 
+class Intent;
 struct ParkLoadResult;
 
 enum class GameCommand : int32_t
@@ -166,7 +168,7 @@ void pause_toggle();
 bool game_is_paused();
 bool game_is_not_paused();
 void save_game();
-void* create_save_game_as_intent();
+std::unique_ptr<Intent> create_save_game_as_intent();
 void save_game_as();
 void save_game_cmd(u8string_view name = {});
 void save_game_with_name(u8string_view name);
