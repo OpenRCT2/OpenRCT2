a07a946bb0c281dd265970a394ff399f58263cc4
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2022-07-23 11:28:04 -0400

    Add fixes requested

--- src/openrct2-ui/windows/ServerStart.cpp
+++ src/openrct2-ui/windows/ServerStart.cpp
@@ -24,12 +24,6 @@
 
 using namespace OpenRCT2;
 
-static char _port[7];
-static char _name[65];
-static char _description[MAX_SERVER_DESCRIPTION_LENGTH];
-static char _greeting[CHAT_INPUT_SIZE];
-static char _password[33];
-
 // clang-format off
 enum {
     WIDX_BACKGROUND,
@@ -47,48 +41,38 @@ enum {
     WIDX_START_SERVER,
     WIDX_LOAD_SERVER
 };
-// clang-format on
+
 static constexpr const int32_t WW = 300;
 static constexpr const int32_t WH = 154;
 
 static rct_widget _windowServerStartWidgets[] = {
     MakeWidget({ 0, 0 }, { WW, WH }, WindowWidgetType::Frame, WindowColour::Primary), // panel / background
-    MakeWidget(
-        { 1, 1 }, { 298, 14 }, WindowWidgetType::Caption, WindowColour::Primary, STR_START_SERVER,
-        STR_WINDOW_TITLE_TIP), // title bar
-    MakeWidget(
-        { WW - 13, 2 }, { 11, 12 }, WindowWidgetType::CloseBox, WindowColour::Primary, STR_CLOSE_X,
-        STR_CLOSE_WINDOW_TIP),                                                                // close x button
+    MakeWidget({ 1, 1 }, { 298, 14 }, WindowWidgetType::Caption, WindowColour::Primary, STR_START_SERVER,STR_WINDOW_TITLE_TIP), // title bar
+    MakeWidget({ WW - 13, 2 }, { 11, 12 }, WindowWidgetType::CloseBox, WindowColour::Primary, STR_CLOSE_X,STR_CLOSE_WINDOW_TIP), // close x button
     MakeWidget({ 120, 20 }, { 173, 13 }, WindowWidgetType::TextBox, WindowColour::Secondary), // port text box
     MakeWidget({ 120, 36 }, { 173, 13 }, WindowWidgetType::TextBox, WindowColour::Secondary), // name text box
     MakeWidget({ 120, 52 }, { 173, 13 }, WindowWidgetType::TextBox, WindowColour::Secondary), // description text box
     MakeWidget({ 120, 68 }, { 173, 13 }, WindowWidgetType::TextBox, WindowColour::Secondary), // greeting text box
     MakeWidget({ 120, 84 }, { 173, 13 }, WindowWidgetType::TextBox, WindowColour::Secondary), // password text box
-    MakeSpinnerWidgets(
-        { 120, 100 }, { 173, 12 }, WindowWidgetType::Spinner, WindowColour::Secondary,
-        STR_SERVER_MAX_PLAYERS_VALUE), // max players (3 widgets)
-    MakeWidget(
-        { 6, 117 }, { 287, 14 }, WindowWidgetType::Checkbox, WindowColour::Secondary, STR_ADVERTISE,
-        STR_ADVERTISE_SERVER_TIP), // advertise checkbox
-    MakeWidget(
-        { 6, WH - 6 - 13 }, { 101, 14 }, WindowWidgetType::Button, WindowColour::Secondary,
-        STR_NEW_GAME), // start server button
+    MakeSpinnerWidgets({ 120, 100 }, { 173, 12 }, WindowWidgetType::Spinner, WindowColour::Secondary,STR_SERVER_MAX_PLAYERS_VALUE), // max players (3 widgets)
+    MakeWidget({ 6, 117 }, { 287, 14 }, WindowWidgetType::Checkbox, WindowColour::Secondary, STR_ADVERTISE,STR_ADVERTISE_SERVER_TIP), // advertise checkbox
+    MakeWidget({ 6, WH - 6 - 13 }, { 101, 14 }, WindowWidgetType::Button, WindowColour::Secondary,STR_NEW_GAME), // start server button
     MakeWidget({ 112, WH - 6 - 13 }, { 101, 14 }, WindowWidgetType::Button, WindowColour::Secondary, STR_LOAD_GAME), // None
     WIDGETS_END,
 };
-
+// clang-format on
 class ServerStartWindow final : public Window
 {
 public:
     void OnOpen() override
     {
-        _windowServerStartWidgets[WIDX_PORT_INPUT].string = _port;
-        _windowServerStartWidgets[WIDX_NAME_INPUT].string = _name;
-        _windowServerStartWidgets[WIDX_DESCRIPTION_INPUT].string = _description;
-        _windowServerStartWidgets[WIDX_GREETING_INPUT].string = _greeting;
-        _windowServerStartWidgets[WIDX_PASSWORD_INPUT].string = _password;
         widgets = _windowServerStartWidgets;
-        WindowInitScrollWidgets(this);
+        widgets[WIDX_PORT_INPUT].string = _port;
+        widgets[WIDX_NAME_INPUT].string = _name;
+        widgets[WIDX_DESCRIPTION_INPUT].string = _description;
+        widgets[WIDX_GREETING_INPUT].string = _greeting;
+        widgets[WIDX_PASSWORD_INPUT].string = _password;
+        InitScrollWidgets();
         no_list_items = 0;
         selected_list_item = -1;
         frame_no = 0;
@@ -105,15 +89,12 @@ public:
         safe_strcpy(_description, gConfigNetwork.server_description.c_str(), sizeof(_description));
         safe_strcpy(_greeting, gConfigNetwork.server_greeting.c_str(), sizeof(_greeting));
     }
-    void OnClose() override
-    {
-    }
     void OnMouseUp(rct_widgetindex widgetIndex) override
     {
         switch (widgetIndex)
         {
             case WIDX_CLOSE:
-                window_close(this);
+                Close();
                 break;
             case WIDX_PORT_INPUT:
                 window_start_textbox(this, widgetIndex, STR_STRING, _port, 6);
@@ -136,7 +117,7 @@ public:
                     gConfigNetwork.maxplayers++;
                 }
                 config_save_default();
-                OnPrepareDraw();
+                Invalidate();
                 break;
             case WIDX_MAXPLAYERS_DECREASE:
                 if (gConfigNetwork.maxplayers > 1)
@@ -144,12 +125,12 @@ public:
                     gConfigNetwork.maxplayers--;
                 }
                 config_save_default();
-                OnPrepareDraw();
+                Invalidate();
                 break;
             case WIDX_ADVERTISE_CHECKBOX:
                 gConfigNetwork.advertise = !gConfigNetwork.advertise;
                 config_save_default();
-                OnPrepareDraw();
+                Invalidate();
                 break;
             case WIDX_START_SERVER:
                 network_set_password(_password);
@@ -163,6 +144,15 @@ public:
                 context_open_intent(&intent);
                 break;
         }
+    }
+    void OnPrepareDraw() override
+    {
+        ColourSchemeUpdateByClass(this, WC_SERVER_LIST);
+
+        WidgetSetCheckboxValue(this, WIDX_ADVERTISE_CHECKBOX, gConfigNetwork.advertise);
+        auto ft = Formatter::Common();
+        ft.Increment(18);
+        ft.Add<uint16_t>(gConfigNetwork.maxplayers);
     }
     void OnUpdate() override
     {
@@ -180,16 +170,18 @@ public:
         if (text.empty())
             return;
 
+        std::string temp = static_cast<std::string>(text);
+
         switch (widgetIndex)
         {
             case WIDX_PORT_INPUT:
-                if (strcmp(_port, text.data()) == 0)
+                if (strcmp(_port, temp.c_str()) == 0)
                     return;
 
                 std::fill_n(_port, sizeof(_port), 0x00);
                 if (text[0] != '\0')
                 {
-                    safe_strcpy(_port, text.data(), sizeof(_port));
+                    safe_strcpy(_port, temp.c_str(), sizeof(_port));
                 }
 
                 gConfigNetwork.default_port = atoi(_port);
@@ -198,13 +190,13 @@ public:
                 widget_invalidate(this, WIDX_NAME_INPUT);
                 break;
             case WIDX_NAME_INPUT:
-                if (strcmp(_name, text.data()) == 0)
+                if (strcmp(_name, temp.c_str()) == 0)
                     return;
 
                 std::fill_n(_name, sizeof(_name), 0x00);
                 if (text[0] != '\0')
                 {
-                    safe_strcpy(_name, text.data(), sizeof(_name));
+                    safe_strcpy(_name, temp.c_str(), sizeof(_name));
                 }
 
                 if (_name[0] != '\0')
@@ -216,13 +208,13 @@ public:
                 widget_invalidate(this, WIDX_NAME_INPUT);
                 break;
             case WIDX_DESCRIPTION_INPUT:
-                if (strcmp(_description, text.data()) == 0)
+                if (strcmp(_description, temp.c_str()) == 0)
                     return;
 
                 std::fill_n(_description, sizeof(_description), 0x00);
                 if (text[0] != '\0')
                 {
-                    safe_strcpy(_description, text.data(), sizeof(_description));
+                    safe_strcpy(_description, temp.c_str(), sizeof(_description));
                 }
 
                 if (_description[0] != '\0')
@@ -234,13 +226,13 @@ public:
                 widget_invalidate(this, WIDX_DESCRIPTION_INPUT);
                 break;
             case WIDX_GREETING_INPUT:
-                if (strcmp(_greeting, text.data()) == 0)
+                if (strcmp(_greeting, temp.c_str()) == 0)
                     return;
 
                 std::fill_n(_greeting, sizeof(_greeting), 0x00);
                 if (text[0] != '\0')
                 {
-                    safe_strcpy(_greeting, text.data(), sizeof(_greeting));
+                    safe_strcpy(_greeting, temp.c_str(), sizeof(_greeting));
                 }
 
                 if (_greeting[0] != '\0')
@@ -252,33 +244,23 @@ public:
                 widget_invalidate(this, WIDX_GREETING_INPUT);
                 break;
             case WIDX_PASSWORD_INPUT:
-                if (strcmp(_password, text.data()) == 0)
+                if (strcmp(_password, temp.c_str()) == 0)
                     return;
 
                 std::fill_n(_password, sizeof(_password), 0x00);
                 if (text[0] != '\0')
                 {
-                    safe_strcpy(_password, text.data(), sizeof(_password));
+                    safe_strcpy(_password, temp.c_str(), sizeof(_password));
                 }
 
                 widget_invalidate(this, WIDX_PASSWORD_INPUT);
                 break;
         }
     }
-    void OnPrepareDraw() override
-    {
-        ColourSchemeUpdateByClass(this, WC_SERVER_LIST);
-
-        WidgetSetCheckboxValue(this, WIDX_ADVERTISE_CHECKBOX, gConfigNetwork.advertise);
-        auto ft = Formatter::Common();
-        ft.Increment(18);
-        ft.Add<uint16_t>(gConfigNetwork.maxplayers);
-    }
 
     void OnDraw(rct_drawpixelinfo& dpi) override
     {
-        WindowDrawWidgets(this, &dpi);
-
+        DrawWidgets(dpi);
         DrawTextBasic(&dpi, windowPos + ScreenCoordsXY{ 6, widgets[WIDX_PORT_INPUT].top }, STR_PORT, {}, { colours[1] });
         DrawTextBasic(&dpi, windowPos + ScreenCoordsXY{ 6, widgets[WIDX_NAME_INPUT].top }, STR_SERVER_NAME, {}, { colours[1] });
         DrawTextBasic(
@@ -292,6 +274,11 @@ public:
     }
 
 private:
+    static char _port[7];
+    static char _name[65];
+    static char _description[MAX_SERVER_DESCRIPTION_LENGTH];
+    static char _greeting[CHAT_INPUT_SIZE];
+    static char _password[33];
     static void ScenarioSelectCallback(const utf8* path)
     {
         game_notify_map_change();
