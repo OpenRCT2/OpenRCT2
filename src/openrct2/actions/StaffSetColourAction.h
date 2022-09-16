784589a4f136936e0f32defe35055faa9bd00a71
Austin Burk <apburk@gmail.com>
2022-09-16 17:20:51 -0400

    Implement staffsetcolour API method Example usage (set handyman uniforms to orange): `context.executeAction("staffsetcolour", { "staffType": 0, "colour": 20 })`

--- src/openrct2/actions/StaffSetColourAction.h
+++ src/openrct2/actions/StaffSetColourAction.h
@@ -21,6 +21,8 @@ public:
     StaffSetColourAction() = default;
     StaffSetColourAction(StaffType staffType, uint8_t colour);
 
+    void AcceptParameters(GameActionParameterVisitor& visitor) override;
+
     uint16_t GetActionFlags() const override;
 
     void Serialise(DataSerialiser& stream) override;
