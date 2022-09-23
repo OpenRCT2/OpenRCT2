d6ca1a3da939df4a714ffdef4a32833e1ce6b58e
Meehoi <beam_baimon@hotmail.com>
2022-09-24 03:51:05 +0700

    Fix #18035: Favourited servers don't get their online status updated
    From fetching flow, list will always have favourited servers but no code to update those entries, only append.
    This code updates those favourited servers in the list with new data from internet by matching address.
    This will fix list having 2 duplicate servers too. (one favourited and one not)

--- src/openrct2/network/ServerList.h
+++ src/openrct2/network/ServerList.h
@@ -60,6 +60,7 @@ public:
     size_t GetCount() const;
     void Add(const ServerListEntry& entry);
     void AddRange(const std::vector<ServerListEntry>& entries);
+    void AddOrUpdateRange(const std::vector<ServerListEntry>& entries);
     void Clear() noexcept;
 
     void ReadAndAddFavourites();
