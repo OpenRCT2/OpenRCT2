8aab113e745095a6cbb301f09f7598077731b7bf
ebstalk <114045456+ebstalk@users.noreply.github.com>
2022-09-27 21:45:37 -0500

    Update Paint.Entity.cpp

--- src/openrct2/paint/Paint.Entity.cpp
+++ src/openrct2/paint/Paint.Entity.cpp
@@ -85,11 +85,11 @@ void EntityPaintSetup(paint_session& session, const CoordsXY& pos)
             {
                 continue;
             }
-            if (entityPos.x < gClipSelectionA.x || entityPos.x > gClipSelectionB.x)
+            if (entityPos.x < gClipSelectionA.x || entityPos.x > gClipSelectionB.x + 28)
             {
                 continue;
             }
-            if (entityPos.y < gClipSelectionA.y || entityPos.y > gClipSelectionB.y)
+            if (entityPos.y < gClipSelectionA.y || entityPos.y > gClipSelectionB.y + 28)
             {
                 continue;
             }
