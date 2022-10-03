b78c977e12459bcf807f90573e830231bf3203f4
Meehoi <beam_baimon@hotmail.com>
2022-10-03 19:53:36 +0700

    fix #18174: Height marks on land do not appear

--- src/openrct2/paint/tile_element/Paint.Surface.cpp
+++ src/openrct2/paint/tile_element/Paint.Surface.cpp
@@ -1088,11 +1088,11 @@ void PaintSurface(paint_session& session, uint8_t direction, uint16_t height, co
         int32_t surfaceHeight = tile_element_height({ x + 16, y + 16 });
         int32_t dx = surfaceHeight + 3;
 
-        int32_t image_id = (SPR_HEIGHT_MARKER_BASE + dx / 16) | 0x20780000;
+        int32_t image_id = (SPR_HEIGHT_MARKER_BASE + dx / 16);
         image_id += get_height_marker_offset();
         image_id -= gMapBaseZ;
 
-        PaintAddImageAsParent(session, ImageId(image_id), { 16, 16, surfaceHeight }, { 1, 1, 0 });
+        PaintAddImageAsParent(session, ImageId(image_id, COLOUR_OLIVE_GREEN), { 16, 16, surfaceHeight }, { 1, 1, 0 });
     }
 
     bool has_surface = false;
