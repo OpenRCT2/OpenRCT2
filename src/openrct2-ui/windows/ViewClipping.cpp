33178254fcb1e2335de1379cc28d81fc7c75a3bc
Huu Kim Nguyen <73300809+CoderUndefined@users.noreply.github.com>
2022-10-05 23:37:28 +0200

    Update ViewClipping.cpp
    last attempt

--- src/openrct2-ui/windows/ViewClipping.cpp
+++ src/openrct2-ui/windows/ViewClipping.cpp
@@ -342,13 +342,13 @@ public:
     }
 
     void OnOpen() override
-    {   
+    {
         if (_previousClipSelectionA == NULL || _previousClipSelectionB == NULL) 
         {
             _previousClipSelectionA = { 0, 0 };
             _previousClipSelectionB = gMapSize.ToCoordsXY();
         }
-        
+
         this->widgets = window_view_clipping_widgets;
         this->hold_down_widgets = (1ULL << WIDX_CLIP_HEIGHT_INCREASE) | (1UL << WIDX_CLIP_HEIGHT_DECREASE);
         WindowInitScrollWidgets(this);
