bba54d2ecc78bc86a5bc1a04445d687c0a8c3d43
Rik Smeets <30838294+rik-smeets@users.noreply.github.com>
2022-10-08 21:46:58 +0200

    Fix guests 'waiting' on extended railway crossings
    On extended railway crossings, guests would stop to 'wait' for the passing train, which would then phase right through them. This change at least allows guests a chance to get to a safe place.

--- src/openrct2/entity/Peep.cpp
+++ src/openrct2/entity/Peep.cpp
@@ -324,6 +324,12 @@ bool Peep::CheckForPath()
 bool Peep::PathIsBlockedByVehicle()
 {
     auto curPos = TileCoordsXYZ(GetLocation());
+    if (FootpathIsBlockedByVehicle(curPos))
+    {
+        // If current position is blocked, try to get out of the way
+        return false;
+    }
+
     auto dstPos = TileCoordsXYZ(CoordsXYZ{ GetDestination(), NextLoc.z });
     if ((curPos.x != dstPos.x || curPos.y != dstPos.y) && FootpathIsBlockedByVehicle(dstPos))
     {
