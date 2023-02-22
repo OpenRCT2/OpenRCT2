4e50c11428eb22a6e2f03489b7bcc32660c95dc2
Trevor Finney <8711258+finneyt@users.noreply.github.com>
2023-02-22 09:11:14 -0500

    Fix invisible color scheme sprite

--- src/openrct2-ui/windows/Dropdown.cpp
+++ src/openrct2-ui/windows/Dropdown.cpp
@@ -459,7 +459,7 @@ void WindowDropdownShowColour(WindowBase* w, Widget* widget, uint8_t dropdownCol
             defaultIndex = orderedColour;
 
         // Use special graphic for Invisible colour
-        auto imageId = (orderedColour == COLOUR_OFFSET_INVISIBLE) ? ImageId(SPR_G2_ICON_PALETTE_INVISIBLE - 1, COLOUR_WHITE) : ImageId(SPR_PALETTE_BTN, orderedColour);
+        auto imageId = (orderedColour == COLOUR_OFFSET_INVISIBLE) ? ImageId(SPR_G2_ICON_PALETTE_INVISIBLE, COLOUR_WHITE) : ImageId(SPR_PALETTE_BTN, orderedColour);
 
         gDropdownItems[i].Format = Dropdown::FormatColourPicker;
         gDropdownItems[i].Args = (i << 32) | imageId.ToUInt32();
