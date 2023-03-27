1f7ef646279516a83112378aee65699410be06a1
Michał Janiszewski <janisozaur@users.noreply.github.com>
2023-03-28 00:16:55 +0200

    Fix #19574: Handle exits in null locations (#19727)
    In park/replay from dump #19574 there was a calamity that happened
    likely due to a plugin. Pretty much all rides were deleted in single
    tick, which caused the park to mark some locations as null and then it
    tripped on one of the many assertions. In many of the places it already
    bails out if ride or vehicle is a nullptr, so I followed with similar
    approach for invalid ride exits.

--- src/openrct2/world/Footpath.h
+++ src/openrct2/world/Footpath.h
@@ -180,9 +180,9 @@ extern uint8_t gFootpathConstructSlope;
 extern uint8_t gFootpathGroundFlags;
 
 // Given a direction, this will return how to increase/decrease the x and y coordinates.
-extern const CoordsXY DirectionOffsets[NumOrthogonalDirections];
-extern const CoordsXY BinUseOffsets[NumOrthogonalDirections];
-extern const CoordsXY BenchUseOffsets[NumOrthogonalDirections * 2];
+extern const std::array<CoordsXY, NumOrthogonalDirections> DirectionOffsets;
+extern const std::array<CoordsXY, NumOrthogonalDirections> BinUseOffsets;
+extern const std::array<CoordsXY, NumOrthogonalDirections * 2> BenchUseOffsets;
 
 TileElement* MapGetFootpathElement(const CoordsXYZ& coords);
 void FootpathInterruptPeeps(const CoordsXYZ& footpathPos);
