5adbea9ac829b76507543e4c09b2000138ffbbcf
Karst <karstvangalenlast@gmail.com>
2023-03-30 19:41:42 +0200

    Select OpenMusic by default when opening a new scenario

--- src/openrct2/rct12/RCT12.cpp
+++ src/openrct2/rct12/RCT12.cpp
@@ -750,6 +750,11 @@ static constexpr std::string_view _musicStyles[] = {
     "rct2.music.pirate",
     "rct2.music.rock3",
     "rct2.music.candy",
+    "openrct2.music.galaxy",
+    "openrct2.music.acid",
+    "openrct2.music.dodgems",
+    "openrct2.music.blizzard",
+    "openrct2.music.extraterrestrial",
 };
 
 std::string_view GetStationIdentifierFromStyle(uint8_t style)
