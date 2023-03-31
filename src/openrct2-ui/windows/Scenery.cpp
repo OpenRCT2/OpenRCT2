38f7c73869bd5b1dc80fb86fd53bd54d16865a69
fidwell <5436387+fidwell@users.noreply.github.com>
2023-03-30 20:48:20 -0400

    Fixing type conversion warning

--- src/openrct2-ui/windows/Scenery.cpp
+++ src/openrct2-ui/windows/Scenery.cpp
@@ -1232,7 +1232,7 @@ private:
 
     int32_t GetTabRowCount()
     {
-        return std::max<int32_t>((_tabEntries.size() + MaxTabsPerRow - 1) / MaxTabsPerRow, 0);
+        return std::max<int32_t>((static_cast<int32_t>(_tabEntries.size()) + MaxTabsPerRow - 1) / MaxTabsPerRow, 0);
     }
 
     int32_t GetMaxTabCountInARow()
