c1b709c674777cabd3d625c324745349e23b880c
Tom <tr.charlesworth1@gmail.com>
2023-04-08 13:37:38 +0000

    Refactor window to class: Research (#13794)
    - Fix style issues

--- src/openrct2-ui/windows/Research.cpp
+++ src/openrct2-ui/windows/Research.cpp
@@ -295,7 +295,6 @@ class ResearchWindow final : public Window
         DrawTabImage(dpi, WINDOW_RESEARCH_PAGE_DEVELOPMENT, SPR_TAB_FINANCES_RESEARCH_0);
         DrawTabImage(dpi, WINDOW_RESEARCH_PAGE_FUNDING, SPR_TAB_FINANCES_SUMMARY_0);
     }
-
 };
 
 WindowBase* WindowResearchOpen()
@@ -347,7 +346,8 @@ void WindowResearchDevelopmentDraw(WindowBase* w, DrawPixelInfo& dpi, WidgetInde
 {
     // Offset the widget index to allow reuse from other windows
     baseWidgetIndex = baseWidgetIndex - WIDX_CURRENTLY_IN_DEVELOPMENT_GROUP;
-    auto screenCoords = w->windowPos + ScreenCoordsXY{ 10, w->widgets[WIDX_CURRENTLY_IN_DEVELOPMENT_GROUP + baseWidgetIndex].top + 12 };
+    auto screenCoords = w->windowPos
+        + ScreenCoordsXY{ 10, w->widgets[WIDX_CURRENTLY_IN_DEVELOPMENT_GROUP + baseWidgetIndex].top + 12 };
 
     if (gResearchProgressStage == RESEARCH_STAGE_FINISHED_ALL)
     {
@@ -488,8 +488,13 @@ void WindowResearchFundingMouseDown(WindowBase* w, WidgetIndex widgetIndex, Widg
         gDropdownItems[i].Args = ResearchFundingLevelNames[i];
     }
     WindowDropdownShowTextCustomWidth(
-        { w->windowPos.x + dropdownWidget->left, w->windowPos.y + dropdownWidget->top }, dropdownWidget->height() + 1, w->colours[1], 0,
-        Dropdown::Flag::StayOpen, 4, dropdownWidget->width() - 3);
+        { w->windowPos.x + dropdownWidget->left, w->windowPos.y + dropdownWidget->top },
+        dropdownWidget->height() + 1,
+         w->colours[1],
+         0,
+         Dropdown::Flag::StayOpen,
+         4,
+         dropdownWidget->width() - 3);
 
     int32_t currentResearchLevel = gResearchFundingLevel;
     Dropdown::SetChecked(currentResearchLevel, true);
@@ -581,9 +586,9 @@ void WindowResearchFundingPrepareDraw(WindowBase* w, WidgetIndex baseWidgetIndex
 }
 
 /**
-*
-*  rct2: 0x0069DAF0
-*/
+ *
+ *  rct2: 0x0069DAF0
+ */
 void WindowResearchFundingDraw(WindowBase* w, DrawPixelInfo& dpi)
 {
     if (gParkFlags & PARK_FLAGS_NO_MONEY)
