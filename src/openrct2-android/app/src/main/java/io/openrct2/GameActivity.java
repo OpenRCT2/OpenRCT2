78aa02fe8a3c335a8210068a9f6201e84e960c9e
undermark5 <nsshurtz@gmail.com>
2023-04-13 04:30:18 -0500

    Fix #13130: Android respects device locale (#19419)
    Co-authored-by: Tulio Leao <tupaschoal@gmail.com>

--- src/openrct2-android/app/src/main/java/io/openrct2/GameActivity.java
+++ src/openrct2-android/app/src/main/java/io/openrct2/GameActivity.java
@@ -1,13 +1,78 @@
 package io.openrct2;
 
+import android.icu.util.Currency;
+import android.icu.util.LocaleData;
+import android.icu.util.ULocale;
+import android.os.Build;
 import android.view.View;
 
 import org.libsdl.app.SDLActivity;
+
+import java.util.Locale;
 
 public class GameActivity extends SDLActivity {
 
     public float getDefaultScale() {
         return getResources().getDisplayMetrics().density;
+    }
+
+    public String getDefaultLocale(String[] supportedTags) {
+        Locale deviceLocale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            deviceLocale = getResources().getConfiguration().getLocales().get(0);
+        } else {
+            deviceLocale = getResources().getConfiguration().locale;
+        }
+
+        for (String supportedTag : supportedTags) {
+            if (supportedTag.isEmpty()) continue;
+            String[] splits = supportedTag.split("-");
+            String language = splits[0];
+            String country = splits[1];
+            if (deviceLocale.getLanguage().equals(language) && deviceLocale.getCountry().equals(country)) {
+                return supportedTag;
+            }
+        }
+
+        Locale canadaEn = Locale.CANADA;
+        if (canadaEn.getLanguage().equals(deviceLocale.getLanguage()) && canadaEn.getCountry().equals(deviceLocale.getCountry())) {
+            return "en-US";
+        }
+
+        for (String supportedTag : supportedTags) {
+            if (supportedTag.isEmpty()) continue;
+            String[] splits = supportedTag.split("-");
+            String language = splits[0];
+            if (deviceLocale.getLanguage().equals(language)) {
+                return supportedTag;
+            }
+        }
+        return "en-UK";
+    }
+
+    public String getLocaleCurrency() {
+        Locale deviceLocale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            deviceLocale = getResources().getConfiguration().getLocales().get(0);
+            return Currency.getInstance(deviceLocale).getCurrencyCode();
+        } else {
+            deviceLocale = getResources().getConfiguration().locale;
+            return java.util.Currency.getInstance(deviceLocale).getCurrencyCode();
+        }
+    }
+
+    public boolean isImperialLocaleMeasurementFormat() {
+        Locale deviceLocale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            return LocaleData.getMeasurementSystem(ULocale.forLocale(getResources().getConfiguration().getLocales().get(0))) == LocaleData.MeasurementSystem.US;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            deviceLocale = getResources().getConfiguration().getLocales().get(0);
+        } else {
+            deviceLocale = getResources().getConfiguration().locale;
+        }
+        String localeCountry = deviceLocale.getCountry();
+        return localeCountry.equals(Locale.US.getCountry()) || localeCountry.equals(new Locale("xx", "LR").getCountry()) || localeCountry.equals(new Locale("xx", "MM").getCountry());
     }
 
     @Override
