4913f29c2cbacee40dde6db869ec20f79a338d66
Chase <chasepercy@yahoo.co.nz>
2023-04-14 17:07:56 +0800

    Fix formatting issues

--- src/openrct2/actions/RideDemolishAction.cpp
+++ src/openrct2/actions/RideDemolishAction.cpp
@@ -65,7 +65,8 @@ GameActions::Result RideDemolishAction::Query() const
     }
 
     if ((ride->lifecycle_flags & (RIDE_LIFECYCLE_INDESTRUCTIBLE | RIDE_LIFECYCLE_INDESTRUCTIBLE_TRACK)
-        && _modifyType == RIDE_MODIFY_DEMOLISH) && !gCheatsMakeAllDestructible)
+         && _modifyType == RIDE_MODIFY_DEMOLISH)
+        && !gCheatsMakeAllDestructible)
     {
         return GameActions::Result(
             GameActions::Status::NoClearance, STR_CANT_DEMOLISH_RIDE,
