664f4ef586302096d6a2d8dd1c1c5232ced4161c
Michał Janiszewski <janisozaur@gmail.com>
2023-05-07 22:03:48 +0200

    Add vpx init code

--- src/openrct2-ui/drawing/engines/SoftwareDrawingEngine.cpp
+++ src/openrct2-ui/drawing/engines/SoftwareDrawingEngine.cpp
@@ -8,7 +8,6 @@
  *****************************************************************************/
 
 #include "DrawingEngineFactory.hpp"
-#include "vpx/vpx_encoder.h"
 
 #include <SDL.h>
 #include <algorithm>
@@ -19,6 +18,8 @@
 #include <openrct2/drawing/IDrawingEngine.h>
 #include <openrct2/drawing/X8DrawingEngine.h>
 #include <openrct2/ui/UiContext.h>
+#include <vpx/vp8cx.h>
+#include <vpx/vpx_encoder.h>
 
 using namespace OpenRCT2;
 using namespace OpenRCT2::Drawing;
@@ -47,11 +48,143 @@ typedef struct VpxInterface
 
 #define VP9_FOURCC 0x30395056
 
-extern "C" vpx_codec_iface_t* vpx_codec_vp9_cx();
-
 static const VpxInterface vpx_encoders[] = {
     { "vp9", VP9_FOURCC, &vpx_codec_vp9_cx },
 };
+
+struct VpxVideoWriterStruct
+{
+    VpxVideoInfo info;
+    FILE* file;
+    int frame_count;
+};
+
+typedef struct VpxVideoWriterStruct VpxVideoWriter;
+
+typedef enum
+{
+    kContainerIVF
+} VpxContainer;
+
+static void mem_put_le16(void* dst, uint16_t data)
+{
+    memcpy(dst, &data, 2);
+}
+static void mem_put_le32(void* dst, uint32_t data)
+{
+    memcpy(dst, &data, 4);
+}
+static void ivf_write_file_header_with_video_info(
+    FILE* outfile, unsigned int fourcc, int frame_cnt, int frame_width, int frame_height, vpx_rational_t timebase)
+{
+    char header[32];
+
+    header[0] = 'D';
+    header[1] = 'K';
+    header[2] = 'I';
+    header[3] = 'F';
+    mem_put_le16(header + 4, 0);             // version
+    mem_put_le16(header + 6, 32);            // header size
+    mem_put_le32(header + 8, fourcc);        // fourcc
+    mem_put_le16(header + 12, frame_width);  // width
+    mem_put_le16(header + 14, frame_height); // height
+    mem_put_le32(header + 16, timebase.den); // rate
+    mem_put_le32(header + 20, timebase.num); // scale
+    mem_put_le32(header + 24, frame_cnt);    // length
+    mem_put_le32(header + 28, 0);            // unused
+
+    fwrite(header, 1, 32, outfile);
+}
+
+static void ivf_write_file_header(FILE* outfile, const struct vpx_codec_enc_cfg* cfg, unsigned int fourcc, int frame_cnt)
+{
+    ivf_write_file_header_with_video_info(outfile, fourcc, frame_cnt, cfg->g_w, cfg->g_h, cfg->g_timebase);
+}
+/*
+static void ivf_write_frame_header(FILE* outfile, int64_t pts, size_t frame_size)
+{
+    char header[12];
+
+    mem_put_le32(header, static_cast<int>(frame_size));
+    mem_put_le32(header + 4, static_cast<int>(pts & 0xFFFFFFFF));
+    mem_put_le32(header + 8, static_cast<int>(pts >> 32));
+    fwrite(header, 1, 12, outfile);
+}
+*/
+
+static void write_header(FILE* file, const VpxVideoInfo* info, int frame_count)
+{
+    struct vpx_codec_enc_cfg cfg;
+    cfg.g_w = info->frame_width;
+    cfg.g_h = info->frame_height;
+    cfg.g_timebase.num = info->time_base.numerator;
+    cfg.g_timebase.den = info->time_base.denominator;
+
+    ivf_write_file_header(file, &cfg, info->codec_fourcc, frame_count);
+}
+
+static VpxVideoWriter* vpx_video_writer_open(const char* filename, VpxContainer container, const VpxVideoInfo* info)
+{
+    if (container == kContainerIVF)
+    {
+        VpxVideoWriter* writer = NULL;
+        FILE* const file = fopen(filename, "wb");
+        if (!file)
+        {
+            fprintf(stderr, "%s can't be written to.\n", filename);
+            return NULL;
+        }
+        writer = static_cast<VpxVideoWriter*>(malloc(sizeof(*writer)));
+        if (!writer)
+        {
+            fprintf(stderr, "Can't allocate VpxVideoWriter.\n");
+            return NULL;
+        }
+        writer->frame_count = 0;
+        writer->info = *info;
+        writer->file = file;
+
+        write_header(writer->file, info, 0);
+
+        return writer;
+    }
+    fprintf(stderr, "VpxVideoWriter supports only IVF.\n");
+    return NULL;
+}
+
+static void vpx_video_writer_close(VpxVideoWriter* writer)
+{
+    if (writer)
+    {
+        // Rewriting frame header with real frame count
+        rewind(writer->file);
+        write_header(writer->file, &writer->info, writer->frame_count);
+
+        fclose(writer->file);
+        free(writer);
+    }
+}
+/*
+static int vpx_video_writer_write_frame(VpxVideoWriter* writer, const uint8_t* buffer, size_t size, int64_t pts)
+{
+    ivf_write_frame_header(writer->file, pts, size);
+    if (fwrite(buffer, 1, size, writer->file) != size)
+        return 0;
+
+    ++writer->frame_count;
+
+    return 1;
+}
+*/
+static void die_codec(vpx_codec_ctx_t* ctx, const char* s)
+{
+    const char* detail = vpx_codec_error_detail(ctx);
+
+    LOG_ERROR("%s: %s\n", s, vpx_codec_error(ctx));
+    if (detail)
+        LOG_ERROR("    %s\n", detail);
+    exit(EXIT_FAILURE);
+}
 
 class SoftwareDrawingEngine final : public X8DrawingEngine
 {
@@ -64,6 +197,8 @@ private:
     VpxVideoInfo info{};
     const VpxInterface* encoder = NULL;
     vpx_image_t raw{};
+    VpxVideoWriter* writer = NULL;
+    vpx_codec_ctx_t codec{};
 
 public:
     explicit SoftwareDrawingEngine(const std::shared_ptr<IUiContext>& uiContext)
@@ -121,10 +256,42 @@ public:
             LOG_FATAL("Invalid frame size: %dx%d", info.frame_width, info.frame_height);
         }
 
-        if (!vpx_img_alloc(&raw, VPX_IMG_FMT_I420, info.frame_width, info.frame_height, 1))
+        if (!vpx_img_alloc(&raw, VPX_IMG_FMT_I444, info.frame_width, info.frame_height, 1))
         {
             LOG_FATAL("Failed to allocate image.");
         }
+        LOG_INFO("Using %s\n", vpx_codec_iface_name(encoder->codec_interface()));
+        vpx_codec_enc_cfg_t cfg{};
+        vpx_codec_err_t res = vpx_codec_enc_config_default(encoder->codec_interface(), &cfg, 0);
+        if (res)
+        {
+            die_codec(&codec, "Failed to get default codec config.");
+        }
+        cfg.g_w = info.frame_width;
+        cfg.g_h = info.frame_height;
+        cfg.g_timebase.num = info.time_base.numerator;
+        cfg.g_timebase.den = info.time_base.denominator;
+        cfg.g_threads = 8;
+
+        writer = vpx_video_writer_open("/tmp/out.webm", kContainerIVF, &info);
+        if (!writer)
+        {
+            LOG_FATAL("Failed to open %s for writing.", "/tmp/out.webm");
+        }
+        if (vpx_codec_enc_init(&codec, encoder->codec_interface(), &cfg, 0))
+        {
+            LOG_FATAL("Failed to initialize encoder");
+        }
+        if (vpx_codec_control_(&codec, VP9E_SET_LOSSLESS, 1))
+        {
+            die_codec(&codec, "Failed to use lossless mode");
+        }
+        vpx_img_free(&raw);
+        if (vpx_codec_destroy(&codec))
+        {
+            die_codec(&codec, "Failed to destroy codec.");
+        }
+        vpx_video_writer_close(writer);
 
         ConfigureBits(width, height, _surface->pitch);
     }
