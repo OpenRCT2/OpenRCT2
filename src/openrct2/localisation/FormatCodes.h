5986a71e71f851bf5b51078cbd6b20da86a5d4c9
Gymnasiast <m.o.steenbeek@gmail.com>
2023-06-13 22:33:13 +0200

    Add Cyrillic characters Ѕѕ, Іі, Її, and Јј to the sprite font

--- src/openrct2/localisation/FormatCodes.h
+++ src/openrct2/localisation/FormatCodes.h
@@ -213,6 +213,10 @@ namespace UnicodeChar
 
     // Cyrillic alphabet
     constexpr char32_t cyrillic_io_uc = 0x401;
+    constexpr char32_t cyrillic_dze_uc = 0x0405;
+    constexpr char32_t cyrillic_dotted_i_uc = 0x0406;
+    constexpr char32_t cyrillic_yi_uc = 0x0407;
+    constexpr char32_t cyrillic_je_uc = 0x0408;
     constexpr char32_t cyrillic_a_uc = 0x410;
     constexpr char32_t cyrillic_be_uc = 0x411;
     constexpr char32_t cyrillic_ve_uc = 0x412;
@@ -280,6 +284,10 @@ namespace UnicodeChar
     constexpr char32_t cyrillic_ya = 0x44F;
 
     constexpr char32_t cyrillic_io = 0x451;
+    constexpr char32_t cyrillic_dze = 0x0455;
+    constexpr char32_t cyrillic_dotted_i = 0x0456;
+    constexpr char32_t cyrillic_yi = 0x0457;
+    constexpr char32_t cyrillic_je = 0x0458;
 
     // Punctuation
     constexpr char32_t left_brace = 0x7B;
