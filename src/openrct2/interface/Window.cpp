42ff4716172d07c4555c7519e9d91c379b1a95ed
Matthias Moninger <5415177+ZehMatt@users.noreply.github.com>
2023-06-22 16:52:14 +0300

    Fix periodic window update function being called every frame

--- src/openrct2/interface/Window.cpp
+++ src/openrct2/interface/Window.cpp
@@ -129,13 +129,10 @@ void WindowUpdateAllViewports()
  */
 void WindowUpdateAll()
 {
-    // WindowUpdateAllViewports();
-
-    // 1000 tick update
-    gWindowUpdateTicks += gCurrentDeltaTime;
-    if (gWindowUpdateTicks >= 1000)
+    // Periodic update happens every second so 40 ticks.
+    if (gCurrentRealTimeTicks >= gWindowUpdateTicks)
     {
-        gWindowUpdateTicks = 0;
+        gWindowUpdateTicks = gCurrentRealTimeTicks + GAME_UPDATE_FPS;
 
         WindowVisitEach([](WindowBase* w) { WindowEventPeriodicUpdateCall(w); });
     }
