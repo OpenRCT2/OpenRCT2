ec2c8f150e0f3f8b812e6cca7883c0783febb84c
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-06-30 20:35:48 +0300

    Hopefully last of the string things

--- test/tests/Pathfinding.cpp
+++ test/tests/Pathfinding.cpp
@@ -11,6 +11,7 @@
 #include <openrct2/Game.h>
 #include <openrct2/OpenRCT2.h>
 #include <openrct2/ParkImporter.h>
+#include <openrct2/core/String.hpp>
 #include <openrct2/platform/Platform.h>
 #include <openrct2/world/Footpath.h>
 #include <openrct2/world/Map.h>
@@ -57,7 +58,7 @@ protected:
         for (auto& ride : GetRideManager())
         {
             auto thisName = ride.GetName();
-            if (!_strnicmp(thisName.c_str(), name, sizeof(thisName)))
+            if (String::StartsWith(thisName, u8string{ name }, true))
             {
                 return &ride;
             }
