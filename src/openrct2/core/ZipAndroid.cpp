78a36f7f2ae1c98e16837479456022dcd1f3589f
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-06-30 21:18:32 +0300

    Replace STUB directly with the warning

--- src/openrct2/core/ZipAndroid.cpp
+++ src/openrct2/core/ZipAndroid.cpp
@@ -121,17 +121,17 @@ public:
 
     void SetFileData(std::string_view path, std::vector<uint8_t>&& data) override
     {
-        LOG_WARNING("Function %s at %s:%d is a stub.", __PRETTY_FUNCTION__, __FILE__, __LINE__)
+        LOG_WARNING("Function %s at %s:%d is a stub.", __PRETTY_FUNCTION__, __FILE__, __LINE__);
     }
 
     void DeleteFile(std::string_view) override
     {
-        LOG_WARNING("Function %s at %s:%d is a stub.", __PRETTY_FUNCTION__, __FILE__, __LINE__)
+        LOG_WARNING("Function %s at %s:%d is a stub.", __PRETTY_FUNCTION__, __FILE__, __LINE__);
     }
 
     void RenameFile(std::string_view, std::string_view) override
     {
-        LOG_WARNING("Function %s at %s:%d is a stub.", __PRETTY_FUNCTION__, __FILE__, __LINE__)
+        LOG_WARNING("Function %s at %s:%d is a stub.", __PRETTY_FUNCTION__, __FILE__, __LINE__);
     }
 };
 
