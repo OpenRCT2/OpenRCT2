1fbc328e27ffc3265a8028410dafdb56520880aa
Spacek531 <spacek531@comcast.net>
2023-09-26 16:56:15 -0700

    bump parkfile version

--- src/openrct2/park/ParkFile.h
+++ src/openrct2/park/ParkFile.h
@@ -9,7 +9,7 @@ struct ObjectRepositoryItem;
 namespace OpenRCT2
 {
     // Current version that is saved.
-    constexpr uint32_t PARK_FILE_CURRENT_VERSION = 31;
+    constexpr uint32_t PARK_FILE_CURRENT_VERSION = 32;
 
     // The minimum version that is forwards compatible with the current version.
     constexpr uint32_t PARK_FILE_MIN_VERSION = 30;
