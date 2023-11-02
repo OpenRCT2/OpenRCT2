10bf09caf59959a6129d62d01698af569130b4c9
Realsteel89 <77458610+realsteel89@users.noreply.github.com>
2023-11-02 22:48:15 +0100

    Add Steel Spinning Roller Coaster

--- src/openrct2/ride/TrackPaint.h
+++ src/openrct2/ride/TrackPaint.h
@@ -494,3 +494,7 @@ namespace AlpineRC
     TRACK_PAINT_FUNCTION GetTrackPaintFunction(int32_t trackType);
 }
 TRACK_PAINT_FUNCTION GetTrackPaintFunctionClassicWoodenRC(int32_t trackType);
+namespace SpinningRC
+{
+    TRACK_PAINT_FUNCTION GetTrackPaintFunction(int32_t trackType);
+}
