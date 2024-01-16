6cd76dd431bd3fb330faf12786bf103c8bbb3a35
Tulio Leao <tupaschoal@gmail.com>
2024-01-15 22:23:26 -0300

    Add investigation TODO to s4 and s6 importers

--- src/openrct2/rct1/S4Importer.cpp
+++ src/openrct2/rct1/S4Importer.cpp
@@ -2493,6 +2493,7 @@ namespace RCT1
 
         void FixLandOwnership()
         {
+            // TODO: Land ownership is applied even when loading saved scenario. Should it?
             RCT12::FetchAndApplyScenarioPatch(_s4.ScenarioName);
             // TODO: investigate why Katie's Dreamland.s4 and .sea have different patches
         }
