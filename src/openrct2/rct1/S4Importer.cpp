178a2239bb2a09375e779e98a3c1c7d3554105d6
Tulio Leao <tupaschoal@gmail.com>
2024-01-20 12:25:48 -0300

    Convert S6 scenarios water fixes to patches

--- src/openrct2/rct1/S4Importer.cpp
+++ src/openrct2/rct1/S4Importer.cpp
@@ -189,7 +189,9 @@ namespace RCT1
             ImportScenarioNameDetails(gameState);
             ImportScenarioObjective(gameState);
             ImportSavedView();
-            FixLandOwnership();
+
+            // TODO: investigate why Katie's Dreamland.s4 and .sea have different patches
+            RCT12::FetchAndApplyScenarioPatch(_s4.ScenarioName, _isScenario);
             FixUrbanPark();
             FixNextGuestNumber(gameState);
             CountBlockSections();
@@ -2489,13 +2491,6 @@ namespace RCT1
             auto asUtf8 = RCT2StringToUTF8(originalStringView, RCT2LanguageId::EnglishUK);
             auto justText = RCT12RemoveFormattingUTF8(asUtf8);
             return justText.data();
-        }
-
-        void FixLandOwnership()
-        {
-            // TODO: Land ownership is applied even when loading saved scenario. Should it?
-            RCT12::FetchAndApplyScenarioPatch(_s4.ScenarioName);
-            // TODO: investigate why Katie's Dreamland.s4 and .sea have different patches
         }
 
         /**
