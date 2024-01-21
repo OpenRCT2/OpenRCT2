b47cd0cfd804f9b773dd7ea6d6cfc6ce0eac980a
Tulio Leao <tupaschoal@gmail.com>
2024-01-21 08:19:40 -0300

    Use vector over initializer_list on FixLandOwnershipTilesWithOwnership

--- src/openrct2/world/Map.cpp
+++ src/openrct2/world/Map.cpp
@@ -2225,18 +2225,18 @@ uint16_t CheckMaxAllowableLandRightsForTile(const CoordsXYZ& tileMapPos)
     return destOwnership;
 }
 
-void FixLandOwnershipTilesWithOwnership(std::initializer_list<TileCoordsXY> tiles, uint8_t ownership, bool doNotDowngrade)
+void FixLandOwnershipTilesWithOwnership(std::vector<TileCoordsXY> tiles, uint8_t ownership, bool doNotDowngrade)
 {
-    for (const TileCoordsXY* tile = tiles.begin(); tile != tiles.end(); ++tile)
+    for (const auto& tile : tiles)
     {
-        auto surfaceElement = MapGetSurfaceElementAt(*tile);
+        auto surfaceElement = MapGetSurfaceElementAt(tile);
         if (surfaceElement != nullptr)
         {
             if (doNotDowngrade && surfaceElement->GetOwnership() == OWNERSHIP_OWNED)
                 continue;
 
             surfaceElement->SetOwnership(ownership);
-            ParkUpdateFencesAroundTile({ (*tile).x * 32, (*tile).y * 32 });
+            ParkUpdateFencesAroundTile({ tile.x * 32, tile.y * 32 });
         }
     }
 }
