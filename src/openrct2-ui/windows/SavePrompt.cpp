9bd722559966576fe8cced8f96d9bda00c3794eb
Gymnasiast <m.o.steenbeek@gmail.com>
2024-01-24 21:51:11 +0100

    Apply other code review suggestions

--- src/openrct2-ui/windows/SavePrompt.cpp
+++ src/openrct2-ui/windows/SavePrompt.cpp
@@ -21,6 +21,8 @@
 #include <openrct2/scenario/Scenario.h>
 #include <openrct2/windows/Intent.h>
 
+using namespace OpenRCT2;
+
 static constexpr int32_t WH_SAVE = 54;
 static constexpr int32_t WW_SAVE = 260;
 static constexpr int32_t WH_QUIT = 38;
@@ -100,7 +102,7 @@ public:
         if (NetworkGetMode() == NETWORK_MODE_NONE)
         {
             gGamePaused |= GAME_PAUSED_MODAL;
-            OpenRCT2::Audio::StopAll();
+            Audio::StopAll();
         }
 
         WindowInvalidateByClass(WindowClass::TopToolbar);
@@ -127,7 +129,7 @@ public:
         if (NetworkGetMode() == NETWORK_MODE_NONE)
         {
             gGamePaused &= ~GAME_PAUSED_MODAL;
-            OpenRCT2::Audio::Resume();
+            Audio::Resume();
         }
 
         WindowInvalidateByClass(WindowClass::TopToolbar);
@@ -160,7 +162,7 @@ public:
                 {
                     intent = std::make_unique<Intent>(WindowClass::Loadsave);
                     intent->PutExtra(INTENT_EXTRA_LOADSAVE_TYPE, LOADSAVETYPE_SAVE | LOADSAVETYPE_LANDSCAPE);
-                    intent->PutExtra(INTENT_EXTRA_PATH, OpenRCT2::GetGameState().ScenarioName);
+                    intent->PutExtra(INTENT_EXTRA_PATH, GetGameState().ScenarioName);
                 }
                 else
                 {
