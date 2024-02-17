ed09782d077477b18f373206f9f70eb06ce07180
MaxMallon <maxmallon@hotmail.com>
2024-02-17 21:32:11 +0100

    Fix #21418 - Cannot place scenery high

--- src/openrct2-ui/windows/TopToolbar.cpp
+++ src/openrct2-ui/windows/TopToolbar.cpp
@@ -1700,7 +1700,7 @@ private:
 
         auto screenPos = sourceScreenPos;
         uint16_t maxPossibleHeight = ZoomLevel::max().ApplyTo(
-            std::numeric_limits<decltype(TileElement::BaseHeight)>::max() - 32);
+            std::numeric_limits<decltype(TileElement::BaseHeight)>::max() - 1);
         bool can_raise_item = false;
 
         const auto* sceneryEntry = OpenRCT2::ObjectManager::GetObjectEntry<SmallSceneryEntry>(sceneryIndex);
@@ -1930,7 +1930,7 @@ private:
 
         auto screenPos = sourceScreenPos;
         uint16_t maxPossibleHeight = ZoomLevel::max().ApplyTo(
-            std::numeric_limits<decltype(TileElement::BaseHeight)>::max() - 32);
+            std::numeric_limits<decltype(TileElement::BaseHeight)>::max() - 1);
 
         auto* wallEntry = OpenRCT2::ObjectManager::GetObjectEntry<WallSceneryEntry>(sceneryIndex);
         if (wallEntry != nullptr)
