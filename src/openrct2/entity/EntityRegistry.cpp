70453eccf286f650122ed733368908cde67d7c0b
Tulio Leao <tupaschoal@gmail.com>
2024-02-25 09:42:25 -0300

    Unify function to get string from hex

--- src/openrct2/entity/EntityRegistry.cpp
+++ src/openrct2/entity/EntityRegistry.cpp
@@ -98,17 +98,7 @@ uint16_t GetNumFreeEntities()
 
 std::string EntitiesChecksum::ToString() const
 {
-    std::string result;
-
-    result.reserve(raw.size() * 2);
-    for (auto b : raw)
-    {
-        char buf[3];
-        snprintf(buf, 3, "%02x", static_cast<int32_t>(b));
-        result.append(buf);
-    }
-
-    return result;
+    return String::StringFromHex(raw);
 }
 
 EntityBase* TryGetEntity(EntityId entityIndex)
