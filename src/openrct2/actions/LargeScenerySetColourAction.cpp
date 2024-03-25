7eb32234cca020eb2ec6c45fd67cf8fb6e2fe721
Peter Froud <pfroud@gmail.com>
2024-03-24 21:07:34 -0700

    Use `if... else if`

--- src/openrct2/actions/LargeScenerySetColourAction.cpp
+++ src/openrct2/actions/LargeScenerySetColourAction.cpp
@@ -78,14 +78,12 @@ GameActions::Result LargeScenerySetColourAction::QueryExecute(bool isExecuting) 
         LOG_ERROR("Invalid primary colour %u", _primaryColour);
         return GameActions::Result(GameActions::Status::InvalidParameters, STR_CANT_REPAINT_THIS, STR_ERR_INVALID_COLOUR);
     }
-
-    if (_secondaryColour >= COLOUR_COUNT)
+    else if (_secondaryColour >= COLOUR_COUNT)
     {
         LOG_ERROR("Invalid secondary colour %u", _secondaryColour);
         return GameActions::Result(GameActions::Status::InvalidParameters, STR_CANT_REPAINT_THIS, STR_ERR_INVALID_COLOUR);
     }
-
-    if (_tertiaryColour >= COLOUR_COUNT)
+    else if (_tertiaryColour >= COLOUR_COUNT)
     {
         LOG_ERROR("Invalid tertiary colour %u", _tertiaryColour);
         return GameActions::Result(GameActions::Status::InvalidParameters, STR_CANT_REPAINT_THIS, STR_ERR_INVALID_COLOUR);
