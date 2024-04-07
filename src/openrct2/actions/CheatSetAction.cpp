fe6da027181f86c35e5eb8e20e1a481ede36e405
JDCustard <jevan-dean@hotmail.com>
2024-04-07 20:39:04 +0100

    Close #21728: Apply "Fix all rides" even if mechanic is already working
    Added checks for mechanic fixing plus handles inspecting and calling.

--- src/openrct2/actions/CheatSetAction.cpp
+++ src/openrct2/actions/CheatSetAction.cpp
@@ -512,13 +512,22 @@ void CheatSetAction::FixBrokenRides() const
 {
     for (auto& ride : GetRideManager())
     {
-        if ((ride.mechanic_status != RIDE_MECHANIC_STATUS_FIXING)
-            && (ride.lifecycle_flags & (RIDE_LIFECYCLE_BREAKDOWN_PENDING | RIDE_LIFECYCLE_BROKEN_DOWN)))
+        if (ride.lifecycle_flags & (RIDE_LIFECYCLE_BREAKDOWN_PENDING | RIDE_LIFECYCLE_BROKEN_DOWN))
         {
             auto mechanic = RideGetAssignedMechanic(ride);
+
             if (mechanic != nullptr)
             {
-                mechanic->RemoveFromRide();
+                if (ride.mechanic_status == RIDE_MECHANIC_STATUS_FIXING)
+                {
+                    mechanic->RideSubState = PeepRideSubState::ApproachExit;
+                }
+                else if (
+                    ride.mechanic_status == RIDE_MECHANIC_STATUS_CALLING
+                    || ride.mechanic_status == RIDE_MECHANIC_STATUS_HEADING)
+                {
+                    mechanic->RemoveFromRide();
+                }
             }
 
             RideFixBreakdown(ride, 0);
