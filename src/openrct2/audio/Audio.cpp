8258c91faee38ef30dbd71f4ef121d09195400fe
ncerroneumich <ncerrone@umich.edu>
2024-04-13 12:24:02 -0400

    Rename OpenRCT2Title to kOpenRCT2Title

--- src/openrct2/audio/Audio.cpp
+++ src/openrct2/audio/Audio.cpp
@@ -262,7 +262,7 @@ namespace OpenRCT2::Audio
     static std::map<TitleMusicKind, std::string_view> GetAvailableMusicMap()
     {
         auto musicMap = std::map<TitleMusicKind, std::string_view>{
-            { TitleMusicKind::OpenRCT2, AudioObjectIdentifiers::OpenRCT2Title },
+            { TitleMusicKind::OpenRCT2, AudioObjectIdentifiers::kOpenRCT2Title },
             { TitleMusicKind::RCT2, AudioObjectIdentifiers::kRCT2Title },
         };
 
