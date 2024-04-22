c4cc84c4543fa866a4a6a528314ba5430e6d98f4
Claudio Tiecher <ctiecher@gmail.com>
2024-04-22 21:36:31 +0200

    Part of #21421: apply clang format

--- src/openrct2-ui/windows/Park.cpp
+++ src/openrct2-ui/windows/Park.cpp
@@ -856,8 +856,7 @@ static constexpr WindowParkAward _parkAwards[] = {
                     utf8 _moneyInputText[kMoneyStringMaxlength] = {};
                     MoneyToString(Park::GetEntranceFee(), _moneyInputText, kMoneyStringMaxlength, false);
                     WindowTextInputRawOpen(
-                        this, WIDX_PRICE, STR_ENTER_NEW_VALUE, STR_ENTER_NEW_VALUE, {}, _moneyInputText,
-                        kMoneyStringMaxlength);
+                        this, WIDX_PRICE, STR_ENTER_NEW_VALUE, STR_ENTER_NEW_VALUE, {}, _moneyInputText, kMoneyStringMaxlength);
                 }
             }
         }
