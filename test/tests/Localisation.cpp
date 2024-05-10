59cbfa44d47013af60b3eada9be0be9d06b9c5c0
Aaron van Geffen <aaron@aaronweb.net>
2024-05-10 20:42:20 +0200

    Revert initial change to localisation test

--- test/tests/Localisation.cpp
+++ test/tests/Localisation.cpp
@@ -7,10 +7,11 @@
  * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
 
+#include "openrct2/localisation/Localisation.h"
+
 #include "helpers/StringHelpers.hpp"
 
 #include <gtest/gtest.h>
-#include <openrct2/localisation/Language.h>
 
 class Localisation : public testing::Test
 {
