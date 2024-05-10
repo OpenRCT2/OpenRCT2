d35438687a92e066d599cf41437438f08693281a
Aaron van Geffen <aaron@aaronweb.net>
2024-05-10 18:02:47 +0200

    Fix UiContext.Linux.cpp

--- src/openrct2-ui/UiContext.Linux.cpp
+++ src/openrct2-ui/UiContext.Linux.cpp
@@ -18,7 +18,8 @@
 #    include <openrct2/core/Path.hpp>
 #    include <openrct2/core/String.hpp>
 #    include <openrct2/core/StringBuilder.h>
-#    include <openrct2/localisation/Localisation.h>
+#    include <openrct2/localisation/Language.h>
+#    include <openrct2/localisation/StringIds.h>
 #    include <openrct2/platform/Platform.h>
 #    include <openrct2/ui/UiContext.h>
 #    include <sstream>
