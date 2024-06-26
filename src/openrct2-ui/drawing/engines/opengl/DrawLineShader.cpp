75c7377de7e79f6818585ca2078f3dcea0a06849
Michael Bernardi <mrmbernardi@gmail.com>
2024-06-27 04:44:32 +1000

    Line drawing code now supports all zoom levels.

--- src/openrct2-ui/drawing/engines/opengl/DrawLineShader.cpp
+++ src/openrct2-ui/drawing/engines/opengl/DrawLineShader.cpp
@@ -51,7 +51,6 @@ DrawLineShader::DrawLineShader()
         vVertMat + 3, 2, GL_FLOAT, GL_FALSE, sizeof(VDStruct), reinterpret_cast<void*>(offsetof(VDStruct, mat[3])));
 
     glBindBuffer(GL_ARRAY_BUFFER, _vboInstances);
-    glVertexAttribIPointer(vClip, 4, GL_INT, sizeof(DrawLineCommand), reinterpret_cast<void*>(offsetof(DrawLineCommand, clip)));
     glVertexAttribIPointer(
         vBounds, 4, GL_INT, sizeof(DrawLineCommand), reinterpret_cast<void*>(offsetof(DrawLineCommand, bounds)));
     glVertexAttribIPointer(
@@ -69,7 +68,6 @@ DrawLineShader::DrawLineShader()
     glEnableVertexAttribArray(vColour);
     glEnableVertexAttribArray(vDepth);
 
-    glVertexAttribDivisor(vClip, 1);
     glVertexAttribDivisor(vBounds, 1);
     glVertexAttribDivisor(vColour, 1);
     glVertexAttribDivisor(vDepth, 1);
@@ -87,7 +85,6 @@ void DrawLineShader::GetLocations()
 {
     uScreenSize = GetUniformLocation("uScreenSize");
 
-    vClip = GetAttributeLocation("vClip");
     vBounds = GetAttributeLocation("vBounds");
     vColour = GetAttributeLocation("vColour");
     vDepth = GetAttributeLocation("vDepth");
