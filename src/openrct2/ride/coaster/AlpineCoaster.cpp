725f2c9576659c74ce20fca594d9d8471e2b3d9e
Aaron van Geffen <aaron@aaronweb.net>
2024-07-25 00:49:56 +0200

    Update outdated copyright notice in coaster files (#22373)

--- src/openrct2/ride/coaster/AlpineCoaster.cpp
+++ src/openrct2/ride/coaster/AlpineCoaster.cpp
@@ -1,18 +1,11 @@
-#pragma region Copyright(c) 2014 - 2022 OpenRCT2 Developers
 /*****************************************************************************
- * OpenRCT2, an open source clone of Roller Coaster Tycoon 2.
+ * Copyright (c) 2014-2024 OpenRCT2 developers
  *
- * OpenRCT2 is the work of many authors, a full list can be found in contributors.md
- * For more information, visit https://github.com/OpenRCT2/OpenRCT2
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
  *
- * OpenRCT2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * A full copy of the GNU General Public License can be found in licence.txt
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
-#pragma endregion
 
 #include "../../drawing/Drawing.h"
 #include "../../interface/Viewport.h"
