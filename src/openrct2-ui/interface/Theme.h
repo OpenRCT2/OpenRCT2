76247c55dd5d0cf8d07f92a2247a8c54b63967c2
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2024-07-30 21:47:59 +0100

    Put theme in the Ui namespace

--- src/openrct2-ui/interface/Theme.h
+++ src/openrct2-ui/interface/Theme.h
@@ -12,37 +12,40 @@
 #include <openrct2/interface/Colour.h>
 #include <openrct2/interface/Window.h>
 
-enum
+namespace OpenRCT2::Ui
 {
-    UITHEME_FLAG_PREDEFINED = 1 << 0,
-    UITHEME_FLAG_USE_LIGHTS_RIDE = 1 << 1,
-    UITHEME_FLAG_USE_LIGHTS_PARK = 1 << 2,
-    UITHEME_FLAG_USE_ALTERNATIVE_SCENARIO_SELECT_FONT = 1 << 3,
-    UITHEME_FLAG_USE_FULL_BOTTOM_TOOLBAR = 1 << 4,
-};
+    enum
+    {
+        UITHEME_FLAG_PREDEFINED = 1 << 0,
+        UITHEME_FLAG_USE_LIGHTS_RIDE = 1 << 1,
+        UITHEME_FLAG_USE_LIGHTS_PARK = 1 << 2,
+        UITHEME_FLAG_USE_ALTERNATIVE_SCENARIO_SELECT_FONT = 1 << 3,
+        UITHEME_FLAG_USE_FULL_BOTTOM_TOOLBAR = 1 << 4,
+    };
 
-void ColourSchemeUpdate(WindowBase* window);
-void ColourSchemeUpdateAll();
-void ColourSchemeUpdateByClass(WindowBase* window, WindowClass classification);
+    void ColourSchemeUpdate(WindowBase* window);
+    void ColourSchemeUpdateAll();
+    void ColourSchemeUpdateByClass(WindowBase* window, WindowClass classification);
 
-void ThemeManagerInitialise();
-void ThemeManagerLoadAvailableThemes();
-size_t ThemeManagerGetNumAvailableThemes();
-const utf8* ThemeManagerGetAvailableThemePath(size_t index);
-const utf8* ThemeManagerGetAvailableThemeConfigName(size_t index);
-const utf8* ThemeManagerGetAvailableThemeName(size_t index);
-size_t ThemeManagerGetAvailableThemeIndex();
-void ThemeManagerSetActiveAvailableTheme(size_t index);
-size_t ThemeGetIndexForName(const utf8* name);
+    void ThemeManagerInitialise();
+    void ThemeManagerLoadAvailableThemes();
+    size_t ThemeManagerGetNumAvailableThemes();
+    const utf8* ThemeManagerGetAvailableThemePath(size_t index);
+    const utf8* ThemeManagerGetAvailableThemeConfigName(size_t index);
+    const utf8* ThemeManagerGetAvailableThemeName(size_t index);
+    size_t ThemeManagerGetAvailableThemeIndex();
+    void ThemeManagerSetActiveAvailableTheme(size_t index);
+    size_t ThemeGetIndexForName(const utf8* name);
 
-ColourWithFlags ThemeGetColour(WindowClass wc, uint8_t index);
-void ThemeSetColour(WindowClass wc, uint8_t index, ColourWithFlags colour);
-uint8_t ThemeGetFlags();
-void ThemeSetFlags(uint8_t flags);
-void ThemeSave();
-void ThemeRename(const utf8* name);
-void ThemeDuplicate(const utf8* name);
-void ThemeDelete();
+    ColourWithFlags ThemeGetColour(WindowClass wc, uint8_t index);
+    void ThemeSetColour(WindowClass wc, uint8_t index, ColourWithFlags colour);
+    uint8_t ThemeGetFlags();
+    void ThemeSetFlags(uint8_t flags);
+    void ThemeSave();
+    void ThemeRename(const utf8* name);
+    void ThemeDuplicate(const utf8* name);
+    void ThemeDelete();
 
-uint8_t ThemeDescGetNumColours(WindowClass wc);
-StringId ThemeDescGetName(WindowClass wc);
+    uint8_t ThemeDescGetNumColours(WindowClass wc);
+    StringId ThemeDescGetName(WindowClass wc);
+} // namespace OpenRCT2::Ui
