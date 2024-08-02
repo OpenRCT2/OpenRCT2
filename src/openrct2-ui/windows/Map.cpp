ec071d8586509a36f958deb4cc95fcf7bc360b20
Aaron van Geffen <aaron@aaronweb.net>
2024-08-02 13:41:10 +0200

    Remove superfluous include

--- src/openrct2-ui/windows/Map.cpp
+++ src/openrct2-ui/windows/Map.cpp
@@ -7,7 +7,6 @@
  * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
 
-#include <openrct2-ui/interface/Dropdown.h>
 #include <openrct2-ui/interface/LandTool.h>
 #include <openrct2-ui/interface/Viewport.h>
 #include <openrct2-ui/interface/ViewportInteraction.h>
