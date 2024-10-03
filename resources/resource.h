29018651addc25931f36dd5727e11e7312e847af
yutakastep <127261743+yutakastep@users.noreply.github.com>
2024-10-03 12:29:38 -0400

    Part of OpenRCT2#21421: Update resource.h

--- resources/resource.h
+++ resources/resource.h
@@ -8,9 +8,9 @@
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+constexpr int APS_NEXT_RESOURCE_VALUE = 102;
+constexpr int APS_NEXT_COMMAND_VALUE = 40001;
+constexpr int APS_NEXT_CONTROL_VALUE = 1001;
+constexpr int APS_NEXT_SYMED_VALUE = 101;
 #endif
 #endif
