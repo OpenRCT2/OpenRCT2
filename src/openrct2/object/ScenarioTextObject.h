f7ae40bac89ab14350c0cd831ca4bb578031356b
Aaron van Geffen <aaron@aaronweb.net>
2024-10-27 14:53:27 +0100

    Re-introduce basic scenario text objects

---
+++ src/openrct2/object/ScenarioTextObject.h
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ * Copyright (c) 2014-2024 OpenRCT2 developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
+ *
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#pragma once
+
+#include "../core/IStream.hpp"
+#include "Object.h"
+
+#include <string>
+
+class ScenarioTextObject final : public Object
+{
+public:
+    static constexpr ObjectType kObjectType = ObjectType::ScenarioText;
+
+    void ReadJson(IReadObjectContext* context, json_t& root) override;
+    void Load() override;
+    void Unload() override;
+
+    std::string GetScenarioName();
+    std::string GetParkName();
+    std::string GetScenarioDetails();
+};
