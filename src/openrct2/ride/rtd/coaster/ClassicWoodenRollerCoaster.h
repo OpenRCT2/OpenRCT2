4f93448606e3240911fbc77285cedb1ebd64108b
ZeeMaji <42477864+ZeeMaji@users.noreply.github.com>
2024-11-18 15:49:27 -0500

    Close #23210: Add boosters to classic wooden coaster (cheats only)
    The classic wooden roller coaster can draw boosters from the RCT2 wooden coaster and it also has booster settings set, so the pieces are fully functional and they can be built with ride type changing. This just simply adds them to the extra track pieces so you can use them without ride type switching.

--- src/openrct2/ride/rtd/coaster/ClassicWoodenRollerCoaster.h
+++ src/openrct2/ride/rtd/coaster/ClassicWoodenRollerCoaster.h
@@ -23,7 +23,7 @@ constexpr RideTypeDescriptor ClassicWoodenRollerCoasterRTD =
        .Drawer = GetTrackPaintFunctionClassicWoodenRC,
        .supportType = WoodenSupportType::Truss,
        .enabledTrackGroups = {TrackGroup::flat, TrackGroup::straight, TrackGroup::stationEnd, TrackGroup::liftHill, TrackGroup::flatRollBanking, TrackGroup::verticalLoop, TrackGroup::slope, TrackGroup::slopeSteepUp, TrackGroup::slopeSteepDown, TrackGroup::slopeCurve, TrackGroup::sBend, TrackGroup::curveSmall, TrackGroup::curve, TrackGroup::curveLarge, TrackGroup::brakes, TrackGroup::onridePhoto, TrackGroup::waterSplash, TrackGroup::blockBrakes, TrackGroup::diagBrakes, TrackGroup::diagBlockBrakes, TrackGroup::slopeSteepLong, TrackGroup::halfLoopMedium, TrackGroup::halfLoopLarge},
-       .extraTrackGroups = {},
+       .extraTrackGroups = {TrackGroup::booster},
    }),
    .InvertedTrackPaintFunctions = {},
    .Flags = kRtdFlagsHasThreeColours | kRtdFlagsCommonCoaster | kRtdFlagsCommonCoasterNonAlt |
