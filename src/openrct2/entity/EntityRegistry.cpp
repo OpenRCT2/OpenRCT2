3455fa05b6040330d9883225df00d16db5510575
Michael Bernardi <mrmbernardi@gmail.com>
2024-12-10 22:53:49 +1100

    Remove unused includes

--- src/openrct2/entity/EntityRegistry.cpp
+++ src/openrct2/entity/EntityRegistry.cpp
@@ -10,7 +10,6 @@
 #include "EntityRegistry.h"
 
 #include "../Diagnostic.h"
-#include "../Game.h"
 #include "../GameState.h"
 #include "../core/Algorithm.hpp"
 #include "../core/ChecksumStream.h"
@@ -36,7 +35,6 @@
 #include <cassert>
 #include <cmath>
 #include <iterator>
-#include <numeric>
 #include <vector>
 
 using namespace OpenRCT2;
