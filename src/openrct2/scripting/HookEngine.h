4ccccf0754247ef4d6b77cee4e23c992cf8e0b68
Michael Bernardi <mrmbernardi@gmail.com>
2024-12-25 22:00:47 +1100

    Enable QuickJS REPL

--- src/openrct2/scripting/HookEngine.h
+++ src/openrct2/scripting/HookEngine.h
@@ -9,9 +9,9 @@
 
 #pragma once
 
-#ifdef ENABLE_SCRIPTING
+#ifdef ENABLE_SCRIPTING_REFACTOR
 
-    #include "Duktape.hpp"
+    #include <quickjs.h>
 
     #include <any>
     #include <memory>
@@ -53,10 +53,10 @@ namespace OpenRCT2::Scripting
     {
         uint32_t Cookie;
         std::shared_ptr<Plugin> Owner;
-        DukValue Function;
+        JSValue Function;
 
         Hook() = default;
-        Hook(uint32_t cookie, std::shared_ptr<Plugin> owner, const DukValue& function)
+        Hook(uint32_t cookie, std::shared_ptr<Plugin> owner, const JSValue function)
             : Cookie(cookie)
             , Owner(owner)
             , Function(function)
@@ -84,14 +84,14 @@ namespace OpenRCT2::Scripting
     public:
         HookEngine(ScriptEngine& scriptEngine);
         HookEngine(const HookEngine&) = delete;
-        uint32_t Subscribe(HOOK_TYPE type, std::shared_ptr<Plugin> owner, const DukValue& function);
+        uint32_t Subscribe(HOOK_TYPE type, std::shared_ptr<Plugin> owner, JSValue function);
         void Unsubscribe(HOOK_TYPE type, uint32_t cookie);
         void UnsubscribeAll(std::shared_ptr<const Plugin> owner);
         void UnsubscribeAll();
         bool HasSubscriptions(HOOK_TYPE type) const;
         bool IsValidHookForPlugin(HOOK_TYPE type, Plugin& plugin) const;
         void Call(HOOK_TYPE type, bool isGameStateMutable);
-        void Call(HOOK_TYPE type, const DukValue& arg, bool isGameStateMutable);
+        void Call(HOOK_TYPE type, JSValue arg, bool isGameStateMutable);
         void Call(
             HOOK_TYPE type, const std::initializer_list<std::pair<std::string_view, std::any>>& args, bool isGameStateMutable);
 
