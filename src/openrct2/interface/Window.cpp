e063429b01d9f34b3ac9b1f2cce93acb4ab79eb2
Aaron van Geffen <aaron@aaronweb.net>
2024-12-30 16:53:41 +0100

    Partial revert of #23487

--- src/openrct2/interface/Window.cpp
+++ src/openrct2/interface/Window.cpp
@@ -227,12 +227,6 @@ void WindowSetWindowLimit(int32_t value)
  */
 void WindowClose(WindowBase& w)
 {
-    if (!w.CanClose())
-    {
-        // Something's preventing this window from closing -- bail out early
-        return;
-    }
-
     w.OnClose();
 
     // Remove viewport
