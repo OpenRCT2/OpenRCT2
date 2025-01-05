3b8eab92eaa708d4b335c98a67dc2f537fd9a097
Andrew <5436387+fidwell@users.noreply.github.com>
2025-01-05 10:31:51 -0500

    ﻿Add diagonal downward-inclined brakes to hybrid and single rail coasters

--- src/openrct2/ride/Track.cpp
+++ src/openrct2/ride/Track.cpp
@@ -638,6 +638,7 @@ bool TrackTypeIsBrakes(OpenRCT2::TrackElemType trackType)
         case TrackElemType::Brakes:
         case TrackElemType::DiagBrakes:
         case TrackElemType::Down25Brakes:
+        case TrackElemType::DiagDown25Brakes:
             return true;
         default:
             return false;
