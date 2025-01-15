34be57f781aede7dbd56404ffb9fec6f73390e1c
mix <mixsims@proton.me>
2025-01-15 21:30:29 +0000

    Add larger turns and s bends to the go karts

--- src/openrct2/park/ParkFile.h
+++ src/openrct2/park/ParkFile.h
@@ -20,7 +20,7 @@ namespace OpenRCT2
     struct GameState_t;
 
     // Current version that is saved.
-    constexpr uint32_t kParkFileCurrentVersion = 53;
+    constexpr uint32_t kParkFileCurrentVersion = 54;
 
     // The minimum version that is forwards compatible with the current version.
     constexpr uint32_t kParkFileMinVersion = 53;
@@ -50,6 +50,7 @@ namespace OpenRCT2
     constexpr uint16_t kDiagonalLongFlatToSteepAndDiveLoopVersion = 50;
     constexpr uint16_t kEmbeddedParkPreviewChunk = 52;
     constexpr uint16_t kClimateObjectsVersion = 53;
+    constexpr uint16_t kExtendedGoKartsVersion = 54;
 } // namespace OpenRCT2
 
 class ParkFileExporter
