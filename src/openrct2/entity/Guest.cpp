3ee540e1888a8743db3cd965e8eb6202659c7d3b
matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
2025-01-18 22:41:13 -0600

    Change guest attributes to uint16_t and safeguard from overflow

--- src/openrct2/entity/Guest.cpp
+++ src/openrct2/entity/Guest.cpp
@@ -1655,32 +1655,38 @@ bool Guest::DecideAndBuyItem(Ride& ride, const ShopItem shopItem, money64 price)
     GiveItem(shopItem);
     const auto hasRandomShopColour = ride.HasLifecycleFlag(RIDE_LIFECYCLE_RANDOM_SHOP_COLOURS);
 
-    if (shopItem == ShopItem::TShirt)
-        TshirtColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
-
-    if (shopItem == ShopItem::Hat)
-        HatColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
-
-    if (shopItem == ShopItem::Balloon)
-        BalloonColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
-
-    if (shopItem == ShopItem::Umbrella)
-        UmbrellaColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
-
-    if (shopItem == ShopItem::Map)
-        ResetPathfindGoal();
-
-    if (shopItem == ShopItem::Photo)
-        Photo1RideRef = ride.id;
-
-    if (shopItem == ShopItem::Photo2)
-        Photo2RideRef = ride.id;
-
-    if (shopItem == ShopItem::Photo3)
-        Photo3RideRef = ride.id;
-
-    if (shopItem == ShopItem::Photo4)
-        Photo4RideRef = ride.id;
+    switch (shopItem)
+    {
+        case ShopItem::TShirt:
+            TshirtColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
+            break;
+        case ShopItem::Hat:
+            HatColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
+            break;
+        case ShopItem::Balloon:
+            BalloonColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
+            break;
+        case ShopItem::Umbrella:
+            UmbrellaColour = hasRandomShopColour ? ScenarioRandMax(kColourNumNormal) : ride.track_colour[0].main;
+            break;
+        case ShopItem::Map:
+            ResetPathfindGoal();
+            break;
+        case ShopItem::Photo:
+            Photo1RideRef = ride.id;
+            break;
+        case ShopItem::Photo2:
+            Photo2RideRef = ride.id;
+            break;
+        case ShopItem::Photo3:
+            Photo3RideRef = ride.id;
+            break;
+        case ShopItem::Photo4:
+            Photo4RideRef = ride.id;
+            break;
+        default:
+            break;
+    }
 
     WindowInvalidateFlags |= PEEP_INVALIDATE_PEEP_INVENTORY;
     UpdateAnimationGroup();
@@ -1695,29 +1701,22 @@ bool Guest::DecideAndBuyItem(Ride& ride, const ShopItem shopItem, money64 price)
         }
     }
 
-    if (shopItemDescriptor.IsFood())
-        AmountOfFood++;
-
-    if (shopItemDescriptor.IsDrink())
-        AmountOfDrinks++;
-
-    if (shopItemDescriptor.IsSouvenir())
-        AmountOfSouvenirs++;
-
-    money64* expend_type = &PaidOnSouvenirs;
+    money64* expendType = &PaidOnSouvenirs;
     ExpenditureType expenditure = ExpenditureType::ShopStock;
-
     if (shopItemDescriptor.IsFood())
     {
-        expend_type = &PaidOnFood;
+        AmountOfFood = AddClamp<decltype(AmountOfFood)>(AmountOfFood, 1);
+        expendType = &PaidOnFood;
         expenditure = ExpenditureType::FoodDrinkStock;
     }
-
-    if (shopItemDescriptor.IsDrink())
+    else if (shopItemDescriptor.IsDrink())
     {
-        expend_type = &PaidOnDrink;
+        AmountOfDrinks = AddClamp<decltype(AmountOfDrinks)>(AmountOfDrinks, 1);
+        expendType = &PaidOnDrink;
         expenditure = ExpenditureType::FoodDrinkStock;
     }
+    else if (shopItemDescriptor.IsSouvenir())
+        AmountOfSouvenirs = AddClamp<decltype(AmountOfSouvenirs)>(AmountOfSouvenirs, 1);
 
     if (!(gameState.Park.Flags & PARK_FLAGS_NO_MONEY))
         FinancePayment(shopItemDescriptor.Cost, expenditure);
@@ -1731,7 +1730,7 @@ bool Guest::DecideAndBuyItem(Ride& ride, const ShopItem shopItem, money64 price)
     }
     else if (!(gameState.Park.Flags & PARK_FLAGS_NO_MONEY))
     {
-        SpendMoney(*expend_type, price, expenditure);
+        SpendMoney(*expendType, price, expenditure);
     }
     ride.total_profit += (price - shopItemDescriptor.Cost);
     ride.window_invalidate_flags |= RIDE_INVALIDATE_RIDE_INCOME;
@@ -1764,8 +1763,7 @@ void Guest::OnEnterRide(Ride& ride)
     ride.UpdateSatisfaction(rideSatisfaction);
 
     // Update various peep stats.
-    if (GuestNumRides < 255)
-        GuestNumRides++;
+    GuestNumRides = AddClamp<decltype(GuestNumRides)>(GuestNumRides, 1);
 
     SetHasRidden(ride);
     GuestUpdateFavouriteRide(*this, ride, satisfaction);
