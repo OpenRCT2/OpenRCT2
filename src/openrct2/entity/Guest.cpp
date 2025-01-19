211e3394084a82ccaaee6a41be19c492e834b6c6
matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
2025-01-18 23:23:13 -0600

    Add switch fallback

--- src/openrct2/entity/Guest.cpp
+++ src/openrct2/entity/Guest.cpp
@@ -1688,6 +1688,8 @@ bool Guest::DecideAndBuyItem(Ride& ride, const ShopItem shopItem, money64 price)
         case ShopItem::Photo4:
             Photo4RideRef = ride.id;
             break;
+        default:
+            break; // Fallback
     }
 
     WindowInvalidateFlags |= PEEP_INVALIDATE_PEEP_INVENTORY;
