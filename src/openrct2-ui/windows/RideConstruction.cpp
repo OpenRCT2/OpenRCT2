901d3e415e7a8b46028a3a12c87f17b97fe62f45
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2025-01-19 18:17:26 +0100

    Invalidate floor when updating map selection

--- src/openrct2-ui/windows/RideConstruction.cpp
+++ src/openrct2-ui/windows/RideConstruction.cpp
@@ -3094,6 +3094,11 @@ namespace OpenRCT2::Ui::Windows
 
         window->UpdateMapSelection();
 
+        if (!(gMapSelectFlags & MAP_SELECT_FLAG_ENABLE))
+        {
+            VirtualFloorInvalidate();
+        }
+
         _selectedTrackType = TrackElemType::None;
         if (_rideConstructionState == RideConstructionState::Selected)
         {
