9fd86af3e593e495bd90b69cb5534894bce7b3e9
matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
2025-02-02 22:47:51 -0300

    Fix mysterious duplicated lines in Vehicle.cpp
    I'm not sure how this happened...

--- src/openrct2/ride/Vehicle.cpp
+++ src/openrct2/ride/Vehicle.cpp
@@ -81,9 +81,6 @@ constexpr int16_t kVehicleStoppingSpinSpeed = 600;
 constexpr uint8_t kTrackSpeedShiftAmount = 16;
 constexpr uint8_t kBoosterAccelerationShiftAmount = 16;
 
-constexpr uint8_t kTrackSpeedShiftAmount = 16;
-constexpr uint8_t kBoosterAccelerationShiftAmount = 16;
-
 Vehicle* gCurrentVehicle;
 
 static uint8_t _vehicleBreakdown;
