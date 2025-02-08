e74ed439dfa85e251c29ba933def85fbe0781603
Frédéric Fauberteau <frederic@fauberteau.org>
2025-02-08 11:08:21 +0100

    Support NetBSD

--- src/openrct2/platform/Platform.Posix.cpp
+++ src/openrct2/platform/Platform.Posix.cpp
@@ -7,7 +7,7 @@
  * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
 
-#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__)
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__) || defined(__NetBSD__)
 
     #include "Platform.h"
 
