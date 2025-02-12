0229a8c8cf116e91a0b9ebb7ef0be07e6dcb7469
matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
2025-02-11 21:07:29 -0300

    Fix: Sound effects keep playing when loading another save

--- src/openrct2-ui/windows/ProgressWindow.cpp
+++ src/openrct2-ui/windows/ProgressWindow.cpp
@@ -12,6 +12,7 @@
 #include <openrct2-ui/windows/Windows.h>
 #include <openrct2/Context.h>
 #include <openrct2/SpriteIds.h>
+#include <openrct2/audio/Audio.h>
 #include <openrct2/drawing/Drawing.h>
 #include <openrct2/drawing/Text.h>
 #include <openrct2/localisation/Formatting.h>
@@ -83,6 +84,7 @@ namespace OpenRCT2::Ui::Windows
     public:
         void OnOpen() override
         {
+            Audio::StopSFX();
             SetWidgets(kProgressWindowWidgets);
             WindowInitScrollWidgets(*this);
 
