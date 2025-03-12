b98a5442089a409ee59aae68c630f52a11c6c858
Aaron van Geffen <aaron@aaronweb.net>
2025-03-12 20:57:12 +0100

    Use GetTitleHeightDiff in WindowManager::Create

--- src/openrct2-ui/WindowManager.cpp
+++ src/openrct2-ui/WindowManager.cpp
@@ -842,6 +842,8 @@ public:
         std::unique_ptr<WindowBase>&& wp, WindowClass cls, ScreenCoordsXY pos, int32_t width, int32_t height,
         uint32_t flags) override
     {
+        height += GetTitleHeightDiff();
+
         if (flags & WF_AUTO_POSITION)
         {
             if (flags & WF_CENTRE_SCREEN)
@@ -853,6 +855,8 @@ public:
                 pos = GetAutoPositionForNewWindow(width, height);
             }
         }
+
+        height -= GetTitleHeightDiff();
 
         // Check if there are any window slots left
         // include kWindowLimitReserved for items such as the main viewport and toolbars to not appear to be counted.
