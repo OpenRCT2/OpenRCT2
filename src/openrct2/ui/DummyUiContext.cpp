8fc3b2f0cd1d39fa25843309e18121207c43a970
discollizard <discollizard@github.com>
2025-03-19 08:31:40 -0300

    Revert "implemented dummy title sequence player class to avoid segfault in openrct2-cli initialization"
    This reverts commit 09da6714ad14873e91f98270b272ad5a21ebd204.

--- src/openrct2/ui/DummyUiContext.cpp
+++ src/openrct2/ui/DummyUiContext.cpp
@@ -10,7 +10,6 @@
 #include "../drawing/X8DrawingEngine.h"
 #include "UiContext.h"
 #include "WindowManager.h"
-#include "../scenes/title/DummyTitleSequencePlayer.h"
 
 using namespace OpenRCT2::Drawing;
 
@@ -23,7 +22,6 @@ namespace OpenRCT2::Ui
     {
     private:
         std::unique_ptr<IWindowManager> const _windowManager = CreateDummyWindowManager();
-        std::unique_ptr<ITitleSequencePlayer> const _titleSequencePlayer = std::make_unique<DummyTitleSequencePlayer>();
 
     public:
         void InitialiseScriptExtensions() override
@@ -206,7 +204,7 @@ namespace OpenRCT2::Ui
 
         ITitleSequencePlayer* GetTitleSequencePlayer() override
         {
-            return _titleSequencePlayer.get();
+            return nullptr;
         }
 
         bool HasFilePicker() const override
