b14bb26ee748c487a613419ba64ded40fc0a929f
MaxMallon <maxmallon@hotmail.com>
2025-04-13 21:44:57 +0200

    24146 - up api version

--- src/openrct2/scripting/ScriptEngine.h
+++ src/openrct2/scripting/ScriptEngine.h
@@ -46,7 +46,7 @@ namespace OpenRCT2
 
 namespace OpenRCT2::Scripting
 {
-    static constexpr int32_t kPluginApiVersion = 105;
+    static constexpr int32_t kPluginApiVersion = 106;
 
     // Versions marking breaking changes.
     static constexpr int32_t kApiVersionPeepDeprecation = 33;
