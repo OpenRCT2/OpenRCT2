7f760c6f21357563769388372ab6395ed20e6f6c
Joel Teichroeb <joel@teichroeb.net>
2025-04-21 17:21:43 -0400

    Fix #24173: Allow all game speeds between 1 and 8 if developer mode is on

--- src/openrct2/interface/InteractiveConsole.cpp
+++ src/openrct2/interface/InteractiveConsole.cpp
@@ -904,7 +904,7 @@ static void ConsoleCommandSet(InteractiveConsole& console, const arguments_t& ar
         }
         else if (varName == "game_speed" && InvalidArguments(&invalidArgs, int_valid[0]))
         {
-            ConsoleSetVariableAction<GameSetSpeedAction>(console, varName, std::clamp(int_val[0], 1, 8));
+            ConsoleSetVariableAction<GameSetSpeedAction>(console, varName, int_val[0]);
         }
         else if (varName == "console_small_font" && InvalidArguments(&invalidArgs, int_valid[0]))
         {
