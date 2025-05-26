OpenRCT2/OpenRCT2

commit c383a7778c3b1c95abb61c305eb9050126912c36

Avoid some more unintentional copies

src/openrct2-ui/windows/EditorObjectSelection.cpp
@@ -1139,7 +1139,7 @@ namespace OpenRCT2::Ui::Windows
             if (!tabWidgetsInitialised)
             {
                 tabWidgetsInitialised = true;
-                auto tabWidget = widgets[WIDX_TAB_1];
+                const auto& tabWidget = widgets[WIDX_TAB_1];
                 for (size_t i = 1; i < std::size(ObjectSelectionPages); i++)
                 {
                     widgets.insert(widgets.end() - 1, tabWidget);

src/openrct2-ui/windows/EditorBottomToolbar.cpp
@@ -328,7 +328,7 @@ namespace OpenRCT2::Ui::Windows
 
         void DrawLeftButtonBack(RenderTarget& rt)
         {
-            auto previousWidget = widgets[WIDX_PREVIOUS_IMAGE];
+            const auto& previousWidget = widgets[WIDX_PREVIOUS_IMAGE];
             auto leftTop = windowPos + ScreenCoordsXY{ previousWidget.left, previousWidget.top };
             auto rightBottom = windowPos + ScreenCoordsXY{ previousWidget.right, previousWidget.bottom };
             GfxFilterRect(rt, { leftTop, rightBottom }, FilterPaletteID::Palette51);

src/openrct2-ui/windows/Ride.cpp
@@ -4850,7 +4850,7 @@ namespace OpenRCT2::Ui::Windows
                 }
                 else
                 {
-                    auto typeDescriptor = ride->getRideTypeDescriptor();
+                    const auto& typeDescriptor = ride->getRideTypeDescriptor();
                     int32_t spriteIndex = typeDescriptor.ColourPreview.Track;
                     if (spriteIndex != 0)
                     {

commit 159fe47ccccf3b8a62788c6bb0b96940a4b35ded

Fix object selection tabs
Regression from c383a7778c3b1c95abb61c305eb9050126912c36 (#24481)

src/openrct2-ui/windows/EditorObjectSelection.cpp
@@ -1139,12 +1139,14 @@ namespace OpenRCT2::Ui::Windows
             SetWidgets(_window_editor_object_selection_widgets);
             if (!tabWidgetsInitialised)
             {
-                tabWidgetsInitialised = true;
-                const auto& tabWidget = widgets[WIDX_TAB_1];
+                // Create a new tab widget based on the initial one
+                auto tabWidget = widgets[WIDX_TAB_1];
                 for (size_t i = 1; i < std::size(ObjectSelectionPages); i++)
                 {
                     widgets.insert(widgets.end() - 1, tabWidget);
                 }
+
+                tabWidgetsInitialised = true;
             }
         }
 