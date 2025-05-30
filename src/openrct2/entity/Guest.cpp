OpenRCT2/OpenRCT2

commit 850cc397c389921e139d825b09dcd8ecd350e8e0

Prevent staff statistics from overflowing (#23719)
* Use AddClamp when incrementing staff stats

* Remove decltype from AddClamp

* Use lowercase u in increment

src/openrct2/entity/Guest.cpp
@@ -6273,20 +6273,17 @@ static void PeepUpdateWalkingBreakScenery(Guest* peep)
         return;
     }
 
-    for (auto inner_peep : EntityList<Staff>())
+    for (auto innerPeep : EntityList<Staff>())
     {
-        if (inner_peep->AssignedStaffType != StaffType::Security)
+        if (innerPeep->AssignedStaffType != StaffType::Security || innerPeep->x == kLocationNull)
             continue;
 
-        if (inner_peep->x == kLocationNull)
-            continue;
+        int32_t xDist = abs(innerPeep->x - peep->x);
+        int32_t yDist = abs(innerPeep->y - peep->y);
 
-        int32_t x_diff = abs(inner_peep->x - peep->x);
-        int32_t y_diff = abs(inner_peep->y - peep->y);
-
-        if (std::max(x_diff, y_diff) < 224)
+        if (std::max(xDist, yDist) < 224)
         {
-            inner_peep->StaffVandalsStopped++;
+            innerPeep->StaffVandalsStopped = AddClamp(innerPeep->StaffVandalsStopped, 1u);
             return;
         }
     }

src/openrct2/entity/Staff.cpp
@@ -40,6 +40,7 @@
 #include "../ride/Track.h"
 #include "../ride/Vehicle.h"
 #include "../scenario/Scenario.h"
+#include "../util/Util.h"
 #include "../windows/Intent.h"
 #include "../world/Entrance.h"
 #include "../world/Footpath.h"
@@ -1036,7 +1037,7 @@ void Staff::UpdateMowing()
             surfaceElement->SetGrassLength(GRASS_LENGTH_MOWED);
             MapInvalidateTileZoom0({ NextLoc, surfaceElement->GetBaseZ(), surfaceElement->GetBaseZ() + 16 });
         }
-        StaffLawnsMown++;
+        StaffLawnsMown = AddClamp(StaffLawnsMown, 1u);
         WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
     }
 }
@@ -1096,7 +1097,7 @@ void Staff::UpdateWatering()
 
             tile_element->AsSmallScenery()->SetAge(0);
             MapInvalidateTileZoom0({ actionLoc, tile_element->GetBaseZ(), tile_element->GetClearanceZ() });
-            StaffGardensWatered++;
+            StaffGardensWatered = AddClamp(StaffGardensWatered, 1u);
             WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
         } while (!(tile_element++)->IsLastForTile());
 
@@ -1180,7 +1181,7 @@ void Staff::UpdateEmptyingBin()
         tile_element->AsPath()->SetAdditionStatus(additionStatus);
 
         MapInvalidateTileZoom0({ NextLoc, tile_element->GetBaseZ(), tile_element->GetClearanceZ() });
-        StaffBinsEmptied++;
+        StaffBinsEmptied = AddClamp(StaffBinsEmptied, 1u);
         WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
     }
 }
@@ -1199,7 +1200,7 @@ void Staff::UpdateSweeping()
     {
         // Remove sick at this location
         Litter::RemoveAt(GetLocation());
-        StaffLitterSwept++;
+        StaffLitterSwept = AddClamp(StaffLitterSwept, 1u);
         WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
     }
     if (auto loc = UpdateAction(); loc.has_value())
@@ -2412,13 +2413,13 @@ bool Staff::UpdateFixingFinishFixOrInspect(bool firstRun, int32_t steps, Ride& r
         {
             UpdateRideInspected(CurrentRide);
 
-            StaffRidesInspected++;
+            StaffRidesInspected = AddClamp(StaffRidesInspected, 1u);
             WindowInvalidateFlags |= RIDE_INVALIDATE_RIDE_INCOME | RIDE_INVALIDATE_RIDE_LIST;
             ride.mechanic_status = RIDE_MECHANIC_STATUS_UNDEFINED;
             return true;
         }
 
-        StaffRidesFixed++;
+        StaffRidesFixed = AddClamp(StaffRidesFixed, 1u);
         WindowInvalidateFlags |= RIDE_INVALIDATE_RIDE_INCOME | RIDE_INVALIDATE_RIDE_LIST;
 
         Orientation = PeepDirection << 3;

commit 52c35917796ee6f658eabf1c28cbe070d352b0fc

Invalidate maintenance tab
Missed this before. Also fix an issue I inadvertently caused with PR #23719 when updating the count of vandals stopped

src/openrct2/entity/Guest.cpp
@@ -6404,6 +6404,7 @@ static void GuestUpdateWalkingBreakScenery(Guest& guest)
         if (std::max(xDist, yDist) < 224)
         {
             innerPeep->StaffVandalsStopped = AddClamp(innerPeep->StaffVandalsStopped, 1u);
+            innerPeep->WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
             return;
         }
     }

src/openrct2/ride/Ride.cpp
@@ -1306,6 +1306,7 @@ static void RideInspectionUpdate(Ride& ride)
         return;
 
     ride.lastInspection = AddClamp<uint8_t>(ride.lastInspection, 1);
+    ride.windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_MAINTENANCE;
 
     int32_t inspectionIntervalMinutes = RideInspectionInterval[ride.inspectionInterval];
     // An inspection interval of 0 minutes means the ride is set to never be inspected.