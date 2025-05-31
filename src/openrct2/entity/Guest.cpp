OpenRCT2/OpenRCT2

commit 850cc397c389921e139d825b09dcd8ecd350e8e0

Prevent staff statistics from overflowing (#23719)
* Use AddClamp when incrementing staff stats

* Remove decltype from AddClamp

* Use lowercase u in increment

src/openrct2/entity/Guest.cpp
@@ -6273,20 +6273,17 @@ static void PeepUpdateWalkingBreakScenery(Guest* peep)
         return;
     }
 
-    for (auto inner_peep : EntityList<Staff>())
+    for (auto innerPeep : EntityList<Staff>())
     {
-        if (inner_peep->AssignedStaffType != StaffType::Security)
+        if (innerPeep->AssignedStaffType != StaffType::Security || innerPeep->x == kLocationNull)
             continue;
 
-        if (inner_peep->x == kLocationNull)
-            continue;
+        int32_t xDist = abs(innerPeep->x - peep->x);
+        int32_t yDist = abs(innerPeep->y - peep->y);
 
-        int32_t x_diff = abs(inner_peep->x - peep->x);
-        int32_t y_diff = abs(inner_peep->y - peep->y);
-
-        if (std::max(x_diff, y_diff) < 224)
+        if (std::max(xDist, yDist) < 224)
         {
-            inner_peep->StaffVandalsStopped++;
+            innerPeep->StaffVandalsStopped = AddClamp(innerPeep->StaffVandalsStopped, 1u);
             return;
         }
     }

src/openrct2/entity/Staff.cpp
@@ -40,6 +40,7 @@
 #include "../ride/Track.h"
 #include "../ride/Vehicle.h"
 #include "../scenario/Scenario.h"
+#include "../util/Util.h"
 #include "../windows/Intent.h"
 #include "../world/Entrance.h"
 #include "../world/Footpath.h"
@@ -1036,7 +1037,7 @@ void Staff::UpdateMowing()
             surfaceElement->SetGrassLength(GRASS_LENGTH_MOWED);
             MapInvalidateTileZoom0({ NextLoc, surfaceElement->GetBaseZ(), surfaceElement->GetBaseZ() + 16 });
         }
-        StaffLawnsMown++;
+        StaffLawnsMown = AddClamp(StaffLawnsMown, 1u);
         WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
     }
 }
@@ -1096,7 +1097,7 @@ void Staff::UpdateWatering()
 
             tile_element->AsSmallScenery()->SetAge(0);
             MapInvalidateTileZoom0({ actionLoc, tile_element->GetBaseZ(), tile_element->GetClearanceZ() });
-            StaffGardensWatered++;
+            StaffGardensWatered = AddClamp(StaffGardensWatered, 1u);
             WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
         } while (!(tile_element++)->IsLastForTile());
 
@@ -1180,7 +1181,7 @@ void Staff::UpdateEmptyingBin()
         tile_element->AsPath()->SetAdditionStatus(additionStatus);
 
         MapInvalidateTileZoom0({ NextLoc, tile_element->GetBaseZ(), tile_element->GetClearanceZ() });
-        StaffBinsEmptied++;
+        StaffBinsEmptied = AddClamp(StaffBinsEmptied, 1u);
         WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
     }
 }
@@ -1199,7 +1200,7 @@ void Staff::UpdateSweeping()
     {
         // Remove sick at this location
         Litter::RemoveAt(GetLocation());
-        StaffLitterSwept++;
+        StaffLitterSwept = AddClamp(StaffLitterSwept, 1u);
         WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
     }
     if (auto loc = UpdateAction(); loc.has_value())
@@ -2412,13 +2413,13 @@ bool Staff::UpdateFixingFinishFixOrInspect(bool firstRun, int32_t steps, Ride& r
         {
             UpdateRideInspected(CurrentRide);
 
-            StaffRidesInspected++;
+            StaffRidesInspected = AddClamp(StaffRidesInspected, 1u);
             WindowInvalidateFlags |= RIDE_INVALIDATE_RIDE_INCOME | RIDE_INVALIDATE_RIDE_LIST;
             ride.mechanic_status = RIDE_MECHANIC_STATUS_UNDEFINED;
             return true;
         }
 
-        StaffRidesFixed++;
+        StaffRidesFixed = AddClamp(StaffRidesFixed, 1u);
         WindowInvalidateFlags |= RIDE_INVALIDATE_RIDE_INCOME | RIDE_INVALIDATE_RIDE_LIST;
 
         Orientation = PeepDirection << 3;

commit 8dd7b7976fd03cb477145ca6eeae8710d2649d43

Prevent ride and stall statistics from overflowing (#24378)
* Prevent ride and stall statistic from overflowing

Clamps guestsFavourite, totalCustomers, lastInspection, totalProfit, numPrimaryItemsSold and numSecondaryItemsSold

* Change line of comment in Ride.h

This caused the tooltip with the comment to appear when hovering over notFixedTimeout instead of breakdownSoundModifier in Visual Studio

* Remove redundant overflow prevention

* Invalidate maintenance tab

Missed this before. Also fix an issue I inadvertently caused with PR #23719 when updating the count of vandals stopped

* Deduce lastInspection type in compile time

* Add changelog entry

src/openrct2/entity/Guest.cpp
@@ -1744,10 +1744,10 @@ static bool GuestDecideAndBuyItem(Guest& guest, Ride& ride, const ShopItem shopI
     {
         guest.SpendMoney(*expend_type, price, expenditure);
     }
-    ride.totalProfit += (price - shopItemDescriptor.Cost);
+    ride.totalProfit = AddClamp(ride.totalProfit, price - shopItemDescriptor.Cost);
     ride.windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_INCOME;
     ride.curNumCustomers++;
-    ride.totalCustomers++;
+    ride.totalCustomers = AddClamp(ride.totalCustomers, 1u);
     ride.windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_CUSTOMER;
 
     return true;
@@ -1836,7 +1836,7 @@ void Guest::OnExitRide(Ride& ride)
         }
     }
 
-    ride.totalCustomers++;
+    ride.totalCustomers = AddClamp(ride.totalCustomers, 1u);
     ride.windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_CUSTOMER;
 }
 
@@ -3361,7 +3361,7 @@ static bool PeepShouldUseCashMachine(Guest& guest, RideId rideIndex)
     {
         ride->updateSatisfaction(guest.Happiness >> 6);
         ride->curNumCustomers++;
-        ride->totalCustomers++;
+        ride->totalCustomers = AddClamp(ride->totalCustomers, 1u);
         ride->windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_CUSTOMER;
     }
     return true;
@@ -3433,7 +3433,7 @@ void Guest::UpdateBuying()
 
                 UpdateCurrentAnimationType();
 
-                ride->numPrimaryItemsSold++;
+                ride->numPrimaryItemsSold = AddClamp(ride->numPrimaryItemsSold, 1u);
             }
         }
         else
@@ -3450,7 +3450,7 @@ void Guest::UpdateBuying()
                 item_bought = GuestDecideAndBuyItem(*this, *ride, ride_type->shop_item[1], price);
                 if (item_bought)
                 {
-                    ride->numSecondaryItemsSold++;
+                    ride->numSecondaryItemsSold = AddClamp(ride->numSecondaryItemsSold, 1u);
                 }
             }
 
@@ -3461,7 +3461,7 @@ void Guest::UpdateBuying()
                 item_bought = GuestDecideAndBuyItem(*this, *ride, ride_type->shop_item[0], price);
                 if (item_bought)
                 {
-                    ride->numPrimaryItemsSold++;
+                    ride->numPrimaryItemsSold = AddClamp(ride->numPrimaryItemsSold, 1u);
                 }
             }
         }
@@ -4470,7 +4470,7 @@ void Guest::UpdateRideInExit()
         ShopItem secondaryItem = ride->getRideTypeDescriptor().PhotoItem;
         if (GuestDecideAndBuyItem(*this, *ride, secondaryItem, ride->price[1]))
         {
-            ride->numSecondaryItemsSold++;
+            ride->numSecondaryItemsSold = AddClamp(ride->numSecondaryItemsSold, 1u);
         }
     }
     RideSubState = PeepRideSubState::LeaveExit;
@@ -5203,7 +5203,7 @@ void Guest::UpdateRideShopLeave()
     auto ride = GetRide(CurrentRide);
     if (ride != nullptr)
     {
-        ride->totalCustomers++;
+        ride->totalCustomers = AddClamp(ride->totalCustomers, 1u);
         ride->windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_CUSTOMER;
         ride->updateSatisfaction(Happiness / 64);
     }
@@ -6404,6 +6404,7 @@ static void GuestUpdateWalkingBreakScenery(Guest& guest)
         if (std::max(xDist, yDist) < 224)
         {
             innerPeep->StaffVandalsStopped = AddClamp(innerPeep->StaffVandalsStopped, 1u);
+            innerPeep->WindowInvalidateFlags |= PEEP_INVALIDATE_STAFF_STATS;
             return;
         }
     }

src/openrct2/entity/Peep.cpp
@@ -50,6 +50,7 @@
 #include "../ride/Station.h"
 #include "../ride/Track.h"
 #include "../ui/WindowManager.h"
+#include "../util/Util.h"
 #include "../windows/Intent.h"
 #include "../world/Climate.h"
 #include "../world/ConstructionClearance.h"
@@ -2274,7 +2275,7 @@ static bool PeepInteractWithShop(Peep* peep, const CoordsXYE& coords)
         auto cost = ride->price[0];
         if (cost != 0 && !(getGameState().park.Flags & PARK_FLAGS_NO_MONEY))
         {
-            ride->totalProfit += cost;
+            ride->totalProfit = AddClamp(ride->totalProfit, cost);
             ride->windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_INCOME;
             guest->SpendMoney(cost, ExpenditureType::ParkRideTickets);
         }

src/openrct2/management/Finance.cpp
@@ -170,7 +170,7 @@ void FinancePayRideUpkeep()
             auto upkeep = ride.upkeepCost;
             if (upkeep != kMoney64Undefined)
             {
-                ride.totalProfit -= upkeep;
+                ride.totalProfit = AddClamp(ride.totalProfit, -upkeep);
                 ride.windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_INCOME;
                 FinancePayment(upkeep, ExpenditureType::RideRunningCosts);
             }

src/openrct2/ride/Ride.cpp
@@ -375,7 +375,7 @@ void RideUpdateFavouritedStat()
             auto ride = GetRide(peep->FavouriteRide);
             if (ride != nullptr)
             {
-                ride->guestsFavourite++;
+                ride->guestsFavourite = AddClamp(ride->guestsFavourite, 1u);
                 ride->windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_CUSTOMER;
             }
         }
@@ -1305,9 +1305,8 @@ static void RideInspectionUpdate(Ride& ride)
     if (gLegacyScene == LegacyScene::trackDesigner)
         return;
 
-    ride.lastInspection++;
-    if (ride.lastInspection == 0)
-        ride.lastInspection--;
+    ride.lastInspection = AddClamp<decltype(ride.lastInspection)>(ride.lastInspection, 1);
+    ride.windowInvalidateFlags |= RIDE_INVALIDATE_RIDE_MAINTENANCE;
 
     int32_t inspectionIntervalMinutes = RideInspectionInterval[ride.inspectionInterval];
     // An inspection interval of 0 minutes means the ride is set to never be inspected.

src/openrct2/ride/Ride.h
@@ -274,9 +274,9 @@ struct Ride
     uint8_t downtimeHistory[OpenRCT2::Limits::kDowntimeHistorySize]{};
     uint32_t numPrimaryItemsSold{};
     uint32_t numSecondaryItemsSold{};
-    uint8_t breakdownSoundModifier{};
     // Used to oscillate the sound when ride breaks down.
     // 0 = no change, 255 = max change
+    uint8_t breakdownSoundModifier{};
     uint8_t notFixedTimeout{};
     uint8_t lastCrashType{};
     uint8_t connectedMessageThrottle{};