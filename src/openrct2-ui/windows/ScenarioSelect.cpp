3adb9a9e42103c722b7e33209bc7d829ac829ec0
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-05-31 21:46:56 +0100

    Revert changes

--- src/openrct2-ui/windows/ScenarioSelect.cpp
+++ src/openrct2-ui/windows/ScenarioSelect.cpp
@@ -125,10 +125,13 @@ namespace OpenRCT2::Ui::Windows
         const ScenarioIndexEntry* _highlightedScenario = nullptr;
         ParkPreview _preview;
         BackgroundWorker::Job _previewLoadJob;
-        bool _needsRebuild;
+        bool _initialLoadCommand = true;
 
         void ReportProgress(uint8_t progress)
         {
+            if (_initialLoadCommand)
+                return;
+
             if (progress == 0)
                 GetContext()->OpenProgress(STR_CHECKING_SCENARIO_FILES);
 
@@ -140,16 +143,13 @@ namespace OpenRCT2::Ui::Windows
 
         void LoadScenarioList()
         {
-            if (!_needsRebuild)
-                return;
-
             try
             {
                 ReportProgress(0);
                 ScenarioRepositoryScan();
                 ReportProgress(100);
 
-                _needsRebuild = false;
+                _initialLoadCommand = false;
             }
             catch (const std::exception& e)
             {
@@ -168,6 +168,7 @@ namespace OpenRCT2::Ui::Windows
         {
             SetWidgets(_scenarioSelectWidgets);
 
+            _initialLoadCommand = true;
             LoadScenarioList();
 
             _highlightedScenario = nullptr;
@@ -178,7 +179,6 @@ namespace OpenRCT2::Ui::Windows
 
         void OnLanguageChange() override
         {
-            _needsRebuild = true;
             LoadScenarioList();
         }
 
