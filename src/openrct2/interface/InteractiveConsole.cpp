e57133ff97e05e38414db1d28faee9870d52b402
Kirill Gusev <myaumuraa@gmail.com>
2025-09-08 04:27:59 +0300

    Added error message for replay_start if could not find file

--- src/openrct2/interface/InteractiveConsole.cpp
+++ src/openrct2/interface/InteractiveConsole.cpp
@@ -30,6 +30,7 @@
 #include "../config/Config.h"
 #include "../core/Console.hpp"
 #include "../core/EnumUtils.hpp"
+#include "../core/File.h"
 #include "../core/Guard.hpp"
 #include "../core/Path.hpp"
 #include "../core/String.hpp"
@@ -1467,6 +1468,13 @@ static void ConsoleCommandReplayStart(InteractiveConsole& console, const argumen
     }
 
     std::string name = argv[0];
+    std::string filePath = OpenRCT2::GetContext()->GetPlatformEnvironment().GetDirectoryPath(OpenRCT2::DirBase::user, OpenRCT2::DirId::replayRecordings);
+    filePath = Path::Combine(filePath, name + ".parkrep");
+
+    if (!File::Exists(filePath)) {
+        console.WriteLineError("Could not find replay witn name: " + name);
+        return;
+    }
 
     auto* replayManager = OpenRCT2::GetContext()->GetReplayManager();
     if (replayManager->StartPlayback(name))
