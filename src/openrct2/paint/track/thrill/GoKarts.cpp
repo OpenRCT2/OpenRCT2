ea66706fbb2ada4147099a6f16978c9784f64166
mix <167040362+mixiate@users.noreply.github.com>
2025-09-12 19:42:03 +0100

    Fix go karts steep to flat general support heights

--- src/openrct2/paint/track/thrill/GoKarts.cpp
+++ src/openrct2/paint/track/thrill/GoKarts.cpp
@@ -1895,7 +1895,7 @@ static void TrackUp60ToFlatLongBase(
         PaintUtilPushTunnelRotated(session, direction, height + 8, kTunnelGroup, TunnelSubType::Flat);
     }
     PaintUtilSetSegmentSupportHeight(session, kSegmentsAll, 0xFFFF, 0);
-    static constexpr std::array generalSupportHeights = { 80, 64, 48, 48 };
+    static constexpr std::array generalSupportHeights = { 80, 80, 56, 40 };
     PaintUtilSetGeneralSupportHeight(session, height + generalSupportHeights[trackSequence]);
 }
 
