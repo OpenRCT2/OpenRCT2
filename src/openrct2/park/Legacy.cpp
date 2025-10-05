d348b957f83f21d3f281896e004ec58dd3476bca
mix <mixiate@proton.me>
2025-10-05 15:59:23 +0100

    Add unbanked and banked quarter helices to the Mini Roller Coaster

--- src/openrct2/park/Legacy.cpp
+++ src/openrct2/park/Legacy.cpp
@@ -2990,7 +2990,8 @@ bool TrackTypeMustBeMadeInvisible(ride_type_t rideType, OpenRCT2::TrackElemType 
          || rideType == RIDE_TYPE_FLYING_ROLLER_COASTER || rideType == RIDE_TYPE_GIGA_COASTER
          || rideType == RIDE_TYPE_LSM_LAUNCHED_ROLLER_COASTER || rideType == RIDE_TYPE_SINGLE_RAIL_ROLLER_COASTER
          || rideType == RIDE_TYPE_LOOPING_ROLLER_COASTER || rideType == RIDE_TYPE_LIM_LAUNCHED_ROLLER_COASTER
-         || rideType == RIDE_TYPE_ALPINE_COASTER)
+         || rideType == RIDE_TYPE_ALPINE_COASTER || rideType == RIDE_TYPE_MINI_ROLLER_COASTER
+         || rideType == RIDE_TYPE_SPIRAL_ROLLER_COASTER)
         && parkFileVersion < kUninvertedQuarterHelicesVersion)
     {
         switch (trackType)
