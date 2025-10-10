4240c36b17f9bc4618eaeefca3f6bf36fd9d4c31
matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
2025-10-10 14:59:39 -0300

    Don't ride again if it's precipitating

--- src/openrct2/entity/Guest.cpp
+++ src/openrct2/entity/Guest.cpp
@@ -4730,6 +4730,8 @@ void Guest::UpdateRideApproachSpiralSlide()
             {
                 if (ride->mode == RideMode::singleRidePerAdmission)
                     lastRide = true;
+                if (ClimateIsPrecipitating())
+                    lastRide = true;
                 if ((timesSlidDown + GuestTimeOnRide) > static_cast<uint8_t>(ScenarioRand() & 0xF))
                     lastRide = true;
             }
