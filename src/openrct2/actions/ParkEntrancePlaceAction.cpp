c467a7437cb0b387f1ed2ee65ed2cbc4f4a6e9ff
Robert Gooding <goodingr@users.noreply.github.com>
2025-12-04 14:49:35 +0000

    Fix clang-tidy shadow warning

--- src/openrct2/actions/ParkEntrancePlaceAction.cpp
+++ src/openrct2/actions/ParkEntrancePlaceAction.cpp
@@ -147,11 +147,11 @@ namespace OpenRCT2::GameActions
             }
 
             // Remove any obstructions (e.g. paths)
-            auto res = MapCanConstructWithClearAt(
+            auto clearRes = MapCanConstructWithClearAt(
                 { entranceLoc, zLow, zHigh }, MapPlaceParkEntranceClearFunc, { 0b1111, 0 }, flags | GAME_COMMAND_FLAG_APPLY,
                 kTileSlopeFlat);
-            if (res.Error != Status::Ok)
-                return res;
+            if (clearRes.Error != Status::Ok)
+                return clearRes;
 
             if (!(flags & GAME_COMMAND_FLAG_GHOST))
             {
