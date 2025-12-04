41384ac97a13eefb41310df57936487535a14d59
MarcelVos96 <marcelvos96@gmail.com>
2025-12-04 11:15:31 +0100

    Fix #9895: standup coaster gets incorrect intensity boost from synchronisation

--- src/openrct2/ride/rtd/coaster/StandUpRollerCoaster.h
+++ src/openrct2/ride/rtd/coaster/StandUpRollerCoaster.h
@@ -67,7 +67,7 @@ constexpr RideTypeDescriptor StandUpRollerCoasterRTD =
         false,
         {
             { RatingsModifierType::BonusLength,           6000,             764, 0, 0 },
-            { RatingsModifierType::BonusSynchronisation,  0,                RideRating::make(0, 40), RideRating::make(0, 10), 0 },
+            { RatingsModifierType::BonusSynchronisation,  0,                RideRating::make(0, 40), RideRating::make(0, 5), 0 },
             { RatingsModifierType::BonusTrainLength,      0,                187245, 0, 0 },
             { RatingsModifierType::BonusMaxSpeed,         0,                44281, 123987, 35424 },
             { RatingsModifierType::BonusAverageSpeed,     0,                291271, 436906, 0 },
