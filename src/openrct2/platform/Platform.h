5f7360342ff53af9ab991efa6aa0db246ac1137e
Peppersawce <157759066+Peppersawce@users.noreply.github.com>
2026-01-04 18:21:30 +0100

    Update Platform.h

--- src/openrct2/platform/Platform.h
+++ src/openrct2/platform/Platform.h
@@ -147,7 +147,7 @@ namespace OpenRCT2::Platform
 
     SteamPaths GetSteamPaths();
     bool triggerSteamDownload();
-#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__) || defined(__NetBSD__)             \
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__) || defined(__NetBSD__)              \
     || defined(__HAIKU__)
     std::string GetEnvironmentPath(const char* name);
     std::string GetHomePath();
