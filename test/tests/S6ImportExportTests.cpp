3fb72a47c141502c4e0dc8122ecbef8db9ee9f13
Aaron van Geffen <aaron@aaronweb.net>
2026-01-05 19:29:13 +0100

    Add Drawing.h to PlayTests, S6ImportExportTests

--- test/tests/S6ImportExportTests.cpp
+++ test/tests/S6ImportExportTests.cpp
@@ -24,6 +24,7 @@
 #include <openrct2/core/MemoryStream.h>
 #include <openrct2/core/Path.hpp>
 #include <openrct2/core/String.hpp>
+#include <openrct2/drawing/Drawing.h>
 #include <openrct2/entity/EntityRegistry.h>
 #include <openrct2/entity/EntityTweener.h>
 #include <openrct2/network/Network.h>
