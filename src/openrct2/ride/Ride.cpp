cf798a82763df3f7d60bffb10cb1a9a0156024ae
MarcelVos96 <marcelvos96@gmail.com>
2026-01-14 12:10:13 +0100

    Make code more consistent
    Previous implementation used coordsXYrangedZ in such a way that worked but is inconsistent with how it is generally used in the code.

--- src/openrct2/ride/Ride.cpp
+++ src/openrct2/ride/Ride.cpp
@@ -5422,10 +5422,9 @@ TileElement* GetStationPlatform(const CoordsXYRangedZ& coords)
             /* Check if tileElement is a station platform. */
             if (!tileElement->AsTrack()->IsStation())
                 continue;
-            if (coords.baseZ - coords.clearanceZ > tileElement->GetBaseZ()
-                || coords.baseZ + coords.clearanceZ < tileElement->GetBaseZ())
+            if (coords.baseZ > tileElement->GetBaseZ() || coords.clearanceZ < tileElement->GetBaseZ())
             {
-                /* The base height if tileElement is not within
+                /* The base height of tileElement is not within
                  * the z tolerance. */
                 continue;
             }
@@ -5454,7 +5453,8 @@ static bool CheckForAdjacentStation(const CoordsXYZ& stationCoords, uint8_t dire
     {
         adjX += CoordsDirectionDelta[direction].x;
         adjY += CoordsDirectionDelta[direction].y;
-        TileElement* stationElement = GetStationPlatform({ { adjX, adjY, stationCoords.z }, 2 * kCoordsZStep });
+        TileElement* stationElement = GetStationPlatform(
+            { adjX, adjY, stationCoords.z - 2 * kCoordsZStep, stationCoords.z + 2 * kCoordsZStep });
         if (stationElement != nullptr)
         {
             auto rideIndex = stationElement->AsTrack()->GetRideIndex();
