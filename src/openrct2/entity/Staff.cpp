708667378822d9f9cbc0636dbfbb12b339417c47
datan2856 <Tan920@outlook.com>
2026-02-03 22:44:51 +0800

    Update Staff.cpp

--- src/openrct2/entity/Staff.cpp
+++ src/openrct2/entity/Staff.cpp
@@ -2619,8 +2619,8 @@ money64 GetStaffWage(StaffType type)
             return 55.00_GBP;
     }
 }
-        if (!(gameState.park.flags & PARK_FLAGS_RCT1_INTEREST))
-{
+        if (!(gameState.park.flags & PARK_FLAGS_RCT1_INTEREST)){
+
     switch (type)
     {
         default:
