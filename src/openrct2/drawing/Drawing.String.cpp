9b335e6880ff526fccee77176552439e00590f54
Aaron van Geffen <aaron@aaronweb.net>
2026-04-22 14:52:23 +0900

    Drop old hack from processStringLiteral

--- src/openrct2/drawing/Drawing.String.cpp
+++ src/openrct2/drawing/Drawing.String.cpp
@@ -641,18 +641,8 @@ namespace OpenRCT2::Drawing
                     if (ttfRunIndex.has_value())
                     {
                         // Draw the TTF run
-                        // This error suppression abomination is here to suppress
-                        // https://github.com/OpenRCT2/OpenRCT2/issues/17371. Additionally, we have to suppress the error for
-                        // the error suppression... :'-( https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105937 is fixed in GCC13
-    #if defined(__GNUC__) && !defined(__clang__)
-        #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-    #endif
                         auto len = it.GetIndex() - ttfRunIndex.value();
                         drawStringRawTTF(rt, text.substr(ttfRunIndex.value(), len), info);
-    #if defined(__GNUC__) && !defined(__clang__)
-        #pragma GCC diagnostic pop
-    #endif
                         ttfRunIndex = std::nullopt;
                     }
 
