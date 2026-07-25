14ee8744aa106b20aa67230617a3ef32be07e673
ByteRaid <andrew@byteraid.net>
2026-07-25 09:17:30 -0400

    Add --strip-objects option; pack custom objects by default
    Convert now always packs custom objects into the output .park file, so converted files are self-contained by default. Add a --strip-objects switch to opt out and skip packing, replacing the previous savePluginData-gated behaviour.

--- src/openrct2/command_line/ConvertCommand.cpp
+++ src/openrct2/command_line/ConvertCommand.cpp
@@ -28,11 +28,13 @@ using namespace OpenRCT2::CommandLine;
 namespace OpenRCT2
 {
     static int32_t _compressLevel = kParkFileSaveCompressionLevel;
+    static bool _stripObjects = false;
 
     // clang-format off
     static constexpr CommandLineOptionDefinition kConvertOptions[]
     {
         { CMDLINE_TYPE_INTEGER, &_compressLevel, 'l', "compress-level", "The compression level to use when writing the converted file" },
+        { CMDLINE_TYPE_SWITCH, &_stripObjects, kNAC, "strip-objects", "do not pack custom objects into the converted file" },
         kOptionTableEnd
     };
 
@@ -140,7 +142,7 @@ namespace OpenRCT2
         try
         {
             auto exporter = std::make_unique<ParkFileExporter>();
-            if (Config::Get().general.savePluginData)
+            if (!_stripObjects)
             {
                 exporter->ExportObjectsList = objManager.GetPackableObjects();
             }
