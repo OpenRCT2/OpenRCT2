6d5f40e51e9320b08251ec0d098dc82d8353e6e5
Sjoerd de Bruin <sjoerddebruin@me.com>
2026-09-06 20:34:44 +0200

    Fix #27070: Gate polled input on window focus

--- src/openrct2-ui/input/InputManager.cpp
+++ src/openrct2-ui/input/InputManager.cpp
@@ -141,6 +141,9 @@ void InputManager::processAnalogueInput()
     _analogueScroll.x = 0;
     _analogueScroll.y = 0;
 
+    if (!ContextHasFocus())
+        return;
+
     const int32_t deadzone = Config::Get().general.gamepadDeadzone;
     const float sensitivity = Config::Get().general.gamepadSensitivity;
 
@@ -448,6 +451,12 @@ void InputManager::processChat(const InputEvent& e)
 
 void InputManager::processHoldEvents()
 {
+    _viewScroll.x = 0;
+    _viewScroll.y = 0;
+
+    if (!ContextHasFocus())
+        return;
+
     // Get mouse state
     _mouseState = SDL_GetMouseState(nullptr, nullptr);
 
@@ -458,9 +467,6 @@ void InputManager::processHoldEvents()
     std::memcpy(_keyboardState.data(), keys, numkeys);
 
     // Check view scroll shortcuts
-    _viewScroll.x = 0;
-    _viewScroll.y = 0;
-
     if (!hasTextInputFocus())
     {
         auto& shortcutManager = GetShortcutManager();
