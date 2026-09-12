612b3cb2aae32865fbcce2729ae9b638c37b548b
Gymnasiast <m.o.steenbeek@gmail.com>
2026-09-13 00:32:48 +0200

    Fix #27113: Chat button is shown in editor mode

--- src/openrct2-ui/windows/TopToolbar.cpp
+++ src/openrct2-ui/windows/TopToolbar.cpp
@@ -1087,7 +1087,7 @@ namespace OpenRCT2::Ui::Windows
             auto& config = Config::Get().interface;
 
             widgets[WIDX_MUTE].setVisible(config.toolbarShowMute);
-            widgets[WIDX_CHAT].setVisible(config.toolbarShowChat);
+            widgets[WIDX_CHAT].setVisible(config.toolbarShowChat && !isInEditorMode());
             widgets[WIDX_RESEARCH].setVisible(config.toolbarShowResearch);
             widgets[WIDX_CHEATS].setVisible(config.toolbarShowCheats);
             widgets[WIDX_DEBUG].setVisible(Config::Get().general.debuggingTools);
